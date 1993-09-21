6af382c85fdafde71dbf7eb16d7c7618187119ec
John Gardiner Myers <jm36@andrew.cmu.edu>
1993-09-21 21:03:49 +0000

    flesh out a bit.

--- imap/deliver.c
+++ imap/deliver.c
@@ -7,13 +7,47 @@
 #include <acl.h>
 #include "folder.h"
 
+extern int optind;
+extern char *optarg;
+
 main(argc, argv)
 {
-    int r;
-    struct folder folder;
+    int opt;
+    char *dir = "/usr/user/cyrus/test";
+    int touser = 0;
 
-    r = append_setup(&folder, "/usr/user/cyrus/test", FOLDER_FORMAT_NORMAL,
-		     ACL_POST, 0);
+    while (opt = getopt(argc, argv, "df:r:")) {
+	switch(opt) {
+	case 'd':
+	    touser = 1;
+	    break;
+
+	case 'r':
+	case 'f':
+	    /* Ignore -- /bin/mail compatibility flags */
+	    break;
+
+	default:
+	    usage();
+	}
+    }
+    /* XXX handle changing "dir" */
+    /* XXX handle multiple users & multiple bboards -- copy to temp file */
+    deliver(dir);
+}
+
+usage()
+{
+    fprintf(stderr, "usage: deliver [-r ignored] [-d ignored] [-d] user\n");
+    exit(EX_USAGE);
+}
+
+deliver(path)
+char *path;
+{
+    struct folder folder;
+    
+    r = append_setup(&folder, path, FOLDER_FORMAT_NORMAL, ACL_POST, 0);
     if (r) exit(1);
     
     r = append_fromstream(&folder, stdin);
