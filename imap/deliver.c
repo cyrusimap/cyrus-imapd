dc3052b0065e01768b0488d2f98002d3208e31dc
John Gardiner Myers <jm36@andrew.cmu.edu>
1993-09-23 21:10:13 +0000

    Add error codes, error handling

--- imap/deliver.c
+++ imap/deliver.c
@@ -4,12 +4,16 @@
 
 #include <stdio.h>
 #include <sysexits.h>
+#include <com_err.h>
 
 #include <acl.h>
+#include "imap_err.h"
 #include "mailbox.h"
 
 extern int optind;
 extern char *optarg;
+
+extern int errno;
 
 main(argc, argv)
 {
@@ -50,8 +54,50 @@ char *path;
     struct mailbox mailbox;
     
     r = append_setup(&mailbox, path, MAILBOX_FORMAT_NORMAL, ACL_POST, 0);
-    if (r) exit(1);
-    
-    r = append_fromstream(&mailbox, stdin);
-    exit(r);
+
+    if (!r) {
+	r = append_fromstream(&mailbox, stdin);
+    }
+
+    if (r) {
+	com_err("deliver", r, (r == EX_IOERR) ? error_message(errno) : NULL);
+    }
+
+    exit(convert_code(r));
+}
+
+int convert_code(r)
+int r;
+{
+    switch (r) {
+    case 0:
+	return 0;
+	
+    case IMAP_IOERROR:
+	return EX_IOERR;
+
+    case IMAP_PERMISSION_DENIED:
+	return EX_NOPERM;
+
+    case IMAP_QUOTA_EXCEEDED:
+	return EX_TEMPFAIL;
+
+    case IMAP_MAILBOX_NOTSUPPORTED:
+	return EX_DATAERR;
+
+    case IMAP_MAILBOX_NONEXISTENT:
+	/* XXX Might have been moved to other server */
+	return EX_UNAVAILABLE;
+    }
+	
+    /* Some error we're not expecting. */
+    return EX_SOFTWARE;
+}	
+
+fatal(s, code)
+char *s;
+int code;
+{
+    fprintf(stderr, "deliver: %s\n", s);
+    exit(code);
 }
