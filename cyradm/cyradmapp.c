af7dbaf5bd3fea3aeb75a5f2629f762efc50bc34
John Gardiner Myers <jm36@andrew.cmu.edu>
1994-09-27 17:27:43 +0000

    get working

--- cyradm/cyradmapp.c
+++ cyradm/cyradmapp.c
@@ -1,17 +1,51 @@
+/* cyradmapp.c -- Cyrus administrative client application startup
+ *
+ *	(C) Copyright 1994 by Carnegie Mellon University
+ *
+ *                      All Rights Reserved
+ *
+ * Permission to use, copy, modify, and distribute this software and its 
+ * documentation for any purpose and without fee is hereby granted, 
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in 
+ * supporting documentation, and that the name of CMU not be
+ * used in advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.  
+ * 
+ * CMU DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+ * ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
+ * CMU BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+ * ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ *
+ */
 #include "tcl.h"
 
 extern int main();
 int *tclDummyMainPtry = (int *) main;
 
+extern char cyrinit[];
+
 int Tcl_AppInit(interp)
 Tcl_Interp *interp;
 {
+    char *value;
+    int code;
+    
     if (Tcl_Init(interp) == TCL_ERROR) {
 	return TCL_ERROR;
     }
     if (Cyradm_Init(interp) == TCL_ERROR) {
 	return TCL_ERROR;
     }
+    value = Tcl_GetVar(interp, "tcl_interactive", 0);
+    if (value && *value == '1') {
+	code = Tcl_Eval(interp, cyrinit);
+	if (code != TCL_OK) return code;
+    }
+
     tcl_RcFileName = "~/.cyradmrc";
     return TCL_OK;
 }
