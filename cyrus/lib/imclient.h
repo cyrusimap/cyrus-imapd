4541fb5678a3d3e857c87bd5b735554cc309cca9
John Gardiner Myers <jm36@andrew.cmu.edu>
1994-09-27 16:38:43 +0000

    Initial revision

---
+++ cyrus/lib/imclient.h
@@ -0,0 +1,51 @@
+/* imclient.h -- Streaming IMxP client library
+ *
+ *	(C) Copyright 1994 by Carnegie Mellon University
+ *
+ *                      All Rights Reserved
+ *
+ * Permission to use, copy, modify, and distribute this software and its 
+ * documentation for any purpose and without fee is hereby granted, 
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in 
+ * supporting documentation, and that the name of CMU not be
+ * used in advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.  
+ * 
+ * CMU DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+ * ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
+ * CMU BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+ * ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ *
+ */
+struct imclient_reply {
+    char *keyword;		/* reply keyword */
+    long msgno;			/* message number (-1 = no message number) */
+    char *text;			/* subsequent text */
+};
+
+
+/* Flags for untagged-reply callbacks */
+#define CALLBACK_NUMBERED 1	/* Has a message sequence number */
+#define CALLBACK_NOLITERAL 2	/* Data cannot contain a literal */
+
+
+#ifdef __STDC__
+struct imclient;
+extern int imclient_connect(struct imclient **, char *, char *);
+extern void imclient_close(struct imclient *);
+extern char *imclient_servername(struct imclient *);
+extern void imclient_addcallback(struct imclient *, ...);
+extern void imclient_send(struct imclient *, void (*)(), void *, char *, ...);
+extern void imclient_processoneevent(struct imclient *);
+extern int imclient_authenticate(struct imclient *, struct acte_client **,
+				 char *, int);
+#else
+extern char *imclient_servername();
+extern void imclient_close(), imclient_processoneevent();
+extern void imclient_addcallback();
+extern void imclient_send();
+#endif
