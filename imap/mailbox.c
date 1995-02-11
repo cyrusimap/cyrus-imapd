1fd0c29339447a91adb0152caa01f3545158560e
John Gardiner Myers <jm36@andrew.cmu.edu>
1995-02-11 00:33:59 +0000

    Upgrade to autoconf 2.1

--- imap/mailbox.c
+++ imap/mailbox.c
@@ -30,24 +30,22 @@
 #include <netinet/in.h>
 #include <sys/stat.h>
 
-#ifdef HAVE_UNISTD_H
-#include <unistd.h>
+#if HAVE_DIRENT_H
+# include <dirent.h>
+# define NAMLEN(dirent) strlen((dirent)->d_name)
+#else
+# define dirent direct
+# define NAMLEN(dirent) (dirent)->d_namlen
+# if HAVE_SYS_NDIR_H
+#  include <sys/ndir.h>
+# endif
+# if HAVE_SYS_DIR_H
+#  include <sys/dir.h>
+# endif
+# if HAVE_NDIR_H
+#  include <ndir.h>
+# endif
 #endif
-/* unistd.h defines _POSIX_VERSION on POSIX.1 systems. */
-#if defined(DIRENT) || defined(_POSIX_VERSION)
-#include <dirent.h>
-#else /* not (DIRENT or _POSIX_VERSION) */
-#define dirent direct
-#ifdef SYSNDIR
-#include <sys/ndir.h>
-#endif /* SYSNDIR */
-#ifdef SYSDIR
-#include <sys/dir.h>
-#endif /* SYSDIR */
-#ifdef NDIR
-#include <ndir.n>
-#endif /* NDIR */
-#endif /* not (DIRENT or _POSIX_VERSION) */
 
 #include "config.h"
 #include "acl.h"
