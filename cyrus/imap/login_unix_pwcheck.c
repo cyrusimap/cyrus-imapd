1c788530c71791b160119aaa6703be4768615c1d
John Gardiner Meyers <jgm@andrew.cmu.edu>
1995-10-12 21:53:18 +0000

    Initial revision

---
+++ cyrus/imap/login_unix_pwcheck.c
@@ -0,0 +1,100 @@
+/* login_unix_pwcheck.c -- Unix pwcheck daemon login authentication
+ *
+ *      (C) Copyright 1994 by Carnegie Mellon University
+ *
+ *                      All Rights Reserved
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the name of Carnegie
+ * Mellon University not be used in advertising or publicity
+ * pertaining to distribution of the software without specific,
+ * written prior permission.  Carnegie Mellon University makes no
+ * representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied
+ * warranty.
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ *
+ */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/uio.h>
+
+#include "config.h"
+#include "sysexits.h"
+#include "mailbox.h"
+#include "imapd.h"
+
+/*
+ * Unix pwcheck daemon-authenticated login (shadow password)
+ */
+
+int
+login_plaintext(user, pass, reply)
+char *user;
+char *pass;
+char **reply;
+{
+    int s;
+    struct sockaddr_un srvaddr;
+    int r;
+    struct iovec iov[10];
+    static char response[1024];
+    int start, n;
+
+    s = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (s == -1) return errno;
+
+    memset((char *)&srvaddr, 0, sizeof(srvaddr));
+    srvaddr.sun_family = AF_UNIX;
+    strcpy(srvaddr.sun_path, "/etc/pwcheck/pwcheck");
+    r = connect(s, (struct sockaddr *)&srvaddr, sizeof(srvaddr));
+    if (r == -1) return {
+	*reply = "cannot connect to pwcheck server";
+	return errno;
+    }
+
+    iov[0].iov_base = user;
+    iov[0].iov_len = strlen(user)+1;
+    iov[1].iov_base = pass;
+    iov[1].iov_len = strlen(pass)+1;
+
+    retry_writev(s, &iov, 2);
+
+    start = 0;
+    while (start < sizeof(response) - 1) {
+	n = read(s, response+start, sizeof(response) - 1 - start);
+	if (n < 1) break;
+	start += n;
+    }
+
+    close(s);
+
+    if (start > 1 && !strncmp(response, "OK", 2)) return 0;
+
+    response[start] = '\0';
+    *reply = response;
+    return 1;
+}
+  
+int
+login_authenticate(authtype, mech, authproc)
+char *authtype;
+struct acte_server **mech;
+int (**authproc)();
+{
+    return 1;
+}
+  
