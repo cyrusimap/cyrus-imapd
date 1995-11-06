01c183bcde7ea2839bf9dcae9ac6634b3255164f
John Gardiner Meyers <jgm@andrew.cmu.edu>
1995-11-06 17:36:22 +0000

    Integrate krb_pts stuff into configure.

--- cyrus/lib/auth_krb_pts.h
+++ cyrus/lib/auth_krb_pts.h
@@ -1,3 +1,32 @@
+/* auth_krb_pts.h -- Kerberos authorization with AFS PTServer groups
+ *
+ *	(C) Copyright 1994 by Carnegie Mellon University
+ *
+ *                      All Rights Reserved
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the name of Carnegie
+ * Mellon University not be used in advertising or publicity
+ * pertaining to distribution of the software without specific,
+ * written prior permission.  Carnegie Mellon University makes no
+ * representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied
+ * warranty.
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ *
+ */
+
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
@@ -9,8 +38,8 @@
 #include <afs/ptserver.h>
 #include <afs/cellconfig.h>
 
-#define DBFIL "pts.db"
-#define DBLOCK "ptlock"
+#define DBFIL "/var/ptclient/ptscache.db"
+#define DBLOCK "/var/ptclient/ptcache.lock"
 #define PTCLIENT  "ptloader"
 
 typedef struct {
