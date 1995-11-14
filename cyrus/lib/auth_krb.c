8d89fec5b509dbce6b566aa1fe826156ee39f1eb
John Gardiner Meyers <jgm@andrew.cmu.edu>
1995-11-14 19:20:07 +0000

    Kerberos now uses MAXHOSTNAMELEN instead of MAX_HSTNM.

--- cyrus/lib/auth_krb.c
+++ cyrus/lib/auth_krb.c
@@ -133,7 +133,7 @@ char *real_realm;
     char inst[INST_SZ];
     char realm[REALM_SZ];
     char lrealm[REALM_SZ];
-    char krbhst[MAX_HSTNM];
+    char krbhst[MAXHOSTNAMELEN];
     char *p;
     char buf[1024];
     FILE *mapfile;
@@ -207,7 +207,7 @@ char *identifier;
     char inst[INST_SZ];
     char realm[REALM_SZ];
     char lrealm[REALM_SZ];
-    char krbhst[MAX_HSTNM];
+    char krbhst[MAXHOSTNAMELEN];
     char *p;
     
     if (strcasecmp(identifier, "anonymous") == 0) {
