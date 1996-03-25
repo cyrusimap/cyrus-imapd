45865321c9379dc3ea1783b8f3890c4a90a4574f
John Gardiner Meyers <jgm@andrew.cmu.edu>
1996-03-25 21:44:47 +0000

    Change minor version number to 1.

--- cyrus/imap/mailbox.h
+++ cyrus/imap/mailbox.h
@@ -48,13 +48,12 @@ dont know what to use for bit32
 
 #define MAX_USER_FLAGS (16*8)
 
-
-#define MAILBOX_HEADER_MAGIC "\241\002\213\015Cyrus mailbox header\n\"The best thing about this system was that it had lots of goals.\"\n\t--Jim Morris on Andrew\n"
+#define MAILBOX_OLD_HEADER_MAGIC "\241\002\213\015Cyrus mailbox header\n\"The best thing about this system was that it had lots of goals.\"\n\t--Jim Morris on Andrew\n"
 
 #define MAILBOX_FORMAT_NORMAL	0
 #define MAILBOX_FORMAT_NETNEWS	1
 
-#define MAILBOX_MINOR_VERSION	0
+#define MAILBOX_MINOR_VERSION	1
 
 #define FNAME_HEADER "/cyrus.header"
 #define FNAME_INDEX "/cyrus.index"
