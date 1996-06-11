07e7b13a83e2ef936b892583ca143b4378e5839c
John Gardiner Meyers <jgm@andrew.cmu.edu>
1996-06-11 15:27:29 +0000

    Initial revision

---
+++ cyrus/pwcheck/pwcheck_getpwnam.c
@@ -0,0 +1,56 @@
+/* pwcheck_getpwnam.c -- check passwords using getpwnam()
+ *
+ *      (C) Copyright 1996 by Carnegie Mellon University
+ *
+ *                      All Rights Reserved
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the name of Carnegie
+ * Mellon University not be used in advertising or publicity
+ * pertaining to distribution of the software without specific,
+ * written prior permission.  Carnegie Mellon University makes no
+ * representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied
+ * warranty.
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ *
+ */
+
+#include <pwd.h>
+
+extern char *crypt();
+
+char *pwcheck(userid, password)
+char *userid;
+char *password;
+{
+    struct pwd *pwd;
+
+    pwd = getpwnam(request);
+    if (!pwd) {
+	return "Userid not found";
+    }
+    
+    if (pwd->pw_passwd[0] == '*') {
+	return "Account disabled";
+    }
+
+    if (strcmp(pwd->pw_passwd,
+	       crypt(request + strlen(request) + 1, pwd->pw_passwd)) != 0) {
+	return "Incorrect password";
+    }
+    else {
+	return "OK";
+    }
+}
