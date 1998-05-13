1d6e30c9873375f92588e651e7e1bad07d9c323c
Tim Showalter <tjs@andrew.cmu.edu>
1998-05-13 21:11:30 +0000

    Spontaneously promoted from "Alpha" to "Beta".

--- imap/version.h
+++ imap/version.h
@@ -1,5 +1,5 @@
 #ifdef EXTRA_IDENT
-#define CYRUS_VERSION "v1.5.9-Alpha-" EXTRA_IDENT
+#define CYRUS_VERSION "v1.5.9-Beta-" EXTRA_IDENT
 #else
-#define CYRUS_VERSION "v1.5.9-Alpha" 
+#define CYRUS_VERSION "v1.5.9-Beta" 
 #endif
