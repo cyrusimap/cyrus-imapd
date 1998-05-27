ab0a1696a999470714a604f9dea79e522bf67d21
Tim Showalter <tjs@andrew.cmu.edu>
1998-05-27 19:44:28 +0000

    removed "Beta" from version.h.  see, now it's production.

--- imap/version.h
+++ imap/version.h
@@ -1,5 +1,5 @@
 #ifdef EXTRA_IDENT
-#define CYRUS_VERSION "v1.5.9-Beta-" EXTRA_IDENT
+#define CYRUS_VERSION "v1.5.9-" EXTRA_IDENT
 #else
-#define CYRUS_VERSION "v1.5.9-Beta" 
+#define CYRUS_VERSION "v1.5.9" 
 #endif
