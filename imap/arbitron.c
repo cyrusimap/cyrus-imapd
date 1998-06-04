ac8c672d554cd6e8b7639817f737407362363b0b
Tim Showalter <tjs@andrew.cmu.edu>
1998-06-04 20:17:18 +0000

    Copyright: # changed to *
    Changed call to mboxlist_findall to match new prototype.

--- imap/arbitron.c
+++ imap/arbitron.c
@@ -1,31 +1,32 @@
 /* arbitron.c -- program to report readership statistics
- $Id: arbitron.c,v 1.10 1998/05/15 21:48:07 neplokh Exp $
- 
- # Copyright 1998 Carnegie Mellon University
- # 
- # No warranties, either expressed or implied, are made regarding the
- # operation, use, or results of the software.
- #
- # Permission to use, copy, modify and distribute this software and its
- # documentation is hereby granted for non-commercial purposes only
- # provided that this copyright notice appears in all copies and in
- # supporting documentation.
- #
- # Permission is also granted to Internet Service Providers and others
- # entities to use the software for internal purposes.
- #
- # The distribution, modification or sale of a product which uses or is
- # based on the software, in whole or in part, for commercial purposes or
- # benefits requires specific, additional permission from:
- #
- #  Office of Technology Transfer
- #  Carnegie Mellon University
- #  5000 Forbes Avenue
- #  Pittsburgh, PA  15213-3890
- #  (412) 268-4387, fax: (412) 268-7395
- #  tech-transfer@andrew.cmu.edu
+ *
+ * Copyright 1998 Carnegie Mellon University
+ * 
+ * No warranties, either expressed or implied, are made regarding the
+ * operation, use, or results of the software.
+ *
+ * Permission to use, copy, modify and distribute this software and its
+ * documentation is hereby granted for non-commercial purposes only
+ * provided that this copyright notice appears in all copies and in
+ * supporting documentation.
+ *
+ * Permission is also granted to Internet Service Providers and others
+ * entities to use the software for internal purposes.
+ *
+ * The distribution, modification or sale of a product which uses or is
+ * based on the software, in whole or in part, for commercial purposes or
+ * benefits requires specific, additional permission from:
+ *
+ *  Office of Technology Transfer
+ *  Carnegie Mellon University
+ *  5000 Forbes Avenue
+ *  Pittsburgh, PA  15213-3890
+ *  (412) 268-4387, fax: (412) 268-7395
+ *  tech-transfer@andrew.cmu.edu
  *
  */
+
+/* $Id: arbitron.c,v 1.11 1998/06/04 20:17:18 tjs Exp $ */
 
 #include <stdio.h>
 #include <string.h>
@@ -96,7 +97,7 @@ char **argv;
     }
 
     strcpy(pattern, "*");
-    mboxlist_findall(pattern, 1, 0, 0, do_mailbox);
+    mboxlist_findall(pattern, 1, 0, 0, do_mailbox, NULL);
 
     exit(code);
 }
