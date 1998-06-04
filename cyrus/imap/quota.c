fcb5bdea0ad2c6dabab140d48a401c99be801fe3
Tim Showalter <tjs@andrew.cmu.edu>
1998-06-04 20:23:26 +0000

    Changed mboxlist_findall to accept a rock, changed all calls of mboxlist_findall to pass along a rock.
    Added a NAMESPACE command to imapd.c (that needs the rock).
    
    Changed calls related to mboxlist_findall to also pass along a rock which is
    only used in the NAMESPACE command.

--- cyrus/imap/quota.c
+++ cyrus/imap/quota.c
@@ -1,31 +1,31 @@
 /* quota.c -- program to report/reconstruct quotas
- $Id: quota.c,v 1.22 1998/05/15 21:49:43 neplokh Exp $
- 
- # Copyright 1998 Carnegie Mellon University
- # 
- # No warranties, either expressed or implied, are made regarding the
- # operation, use, or results of the software.
- #
- # Permission to use, copy, modify and distribute this software and its
- # documentation is hereby granted for non-commercial purposes only
- # provided that this copyright notice appears in all copies and in
- # supporting documentation.
- #
- # Permission is also granted to Internet Service Providers and others
- # entities to use the software for internal purposes.
- #
- # The distribution, modification or sale of a product which uses or is
- # based on the software, in whole or in part, for commercial purposes or
- # benefits requires specific, additional permission from:
- #
- #  Office of Technology Transfer
- #  Carnegie Mellon University
- #  5000 Forbes Avenue
- #  Pittsburgh, PA  15213-3890
- #  (412) 268-4387, fax: (412) 268-7395
- #  tech-transfer@andrew.cmu.edu
+ *
+ * Copyright 1998 Carnegie Mellon University
+ * 
+ * No warranties, either expressed or implied, are made regarding the
+ * operation, use, or results of the software.
+ *
+ * Permission to use, copy, modify and distribute this software and its
+ * documentation is hereby granted for non-commercial purposes only
+ * provided that this copyright notice appears in all copies and in
+ * supporting documentation.
+ *
+ * Permission is also granted to Internet Service Providers and others
+ * entities to use the software for internal purposes.
+ *
+ * The distribution, modification or sale of a product which uses or is
+ * based on the software, in whole or in part, for commercial purposes or
+ * benefits requires specific, additional permission from:
+ *
+ *  Office of Technology Transfer
+ *  Carnegie Mellon University
+ *  5000 Forbes Avenue
+ *  Pittsburgh, PA  15213-3890
+ *  (412) 268-4387, fax: (412) 268-7395
+ *  tech-transfer@andrew.cmu.edu
  *
  */
+/* $Id: quota.c,v 1.23 1998/06/04 20:23:30 tjs Exp $ /
 
 #include <stdio.h>
 #include <string.h>
@@ -209,10 +209,11 @@ int nroots;
  * Account for mailbox 'name' when fixing the quota roots
  */
 int
-fixquota_mailbox(name, matchlen, maycreate)
+fixquota_mailbox(name, matchlen, maycreate, rock)
 char *name;
 int matchlen;
 int maycreate;
+void* rock;
 {
     int r;
     struct mailbox mailbox;
@@ -396,7 +397,7 @@ int ispartial;
 	firstquota = 0;
 	partial = ispartial;
 
-	r = mboxlist_findall(pattern, 1, 0, 0, fixquota_mailbox);
+	r = mboxlist_findall(pattern, 1, 0, 0, fixquota_mailbox, NULL);
 	if (r) {
 	    mboxlist_unlock();
 	    return r;
