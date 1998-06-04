cb1deece33022163a04a7f2fcf8a0d366f269d70
Tim Showalter <tjs@andrew.cmu.edu>
1998-06-04 20:15:05 +0000

    bumped to 11.

--- cyrus/imap/version.h
+++ cyrus/imap/version.h
@@ -1,5 +1,5 @@
 #ifdef EXTRA_IDENT
-#define CYRUS_VERSION "v1.5.10-" EXTRA_IDENT
+#define CYRUS_VERSION "v1.5.11-" EXTRA_IDENT
 #else
-#define CYRUS_VERSION "v1.5.10" 
+#define CYRUS_VERSION "v1.5.11" 
 #endif
