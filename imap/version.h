e441821a589ecc2e4a3865c4f74a470ec90be400
Tim Showalter <tjs@andrew.cmu.edu>
1998-07-29 00:35:54 +0000

    bumped to 14, and moved some stuff around.

--- imap/version.h
+++ imap/version.h
@@ -1,5 +1,17 @@
+/* version.h: the version number
+ *
+ * $Id: version.h,v 1.50 1998/07/29 00:35:54 tjs Exp $
+ */
+
+#define _CYRUS_VERSION "v1.5.14"
+
+/* EXTRA_IDENT is a hack to add some version information for which compile
+ * was used to build this version (at CMU, but we don't care what you do with
+ * it.
+ */
+
 #ifdef EXTRA_IDENT
-#define CYRUS_VERSION "v1.5.11-" EXTRA_IDENT
+#define CYRUS_VERSION _CYRUS_VERSION "-" EXTRA_IDENT
 #else
-#define CYRUS_VERSION "v1.5.11" 
+#define CYRUS_VERSION _CYRUS_VERSION
 #endif
