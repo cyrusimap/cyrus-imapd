740558eaf542fe7b903d5d79050d83029cffb90a
Tim Showalter <tjs@andrew.cmu.edu>
1998-09-29 00:38:03 +0000

    modified to call endpwent() and to return only at the end of the function

--- pwcheck/pwcheck_getpwnam.c
+++ pwcheck/pwcheck_getpwnam.c
@@ -1,27 +1,28 @@
 /* pwcheck_getpwnam.c -- check passwords using getpwname()
- # Copyright 1998 Carnegie Mellon University
- # 
- # No warranties, either expressed or implied, are made regarding the
- # operation, use, or results of the software.
- #
- # Permission to use, copy, modify and distribute this software and its
- # documentation is hereby granted for non-commercial purposes only
- # provided that this copyright notice appears in all copies and in
- # supporting documentation.
- #
- # Permission is also granted to Internet Service Providers and others
- # entities to use the software for internal purposes.
- #
- # The distribution, modification or sale of a product which uses or is
- # based on the software, in whole or in part, for commercial purposes or
- # benefits requires specific, additional permission from:
- #
- #  Office of Technology Transfer
- #  Carnegie Mellon University
- #  5000 Forbes Avenue
- #  Pittsburgh, PA  15213-3890
- #  (412) 268-4387, fax: (412) 268-7395
- #  tech-transfer@andrew.cmu.edu
+ *
+ * Copyright 1998 Carnegie Mellon University
+ * 
+ * No warranties, either expressed or implied, are made regarding the
+ * operation, use, or results of the software.
+ *
+ * Permission to use, copy, modify and distribute this software and its
+ * documentation is hereby granted for non-commercial purposes only
+ * provided that this copyright notice appears in all copies and in
+ * supporting documentation.
+ *
+ * Permission is also granted to Internet Service Providers and others
+ * entities to use the software for internal purposes.
+ *
+ * The distribution, modification or sale of a product which uses or is
+ * based on the software, in whole or in part, for commercial purposes or
+ * benefits requires specific, additional permission from:
+ *
+ *  Office of Technology Transfer
+ *  Carnegie Mellon University
+ *  5000 Forbes Avenue
+ *  Pittsburgh, PA  15213-3890
+ *  (412) 268-4387, fax: (412) 268-7395
+ *  tech-transfer@andrew.cmu.edu
  *
  */
 
@@ -33,21 +34,24 @@ char *pwcheck(userid, password)
 char *userid;
 char *password;
 {
+    char* r;
     struct passwd *pwd;
 
     pwd = getpwnam(userid);
     if (!pwd) {
-	return "Userid not found";
+	r = "Userid not found";
     }
-    
-    if (pwd->pw_passwd[0] == '*') {
-	return "Account disabled";
+    else if (pwd->pw_passwd[0] == '*') {
+	r = "Account disabled";
     }
-
-    if (strcmp(pwd->pw_passwd, crypt(password, pwd->pw_passwd)) != 0) {
-	return "Incorrect password";
+    else if (strcmp(pwd->pw_passwd, crypt(password, pwd->pw_passwd)) != 0) {
+	r = "Incorrect password";
     }
     else {
-	return "OK";
+	r = "OK";
     }
+
+    endpwent();
+    
+    return r;
 }
