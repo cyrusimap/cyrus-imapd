e5dabceb618154af07dcdbc7e23359d4f9495232
Tim Showalter <tjs@andrew.cmu.edu>
1999-01-12 12:18:34 +0000

    bump

--- imap/version.h
+++ imap/version.h
@@ -1,9 +1,9 @@
 /* version.h: the version number
  *
- * $Id: version.h,v 1.56 1998/12/04 17:36:04 tjs Exp $
+ * $Id: version.h,v 1.57 1999/01/12 12:18:34 tjs Exp $
  */
 
-#define _CYRUS_VERSION "v1.5.19"
+#define _CYRUS_VERSION "v1.5.20"
 
 /* EXTRA_IDENT is a hack to add some version information for which compile
  * was used to build this version (at CMU, but we don't care what you do with
