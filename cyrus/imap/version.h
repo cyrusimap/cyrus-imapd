322e6ba007f5e3aa3aacff0e70ce24df3fd83eb0
Larry Greenfield <leg@andrew.cmu.edu>
1999-10-19 22:01:24 +0000

    version incremented

--- cyrus/imap/version.h
+++ cyrus/imap/version.h
@@ -1,9 +1,9 @@
 /* version.h: the version number
  *
- * $Id: version.h,v 1.72 1999/10/02 00:43:07 leg Exp $
+ * $Id: version.h,v 1.73 1999/10/19 22:01:24 leg Exp $
  */
 
-#define _CYRUS_VERSION "v1.6.15"
+#define _CYRUS_VERSION "v1.6.16"
 
 /* EXTRA_IDENT is a hack to add some version information for which compile
  * was used to build this version (at CMU, but we don't care what you do with
