eaf883eecf7668d09329305ddc551f405e3173f0
Larry Greenfield <leg@andrew.cmu.edu>
1999-10-22 16:47:08 +0000

    fix by john myers <jgmyers@netscape.com> for message parser getting an incorrect message size on enclosed message

--- cyrus/imap/message.c
+++ cyrus/imap/message.c
@@ -27,7 +27,7 @@
  */
 
 /*
- * $Id: message.c,v 1.68 1999/10/02 00:43:06 leg Exp $
+ * $Id: message.c,v 1.69 1999/10/22 16:47:08 leg Exp $
  */
 
 #ifdef HAVE_UNISTD_H
@@ -1440,11 +1440,17 @@ struct boundary *boundaries;
     message_parse_content(msg, format, &preamble, boundaries);
 
     /* Parse the component body-parts */
-    while (boundaries->count == depth && msg->offset < msg->len) {
+    while (boundaries->count == depth) {
 	body->subpart = (struct body *)xrealloc((char *)body->subpart,
 				 (body->numparts+1)*sizeof(struct body));
 	message_parse_body(msg, format, &body->subpart[body->numparts++],
 			   defaultContentType, boundaries);
+	if (msg->offset == msg->len &&
+	    body->subpath[body->numparts-1].boundary_size == 0) {
+	    /* hit the end of the message, therefore end all pending
+	       multiparts */
+	    boundaries->count = 0;
+	}
     }
 
     if (boundaries->count == depth-1) {
