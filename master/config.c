a84b4249eac1232b02a5f5c0bca2c62b5a094b58
Larry Greenfield <leg@andrew.cmu.edu>
2000-02-15 22:21:52 +0000

    created master process

---
+++ master/config.c
@@ -0,0 +1,272 @@
+/* config.c -- Configuration routines
+ $Id: config.c,v 1.1 2000/02/15 22:21:53 leg Exp $
+ 
+ # Copyright 1998 Carnegie Mellon University
+ # 
+ # No warranties, either expressed or implied, are made regarding the
+ # operation, use, or results of the software.
+ #
+ # Permission to use, copy, modify and distribute this software and its
+ # documentation is hereby granted for non-commercial purposes only
+ # provided that this copyright notice appears in all copies and in
+ # supporting documentation.
+ #
+ # Permission is also granted to Internet Service Providers and others
+ # entities to use the software for internal purposes.
+ #
+ # The distribution, modification or sale of a product which uses or is
+ # based on the software, in whole or in part, for commercial purposes or
+ # benefits requires specific, additional permission from:
+ #
+ #  Office of Technology Transfer
+ #  Carnegie Mellon University
+ #  5000 Forbes Avenue
+ #  Pittsburgh, PA  15213-3890
+ #  (412) 268-4387, fax: (412) 268-7395
+ #  tech-transfer@andrew.cmu.edu
+ *
+ */
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <syslog.h>
+#include <com_err.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sysexits.h>
+
+#if HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+
+#include "config.h"
+
+extern int errno;
+
+#define CONFIG_FILENAME "/etc/imapd.conf"
+
+struct configlist {
+    char *key;
+    char *value;
+};
+
+static struct configlist *configlist;
+static int nconfiglist;
+
+const char *config_dir;
+const char *config_defpartition;
+const char *config_newsspool;
+
+const char *config_servername;
+
+int config_hashimapspool;
+
+static void config_read(void);
+extern void fatal(const char *buf, int code);
+
+int config_init(const char *ident)
+{
+    char buf[100];
+    char *p;
+    const char *val;
+    int umaskval = 0;
+
+    /* initialize_imap_error_table(); */
+
+    openlog(ident, LOG_PID, LOG_LOCAL6);
+
+    config_read();
+
+    /* Look up configdirectory config option */
+    config_dir = config_getstring("configdirectory", (char *)0);
+    if (!config_dir) {
+	fatal("configdirectory option not specified in configuration file",
+	      EX_CONFIG);
+    }
+
+    /* Look up default partition */
+    config_defpartition = config_getstring("defaultpartition", "default");
+    for (p = (char *)config_defpartition; *p; p++) {
+	if (!isalnum((int) *p))
+	  fatal("defaultpartition option contains non-alphanumeric character",
+		EX_CONFIG);
+	if (isupper((int) *p)) *p = tolower((int) *p);
+    }
+    if (!config_partitiondir(config_defpartition)) {
+	sprintf(buf, "partition-%s option not specified in configuration file",
+		config_defpartition);
+	fatal(buf, EX_CONFIG);
+    }
+
+    /* Look up umask */
+    val = config_getstring("umask", "077");
+    while (*val) {
+	if (*val >= '0' && *val <= '7') umaskval = umaskval*8 + *val - '0';
+	val++;
+    }
+    umask(umaskval);
+
+    /* Look up news spool */
+    config_newsspool = config_getstring("newsspool", 0);
+
+    /* look up mailbox hashing */
+    config_hashimapspool = config_getswitch("hashimapspool", 0);
+
+    /* look up the hostname we should present to the user */
+    config_servername = config_getstring("servername", 0);
+    if (!config_servername) {
+	config_servername = malloc(sizeof(char) * 256);
+	if (config_servername != NULL) 
+	    gethostname((char *) config_servername, 256);
+    }
+    if (!config_servername) config_servername = "unknown";
+    return 0;
+}
+
+int config_changeident(const char *ident)
+{
+    closelog();
+    openlog(ident, LOG_PID, LOG_LOCAL6);
+    return 0;
+}
+
+const char *config_getstring(key, def)
+const char *key;
+const char *def;
+{
+    int opt;
+
+    for (opt = 0; opt < nconfiglist; opt++) {
+	if (*key == configlist[opt].key[0] &&
+	    !strcmp(key, configlist[opt].key))
+	  return configlist[opt].value;
+    }
+    return def;
+}
+
+int config_getint(key, def)
+const char *key;
+int def;
+{
+    const char *val = config_getstring(key, (char *)0);
+
+    if (!val) return def;
+    if (!isdigit((int) *val) && (*val != '-' || !isdigit((int) val[1]))) return def;
+    return atoi(val);
+}
+
+int config_getswitch(key, def)
+const char *key;
+int def;
+{
+    const char *val = config_getstring(key, (char *)0);
+
+    if (!val) return def;
+
+    if (*val == '0' || *val == 'n' ||
+	(*val == 'o' && val[1] == 'f') || *val == 'f') {
+	return 0;
+    }
+    else if (*val == '1' || *val == 'y' ||
+	     (*val == 'o' && val[1] == 'n') || *val == 't') {
+	return 1;
+    }
+    return def;
+}
+
+const char *config_partitiondir(partition)
+const char *partition;
+{
+    char buf[80];
+
+    if (strlen(partition) > 70) return 0;
+    strcpy(buf, "partition-");
+    strcat(buf, partition);
+
+    return config_getstring(buf, (char *)0);
+}
+
+#define CONFIGLISTGROWSIZE 10 /* 100 */
+static void
+config_read()
+{
+    FILE *infile;
+    int lineno = 0;
+    int alloced = 0;
+    char buf[4096];
+    char *p, *key;
+
+    infile = fopen(CONFIG_FILENAME, "r");
+    if (!infile) {
+	sprintf(buf, "can't open configuration file %s: %s", CONFIG_FILENAME,
+		strerror(errno));
+	fatal(buf, EX_CONFIG);
+    }
+    
+    while (fgets(buf, sizeof(buf), infile)) {
+	lineno++;
+
+	if (buf[strlen(buf)-1] == '\n') buf[strlen(buf)-1] = '\0';
+	for (p = buf; *p && isspace((int) *p); p++);
+	if (!*p || *p == '#') continue;
+
+	key = p;
+	while (*p && (isalnum((int) *p) || *p == '-' || *p == '_')) {
+	    if (isupper((int) *p)) *p = tolower((int) *p);
+	    p++;
+	}
+	if (*p != ':') {
+	    sprintf(buf,
+		    "invalid option name on line %d of configuration file",
+		    lineno);
+	    fatal(buf, EX_CONFIG);
+	}
+	*p++ = '\0';
+
+	while (*p && isspace((int) *p)) p++;
+	
+	if (!*p) {
+	    sprintf(buf, "empty option value on line %d of configuration file",
+		    lineno);
+	    fatal(buf, EX_CONFIG);
+	}
+
+	if (nconfiglist == alloced) {
+	    alloced += CONFIGLISTGROWSIZE;
+	    configlist = (struct configlist *)
+		realloc((char *)configlist, alloced*sizeof(struct configlist));
+	    if (configlist == NULL) goto abort;
+	}
+
+	configlist[nconfiglist].key = strdup(key);
+	if (!configlist[nconfiglist].key) goto abort;
+	configlist[nconfiglist].value = strdup(p);
+	if (!configlist[nconfiglist].value) goto abort;
+	nconfiglist++;
+    }
+ abort:
+    fclose(infile);
+}
+
+/*
+ * Call proc (expected to be todo_append in reconstruct.c) with
+ * information on each configured partition
+ */
+void config_scanpartition( void (*proc)() )
+{
+    int opt;
+    char *s, *t;
+
+    for (opt = 0; opt < nconfiglist; opt++) {
+	if (!strncmp(configlist[opt].key, "partition-", 10)) {
+	    s = strdup(configlist[opt].value);
+	    if (!s) continue;
+	    t = strdup("");
+	    if (!t) continue;
+	    (*proc)(t, s, configlist[opt].key+10);
+	}
+    }
+}
