ab6db1d13dce5ae9d479e5b98f8b0830f5bee6c7
Larry Greenfield <leg@andrew.cmu.edu>
2000-02-18 02:01:15 +0000

    added IMAP::Cyrus; untested lately

---
+++ cyrus/IMAP-Cyrus/cyradm.sh
@@ -0,0 +1,288 @@
+#! /bin/sh
+case "x$BASH_VERSION" in
+x) exec /usr/bin/perl -MIMAP::Cyrus::Shell -e shell -- ${1+"$@"} ;;
+*) exec /usr/bin/perl -MIMAP::Cyrus::Shell -e shell -- "$@" ;;
+esac
+echo "$0: how did I get here?" >&2
+exit 1
+
+=head1 NAME
+
+cyradm - Cyrus administration shell, alter ego of IMAP::Cyrus::Shell
+
+=head1 SYNOPSIS
+
+  $ cyradm [--user user] [--[no]rc] [--systemrc file] [--userrc file] \
+  > [--port n] [--auth mechanism] [--server] server
+
+but possibly
+
+  $ perl -MIMAP::Cyrus::Shell -e 'run("myscript")'
+
+or even (not recommended)
+
+  use IMAP::Admin::Shell;
+
+  run('myscriptname');
+
+=head1 DESCRIPTION
+
+This module implements B<cyradm> in Perl.  It is a shell around
+L<IMAP::Cyrus::Admin>.  Commands are provided in both Tcl-compatible
+forms and GNU-style long option forms.
+
+=head1 COMMANDS
+
+=over 4
+
+=item C<authenticate> [C<--minssf> I<N>] [C<--maxssf> I<N>] [C<--mechanisms> I<list>] [I<user>]
+
+=item C<auth> [C<--minssf> I<N>] [C<--maxssf> I<N>] [C<--mechanisms> I<list>] [I<user>]
+
+=item C<login> [C<--minssf> I<N>] [C<--maxssf> I<N>] [C<--mechanisms> I<list>] [I<user>]
+
+Authenticate to server.  You must already be connected to a server and
+Cyrus imapd will refuse to allow you to re-authenticate once you have
+authenticated once.
+
+=item C<chdir> I<directory>
+
+=item C<cd> I<directory>
+
+Change directory.  A C<pwd> builtin is not provided, but the default command
+action will run C<pwd> from a shell if invoked.
+
+=item C<createmailbox> [C<--partition> I<partition>] I<mailbox>
+
+=item C<createmailbox> I<mailbox> I<partition>
+
+=item C<create> [C<--partition> I<partition>] I<mailbox>
+
+=item C<create> I<mailbox I<partition>
+
+=item C<cm> [C<--partition> I<partition>] I<mailbox>
+
+=item C<cm> I<mailbox> I<partition>
+
+Create a mailbox on the default or a specified partition.  Both old-style
+and getopt-style usages are accepted (combining them will produce an error).
+
+=item C<deleteaclmailbox> I<mailbox> I<id> [...]
+
+=item C<deleteacl> I<mailbox> I<id> [...]
+
+=item C<dam> I<mailbox> I<id> [...]
+
+Remove ACLs from the specified mailbox.
+
+=item C<deletemailbox> I<mailbox>
+
+=item C<delete> I<mailbox>
+
+=item C<dm> I<mailbox>
+
+Delete the specified mailbox.
+
+Administrators do not have implicit delete rights on mailboxes.  Use the
+B<setaclmailbox> command to grant the C<d> permission to your principal
+if you need to delete a mailbox you do not own.
+
+Note that the online help admits to an optional host argument.  This argument
+is not currently used, and will be rejected with an error if specified; it
+is reserved for IMSP.
+
+=item C<disconnect>
+
+=item C<disc>
+
+Disconnect from the current server.  The prompt will revert to C<cyradmE<gt>>.
+
+=item C<exit> [I<number>]
+
+=item C<quit> [I<number>]
+
+Exit B<cyradm>, optionally with a specific exit status; the exit status of the
+last command will be used if one is not specified.
+
+=item help [command]
+
+=item ? [command]
+
+Show help for C<command> or all commands.
+
+=item listaclmailbox I<mailbox>
+
+=item listacl I<mailbox>
+
+=item lam I<mailbox>
+
+List ACLs on the specified mailbox.
+
+=item C<listmailbox> [C<--subscribed>] [I<pattern> [I<reference>]]
+
+=item C<list> [C<--subscribed>] [I<pattern> [I<reference>]]
+
+=item C<lm> [C<--subscribed>] [I<pattern> [I<reference>]]
+
+List all, or all subscribed, mailboxes matching the specified pattern.
+The pattern may have embedded wildcards C<'*'> or C<'%'>, which match
+anything or anything except the separator character, respectively.
+
+Mailboxes returned will be relative to the specified reference if one
+is specified.  This allows a mailbox list to be limited to a particular
+hierarchy.
+
+In some cases when the C<'%'> wildcard is used to end a pattern, it may
+match an entry which is not a mailbox but which contains other mailboxes.
+In this case, the entry will be parenthesized to indicate that it is a
+root for other mailboxes, as opposed to a mailbox itself.
+
+=item C<listquota> I<root>
+
+=item C<lq> I<root>
+
+List quotas on specified root.  If the specified mailbox path does not have
+a quota assigned, an error will be raised; see L<listquotaroot> for a way to
+find the quota root for a mailbox.
+
+=item C<listquotaroot> I<mailbox>
+
+=item C<lqm> I<mailbox>
+
+=item C<lqr> I<mailbox?>
+
+show quota roots and quotas for mailbox
+
+=item C<renamemailbox> [C<--partition> I<partition>] I<oldname> I<newname>
+
+=item C<rename> [C<--partition> I<partition>] I<oldname> I<newname>
+
+=item C<renm> [C<--partition> I<partition>] I<oldname> I<newname>
+
+=item C<renamemailbox> I<oldname> I<newname> [I<partition>]
+
+=item C<rename> I<oldname> I<newname> [I<partition>]
+
+=item C<renm> I<oldname> I<newname> [I<partition>]
+
+Rename the specified mailbox, optionally moving it to a different partition.
+Both old-style and getopt-style usages are accepted; combining them will
+produce an error.
+
+=item server [--noauthenticate] [server]
+
+=item connect [--noauthenticate] [server]
+
+=item servername [--noauthenticate] [server]
+
+With no arguments, show the current server.  With an argument, connect to that
+server.  It will prompt for automatic login unless the C<--noauthenticate>
+option is specified.  (This may change; in particular, either automatic
+authentication will be removed or all C<authenticate> options will be added.)
+
+When connected to a server, B<cyradm>'s prompt changes from C<cyradmE<gt>> to
+C<servernameE<gt>>, where I<servername> is the fully qualified domain name
+of the connected server.
+
+=item C<setaclmailbox> I<mailbox> I<id> I<rights> [I<id> I<rights> ...]
+
+=item C<setacl> I<mailbox> I<id> I<rights> [I<id> I<rights> ...]
+
+=item C<sam> I<mailbox> I<id> I<rights> [I<id> I<rights> ...]
+
+Set ACLs on a mailbox.  The ACL may be one of the special strings C<none>,
+C<read> (C<lrs>), C<post> (C<lrsp>), C<append> (C<lrsip>), C<write>
+(C<lrswipcd>), or C<all> (C<lrswipcda>), or any combinations of the ACL codes:
+
+=over 4
+
+=item l
+
+Lookup (visible to LIST/LSUB/UNSEEN)
+
+=item r
+
+Read (SELECT, CHECK, FETCH, PARTIAL, SEARCH, COPY source)
+
+=item s
+
+Seen (STORE \SEEN)
+
+=item w
+
+Write flags other than \SEEN and \DELETED
+
+=item i
+
+Insert (APPEND, COPY destination)
+
+=item p
+
+Post (send mail to mailbox)
+
+=item c
+
+Create (subfolders)
+
+=item d
+
+Delete (STORE \DELETED, EXPUNGE)
+
+=item a
+
+Administer (SETACL)
+
+=back
+
+=item C<setquota> I<root> I<resource> I<value> [I<resource> I<value> ...]
+
+=item C<sq> I<root> I<resource> I<value> [I<resource> I<value> ...]
+
+Set a quota on the specified root, which may or may not be an actual mailbox.
+The only I<resource> understood by B<Cyrus> is C<STORAGE>.
+
+=back
+
+=head1 NOTES
+
+GNU-style long options must be given in their entirety; Tcl-style options
+may be abbreviated.
+
+Tcl-style options are provided as a compatibility feature.  They will
+probably go away in the future.
+
+Multiple commands can be given on a line, separated by C<';'> characters.
+
+All commands set an exit status, which at present is not useful.
+
+Unknown commands are passed to a subshell for execution.
+
+The Tcl version of B<cyradm> is used for scripting as well as interactively.
+While this is possible to a limited extent by use of the C<run> method,
+scripting would normally be done with C<IMAP::Cyrus::Admin>, which is far
+more flexible than either interactive C<cyradm> or the Tcl scripting
+mechanism for Cyrus.
+
+B<cyradm> understands B</bin/sh>-style redirection:  any command can have
+its standard or error output redirected, with all B<sh>-style redirections
+(except C<E<lt>E<gt>>) supported.  It does not currently understand pipes
+or backgrounding.
+
+If the C<Term::Readline::Perl> or C<Term::Readline::GNU> modules are
+available, B<cyradm> will use it.
+
+An alias facility is implemented internally, but no access is currently
+provided to it.  This will change, if only to allow some of the predefined
+aliases to be removed if they conflict with useful shell commands.
+
+=head1 AUTHOR
+
+Brandon S. Allbery, allbery@ece.cmu.edu
+
+=head1 SEE ALSO
+
+IMAP::Cyrus::Admin
+Term::ReadLine
+sh(1), perl(1), imapd(8).
+
+=cut
---
+++ cyrus/IMAP-Cyrus/t/01-imclient.t
@@ -0,0 +1,107 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+
+######################### We start with some black magic to print on failure.
+
+# Change 1..1 below to 1..last_test_to_print .
+# (It may become useful if the test is moved to ./t subdirectory.)
+
+END {print "not ok 1\n" unless $loaded;}
+use IMAP::Cyrus;
+$loaded = 1;
+print "ok 1\n";
+
+######################### End of black magic.
+
+# find a server
+$old = select(STDERR); $| = 1; select($old);
+$server = $ENV{IMAPSERVER} || $ENV{BATISERVER};
+while (!defined($server) || $server eq '') {
+  print STDERR "enter an IMAP server to use for testing: ";
+  chomp($server = scalar(<STDIN>));
+  # needed so ->servername test will work; imclient does this internally
+  $server = (gethostbyname($server))[0];
+}
+
+# create an imclient object
+$client = IMAP::Cyrus->new($server);
+print "not " if !defined($client);
+print "ok 2\n";
+
+# try authenticating as the current user, plaintext
+# (tests send in simple mode)
+$user = $ENV{USER} || $ENV{LOGNAME} || (getpwuid($<))[0];
+system "stty -echo";
+print STDERR "Password: ";
+chomp($pass = scalar(<STDIN>));
+print STDERR "\n";
+system "stty echo";
+$plen = length($pass);
+print "not " if !$client->_send(undef, undef, "LOGIN $user {$plen}\r\n$pass");
+$pass = "\0" x length($pass);
+$plen = 0;
+print "ok 3\n";
+
+# verify it's pointing at the server
+$aserver = $client->servername;
+print "not " if $aserver ne $server;
+print "ok 4\n";
+
+# reauthenticate with the proper method (ugh)
+$client = IMAP::Cyrus->new($server);
+print "not " if !$client->authenticate('PLAIN');
+print "ok 5\n";
+
+# list the authentication methods available (tests send and callbacks)
+@caps = ();
+$didcap = 0;
+sub caps_cb {
+  my %cb = @_;
+  $didcap = 1;
+  push(@caps, map {s/^AUTH=// ? ($_) : ()} split(/ /, $cb{-text}));
+}
+$client->addcallback({-trigger => 'CAPABILITY', -callback => \&caps_cb});
+print "ok 6\n";
+$done = 0;
+sub done_cb {
+  my %cb = @_;
+  $ {$cb{-rock}} = 1;
+}
+$client->send(\&done_cb, \$done, 'CAPABILITY');
+$client->processoneevent until $done;
+print "not " unless $didcap;
+print "ok 7\n";
+
+# if we support kerberos 4 or gssapi auth, log in that way.
+foreach $cap (@caps) {
+  $client = IMAP::Cyrus->new($server);
+  # this is not fatal because someone might not have e.g. Krb5 tickets
+  print STDERR "authentication via $cap failed\n"
+    if !$client->authenticate($cap);
+}
+print "ok 8\n";
+
+# more advanced send usage
+$client = IMAP::Cyrus->new($server);
+print STDERR "enter a different user to authenticate (plaintext) as: ";
+chomp($auser = scalar(<STDIN>));
+system "stty -echo";
+print STDERR "Password: ";
+chomp($pass = scalar(<STDIN>));
+print STDERR "\n";
+system "stty echo";
+print "not " if !$client->send(undef, undef, 'LOGIN %a %s', $auser, $pass);
+print "ok 9\n";
+
+# authentication with extra parameters
+$client = IMAP::Cyrus->new($server);
+print "not " if !$client->authenticate(-mechanism => 'PLAIN',
+				       -service => 'imap',
+				       -user => $auser,
+				       -password => $pass,
+				       -minssf => 0,
+				       -maxssf => 10000);
+$pass = "\0" x length($pass);
+print "ok 10\n";
+
+BEGIN { $| = 1; print "1..10\n"; }
