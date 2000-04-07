3de4f90a28bb786adfcb9c367678d3bd1552e609
Walter Wong <wcw@andrew.cmu.edu>
2000-04-07 13:18:00 +0000

    fix typo in version.h add extra minor version to to version number

--- cyrus/imap/version.h
+++ cyrus/imap/version.h
@@ -1,9 +1,9 @@
 /* version.h: the version number
  *
- * $Id: version.h,v 1.82 2000/02/15 22:21:26 leg Exp $
+ * $Id: version.h,v 1.83 2000/04/07 13:18:00 wcw Exp $
  */
 
-#define _CYRUS_VERSION "v2.0"
+#define _CYRUS_VERSION "v2.0.0"
 
 /* EXTRA_IDENT is a hack to add some version information for which compile
  * was used to build this version (at CMU, but we don't care what you do with
@@ -16,6 +16,6 @@
 #define CYRUS_VERSION _CYRUS_VERSION
 #endif
 
-/* CAPABILITIES are now defined here, no including sasl ones */
+/* CAPABILITIES are now defined here, not including sasl ones */
 #define CAPABILITY_STRING "IMAP4 IMAP4rev1 ACL QUOTA LITERAL+ NAMESPACE " \
 	"UIDPLUS X-NON-HIERARCHICAL-RENAME NO_ATOMIC_RENAME UNSELECT"
