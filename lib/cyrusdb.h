7d110490dbf127c62394d7ef63118690a27a56c4
Larry Greenfield <leg@andrew.cmu.edu>
2000-04-25 02:41:09 +0000

    Added generic db interface; not yet used

---
+++ lib/cyrusdb.h
@@ -0,0 +1,96 @@
+/* 
+ * Copyright (c) 2000 Carnegie Mellon University.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The name "Carnegie Mellon University" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For permission or any other legal
+ *    details, please contact  
+ *      Office of Technology Transfer
+ *      Carnegie Mellon University
+ *      5000 Forbes Avenue
+ *      Pittsburgh, PA  15213-3890
+ *      (412) 268-4387, fax: (412) 268-7395
+ *      tech-transfer@andrew.cmu.edu
+ *
+ * 4. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Computing Services
+ *     at Carnegie Mellon University (http://www.cmu.edu/computing/)."
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+struct db;
+struct txn;
+
+enum cyrusdb_ret {
+    CYRUSDB_OK = 0,
+    CYRUSDB_IOERROR = 1,
+    CYRUSDB_AGAIN = 2
+};
+
+enum cyrusdb_initflags {
+    CYRUSDB_RECOVER = 0x01
+};
+
+typedef int foreach_cb(void *rock,
+		       char *key, int keylen,
+		       char *data, int datalen);
+
+struct cyrusdb_backend {
+    const char *name;
+
+    int (*init)(const char *dbdir, int myflags);
+    int (*done)(void);
+    int (*sync)(void);
+
+    int (*open)(const char *fname, struct db **ret);
+    int (*close)(struct db *db);
+    
+    int (*fetch)(struct db *mydb, 
+		 char *key, int keylen,
+		 char **data, int *datalen,
+		 struct txn **mytid);
+    int (*fetchlock)(struct db *mydb, 
+		     char *key, int keylen,
+ 		     char **data, int *datalen,
+		     struct txn **mytid);
+    int (*foreach)(struct db *mydb,
+		   char *prefix, int prefixlen,
+		   foreach_cb *cb, void *rock, 
+		   struct txn **tid);
+    int (*create)(struct db *db, 
+		  char *key, int keylen,
+		  char *data, int datalen,
+		  struct txn **tid);
+    int (*store)(struct db *db, 
+		 char *key, int keylen,
+		 char *data, int datalen,
+		 struct txn **tid);
+    int (*delete)(struct db *db, 
+		  char *key, int keylen,
+		  struct txn **tid);
+    
+    int (*commit)(struct db *db, struct txn *tid);
+    int (*abort)(struct db *db, struct txn *tid);
+};
+
+extern struct cyrusdb_backend cyrusdb_db3;
