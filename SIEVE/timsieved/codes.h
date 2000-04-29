e45a302eee1b74131bdcca26380b94e4d3e4397d
Tim Martin <tmartin@andrew.cmu.edu>
2000-04-29 18:41:36 +0000

    rearranging perlsieve dir

---
+++ SIEVE/timsieved/codes.h
@@ -0,0 +1,16 @@
+#ifndef _CODES_H_
+#define _CODES_H_
+
+
+
+#define SIEVE_OK 0
+#define SIEVE_FAIL -1
+
+#define SIEVE_BADPORT -5
+#define SIEVE_NOEXIST -6
+
+
+#define TRUE 1
+#define FALSE 0
+
+#endif
