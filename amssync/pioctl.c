503bc59fc71399d8df303e35cb7e27e3ef4f58c0
Larry Greenfield <leg@andrew.cmu.edu>
2000-05-04 03:29:09 +0000

    removing amssync

--- amssync/pioctl.c
+++
@@ -1,8 +0,0 @@
-/* Stub routine to get rid of the stupid NFS translator cruft */
-
-pioctl(path, cmd, cmarg, follow)
-char *path, *cmarg;
-int cmd, follow;
-{
-    return lpioctl(path, cmd, cmarg, follow);
-}
