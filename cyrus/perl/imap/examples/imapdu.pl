7e13c23eb83400ac5c6001e2e98a34166ea30c8c
Tim Martin <tmartin@andrew.cmu.edu>
2000-05-29 19:06:28 +0000

    imapdu bugfixes

--- cyrus/perl/imap/examples/imapdu.pl
+++ cyrus/perl/imap/examples/imapdu.pl
@@ -1,17 +1,90 @@
+#! /usr/bin/perl -w
+# 
+# Copyright (c) 2000 Carnegie Mellon University.  All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer. 
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+#
+# 3. The name "Carnegie Mellon University" must not be used to
+#    endorse or promote products derived from this software without
+#    prior written permission. For permission or any other legal
+#    details, please contact  
+#      Office of Technology Transfer
+#      Carnegie Mellon University
+#      5000 Forbes Avenue
+#      Pittsburgh, PA  15213-3890
+#      (412) 268-4387, fax: (412) 268-7395
+#      tech-transfer@andrew.cmu.edu
+#
+# 4. Redistributions of any form whatsoever must retain the following
+#    acknowledgment:
+#    "This product includes software developed by Computing Services
+#     at Carnegie Mellon University (http://www.cmu.edu/computing/)."
+#
+# CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+# THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+# FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+# AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+# OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+# This script WON'T work for you.  Guaranteed.
+# It checks CMU ECE policy, and your policy *will* be different.
+# Use it only as an example of how to use IMAP::Cyrus::Admin, as it will
+# almost certainly be useless to you as an actual program.
+#
+# This script sanity-checks departmental accounts against both the password
+# file and Cyrus.  It makes a LOT of CMU ECE-specific assumptions.  It also
+# doesn't do much with Cyrus aside from authenticating and getting a list of
+# top-level mailboxes, but this is currently all I have in the way of Perl
+# that uses IMAP::Cyrus.
+#
+
 use Getopt::Long;
 use Cyrus::IMAP;
 use Cyrus::IMAP::Admin;
 
-GetOptions("s|server=s" => \$server,
-           "w|where=s" => \$where,
-           "u|user=s" => \$user);
+sub usage {
+  print "imapdu - Summarize disk usage of mailboxes recursively\n";
+  print "  usage:\n";
+  print "    imapdu [-u user] <server> <pattern>\n";
+  print "\n";
+  print "  example: \n";
+  print "    imapdu cyrus.andrew.cmu.edu inbox\n";
+  print "\n";
+  exit 0;
+}
+
+GetOptions("u|user=s" => \$user);
 
 if (@ARGV) {
-    $re = shift(@ARGV);
+    $server = shift(@ARGV);
+} else {
+  usage;
+}
+
+if (@ARGV) {
+    $where = shift(@ARGV);
+} else {
+  usage;
+}
+
+if ((!defined $server) || (!defined $where)) {
+  usage;
 }
 
 my $cyrus = Cyrus::IMAP->new($server);
-$cyrus->authenticate(-user => $user, -maxssf => 0);
+$cyrus->authenticate(-user => $user); #to debug -maxsff => 0
 
 #list mailboxes in inbox.*
 my @info = ();
@@ -55,9 +128,9 @@ foreach $a (@info) {
 
   @z = split(/\./, $a);
   
-  undef $str;
+  $str = "";
   foreach $y (@z) {
-    if (defined $str) {
+    if ($str ne "") {
       $str=$str.".";
     }
     $str=$str.$y;
@@ -80,8 +153,7 @@ sub sizeofmailbox {
 
   #select something
   my @info = ();
-  $flags = Cyrus::IMAP::CALLBACK_NUMBERED;
-  $cyrus->addcallback({-trigger => 'FLAGS', -flags => $flags,
+  $cyrus->addcallback({-trigger => 'FLAGS',
 		       -callback => sub {
 			 
 		       },
@@ -116,7 +188,7 @@ sub sizeofmailbox {
 		    }, 
   -rock => \%info});
 
-  my ($rc, $msg) = $cyrus->send('', '', 'FETCH 1:* RFC822.SIZE');
+  ($rc, $msg) = $cyrus->send('', '', 'FETCH 1:* RFC822.SIZE');
   $cyrus->addcallback({-trigger => 'FETCH'});
 
   ($info{'totalsize'}, $info{'messages'});
