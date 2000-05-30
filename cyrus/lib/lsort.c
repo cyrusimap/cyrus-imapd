96761ee18e4ec5e2eb687223b41d4cfe87029dca
Ken Murchison <murch@andrew.cmu.edu>
2000-05-30 21:04:32 +0000

    added lsort (linked list sorting) for SORT/THREAD

---
+++ cyrus/lib/lsort.c
@@ -0,0 +1,74 @@
+/* lsort.c -- linked list (merge) sort
+ *
+ * public domain code by Ray Gardner.
+ *
+ * Modified by Ken Murchison to use getnext(), setnext() and call_data
+ * parameters.
+ */
+
+#include "lsort.h"
+#include <stdlib.h>
+
+/* merge two sorted lists */
+static void *lmerge(void *p, void *q,
+		    void *(*getnext)(void *),
+		    void (*setnext)(void *, void *), 
+		    int (*compar)(void *, void *, void *),
+		    void *call_data)
+{
+    void *r, *m;
+
+    /* the lowest item in p/q starts the new list */
+    if (compar(p, q, call_data) < 0) {
+      m = r = p;
+      p = getnext(p);
+    }
+    else {
+      m = r = q;
+      q = getnext(q);
+    }
+
+    /* merge the rest of p/q */
+    while (p && q) {
+	if (compar(p, q, call_data) < 0) {
+	    setnext(r, p);
+	    r = p;
+	    p = getnext(p);
+	}
+	else {
+	    setnext(r, q);
+	    r = q;
+	    q = getnext(q);
+	}
+    }
+
+    /* tack remainder of p/q onto the end */
+    setnext(r, p ? p : q);
+
+    return m;
+}
+
+void *lsort(void *p,
+	    void *(*getnext)(void *),
+	    void (*setnext)(void *, void *), 
+	    int (*compar)(void *, void *, void *),
+	    void *call_data)
+{
+    void *q, *r;
+
+    if (p) {
+	/* split list in half */
+	q = p;
+	for (r = getnext(q); r && (r = getnext(r)) != NULL; r = getnext(r))
+	    q = getnext(q);
+	r = getnext(q);
+	setnext(q, NULL);
+
+	/* sort each half recursively and merge the results */
+	if (r)
+	    p = lmerge(lsort(p, getnext, setnext, compar, call_data),
+		       lsort(r, getnext, setnext, compar, call_data),
+		       getnext, setnext, compar, call_data);
+    }
+    return p;
+}
---
+++ cyrus/lib/lsort.h
@@ -0,0 +1,23 @@
+/* lsort.h -- linked list (merge) sort
+ */
+
+#ifndef INCLUDED_LSORT_H
+#define INCLUDED_LSORT_H
+
+/*
+ * returns: head of the sorted list
+ *
+ * list: head of the linked list
+ * getnext: function that returns the next node of the given node
+ * setnext: function that sets the next node of the first arg to the second arg
+ * compar: function that compares two nodes (first two args).  The third arg
+ *   is a pointer to user data that may be used in comparing the nodes
+ * call_data: pointer to user data that is passed to compar()
+ */
+extern void *lsort(void *list,
+		   void *(*getnext)(void *),
+		   void (*setnext)(void *, void *),
+		   int (*compar)(void *, void *, void *), 
+		   void *call_data);
+
+#endif /* INCLUDED_LSORT_H */
