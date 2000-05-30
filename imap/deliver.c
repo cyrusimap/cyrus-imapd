590f17f3fd2961cf78ad941c06c16443c95d7044
Larry Greenfield <leg@andrew.cmu.edu>
2000-05-30 01:52:13 +0000

    ready to release

--- imap/deliver.c
+++ imap/deliver.c
@@ -106,7 +106,8 @@ static void
 usage()
 {
     fprintf(stderr, 
-	    "421-4.3.0 usage: deliver [-m mailbox] [-a auth] [-r return_path] [-l(lmtp)] [-D(debug)]\r\n");
+	    "421-4.3.0 usage: deliver [-m mailbox] [-a auth]"
+	    " [-r return_path] [-l] [-D]\r\n");
     fprintf(stderr, "421 4.3.0 %s\n", CYRUS_VERSION);
     exit(EC_USAGE);
 }
