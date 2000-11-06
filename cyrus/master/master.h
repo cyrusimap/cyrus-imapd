a0e33273b0a6e59fa1ae02b266cd10109f7481de
Larry Greenfield <leg@andrew.cmu.edu>
2000-11-06 18:57:05 +0000

    whoops, forgot master.h for snmp monitoring

---
+++ cyrus/master/master.h
@@ -0,0 +1,27 @@
+#ifndef HAVE_MASTER_H
+#define HAVE_MASTER_H
+
+/* needed for possible SNMP monitoring */
+struct service {
+    char *name;
+    char *listen;
+    char *proto;
+    char *const *exec;
+
+    int socket;
+    struct sockaddr *saddr;
+
+    int ready_workers;
+    int desired_workers;
+    int stat[2];
+
+    /* stats */
+    int nforks;
+    int nactive;
+};
+
+extern struct service *Services;
+extern int allocservices;
+extern int nservices;
+
+#endif /* HAVE_MASTER_H */
