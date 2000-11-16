3e916378850b9281ba1be665419e4f616b0eab6a
Ken Murchison <murch@andrew.cmu.edu>
2000-11-16 04:47:55 +0000

    REALLY fixed In-Reply-To header problem in send_response()

--- cyrus/imap/lmtpd.c
+++ cyrus/imap/lmtpd.c
@@ -1,6 +1,6 @@
 /* lmtpd.c -- Program to deliver mail to a mailbox
  *
- * $Id: lmtpd.c,v 1.51 2000/11/11 05:02:50 ken3 Exp $
+ * $Id: lmtpd.c,v 1.52 2000/11/16 04:47:55 ken3 Exp $
  * Copyright (c) 1999-2000 Carnegie Mellon University.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -42,7 +42,7 @@
  *
  */
 
-/*static char _rcsid[] = "$Id: lmtpd.c,v 1.51 2000/11/11 05:02:50 ken3 Exp $";*/
+/*static char _rcsid[] = "$Id: lmtpd.c,v 1.52 2000/11/16 04:47:55 ken3 Exp $";*/
 
 #include <config.h>
 
@@ -806,7 +806,7 @@ int send_response(void *ac, void *ic, void *sc, void *mc, const char **errmsg)
     time_t t;
     pid_t sm_pid, p;
     sieve_send_response_context_t *src = (sieve_send_response_context_t *) ac;
-    message_data_t *m = (message_data_t *) mc;
+    message_data_t *md = ((mydata_t *) mc)->m;
     script_data_t *sdata = (script_data_t *) sc;
 
     smbuf[0] = "sendmail";
@@ -852,8 +852,7 @@ int send_response(void *ac, void *ic, void *sc, void *mc, const char **errmsg)
 	    break;
 	}
     fprintf(sm, "Subject: %s\r\n", src->subj);
-    if (m->id)
-	fprintf(sm, "In-Reply-To: %s\r\n", m->id);
+    fprintf(sm, "In-Reply-To: %s\r\n", md->id);
     fprintf(sm, "Auto-Submitted: auto-replied (vacation)\r\n");
     if (src->mime) {
 	fprintf(sm, "MIME-Version: 1.0\r\n");
