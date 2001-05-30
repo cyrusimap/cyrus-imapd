b1939e33e598c998cfdbbde8a96c21a188705555
Larry Greenfield <leg@andrew.cmu.edu>
2001-05-30 17:05:19 +0000

    bumped version to 2.0.14

--- cyrus/imap/version.h
+++ cyrus/imap/version.h
@@ -36,10 +36,10 @@
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
  * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- * $Id: version.h,v 1.103 2001/04/26 22:38:00 leg Exp $
+ * $Id: version.h,v 1.104 2001/05/30 17:05:19 leg Exp $
  */
 
-#define _CYRUS_VERSION "v2.0.13"
+#define _CYRUS_VERSION "v2.0.14"
 
 /* EXTRA_IDENT is a hack to add some version information for which compile
  * was used to build this version (at CMU, but we don't care what you do with
