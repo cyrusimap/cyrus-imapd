f2bb5fae040310c15e59530611fc16335d4b2b39
Larry Greenfield <leg@andrew.cmu.edu>
2001-08-17 21:03:28 +0000

    routine to format an rfc822 date

---
+++ cyrus/lib/rfc822date.c
@@ -0,0 +1,77 @@
+/* rfc822date.c -- Generate an 822 date
+ $Id: rfc822date.c,v 1.1.2.1 2001/08/17 21:03:28 leg Exp $
+ 
+ * Copyright (c) 1998-2001 Carnegie Mellon University.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The name "Carnegie Mellon University" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For permission or any other legal
+ *    details, please contact  
+ *      Office of Technology Transfer
+ *      Carnegie Mellon University
+ *      5000 Forbes Avenue
+ *      Pittsburgh, PA  15213-3890
+ *      (412) 268-4387, fax: (412) 268-7395
+ *      tech-transfer@andrew.cmu.edu
+ *
+ * 4. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Computing Services
+ *     at Carnegie Mellon University (http://www.cmu.edu/computing/)."
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+#include <time.h>
+
+#include "assert.h"
+#include "rfc822date.h"
+#include "gmtoff.h"
+
+static char *month[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                         "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+static char *wday[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+/* 'buf' must be at least 80 characters */
+void rfc822date_gen(char *buf, time_t t)
+{
+    struct tm *tm;
+    long gmtoff;
+    int gmtnegative = 0;
+
+    assert(buf != NULL);
+
+    tm = localtime(&t);
+    gmtoff = gmtoff_of(tm, t);
+    if (gmtoff < 0) {
+	gmtoff = -gmtoff;
+	gmtnegative = 1;
+    }
+    gmtoff /= 60;
+
+    snprintf(buf, 80, "%s, %02d %s %4d %02d:%02d:%02d %c%.2lu%.2lu",
+	     wday[tm->tm_wday], 
+	     tm->tm_mday, month[tm->tm_mon], tm->tm_year + 1900,
+	     tm->tm_hour, tm->tm_min, tm->tm_sec,
+	     gmtnegative ? '-' : '+', gmtoff / 60, gmtoff % 60);
+}
