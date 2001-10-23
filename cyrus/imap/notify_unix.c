be671e2aee4f8c3b7d4b6392aaf76aee77490354
Larry Greenfield <leg@andrew.cmu.edu>
2001-10-23 20:16:09 +0000

    use writev

--- cyrus/imap/notify_unix.c
+++ cyrus/imap/notify_unix.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <sys/time.h>
 #include <errno.h>
 #include <string.h>
@@ -18,7 +19,6 @@
 const char *notify_method_desc = "unix";
 
 #define FNAME_NOTIFY_SOCK "/socket/notify"
-#define DIRSIZE 8192
 
 void notify(const char *class,
 	    const char *instance,
@@ -26,21 +26,20 @@ void notify(const char *class,
 	    const char *mailbox,
 	    const char *message)
 {
-       
-    char messageToSend[2048];
-    char dir[DIRSIZE];
-
     char buf[80];
     const char *notify_sock;
     struct sockaddr myname;
 
-    int   sock, adrlen, cnt;
+    int   sock, addrlen, cnt;
     fd_set read_template, write_template; 
     struct timeval wait; 
 
+    struct iovec iov[20];
+    int num_iov;
+
     sock = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sock < 0) {
-	syslog(LOG_ERR, "client socket failure %d\n", errno);
+	syslog(LOG_ERR, "notify_unix: socket(): %m");
 	return;
     }
 
@@ -53,20 +52,42 @@ void notify(const char *class,
 	strcpy(myname.sa_data, config_dir);
 	strcat(myname.sa_data, FNAME_NOTIFY_SOCK);
     }
-    adrlen = strlen(myname.sa_data) +
-	sizeof(myname.sa_family);
+    addrlen = strlen(myname.sa_data) + sizeof(myname.sa_family);
+    
+    /* put us in non-blocking mode */
+    fdflags = fcntl(sock, F_GETFD, 0);
+    if (fdflags != -1) fdflags = fcntl(sock, F_SETFL, O_NONBLOCK | fdflags);
+    if (fdflags == -1) { 
+	syslog(LOG_ERR, 
+	       "notify_unix: setting socket to nonblocking: fcntl(): %m")
+	close(sock); 
+	return -1; 
+    }
 
-    if (connect( sock, &myname, adrlen) < 0) {
-	syslog(LOG_ERR, "client connect failure %d\n", errno);
+    if (connect( sock, &myname, addrlen) < 0) {
+	syslog(LOG_ERR, "notify_unix: connect(): %m");
 	return;
     }
+
     /*  . . . . .  */
 
-    memset( messageToSend, 0, sizeof(messageToSend));
-    sprintf (messageToSend,"%s\n%s\n%s\n%s\n%s\n",class,instance,user,mailbox);
-    sprintf (messageToSend+strlen(messageToSend),"%.*s\n",
-	     sizeof(messageToSend) - strlen(messageToSend) - 2, mailbox);
-    cnt = write(sock, messageToSend, strlen(messageToSend));
+    num_iov = 0;
+
+    WRITEV_ADDSTR_TO_IOVEC(iov, num_iov, class);
+    WRITEV_ADDSTR_TO_IOVEC(iov, num_iov, "\n");
+    WRITEV_ADDSTR_TO_IOVEC(iov, num_iov, instance);
+    WRITEV_ADDSTR_TO_IOVEC(iov, num_iov, "\n");
+    WRITEV_ADDSTR_TO_IOVEC(iov, num_iov, user);
+    WRITEV_ADDSTR_TO_IOVEC(iov, num_iov, "\n");
+    WRITEV_ADDSTR_TO_IOVEC(iov, num_iov, mailbox);
+    WRITEV_ADDSTR_TO_IOVEC(iov, num_iov, "\n");
+    WRITEV_ADDSTR_TO_IOVEC(iov, num_iov, message);
+    WRITEV_ADDSTR_TO_IOVEC(iov, num_iov, "\n");
+    cnt = retry_writev(sock, iov, num_iov);
+
+    if (cnt < 0) {
+	syslog(LOG_ERR, "notify_unix: retry_writev(): %m");
+    }
 
     close(sock);
 }
