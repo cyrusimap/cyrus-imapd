2a1f1c060a3cc50e97ef3bf2dc994fbae6cc8d41
Ken Murchison <murch@andrew.cmu.edu>
2001-10-25 14:59:54 +0000

    portability fixes

--- cyrus/imap/xversion.sh
+++ cyrus/imap/xversion.sh
@@ -3,19 +3,22 @@
 # xversion.sh: extract the timestamp from the $Id: string
 # in every source file and use the most recent as the CVSDATE
 #
-# $Id: xversion.sh,v 1.4 2001/10/22 16:42:56 ken3 Exp $
+# $Id: xversion.sh,v 1.5 2001/10/25 14:59:54 ken3 Exp $
+
+if [ "$AWK" = "" ]; then
+    AWK=awk
+fi
 
 DATEPAT=[1-2][0-9][0-9][0-9]/[0-1][0-9]/[0-3][0-9]
 TIMEPAT=[0-2][0-9]:[0-5][0-9]:[0-5][0-9]
 
-echo "/* Generated automatically by xversion.sh */" > xversion.h
-echo >> xversion.h
+printf "/* Generated automatically by xversion.sh */\n\n" > xversion.h
 
-echo -n "#define CVSDATE " >> xversion.h
+printf "#define CVSDATE " >> xversion.h
 
 find .. -name '*.[chly]' -print | \
 	xargs egrep '\$Id: ' | \
-	awk ' # extract timestamp and surround with quotes
+	$AWK ' # extract timestamp and surround with quotes
 	match ($0, pattern) {
 	    printf "\"%s\"\n", substr($0, RSTART, RLENGTH)
 	}' pattern="$DATEPAT $TIMEPAT" | \
