1111f009076991aafdd71e5b3ec0f5ef970afd65
Rob Siemborski <rjs3@andrew.cmu.edu>
2002-01-22 22:31:52 +0000

    slave/client separation, master/slave talking to eachother fairly reliably

---
+++ imap/mupdate-slave.c
@@ -0,0 +1,208 @@
+/* mupdate-slave.c -- cyrus murder database clients
+ *
+ * $Id: mupdate-slave.c,v 1.1 2002/01/22 22:31:52 rjs3 Exp $
+ * Copyright (c) 2001 Carnegie Mellon University.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The name "Carnegie Mellon University" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For permission or any other legal
+ *    details, please contact  
+ *      Office of Technology Transfer
+ *      Carnegie Mellon University
+ *      5000 Forbes Avenue
+ *      Pittsburgh, PA  15213-3890
+ *      (412) 268-4387, fax: (412) 268-7395
+ *      tech-transfer@andrew.cmu.edu
+ *
+ * 4. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Computing Services
+ *     at Carnegie Mellon University (http://www.cmu.edu/computing/)."
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <config.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <ctype.h>
+#include <sasl/sasl.h>
+#include <sasl/saslutil.h>
+#include <syslog.h>
+#ifdef __STDC__
+#include <stdarg.h>
+#else
+#include <varargs.h>
+#endif
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#include <sys/types.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#ifdef HAVE_SYS_SELECT_H
+#include <sys/select.h>
+#endif
+
+#include "prot.h"
+#include "xmalloc.h"
+#include "imapconf.h"
+#include "assert.h"
+#include "imparse.h"
+#include "iptostring.h"
+#include "mupdate.h"
+#include "mupdate_err.h"
+#include "exitcodes.h"
+
+void mupdate_listen(mupdate_handle *handle, int pingtimeout)
+{
+    int gotdata = 0, firsttime = 1;
+    fd_set read_set;
+    int highest_fd;
+    
+    if (!handle || !handle->saslcompleted) return;
+
+    highest_fd = handle->sock + 1;
+    
+    /* ask for updates and set nonblocking */
+    prot_printf(handle->pout, "U01 UPDATE\r\n");
+    prot_NONBLOCK(handle->pin);
+
+    while(1) {
+	struct timeval tv;
+
+	tv.tv_sec = 15;
+	tv.tv_usec = 0;
+
+	prot_flush(handle->pout);
+	
+	FD_ZERO(&read_set);
+	FD_SET(handle->sock, &read_set);
+
+	gotdata = select(highest_fd, &read_set, NULL, NULL, &tv);
+
+	if (gotdata > 0) {
+	    /* If there is a fatal error, die, other errors ignore */
+	    if(mupdate_scarf(handle, cmd_change, NULL, firsttime) > 0) return;
+	    else {
+		firsttime = 0;
+		continue;
+	    }
+	} else if(gotdata == 0) {
+	    /* timed out, send a NOOP */
+	    prot_printf(handle->pout, "N%u NOOP\r\n", handle->tag++);
+	    prot_flush(handle->pout);
+
+	    /* wait 'pingtimeout' seconds for response */
+	    FD_ZERO(&read_set);
+	    FD_SET(handle->sock, &read_set);
+
+	    tv.tv_sec = pingtimeout;
+	    tv.tv_usec = 0;
+	    
+	    gotdata = select(highest_fd, &read_set, NULL, NULL, &tv);
+	    if(gotdata <= 0) {
+		/* We died, reconnect */
+		syslog(LOG_ERR, "master did not respond to NOOP in %d seconds",
+		       pingtimeout);
+		return;
+	    }
+	} else {
+	    syslog(LOG_ERR, "select failed");
+	    return;
+	}
+    }
+}
+
+void *mupdate_client_start(void *rock __attribute__((unused)))
+{
+    const char *server, *port, *num;
+    mupdate_handle *h;
+    int connection_count = 0;
+    int retries = 15;
+    int retry_delay = 20;
+    int ret;
+
+    server = config_getstring("mupdate_server", NULL);
+    if(server == NULL) {
+	fatal("couldn't get mupdate server name", EC_UNAVAILABLE);
+    }
+
+    port = config_getstring("mupdate_port",NULL);
+
+    num = config_getstring("mupdate_retry_count",NULL);
+    if(num && imparse_isnumber(num)) {
+	retries = atoi(num);
+	if(retries < 0) {
+	    fatal("invalid value for mupdate_retry_count", EC_UNAVAILABLE);
+	}
+    }
+
+    num = config_getstring("mupdate_retry_delay",NULL);
+    if(num && imparse_isnumber(num)) {
+	retry_delay = atoi(num);
+	if(retry_delay < 0) {
+	    fatal("invalid value for mupdate_retry_delay", EC_UNAVAILABLE);
+	}
+    }
+    
+    while(1) {
+	ret = mupdate_connect(server, port, &h, NULL);
+	if(ret) {
+	    syslog(LOG_ERR,"couldn't connect to mupdate server");
+	    goto retry;
+	}
+   
+	/* Successful Connection, reset counter: */
+	connection_count = -1;
+	syslog(LOG_ERR, "successful mupdate connection to %s", server);
+
+	mupdate_listen(h, retry_delay);
+
+    retry:
+	/* Cleanup */
+	prot_free(h->pin);
+	prot_free(h->pout);
+	close(h->sock);
+	sasl_dispose(&h->saslconn);
+	free(h); h = NULL;
+	
+	/* Should we retry? */
+	if(++connection_count < retries) {
+	    syslog(LOG_ERR,
+		   "retrying connection to mupdate server in %d seconds",
+		   retry_delay);
+	} else {
+	    syslog(LOG_ERR,
+		   "too many connection retries. dying.");
+	    break;
+	}
+	
+	/* Wait before retrying */
+	sleep(retry_delay);
+    }
+
+    return NULL;
+}
+
+
---
+++ imap/mupdatetest.c
@@ -0,0 +1,201 @@
+/* mupdatetest.c -- cyrus murder database test client
+ *
+ * $Id: mupdatetest.c,v 1.1 2002/01/22 22:31:53 rjs3 Exp $
+ * Copyright (c) 2001 Carnegie Mellon University.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The name "Carnegie Mellon University" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For permission or any other legal
+ *    details, please contact  
+ *      Office of Technology Transfer
+ *      Carnegie Mellon University
+ *      5000 Forbes Avenue
+ *      Pittsburgh, PA  15213-3890
+ *      (412) 268-4387, fax: (412) 268-7395
+ *      tech-transfer@andrew.cmu.edu
+ *
+ * 4. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Computing Services
+ *     at Carnegie Mellon University (http://www.cmu.edu/computing/)."
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <config.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <ctype.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "mupdate.h"
+#include "prot.h"
+#include "mupdate-client.h"
+
+void fatal(const char *message, int code)
+{
+    fprintf(stderr, "fatal error: %s\n", message);
+    exit(code);
+}
+
+#define LOGOUT "L01 LOGOUT\r\n"
+
+void interactive(int sock, struct protstream *pin, struct protstream *pout)
+{
+  char buf[2048];
+  fd_set read_set, rset;
+  fd_set write_set, wset;
+  int nfds;
+  int nfound;
+  int count;
+  int fd = 0;
+  int donewritingfile = 0;
+  
+  FD_ZERO(&read_set);
+  FD_SET(fd, &read_set);  
+  FD_SET(sock, &read_set);
+
+  FD_ZERO(&write_set);
+  FD_SET(sock, &write_set);
+
+  nfds = getdtablesize();
+
+  /* loop reading from network and from stdin if applicable */
+  while(1) {
+      rset = read_set;
+      wset = write_set;
+      nfound = select(nfds, &rset, &wset, NULL, NULL);
+      if (nfound < 0) {
+	  perror("select");
+	  fatal("select", errno);
+      }
+
+      if ((FD_ISSET(0, &rset)) && (FD_ISSET(sock, &wset)))  {
+	  if (fgets(buf, sizeof (buf) - 1, stdin) == NULL) {
+	      printf(LOGOUT);
+	      prot_write(pout, LOGOUT, sizeof (LOGOUT));
+	      FD_CLR(0, &read_set);
+	  } else {
+	      count = strlen(buf);
+	      buf[count - 1] = '\r';
+	      buf[count] = '\n';
+	      buf[count + 1] = '\0';
+	      prot_write(pout, buf, count + 1);
+	  }
+	  prot_flush(pout);
+      } else if (FD_ISSET(sock, &rset)) {
+	  do {
+	      count = prot_read(pin, buf, sizeof (buf) - 1);
+	      if (count == 0) {
+		  if (prot_error(pin)) {
+		      printf("Protection error: %s\n", prot_error(pin));
+		  }
+		  close(sock);
+		  printf("Connection closed.\n");
+		  return;
+	      }
+	      if (count < 0) {
+		  perror("read");
+		  fatal("prot_read", errno);
+	      }
+	      buf[count] = '\0';
+	      printf("%s", buf); 
+	  } while (pin->cnt > 0);
+      } else if ((FD_ISSET(fd, &rset)) && (FD_ISSET(sock, &wset))
+		 && (donewritingfile == 0)) {
+	  /* read from disk */	
+	  int numr = read(fd, buf, sizeof(buf));
+
+	  /* and send out over wire */
+	  if (numr < 0)
+	  {
+	      perror("read");
+	      fatal("read", errno);
+	  } else if (numr==0) {
+	      donewritingfile = 1;
+
+	      FD_CLR(fd,&read_set);
+
+	      /* send LOGOUT */
+	      printf(LOGOUT);
+	      prot_write(pout, LOGOUT, sizeof (LOGOUT));	      
+	      prot_flush(pout);
+	  } else {
+	      /* echo for the user */
+	      write(1, buf, numr);
+	      prot_write(pout, buf, numr);
+	      prot_flush(pout);
+	  }
+      } else {
+	  /* if can't do anything else sleep */
+	  usleep(1000);
+      }
+  }
+}
+
+int main(int argc, char **argv) 
+{
+    mupdate_handle *h;
+    const char *server=NULL, *port=NULL;
+    int c;
+    int flag_fail = 0;
+
+    sasl_client_init(NULL);
+
+    while((c = getopt(argc, argv, "s:p:")) != EOF) {
+	switch(c) {
+	case 's':
+	    server = optarg;
+	    break;
+	case 'p':
+	    port = optarg;
+	    break;
+	default:
+	    flag_fail = 1;
+	    break;
+	}
+    }
+
+    if(!server || flag_fail) {
+	printf("Usage: %s -s server [-p port]\n", argv[0]);
+	return 1;
+    }
+    
+    if(mupdate_connect(server, port, &h, NULL)) {
+	printf("mupdate_connect failure\n");
+	return 1;
+    }
+
+    printf("Connected to mupdate server: %s\r\n", server);
+
+    interactive(h->sock, h->pin, h->pout);
+
+    /* Perform Requested Operation */
+    mupdate_disconnect(&h);
+
+    return 0;
+}
+
+
