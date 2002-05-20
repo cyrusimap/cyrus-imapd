2f723ef21b0a3e69af710623bc50b86c023ac9f4
Rob Siemborski <rjs3@andrew.cmu.edu>
2002-05-20 23:29:37 +0000

    make use index.h

--- imap/index.c
+++ imap/index.c
@@ -41,7 +41,7 @@
  *
  */
 /*
- * $Id: index.c,v 1.178 2002/05/13 20:32:03 rjs3 Exp $
+ * $Id: index.c,v 1.179 2002/05/20 23:29:37 rjs3 Exp $
  */
 #include <config.h>
 
@@ -78,6 +78,8 @@
 #include "stristr.h"
 #include "search_engines.h"
 
+#include "index.h"
+
 extern int errno;
 
 extern void printastring (const char *s);
@@ -108,91 +110,6 @@ static int keepingseen;		/* Nonzero if /Seen is meaningful */
 static unsigned allseen;	/* Last UID if all msgs /Seen last checkpoint */
 struct seen *seendb;		/* Seen state database object */
 static char *seenuids;		/* Sequence of UID's from last seen checkpoint */
-
-/* Access macros for the memory-mapped index file data */
-#define INDEC_OFFSET(msgno) (index_base+start_offset+(((msgno)-1)*record_size))
-#define UID(msgno) ntohl(*((bit32 *)(INDEC_OFFSET(msgno)+OFFSET_UID)))
-#define INTERNALDATE(msgno) ntohl(*((bit32 *)(INDEC_OFFSET(msgno)+OFFSET_INTERNALDATE)))
-#define SENTDATE(msgno) ntohl(*((bit32 *)(INDEC_OFFSET(msgno)+OFFSET_SENTDATE)))
-#define SIZE(msgno) ntohl(*((bit32 *)(INDEC_OFFSET(msgno)+OFFSET_SIZE)))
-#define HEADER_SIZE(msgno) ntohl(*((bit32 *)(INDEC_OFFSET(msgno)+OFFSET_HEADER_SIZE)))
-#define CONTENT_OFFSET(msgno) ntohl(*((bit32 *)(INDEC_OFFSET(msgno)+OFFSET_CONTENT_OFFSET)))
-#define CACHE_OFFSET(msgno) ntohl(*((bit32 *)(INDEC_OFFSET(msgno)+OFFSET_CACHE_OFFSET)))
-#define LAST_UPDATED(msgno) ((time_t)ntohl(*((bit32 *)(INDEC_OFFSET(msgno)+OFFSET_LAST_UPDATED))))
-#define SYSTEM_FLAGS(msgno) ntohl(*((bit32 *)(INDEC_OFFSET(msgno)+OFFSET_SYSTEM_FLAGS)))
-#define USER_FLAGS(msgno,i) ntohl(*((bit32 *)(INDEC_OFFSET(msgno)+OFFSET_USER_FLAGS+((i)*4))))
-
-/* Access assistance macros for memory-mapped cache file data */
-#define CACHE_ITEM_BIT32(ptr) (ntohl(*((bit32 *)(ptr))))
-#define CACHE_ITEM_LEN(ptr) CACHE_ITEM_BIT32(ptr)
-#define CACHE_ITEM_NEXT(ptr) ((ptr)+4+((3+CACHE_ITEM_LEN(ptr))&~3))
-
-/* Cached envelope token positions */
-enum {
-    ENV_DATE = 0,
-    ENV_SUBJECT,
-    ENV_FROM,
-    ENV_SENDER,
-    ENV_REPLYTO,
-    ENV_TO,
-    ENV_CC,
-    ENV_BCC,
-    ENV_INREPLYTO,
-    ENV_MSGID
-};
-#define NUMENVTOKENS (10)
-
-/* Special "sort criteria" to load message-id and references/in-reply-to
- * into msgdata array for threaders that need them.
- */
-#define LOAD_IDS	256
-
-struct copyargs {
-    struct copymsg *copymsg;
-    int nummsg;
-    int msgalloc;
-};
-
-struct mapfile {
-    const char *base;
-    unsigned long size;
-};
-
-typedef struct msgdata {
-    unsigned msgno;		/* message number */
-    char *msgid;		/* message ID */
-    char **ref;			/* array of references */
-    int nref;			/* number of references */
-    time_t date;		/* sent date & time of message
-				   from Date: header (adjusted by time zone) */
-    char *cc;			/* local-part of first "cc" address */
-    char *from;			/* local-part of first "from" address */
-    char *to;			/* local-part of first "to" address */
-    char *xsubj;		/* extracted subject text */
-    unsigned xsubj_hash;	/* hash of extracted subject text */
-    int is_refwd;		/* is message a reply or forward? */
-    char **annot;		/* array of annotation attribute values
-				   (stored in order of sortcrit) */
-    int nannot;			/* number of annotation values */
-    struct msgdata *next;
-} MsgData;
-
-typedef struct thread {
-    MsgData *msgdata;		/* message data */
-    struct thread *parent;	/* parent message */
-    struct thread *child;	/* first child message */
-    struct thread *next;	/* next sibling message */
-} Thread;
-
-struct rootset {
-    Thread *root;
-    unsigned nroot;
-};
-
-struct thread_algorithm {
-    char *alg_name;
-    void (*threader)(unsigned *msgno_list, int nmsg, int usinguid);
-};
 
 /* Forward declarations */
 typedef int index_sequenceproc_t(struct mailbox *mailbox, unsigned msgno,
