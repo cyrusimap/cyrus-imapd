9f067f9363cf091808dd87764f961d64f1eba6a1
Rob Siemborski <rjs3@andrew.cmu.edu>
2002-08-30 18:40:20 +0000

    Ready for 2.1.9

--- imap/version.h
+++ imap/version.h
@@ -36,10 +36,10 @@
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
  * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- * $Id: version.h,v 1.118 2002/08/27 14:56:46 rjs3 Exp $
+ * $Id: version.h,v 1.119 2002/08/30 18:40:23 rjs3 Exp $
  */
 
-#define _CYRUS_VERSION "v2.1.8"
+#define _CYRUS_VERSION "v2.1.9"
 
 /* EXTRA_IDENT is a hack to add some version information for which compile
  * was used to build this version (at CMU, but we don't care what you do with
