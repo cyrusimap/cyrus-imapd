de937ffc3b3beb56bb8d601bb8dccbab548863fe
Ken Murchison <murch@andrew.cmu.edu>
2003-01-10 21:53:08 +0000

    HACK for MS Outlook's incorrect use of the old-style SASL discovery method

--- cyrus/imap/pop3d.c
+++ cyrus/imap/pop3d.c
@@ -40,7 +40,7 @@
  */
 
 /*
- * $Id: pop3d.c,v 1.130 2002/12/06 04:46:28 ken3 Exp $
+ * $Id: pop3d.c,v 1.131 2003/01/10 21:53:08 ken3 Exp $
  */
 #include <config.h>
 
@@ -613,8 +613,17 @@ static void cmdloop(void)
 		}
 	    }
 	    if (!*arg) {
-		prot_printf(popd_out, "-ERR Syntax error\r\n");
-		continue;
+		if (strcasecmp(inputbuf, "auth") == 0) {
+		    /* HACK for MS Outlook's incorrect use of the old-style
+		     * SASL discovery method.
+		     * Outlook uses "AUTH \r\n" instead if "AUTH\r\n"
+		     */
+		    arg = 0;
+		}
+		else {
+		    prot_printf(popd_out, "-ERR Syntax error\r\n");
+		    continue;
+		}
 	    }
 	}
 	else {
