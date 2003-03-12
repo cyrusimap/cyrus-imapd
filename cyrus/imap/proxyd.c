47614bf3fdc2d946be607dc58471a781e899acf6
Ken Murchison <murch@andrew.cmu.edu>
2003-03-12 21:17:57 +0000

    whoops, that's an inverted in pipe_until_tag...

--- cyrus/imap/proxyd.c
+++ cyrus/imap/proxyd.c
@@ -39,7 +39,7 @@
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: proxyd.c,v 1.131.2.52 2003/03/12 20:34:44 ken3 Exp $ */
+/* $Id: proxyd.c,v 1.131.2.53 2003/03/12 21:17:57 ken3 Exp $ */
 
 #include <config.h>
 
@@ -299,7 +299,7 @@ static int pipe_until_tag(struct backend *s, char *tag, int force_notfatal)
 
     s->timeout->mark = time(NULL) + IDLE_TIMEOUT;
     
-    if(taglen < sizeof(buf)) {
+    if(taglen >= sizeof(buf)) {
 	fatal("tag too large",EC_TEMPFAIL);
     }
 
