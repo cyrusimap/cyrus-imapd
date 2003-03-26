1176941b99102f5ab6841c57de3da2682de70c31
Rob Siemborski <rjs3@andrew.cmu.edu>
2003-03-26 15:42:33 +0000

    spurious include

--- cyrus/imap/proxyd.c
+++ cyrus/imap/proxyd.c
@@ -39,7 +39,7 @@
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: proxyd.c,v 1.153 2003/03/12 20:57:09 rjs3 Exp $ */
+/* $Id: proxyd.c,v 1.154 2003/03/26 15:42:33 rjs3 Exp $ */
 
 #undef PROXY_IDLE
 
@@ -84,7 +84,6 @@
 #include "charset.h"
 #include "imparse.h"
 #include "iptostring.h"
-#include "mkgmtime.h"
 #include "exitcodes.h"
 #include "imap_err.h"
 #include "mboxname.h"
