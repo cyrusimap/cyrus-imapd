d3de78f13ab3abd318cde823e0c8245b54fa55b7
Rob Siemborski <rjs3@andrew.cmu.edu>
2003-07-31 02:57:13 +0000

    working arbitron for per-user seen state

--- cyrus/imap/arbitron.c
+++ cyrus/imap/arbitron.c
@@ -39,7 +39,7 @@
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: arbitron.c,v 1.25 2003/04/22 17:40:27 rjs3 Exp $ */
+/* $Id: arbitron.c,v 1.26 2003/07/31 02:57:13 rjs3 Exp $ */
 
 #include <config.h>
 
@@ -50,6 +50,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <dirent.h>
 #include <errno.h>
 #include <syslog.h>
 #include <sys/types.h>
@@ -61,32 +62,45 @@
 #include "assert.h"
 #include "imapconf.h"
 #include "exitcodes.h"
+#include "hash.h"
 #include "imap_err.h"
 #include "mailbox.h"
-#include "xmalloc.h"
+#include "mpool.h"
 #include "mboxlist.h"
 #include "convert_code.h"
 #include "seen.h"
+#include "xmalloc.h"
+
+#define DB (CONFIG_DB_SEEN)
 
 extern int optind;
 extern char *optarg;
 
-int code = 0;
+/* Maintain the mailbox list */
+/* xxx it'd be nice to generate a subscriber list too */
+struct arb_mailbox_data {
+/*    const char *id; */
+    char *name;
+    int readers;
+/*    int subscribers; */
+};
+
+struct mpool *arb_pool;
+hash_table mailbox_table;
 
 time_t report_time, prune_time = 0;
+int code = 0;
 
 /* current namespace */
 static struct namespace arb_namespace;
 
 /* forward declarations */
 void usage(void);
-int do_mailbox();
-int arbitron(char *name);
-
-struct arbitronargs {
-    char *name;
-    unsigned read_count;
-};
+void run_users(void);
+void make_report(char *key, void *data, void *rock);
+void process_seen(const char *path);
+void process_subs(const char *user);
+int do_mailbox(const char *name, int matchlen, int maycreate, void *rock);
 
 int main(int argc,char **argv)
 {
@@ -121,8 +135,12 @@ int main(int argc,char **argv)
 	}
     }
 
+    /* Init Cyrus Backend Foo */
     config_init(alt_config, "arbitron");
 
+    mboxlist_init(0);
+    mboxlist_open(NULL);
+    
     /* Set namespace -- force standard (internal) */
     if ((r = mboxname_init_namespace(&arb_namespace, 1)) != 0) {
 	syslog(LOG_ERR, error_message(r));
@@ -136,101 +154,208 @@ int main(int argc,char **argv)
 	prune_time = time(0) - (prune_months*60*60*24*31);
     }
 
+    /* Allocate our shared memory pools */
+    arb_pool = new_mpool(0);
+    construct_hash_table(&mailbox_table, 1023, 1);
+
     /* Translate any separators in mailboxname */
     mboxname_hiersep_tointernal(&arb_namespace, pattern);
-
+    
+    /* Get the mailbox list */
+    fprintf(stderr, "Loading Mailboxes.\n");
     (*arb_namespace.mboxlist_findall)(&arb_namespace, pattern, 1, 0, 0,
 				      do_mailbox, NULL);
 
-    exit(code);
+    fprintf(stderr, "Loading Users");
+    
+    /* Now do all the users */
+    run_users();
 
-    return -1; /* never reaches */
+    fprintf(stderr, "\n");    
+
+    /* And print the report */
+    hash_enumerate(&mailbox_table, make_report, NULL);    
+
+    /* Free Resources */
+    free_hash_table(&mailbox_table, NULL);    
+    free_mpool(arb_pool);    
+    mboxlist_close();
+    mboxlist_done();    
+
+    return code;
 }
 
 void usage(void)
 {
     fprintf(stderr,
-	    "usage: arbitron [-C <alt_config] [-d days]"
+	    "usage: arbitron [-C alt_config] [-d days]"
 	    " [-p months] [mboxpattern]\n");
     exit(EC_USAGE);
 }    
 
-int
-do_mailbox(name, matchlen, maycreate)
-char *name;
-int matchlen;
-int maycreate;
+int do_mailbox(const char *name, int matchlen, int maycreate, void *rock)
 {
     int r;
+    struct mailbox mbox;
 
-    r = arbitron(name);
-    if (r) {
-	com_err(name, r, (r == IMAP_IOERROR) ? error_message(errno) : NULL);
-	code = convert_code(r);
+    r = mailbox_open_header(name, NULL, &mbox);
+    if(!r) {
+	struct arb_mailbox_data *d = mpool_malloc(arb_pool,
+						  sizeof(struct arb_mailbox_data));
+    
+	d->name = xstrdup(name);
+	d->readers = 0;
+
+/*	printf("inserting %s (key %s)\n", name, mbox.uniqueid); */
+
+	hash_insert(mbox.uniqueid, d, &mailbox_table);
+
+	mailbox_close(&mbox);
     }
 
     return 0;
 }
 
-int
-reportproc(rock, line)
-void *rock;
-const char *line;
+void run_users(void) 
 {
-    struct arbitronargs *arbitronargs = (struct arbitronargs *)rock;
-    const char *tab = strchr(line, '\t');
-    int useridlen = tab - line;
-
-    /* Don't report users reading their own private mailboxes */
-    if (!strncasecmp(arbitronargs->name, "user.", 5) &&
-	!memchr(line, '.', useridlen) &&
-	!strncasecmp(arbitronargs->name+5, line, useridlen) &&
-	(arbitronargs->name[5+useridlen] == '.' ||
-	 arbitronargs->name[5+useridlen] == '\0')) {
-	return 0;
+    char prefix[MAX_MAILBOX_PATH+1],path[MAX_MAILBOX_PATH+1],
+	file[MAX_MAILBOX_PATH+1];    
+    DIR *dirp, *dirq;
+    struct dirent *dirent1, *dirent2;
+    
+    snprintf(prefix, sizeof(prefix), "%s%s", config_dir, FNAME_USERDIR);
+    
+    dirp = opendir(prefix);
+    if(!dirp) {
+	fatal("can't open user directory", EC_SOFTWARE);
     }
 
-    arbitronargs->read_count++;
-    return 0;
-}
-
-int arbitron(char *name)
-{
-    int r;
-    struct mailbox mailbox;
-    struct arbitronargs arbitronargs;
-    char buf[MAX_MAILBOX_PATH+1];
-
-    /* Open/lock header */
-    r = mailbox_open_header(name, 0, &mailbox);
-    if (r) {
-	return r;
-    }
-
-    r = mailbox_open_index(&mailbox);
-    if (r) {
-	mailbox_close(&mailbox);
-	return r;
-    }
-
-    arbitronargs.name = name;
-    arbitronargs.read_count = 0;
-
-    r = seen_reconstruct(&mailbox, report_time, prune_time,
-			 reportproc, (void *)&arbitronargs);
-    mailbox_close(&mailbox);
-
-    if (!r) {
-	if (arbitronargs.read_count ||
-	    strncasecmp(name, "user.", 5) != 0) {
-	    /* Convert internal name to external */
-	    (*arb_namespace.mboxname_toexternal)(&arb_namespace, name,
-						 "cyrus", buf);
-	    printf("%u %s\n", arbitronargs.read_count, buf);
+    while((dirent1 = readdir(dirp)) != NULL) {
+	if(!strcmp(dirent1->d_name, ".") || !strcmp(dirent1->d_name,"..")) {
+	    continue;	    
 	}
+	
+	snprintf(path, sizeof(path), "%s%s", prefix, dirent1->d_name);
+/*	printf("trying %s\n",path); */
+	
+	dirq = opendir(path);
+	if(dirq) {	    
+	    fprintf(stderr, ".");	    
+	    while(dirq && ((dirent2 = readdir(dirq)) != NULL)) {
+		size_t len;
+		
+		if(!strcmp(dirent2->d_name, ".") ||
+		   !strcmp(dirent2->d_name,"..")) {
+		    continue;	    
+		}
+
+		len = strlen(dirent2->d_name);
+
+	        /* 5 is magic number for strlen(".seen") */
+		if(len > 5 && !strcmp(dirent2->d_name + len - 5, ".seen")) {
+		    snprintf(file, sizeof(file),
+			     "%s/%s", path, dirent2->d_name);
+/*		    printf("got file %s\n",file); */
+		    process_seen(file);
+		} else if (len > 5 &&
+			   !strcmp(dirent2->d_name + len - 5, ".subs")) {
+		    *(dirent2->d_name + len - 5) = '\0';		    
+		    process_subs(dirent2->d_name);		    
+		}		
+	    }
+	    closedir(dirq);
+	}
+	    
+    }    
+    closedir(dirp);
+
+}
+
+static int process_user_cb(void *rockp,
+			   const char *key, int keylen,
+			   const char *tmpdata __attribute__((unused)),
+			   int tmpdatalen __attribute__((unused))) 
+{
+    /* Only called to do deletes */
+    printf("pruning entry\n");
+    
+    DB->delete((struct db *)rockp, key, keylen, NULL, 0);    
+
+    return 0;    
+}
+
+/* We can cheat and do all we need to in this function */
+static int process_user_p(void *rockp __attribute__((unused)),
+			  const char *key,
+			  int keylen,
+			  const char *data,
+			  int datalen) 
+{
+    int ret = 0;    
+    long version, lastread;
+    char *p;    
+    char buf[64];
+    struct arb_mailbox_data *mbox;
+
+    /* remember that 'data' may not be null terminated ! */
+    version = strtol(data, &p, 10); data = p;
+    /* xxx not checking version */
+    lastread = strtol(data, &p, 10); data = p;
+    
+    memcpy(buf, key, keylen);
+    buf[keylen] = '\0';
+
+    mbox = hash_lookup(buf, &mailbox_table);
+    if(mbox && lastread >= report_time) {
+/*	printf("got %s\n", mbox->name);	     */
+	mbox->readers++;
     }
 
-    return r;
+    /* Check for pruning even if mailbox isn't valid */
+    if(lastread < prune_time) {
+	ret = 1;
+    }	
+
+    /* Only return true if we need to prune this guy */
+    return ret;    
+}
+
+void process_seen(const char *path) 
+{
+    int r;    
+    struct db *tmp = NULL;
+
+    r = DB->open(path, &tmp);
+    if(r) goto done;
+    
+    DB->foreach(tmp, "", 0, process_user_p, process_user_cb, tmp, NULL);
+
+ done:
+    if(tmp) DB->close(tmp);
+}
+
+void process_subs(const char *user) 
+{
+    /* xxx for this to be reasonable to do, we need to 
+     * have a lookup-mailbox-by-name hash table as well
+     *
+     * can probably just use mboxlist_findsub() directly
+     *
+     * stubbing out for now
+     */
+}
+
+void make_report(char *key, void *data, void *rock) 
+{
+    struct arb_mailbox_data *mbox = (struct arb_mailbox_data *)data;
+
+    /* Skip underread user mailboxes */
+    if(!strncasecmp(mbox->name, "user.", 5) && mbox->readers <= 1)
+	return;    
+
+    mboxname_hiersep_toexternal(&arb_namespace, mbox->name);
+
+    printf("%s %d\n", mbox->name, mbox->readers);    
 }
 
 void fatal(const char* s, int code)
