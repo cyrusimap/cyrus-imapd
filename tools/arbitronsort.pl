dee865cf23fb8b846cbabac6ea51294645a4ec8d
Rob Siemborski <rjs3@andrew.cmu.edu>
2003-08-04 17:13:24 +0000

    update to deal with subscribers

--- tools/arbitronsort.pl
+++ tools/arbitronsort.pl
@@ -1,10 +1,19 @@
 #!/usr/local/bin/perl
 
+#
+# This script takes the output of arbitron (run without the -o option)
+# and prints out first
+# a ranking of mailboxes by # of people who selected the mailbox
+# and then a ranking of mailbox by # of subscribers.
+#
+
 %data = ();
+%subs = ();
 
 while(<>) {
-    /^(.*) (\d+)$/;
+    /^(.*) (\d+) (\d+)$/;
     $data{$1} = $2;
+    $subs{$1} = $3;
 }
 
 $rank = 1;
@@ -12,3 +21,12 @@ $rank = 1;
 foreach $key (sort { $data{$b} <=> $data{$a} } (sort keys %data)) {
     print $rank++ . ": $key - $data{$key}\n";
 }
+
+print "\n\n";
+
+$rank = 1;
+
+foreach $key (sort { $subs{$b} <=> $subs{$a} } (sort keys %subs)) {
+    print $rank++ . ": $key - $subs{$key}\n";
+}
+
