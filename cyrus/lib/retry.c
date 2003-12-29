c074cacafca1184b318907517f60344f35617931
Rob Siemborski <rjs3@andrew.cmu.edu>
2003-12-29 21:25:30 +0000

    fix bug in retry_read (noticed by  Leandro Santi <lesanti@uolsinectis.com.ar>)

--- cyrus/lib/retry.c
+++ cyrus/lib/retry.c
@@ -1,6 +1,6 @@
 /* retry.c -- keep trying write system calls
- * $Id: retry.c,v 1.21 2003/10/22 18:50:12 rjs3 Exp $
- * 
+ $Id: retry.c,v 1.19.2.1 2003/12/29 21:25:30 rjs3 Exp $
+ 
  * Copyright (c) 1998-2003 Carnegie Mellon University.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -77,7 +77,7 @@ int retry_read(int fd, char *buf, size_t nbyte)
 
 	nread += n;
 
-	if (((size_t) nread) >= nbyte) return nread;
+	if (((size_t) n) >= nbyte) return nread;
 
 	buf += n;
 	nbyte -= n;
