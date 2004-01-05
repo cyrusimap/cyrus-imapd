32676f2bbf634a2a3ad1ffa5d5cf46be570163e8
Rob Siemborski <rjs3@andrew.cmu.edu>
2004-01-05 14:57:14 +0000

    add news2mail annotation to mboxconfig

--- perl/imap/IMAP/Admin.pm
+++ perl/imap/IMAP/Admin.pm
@@ -37,7 +37,7 @@
 # AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
 # OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
-# $Id: Admin.pm,v 1.39 2003/10/22 18:50:19 rjs3 Exp $
+# $Id: Admin.pm,v 1.40 2004/01/05 14:57:14 rjs3 Exp $
 
 package Cyrus::IMAP::Admin;
 use strict;
@@ -742,7 +742,8 @@ sub getinfo {
 sub mboxconfig {
   my ($self, $mailbox, $entry, $value) = @_;
 
-  my %values = ( "expire" => "/vendor/cmu/cyrus-imapd/expire",
+  my %values = ( "news2mail" => "/vendor/cmu/cyrus-imapd/news2mail",
+		 "expire" => "/vendor/cmu/cyrus-imapd/expire",
 		 "squat" => "/vendor/cmu/cyrus-imapd/squat" );
 
   if(!$self->{support_annotatemore}) {
