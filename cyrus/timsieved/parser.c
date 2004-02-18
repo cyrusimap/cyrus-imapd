007b66e8524c03114a58145650e77b54d69929df
Rob Siemborski <rjs3@andrew.cmu.edu>
2004-02-18 14:42:33 +0000

    correct sasl_decode64 handling (Ed Bardsley <ebardsle@andrew.cmu.edu>)

--- cyrus/timsieved/parser.c
+++ cyrus/timsieved/parser.c
@@ -1,7 +1,7 @@
 /* parser.c -- parser used by timsieved
  * Tim Martin
  * 9/21/99
- * $Id: parser.c,v 1.34 2003/10/24 16:23:12 rjs3 Exp $
+ * $Id: parser.c,v 1.35 2004/02/18 14:42:33 rjs3 Exp $
  */
 /*
  * Copyright (c) 1998-2003 Carnegie Mellon University.  All rights reserved.
@@ -549,8 +549,13 @@ static int cmd_authenticate(struct protstream *sieved_out,
     {
       clientin=(char *) malloc(str->len*2);
 
-      sasl_result=sasl_decode64(string_DATAPTR(str), str->len,
-				clientin, str->len*2, &clientinlen);
+      if (str->len) {
+	  sasl_result=sasl_decode64(string_DATAPTR(str), str->len,
+				    clientin, str->len*2, &clientinlen);
+      } else {
+	  clientinlen = 0;
+	  sasl_result = SASL_OK;
+      }
 
       if (sasl_result!=SASL_OK)
       {
