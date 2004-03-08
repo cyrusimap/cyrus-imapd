5e362593bc4d21c7d065833e24f64c67c2fa3b2a
Rob Siemborski <rjs3@andrew.cmu.edu>
2004-03-08 19:30:43 +0000

    don't bother kicking if we're not streaming

--- cyrus/imap/mupdate.c
+++ cyrus/imap/mupdate.c
@@ -1,6 +1,6 @@
 /* mupdate.c -- cyrus murder database master 
  *
- * $Id: mupdate.c,v 1.83 2004/03/08 17:31:05 rjs3 Exp $
+ * $Id: mupdate.c,v 1.84 2004/03/08 19:30:43 rjs3 Exp $
  * Copyright (c) 1998-2003 Carnegie Mellon University.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -764,9 +764,9 @@ mupdate_docmd_result_t docmd(struct conn *c)
 	else if (!strcmp(c->cmd.s, "Noop")) {
 	    CHECKNEWLINE(c, ch);
 	    
-	    /* Make *very* sure we are up-to-date */
-	    kick_mupdate();
 	    if (c->streaming) {
+		/* Make *very* sure we are up-to-date */
+		kick_mupdate();
 		sendupdates(c, 0); /* don't flush pout though */
 	    }
 	    
