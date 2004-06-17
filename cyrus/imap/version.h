30dbd429f5b969010415417b2c98eace920444c1
Rob Siemborski <rjs3@andrew.cmu.edu>
2004-06-17 18:54:29 +0000

    ready for 2.2.6

--- cyrus/imap/version.h
+++ cyrus/imap/version.h
@@ -37,10 +37,10 @@
  * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- * $Id: version.h,v 1.132 2004/05/27 16:40:09 rjs3 Exp $
+ * $Id: version.h,v 1.133 2004/06/17 18:54:31 rjs3 Exp $
  */
 
-#define _CYRUS_VERSION "v2.2.5"
+#define _CYRUS_VERSION "v2.2.6"
 
 /* EXTRA_IDENT is a hack to add some version information for which compile
  * was used to build this version (at CMU, but we don't care what you do with
