0701da02a2a2cf53316559bbc16f0464cb021653
Derrick Brashear <shadow@dementia.org>
2004-09-13 22:06:04 +0000

    update to call getstring with correct number of arguments

--- cyrus/master/service.c
+++ cyrus/master/service.c
@@ -39,7 +39,7 @@
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: service.c,v 1.45.2.4 2004/03/24 19:53:17 ken3 Exp $ */
+/* $Id: service.c,v 1.45.2.5 2004/09/13 22:06:04 shadow Exp $ */
 
 #include <config.h>
 
@@ -145,7 +145,7 @@ static int libwrap_ask(struct request_info *r __attribute__((unused)),
 #endif
 
 extern void cyrus_init(const char *, const char *, unsigned);
-extern const char *config_getstring(const char *key, const char *def);
+extern const char *config_getstring(const char *key);
 extern const char *config_dir;
 
 static int getlockfd(char *service, int id)
@@ -365,7 +365,7 @@ int main(int argc, char **argv, char **envp)
     if (call_debugger) {
 	char debugbuf[1024];
 	int ret;
-	const char *debugger = config_getstring("debug_command", NULL);
+	const char *debugger = config_getstring("debug_command");
 	if (debugger) {
 	    snprintf(debugbuf, sizeof(debugbuf), debugger, 
 		     argv[0], getpid(), service);
