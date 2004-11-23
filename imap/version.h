0c27454baef594106bf6c929083be74ba4d46d0e
Derrick Brashear <shadow@dementia.org>
2004-11-23 17:52:43 +0000

    make 2.2.10

--- imap/version.h
+++ imap/version.h
@@ -37,10 +37,10 @@
  * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- * $Id: version.h,v 1.136 2004/11/22 21:23:17 shadow Exp $
+ * $Id: version.h,v 1.137 2004/11/23 17:52:52 shadow Exp $
  */
 
-#define _CYRUS_VERSION "v2.2.9"
+#define _CYRUS_VERSION "v2.2.10"
 
 /* EXTRA_IDENT is a hack to add some version information for which compile
  * was used to build this version (at CMU, but we don't care what you do with
