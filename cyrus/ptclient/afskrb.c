61d91e0321b8e28268d36bca43fb6882de145448
Derrick Brashear <shadow@dementia.org>
2005-01-19 07:34:50 +0000

    avoid caching a permission denied answer from pts for more than a minute

--- cyrus/ptclient/afskrb.c
+++ cyrus/ptclient/afskrb.c
@@ -41,7 +41,7 @@
  */
 
 static char rcsid[] __attribute__((unused)) = 
-      "$Id: afskrb.c,v 1.2.2.5 2005/01/06 20:04:03 shadow Exp $";
+      "$Id: afskrb.c,v 1.2.2.6 2005/01/19 07:34:50 shadow Exp $";
 
 #include <config.h>
 
@@ -72,6 +72,7 @@ static char rcsid[] __attribute__((unused)) =
 /* AFS stuff */
 #include <des.h> /* for int32, necessary for the AFS includes below */
 #include <afs/ptserver.h>
+#include <afs/pterror.h>
 #include <afs/cellconfig.h>
 #include <rx/rxkad.h>
 #include <afs/auth.h>
@@ -486,7 +487,7 @@ struct auth_state *ptsmodule_make_authstate(const char *identifier,
     if (rc == PRPERM) {
 	newstate->mark = time(0) + 60 - 
 	    (config_getint(IMAPOPT_PTSCACHE_TIMEOUT) > 60)?
-	    config_getint(IMAPOPT_PTSCACHE_TIMEOUT) : 60);
+	    config_getint(IMAPOPT_PTSCACHE_TIMEOUT) : 60;
     } else
 	newstate->mark = time(0);
 
