6aaa6187dc9d02a0681b04d1caf7f336b46bce63
Derrick Brashear <shadow@dementia.org>
2005-02-22 05:58:00 +0000

    handle literals in listmailbox in imap admin perl module

--- perl/imap/IMAP/Admin.pm
+++ perl/imap/IMAP/Admin.pm
@@ -37,7 +37,7 @@
 # AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
 # OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
-# $Id: Admin.pm,v 1.45 2004/02/19 22:50:12 rjs3 Exp $
+# $Id: Admin.pm,v 1.46 2005/02/22 05:58:00 shadow Exp $
 
 package Cyrus::IMAP::Admin;
 use strict;
@@ -337,6 +337,7 @@ sub listmailbox {
 			next unless $d{-text} =~ s/^\(([^\)]*)\) //;
 			my $attrs = $1;
 			my $sep = '';
+			my $mbox;
 			# NIL or (attrs) "sep" "str"
 			if ($d{-text} =~ /^N/) {
 			  return if $d{-text} !~ s/^NIL//;
@@ -345,8 +346,10 @@ sub listmailbox {
 			  $sep = $1;
 			}
 			return unless $d{-text} =~ s/^ //;
-			my $mbox;
-			if ($d{-text} =~ /\"(([^\\\"]*\\)*[^\\\"]*)\"/) {
+                        if ($d{-text} =~ /{\d+}(.*)/) {
+			  # cope with literals (?)
+			  (undef, $mbox) = split(/\n/, $d{-text});
+                        } elsif ($d{-text} =~ /\"(([^\\\"]*\\)*[^\\\"]*)\"/) {
 			  ($mbox = $1) =~ s/\\(.)/$1/g;
 			} else {
 			  $d{-text} =~ /^([]!\#-[^-~]+)/;
