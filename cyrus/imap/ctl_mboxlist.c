38c5391ac9adff64ec5ef32899caf2eaa9682d4d
Ken Murchison <murch@andrew.cmu.edu>
2006-04-06 15:33:07 +0000

    ctl_mboxlist now dumps/undumps the mailbox type flags, making it useful for remote mailboxes.

--- cyrus/imap/ctl_mboxlist.c
+++ cyrus/imap/ctl_mboxlist.c
@@ -40,7 +40,7 @@
  *
  */
 
-/* $Id: ctl_mboxlist.c,v 1.49 2005/11/23 13:40:08 murch Exp $ */
+/* $Id: ctl_mboxlist.c,v 1.50 2006/04/06 15:33:08 murch Exp $ */
 
 /* currently doesn't catch signals; probably SHOULD */
 
@@ -187,7 +187,7 @@ static int dump_cb(void *rockp,
     switch (d->op) {
     case DUMP:
 	if(!d->partition || !strcmp(d->partition, part)) {
-	    printf("%s\t%s\t%s\n", name, part, acl);
+	    printf("%s\t%d %s\t%s\n", name, mbtype, part, acl);
 	    if(d->purge) {
 		config_mboxlist_db->delete(mbdb, key, keylen, &(d->tid), 0);
 	    }
@@ -490,7 +490,7 @@ void do_undump(void)
     while (fgets(buf, sizeof(buf), stdin)) {
 	char *name, *partition, *acl;
 	char *p;
-	int tries = 0;
+	int mbtype = 0, tries = 0;
 	
 	line++;
 
@@ -501,6 +501,12 @@ void do_undump(void)
 	    continue;
 	}
 	*p++ = '\0';
+	if (isdigit((int) *p)) {
+	    /* new style dump */
+	    mbtype = strtol(p, &p, 10);
+	    /* skip trailing space */
+	    if (*p == ' ') p++;
+	}
 	partition = p;
 	for (; *p && *p != '\t'; p++) ;
 	if (!*p) {
@@ -523,7 +529,7 @@ void do_undump(void)
 	}
 
 	key = name; keylen = strlen(key);
-	data = mboxlist_makeentry(0, partition, acl); datalen = strlen(data);
+	data = mboxlist_makeentry(mbtype, partition, acl); datalen = strlen(data);
 	
 	tries = 0;
     retry:
