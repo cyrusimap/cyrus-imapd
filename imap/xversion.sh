cfe6a9c2b7213cb79bcd058475073b160b54eead
Ken Murchison <murch@andrew.cmu.edu>
2007-10-17 18:45:48 +0000

    only recreate xversion.h when the CVSDATE actually changes (Gary Mills <mills@cc.umanitoba.ca>)

--- imap/xversion.sh
+++ imap/xversion.sh
@@ -3,18 +3,19 @@
 # xversion.sh: extract the timestamp from the $Id: string
 # in every source file and use the most recent as the CYRUS_CVSDATE
 #
-# $Id: xversion.sh,v 1.7 2003/10/22 18:03:00 rjs3 Exp $
+# $Id: xversion.sh,v 1.8 2007/10/17 18:45:48 murch Exp $
 
 if [ "$AWK" = "" ]; then
     AWK=awk
 fi
 
+TMPF=/tmp/xversion.$$
 DATEPAT=[1-2][0-9][0-9][0-9]/[0-1][0-9]/[0-3][0-9]
 TIMEPAT=[0-2][0-9]:[0-5][0-9]:[0-5][0-9]
 
-printf "/* Generated automatically by xversion.sh */\n\n" > xversion.h
+printf "/* Generated automatically by xversion.sh */\n\n" > $TMPF
 
-printf "#define CYRUS_CVSDATE " >> xversion.h
+printf "#define CYRUS_CVSDATE " >> $TMPF
 
 find .. -name '*.[chly]' -print | \
 	xargs egrep '\$Id: ' | \
@@ -22,4 +23,13 @@ find .. -name '*.[chly]' -print | \
 	match ($0, pattern) {
 	    printf "\"%s\"\n", substr($0, RSTART, RLENGTH)
 	}' pattern="$DATEPAT $TIMEPAT" | \
-	sort | tail -1 >> xversion.h
+	sort | tail -1 >> $TMPF
+
+if [ -f xversion.h ] && cmp -s $TMPF xversion.h
+then
+    rm $TMPF
+else
+    mv $TMPF xversion.h
+    rm -f version.o
+fi
+
