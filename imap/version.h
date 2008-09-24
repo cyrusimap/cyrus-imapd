8ac9a977b0a3617b4802c00f296cb4a76ab0f160
Ken Murchison <murch@andrew.cmu.edu>
2008-09-24 11:11:08 +0000

    getting ready for 2.3.13rc1

--- imap/version.h
+++ imap/version.h
@@ -39,10 +39,10 @@
  * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- * $Id: version.h,v 1.154 2008/04/21 11:17:22 murch Exp $
+ * $Id: version.h,v 1.155 2008/09/24 11:11:08 murch Exp $
  */
 
-#define _CYRUS_VERSION "v2.3.12"
+#define _CYRUS_VERSION "v2.3.13rc1"
 
 /* EXTRA_IDENT is a hack to add some version information for which compile
  * was used to build this version (at CMU, but we don't care what you do with
