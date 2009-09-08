7ca8ac3c63632184344184aa1bd65d2e9e60e3ca
Ken Murchison <murch@andrew.cmu.edu>
2009-09-08 15:47:41 +0000

    Ready for 2.2.13p1 (Fix buffer size calculations for sieve actions_string)

--- cyrus/imap/version.h
+++ cyrus/imap/version.h
@@ -1,13 +1,12 @@
 /* version.h: the version number
- *
- * Copyright (c) 1994-2008 Carnegie Mellon University.  All rights reserved.
+ * Copyright (c) 1998-2003 Carnegie Mellon University.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ *    notice, this list of conditions and the following disclaimer. 
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -16,15 +15,14 @@
  *
  * 3. The name "Carnegie Mellon University" must not be used to
  *    endorse or promote products derived from this software without
- *    prior written permission. For permission or any legal
- *    details, please contact
+ *    prior written permission. For permission or any other legal
+ *    details, please contact  
+ *      Office of Technology Transfer
  *      Carnegie Mellon University
- *      Center for Technology Transfer and Enterprise Creation
- *      4615 Forbes Avenue
- *      Suite 302
- *      Pittsburgh, PA  15213
- *      (412) 268-7393, fax: (412) 268-7395
- *      innovation@andrew.cmu.edu
+ *      5000 Forbes Avenue
+ *      Pittsburgh, PA  15213-3890
+ *      (412) 268-4387, fax: (412) 268-7395
+ *      tech-transfer@andrew.cmu.edu
  *
  * 4. Redistributions of any form whatsoever must retain the following
  *    acknowledgment:
@@ -39,10 +37,10 @@
  * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- * $Id: version.h,v 1.161 2009/09/04 20:58:30 murch Exp $
+ * $Id: version.h,v 1.140.4.1 2009/09/08 15:47:41 murch Exp $
  */
 
-#define _CYRUS_VERSION "v2.3.15rc1"
+#define _CYRUS_VERSION "v2.2.13p1"
 
 /* EXTRA_IDENT is a hack to add some version information for which compile
  * was used to build this version (at CMU, but we don't care what you do with
@@ -55,21 +53,13 @@
 #define CYRUS_VERSION _CYRUS_VERSION
 #endif
 
-enum {
-    CAPA_PREAUTH = 0x1,
-    CAPA_POSTAUTH = 0x2
-};
-
-/* CAPABILITIES are defined here, not including TLS/SASL ones,
-   and those that are configurable */
-#define CAPA_PREAUTH_STRING "IMAP4 IMAP4rev1 LITERAL+ ID"
-
-#define CAPA_POSTAUTH_STRING " ACL RIGHTS=kxte QUOTA " \
-	"MAILBOX-REFERRALS NAMESPACE UIDPLUS " \
+/* CAPABILITIES are now defined here, not including sasl ones */
+#define CAPABILITY_STRING "IMAP4 IMAP4rev1 ACL QUOTA LITERAL+ " \
+	"MAILBOX-REFERRALS NAMESPACE UIDPLUS ID " \
 	"NO_ATOMIC_RENAME UNSELECT " \
 	"CHILDREN MULTIAPPEND BINARY " \
-	"SORT SORT=MODSEQ THREAD=ORDEREDSUBJECT THREAD=REFERENCES " \
-	"ANNOTATEMORE CATENATE CONDSTORE SCAN"
+	"SORT THREAD=ORDEREDSUBJECT THREAD=REFERENCES " \
+	"ANNOTATEMORE"
 
 
 /* Values for ID processing */
