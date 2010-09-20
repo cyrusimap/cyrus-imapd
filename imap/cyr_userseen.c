f8f8231a9ec1f92004869b70705708f317452712
Bron Gondwana <brong@opera.com>
2010-09-20 23:06:55 +1000

    cyr_userseen seen cleanup tool

---
+++ imap/cyr_userseen.c
@@ -0,0 +1,143 @@
+/* ctl_userseen.c - tool to remove seen records for owners.
+ *
+ * Copyright (c) 1994-2008 Carnegie Mellon University.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The name "Carnegie Mellon University" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For permission or any legal
+ *    details, please contact
+ *      Carnegie Mellon University
+ *      Center for Technology Transfer and Enterprise Creation
+ *      4615 Forbes Avenue
+ *      Suite 302
+ *      Pittsburgh, PA  15213
+ *      (412) 268-7393, fax: (412) 268-7395
+ *      innovation@andrew.cmu.edu
+ *
+ * 4. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Computing Services
+ *     at Carnegie Mellon University (http://www.cmu.edu/computing/)."
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * $Id: ctl_cyrusdb.c,v 1.33 2010/01/06 17:01:30 murch Exp $
+ */
+
+#include <config.h>
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <errno.h>
+
+#include "cyrusdb.h"
+#include "global.h"
+#include "exitcodes.h"
+#include "libcyr_cfg.h"
+#include "mailbox.h"
+#include "mboxlist.h"
+#include "seen.h"
+#include "util.h"
+#include "xmalloc.h"
+
+/* config.c stuff */
+const int config_need_data = 0;
+static int do_remove = 0;
+
+void usage(void)
+{
+    fprintf(stderr, "cyr_userseen [-C <altconfig>] -d\n");
+    exit(-1);
+}
+
+/* Callback for use by delete_seen */
+static int deluserseen(char *name,
+		       int matchlen __attribute__((unused)),
+		       int maycreate __attribute__((unused)),
+		       void *rock __attribute__((unused)))
+{
+    struct mailbox *mailbox = NULL;
+    const char *userid;
+    int r;
+
+    r = mailbox_open_irl(name, &mailbox);
+    if (r) return r;
+
+    userid = mboxname_to_userid(name);
+    if (userid) {
+	printf("removing seen for %s on %s\n", userid, mailbox->name);
+	if (do_remove) seen_delete_mailbox(userid, mailbox);
+    }
+
+    mailbox_close(&mailbox);
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    char pattern[2] = { '*', '\0' };
+    extern char *optarg;
+    int opt;
+    char *alt_config = NULL;
+
+    if ((geteuid()) == 0 && (become_cyrus() != 0)) {
+	fatal("must run as the Cyrus user", EC_USAGE);
+    }
+
+    while ((opt = getopt(argc, argv, "C:d")) != EOF) {
+	switch (opt) {
+	case 'C': /* alt config file */
+	    alt_config = optarg;
+	    break;
+
+	case 'd':
+	    do_remove = 1;
+	    break;
+
+	default:
+	    usage();
+	    break;
+	}
+    }
+
+    cyrus_init(alt_config, "cyr_userseen", 0);
+
+    mboxlist_init(0);
+    mboxlist_open(NULL);
+
+    /* build a list of mailboxes - we're using internal names here */
+    mboxlist_findall(NULL, pattern, 1, NULL,
+		     NULL, deluserseen, NULL);
+
+    mboxlist_close();
+    mboxlist_done();
+
+    return 0;
+}
