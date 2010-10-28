7537af0c2cc2f15f5d7e3d9f1983ccd51b7275df
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2010-10-28 16:30:49 +0100

    Use inttypes.h if stdint.h is not available.

--- lib/crc32.h
+++ lib/crc32.h
@@ -5,7 +5,11 @@
 #define CRC32_H
 #include "util.h"
 #include <sys/uio.h>
-#include <stdint.h>
+#ifdef HAVE_STDINT_H
+# include <stdint.h>
+#else
+# include <inttypes.h>
+#endif
 
 uint32_t crc32_map(const char *base, unsigned len);
 uint32_t crc32_buf(struct buf *buf);
