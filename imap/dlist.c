4dfea62eb39e1771ede004bab02d0c0d78dc172c
Bron Gondwana <brong@opera.com>
2010-11-19 23:05:14 +1100

    Bug 3344 - Zero terminate literal transfers too

--- imap/dlist.c
+++ imap/dlist.c
@@ -283,8 +283,9 @@ struct dlist *dlist_buf(struct dlist *dl, const char *name,
 {
     struct dlist *i = dlist_child(dl, name);
     i->type = DL_BUF;
-    i->sval = xmalloc(len);
+    i->sval = xmalloc(len+1);
     memcpy(i->sval, val, len);
+    i->sval[len] = '\0'; /* make it string safe too */
     i->nval = len;
     return i;
 }
