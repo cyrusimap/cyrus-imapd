c6a8dc9133ff8855f868a51332c1da79c5dbcc20
Greg Banks <gnb@fastmail.fm>
2010-12-02 19:03:47 +1100

    unit: test new RFC822 address routines

--- cunit/parseaddr.c
+++ cunit/parseaddr.c
@@ -1,3 +1,4 @@
+#include <malloc.h>
 #include "cunit/cunit.h"
 #include "parseaddr.h"
 
@@ -217,3 +218,78 @@ static void test_group(void)
 }
 
 /* TODO: test the source routing feature */
+
+/* Test the iterator interface */
+static void test_iterator(void)
+{
+    struct address_itr ai;
+    const struct address *a;
+
+    address_itr_init(&ai, "");
+    a = address_itr_next(&ai);
+    CU_ASSERT_PTR_NULL(a);
+    address_itr_fini(&ai);
+
+    address_itr_init(&ai, "Fred Bloggs <fbloggs@fastmail.fm>, Sarah Jane Smith <sjsmith@gmail.com>");
+    a = address_itr_next(&ai);
+    CU_ASSERT_PTR_NOT_NULL_FATAL(a);
+    CU_ASSERT_STRING_EQUAL(a->name, "Fred Bloggs");
+    CU_ASSERT_STRING_EQUAL(a->mailbox, "fbloggs");
+    CU_ASSERT_STRING_EQUAL(a->domain, "fastmail.fm");
+    a = address_itr_next(&ai);
+    CU_ASSERT_PTR_NOT_NULL_FATAL(a);
+    CU_ASSERT_STRING_EQUAL(a->name, "Sarah Jane Smith");
+    CU_ASSERT_STRING_EQUAL(a->mailbox, "sjsmith");
+    CU_ASSERT_STRING_EQUAL(a->domain, "gmail.com");
+    CU_ASSERT_PTR_NULL(a->next);
+    address_itr_fini(&ai);
+}
+
+static void test_canonicalise(void)
+{
+    char *addr;
+
+    addr = address_canonicalise("Fred Bloggs <F.Bloggs@FastMAIL.fm>");
+    CU_ASSERT_STRING_EQUAL(addr, "F.Bloggs@fastmail.fm");
+    free(addr);
+}
+
+static void test_getparts(void)
+{
+    struct address *a;
+    char *s;
+
+    a = NULL;
+    parseaddr_list("Fred Bloggs <fbloggs+foo@FastMAIL.fm>", &a);
+    CU_ASSERT_PTR_NOT_NULL_FATAL(a);
+
+    s = address_get_all(a, 0);
+    CU_ASSERT_STRING_EQUAL(s, "fbloggs+foo@FastMAIL.fm");
+    free(s);
+
+    s = address_get_all(a, 1);
+    CU_ASSERT_STRING_EQUAL(s, "fbloggs+foo@fastmail.fm");
+    free(s);
+
+    s = address_get_localpart(a);
+    CU_ASSERT_STRING_EQUAL(s, "fbloggs+foo");
+    free(s);
+
+    s = address_get_domain(a, 0);
+    CU_ASSERT_STRING_EQUAL(s, "FastMAIL.fm");
+    free(s);
+
+    s = address_get_domain(a, 1);
+    CU_ASSERT_STRING_EQUAL(s, "fastmail.fm");
+    free(s);
+
+    s = address_get_user(a);
+    CU_ASSERT_STRING_EQUAL(s, "fbloggs");
+    free(s);
+
+    s = address_get_detail(a);
+    CU_ASSERT_STRING_EQUAL(s, "foo");
+    free(s);
+
+    parseaddr_free(a);
+}
