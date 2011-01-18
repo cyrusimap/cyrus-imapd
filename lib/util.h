d96b74d9de0dd1772b3941047bbf6031f079dc5a
Greg Banks <gnb@fastmail.fm>
2011-01-18 17:48:45 +1100

    Add buf_printf()
    Which does a printf()-like format with the result appending to a struct
    buf.  The buf is expanded as necessary, safely.  A gcc function
    attribute is provided to ensure that gcc will perform printf format
    checking.
    
    Also add BUF_INITIALIZER, allowing struct bufs to be explicitly
    initialised.

--- lib/util.h
+++ lib/util.h
@@ -173,6 +173,7 @@ struct buf {
     unsigned alloc;
     int flags;
 };
+#define BUF_INITIALIZER	{ NULL, 0, 0, 0 }
 
 const char *buf_cstring(struct buf *buf);
 void buf_getmap(struct buf *buf, const char **base, int *len);
@@ -186,6 +187,8 @@ void buf_appendcstr(struct buf *buf, const char *str);
 void buf_appendbit32(struct buf *buf, bit32 num);
 void buf_appendmap(struct buf *buf, const char *base, int len);
 void buf_putc(struct buf *buf, char c);
+void buf_printf(struct buf *buf, const char *fmt, ...)
+    __attribute__((format(printf,2,3)));
 void buf_free(struct buf *buf);
 
 /* use getpassphrase on machines which support it */
