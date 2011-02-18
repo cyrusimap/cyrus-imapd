42ef278020a0f1b64d2ac4e0f6777f1c2e5b0a1c
Bron Gondwana <brong@opera.com>
2011-02-18 09:54:53 +0100

    cmd_list: fix detection of zero length pattern

--- imap/imapd.c
+++ imap/imapd.c
@@ -6001,7 +6001,7 @@ void cmd_list(char *tag, struct listargs *listargs)
 
     list_callback_calls = 0;
 
-    if (!listargs->pat.data[0] && !(listargs->cmd & LIST_CMD_LSUB)) {
+    if (listargs->pat.count && !*(listargs->pat.data[0]) && !(listargs->cmd & LIST_CMD_LSUB)) {
 	/* special case: query top-level hierarchy separator */
 	prot_printf(imapd_out, "* LIST (\\Noselect) \"%c\" \"\"\r\n",
 		    imapd_namespace.hier_sep);
