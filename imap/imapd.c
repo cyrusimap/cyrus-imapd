cd2266a2e105006e50b6457ea99fec027e3dc997
Bron Gondwana <brong@opera.com>
2011-02-25 14:22:28 +0100

    Bug #3404: if no match, try list_response with exact name

--- imap/imapd.c
+++ imap/imapd.c
@@ -10487,6 +10487,11 @@ static void list_data(struct listargs *listargs)
 		    (*pattern)[strlen(*pattern) - 1] == '%';
 		findall(&imapd_namespace, *pattern, imapd_userisadmin,
 			imapd_userid, imapd_authstate, list_cb, &rock);
+
+		/* handle LIST "" user - and other such "only has children" cases */
+		if (!rock.last_name && !rock.trailing_percent) 
+		    rock.last_name = xstrdup(*pattern);
+
 		list_response(rock.last_name, rock.last_attributes, rock.listargs);
 		free(rock.last_name);
 		rock.last_name = NULL;
