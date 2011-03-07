3616debb4207a6b36fc2d4b53ebdb92182b048b8
Bron Gondwana <brong@opera.com>
2011-03-07 21:58:59 +0100

    LIST: don't return an entry ending with '*' or '%'

--- imap/imapd.c
+++ imap/imapd.c
@@ -10493,13 +10493,13 @@ static void list_data(struct listargs *listargs)
 	    }
 
 	    for (pattern = listargs->pat.data ; pattern && *pattern ; pattern++) {
-		rock.trailing_percent =
-		    (*pattern)[strlen(*pattern) - 1] == '%';
+		char lastchar = (*pattern)[strlen(*pattern) - 1];
+		rock.trailing_percent = (lastchar == '%');
 		findall(&imapd_namespace, *pattern, imapd_userisadmin,
 			imapd_userid, imapd_authstate, list_cb, &rock);
 
 		/* handle LIST "" user - and other such "only has children" cases */
-		if (!rock.last_name && !rock.trailing_percent) 
+		if (!rock.last_name && lastchar != '%' && lastchar != '*')
 		    rock.last_name = xstrdup(*pattern);
 
 		list_response(rock.last_name, rock.last_attributes, rock.listargs);
