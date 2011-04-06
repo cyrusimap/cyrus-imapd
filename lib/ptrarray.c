22ac8229bdecefc99f16cc9a049cf5505b851af6
Greg Banks <gnb@fastmail.fm>
2011-04-06 11:10:19 +1000

    Add ptrarray, an array of pointers.

---
+++ lib/ptrarray.c
@@ -0,0 +1,206 @@
+/* ptrarray.c -- an expanding array of pointers
+ *
+ * Copyright (c) 1994-2011 Carnegie Mellon University.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The name "Carnegie Mellon University" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For permission or any legal
+ *    details, please contact
+ *      Carnegie Mellon University
+ *      Center for Technology Transfer and Enterprise Creation
+ *      4615 Forbes Avenue
+ *      Suite 302
+ *      Pittsburgh, PA  15213
+ *      (412) 268-7393, fax: (412) 268-7395
+ *      innovation@andrew.cmu.edu
+ *
+ * 4. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Computing Services
+ *     at Carnegie Mellon University (http://www.cmu.edu/computing/)."
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Author: Greg Banks
+ * Start Date: 2011/01/11
+ */
+
+#include "ptrarray.h"
+#include <memory.h>
+#include "xmalloc.h"
+
+ptrarray_t *ptrarray_new(void)
+{
+    return xzmalloc(sizeof(ptrarray_t));
+}
+
+void ptrarray_fini(ptrarray_t *pa)
+{
+    if (!pa)
+	return;
+    memset(pa->data, 0, sizeof(void *) * pa->count);
+    free(pa->data);
+    pa->data = NULL;
+    pa->count = 0;
+    pa->alloc = 0;
+}
+
+void ptrarray_free(ptrarray_t *pa)
+{
+    if (!pa)
+	return;
+    ptrarray_fini(pa);
+    free(pa);
+}
+
+/*
+ * Ensure the index @idx exists in the array, if necessary expanding the
+ * array, and if necessary NULL-filling all the intervening elements.
+ * Note that we always ensure an empty slot past the last reported
+ * index, so that we can pass data[] to execve() or other routines that
+ * assume a NULL terminator.
+ */
+#define QUANTUM	    16
+static void ensure_alloc(ptrarray_t *pa, int newalloc)
+{
+    if (newalloc)
+	newalloc++;
+    if (newalloc <= pa->alloc)
+	return;
+    newalloc = ((newalloc + QUANTUM-1) / QUANTUM) * QUANTUM;
+    pa->data = xrealloc(pa->data, sizeof(void *) * newalloc);
+    memset(pa->data+pa->alloc, 0, sizeof(void *) * (newalloc-pa->alloc));
+    pa->alloc = newalloc;
+}
+
+static inline int adjust_index_ro(const ptrarray_t *pa, int idx)
+{
+    if (idx >= pa->count)
+	return -1;
+    else if (idx < 0)
+	idx += pa->count;
+    return idx;
+}
+
+static inline int adjust_index_rw(ptrarray_t *pa, int idx, int len)
+{
+    if (idx >= pa->count) {
+	ensure_alloc(pa, idx+len);
+    } else if (idx < 0) {
+	idx += pa->count;
+	if (idx >= 0 && len)
+	    ensure_alloc(pa, pa->count+len);
+    } else if (len) {
+	ensure_alloc(pa, pa->count+len);
+    }
+    return idx;
+}
+
+void ptrarray_add(ptrarray_t *pa, void *p)
+{
+    if (ptrarray_find(pa, p, 0) < 0)
+	ptrarray_append(pa, p);
+}
+
+void ptrarray_append(ptrarray_t *pa, void *p)
+{
+    ensure_alloc(pa, pa->count+1);
+    pa->data[pa->count++] = p;
+}
+
+void ptrarray_set(ptrarray_t *pa, int idx, void *p)
+{
+    if ((idx = adjust_index_rw(pa, idx, 0)) < 0)
+	return;
+    pa->data[idx] = p;
+}
+
+static inline void _ptrarray_insert(ptrarray_t *pa, int idx, void *p)
+{
+    if (idx < pa->count)
+	memmove(pa->data+idx+1, pa->data+idx,
+		sizeof(void *) * (pa->count-idx));
+    pa->data[idx] = p;
+    pa->count++;
+}
+
+void ptrarray_insert(ptrarray_t *pa, int idx, void *p)
+{
+    if ((idx = adjust_index_rw(pa, idx, 1)) < 0)
+	return;
+    _ptrarray_insert(pa, idx, p);
+}
+
+void *ptrarray_remove(ptrarray_t *pa, int idx)
+{
+    void *p;
+    if ((idx = adjust_index_ro(pa, idx)) < 0)
+	return NULL;
+    p = pa->data[idx];
+    pa->count--;
+    if (idx < pa->count)
+	memmove(pa->data+idx, pa->data+idx+1,
+		sizeof(void *) * (pa->count-idx));
+    return p;
+}
+
+void ptrarray_truncate(ptrarray_t *pa, int newlen)
+{
+    int i;
+
+    if (newlen == pa->count)
+	return;
+
+    if (newlen > pa->count) {
+	ensure_alloc(pa, newlen);
+    } else {
+	for (i = newlen ; i < pa->count ; i++) {
+	    pa->data[i] = NULL;
+	}
+    }
+    pa->count = newlen;
+}
+
+void *ptrarray_nth(const ptrarray_t *pa, int idx)
+{
+    if ((idx = adjust_index_ro(pa, idx)) < 0)
+	return NULL;
+    return pa->data[idx];
+}
+
+void **ptrarray_takevf(ptrarray_t *pa)
+{
+    void **d = pa->data;
+    pa->data = NULL;
+    pa->count = pa->alloc = 0;
+    ptrarray_free(pa);
+    return d;
+}
+
+int ptrarray_find(const ptrarray_t *pa, void *match, int starting)
+{
+    int i;
+
+    for (i = starting ; i < pa->count ; i++)
+	if (match == pa->data[i])
+	    return i;
+    return -1;
+}
---
+++ lib/ptrarray.h
@@ -0,0 +1,85 @@
+/* ptrarray.h -- an expanding array of pointers
+ *
+ * Copyright (c) 1994-2011 Carnegie Mellon University.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The name "Carnegie Mellon University" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For permission or any legal
+ *    details, please contact
+ *      Carnegie Mellon University
+ *      Center for Technology Transfer and Enterprise Creation
+ *      4615 Forbes Avenue
+ *      Suite 302
+ *      Pittsburgh, PA  15213
+ *      (412) 268-7393, fax: (412) 268-7395
+ *      innovation@andrew.cmu.edu
+ *
+ * 4. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Computing Services
+ *     at Carnegie Mellon University (http://www.cmu.edu/computing/)."
+ *
+ * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Author: Greg Banks
+ * Start Date: 2011/01/11
+ */
+
+#ifndef __CYRUS_PTRARRAY_H__
+#define __CYRUS_PTRARRAY_H__
+
+#include <config.h>
+#include <sys/types.h>
+
+typedef struct
+{
+    int count;
+    int alloc;
+    void **data;
+} ptrarray_t;
+
+#define PTRARRAY_INITIALIZER	{ 0, 0, NULL }
+#define ptrarray_init(pa)   (memset((pa), 0, sizeof(ptrarray_t)))
+void ptrarray_fini(ptrarray_t *);
+
+ptrarray_t *ptrarray_new(void);
+void ptrarray_free(ptrarray_t *);
+
+void ptrarray_append(ptrarray_t *, void *);
+void ptrarray_add(ptrarray_t *, void *);
+void ptrarray_set(ptrarray_t *, int idx, void *);
+void ptrarray_insert(ptrarray_t *, int idx, void *);
+void *ptrarray_remove(ptrarray_t *, int idx);
+void *ptrarray_nth(const ptrarray_t *pa, int idx);
+void ptrarray_truncate(ptrarray_t *pa, int newlen);
+
+#define ptrarray_shift(pa)	    ptrarray_remove((pa), 0)
+#define ptrarray_unshift(pa, s)	    ptrarray_insert((pa), 0, (s))
+
+#define ptrarray_pop(pa)	    ptrarray_remove((pa), -1)
+#define ptrarray_push(pa, s)	    ptrarray_append((pa), (s))
+
+void **ptrarray_takevf(ptrarray_t *pa);
+
+int ptrarray_find(const ptrarray_t *pa, void *match,
+		  int starting);
+
+#endif /* __CYRUS_PTRARRAY_H__ */
