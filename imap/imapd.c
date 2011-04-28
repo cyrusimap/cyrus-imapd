9f85d21cefda573fe4d5cf2b36ccc1a4483aadaa
Bron Gondwana <brong@opera.com>
2011-04-28 12:02:37 +0200

    list_response: fix LSUB where the deleted folder has children

--- imap/imapd.c
+++ imap/imapd.c
@@ -10118,6 +10118,26 @@ static void list_response(char *name, int attributes,
 	    attributes |= MBOX_ATTRIBUTE_HASNOCHILDREN;
     }
 
+    if (attributes & (MBOX_ATTRIBUTE_NONEXISTENT | MBOX_ATTRIBUTE_NOSELECT)) {
+	int keep = 0;
+	/* we have to mention this, it has children */
+	if (listargs->cmd & LIST_CMD_LSUB) {
+	    /* subscribed children need a mention */
+	    if (attributes & MBOX_ATTRIBUTE_CHILDINFO_SUBSCRIBED)
+		keep = 1;
+	    /* if mupdate is configured we can't drop out, we might
+	     * be a backend and need to report folders that don't
+	     * exist on this backend - this is awful and complex
+	     * and brittle and should be changed */
+	    if (config_mupdate_server)
+		keep = 1;
+	}
+	else if (attributes & MBOX_ATTRIBUTE_HASCHILDREN)
+	    keep = 1;
+
+	if (!keep) goto done;
+    }
+
     if (listargs->cmd & LIST_CMD_LSUB) {
 	/* \Noselect has a special second meaning with (R)LSUB */
 	if ( !(attributes & MBOX_ATTRIBUTE_SUBSCRIBED)
@@ -10143,24 +10163,6 @@ static void list_response(char *name, int attributes,
 	/* and owned by the user as well */
 	if (!mboxname_userownsmailbox(imapd_userid, mbentry->name))
 	    goto done;
-    }
-    if (attributes & (MBOX_ATTRIBUTE_NONEXISTENT | MBOX_ATTRIBUTE_NOSELECT)) {
-	int keep = 0;
-	/* we have to mention this, it has children */
-	if (attributes & MBOX_ATTRIBUTE_HASCHILDREN)
-	    keep = 1;
-	else if (listargs->cmd & LIST_CMD_LSUB) {
-	    /* subscribed children need a mention */
-	    if (attributes & MBOX_ATTRIBUTE_CHILDINFO_SUBSCRIBED)
-		keep = 1;
-	    /* if mupdate is configured we can't drop out, we might
-	     * be a backend and need to report folders that don't
-	     * exist on this backend - this is awful and complex
-	     * and brittle and should be changed */
-	    if (config_mupdate_server)
-		keep = 1;
-	}
-	if (!keep) goto done;
     }
 
     switch (listargs->cmd) {
