6e14fc3f562db1605babbb020c97ea787d653f40
Greg Banks <gnb@fastmail.fm>
2011-07-20 13:48:29 +1000

    Fix memleak in replica_connect()
    The sasl callbacks structure allocated in replica_connect() was never
    being freed, resulting in a small static memory leak every time the
    sync_client connected.

--- imap/sync_client.c
+++ imap/sync_client.c
@@ -2649,6 +2649,9 @@ void replica_connect(const char *channel)
 	sleep(wait);
     }
 
+    free_callbacks(cb);
+    cb = NULL;
+
     if (!sync_backend) {
 	fprintf(stderr, "Can not connect to server '%s'\n",
 		servername);
