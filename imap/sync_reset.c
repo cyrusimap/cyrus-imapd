5c04c7e2522d2a9f9d11f89cde64c8553ea2a4a4
Bron Gondwana <brong@opera.com>
2011-08-10 11:51:02 +0200

    Bug #3505 - fix sync_reset
    Thanks David Carter <dpc22@cam.ac.uk> for the report and patch

--- imap/sync_reset.c
+++ imap/sync_reset.c
@@ -144,9 +144,7 @@ static int reset_single(const char *userid)
 
     /* Nuke subscriptions */
     list = sync_name_list_create();
-    r = (sync_namespacep->mboxlist_findsub)(sync_namespacep, "*", 1,
-					    (char *)userid, sync_authstate,
-					    addmbox_sub, (void *)list, 1);
+    r = mboxlist_allsubs(userid, addmbox_sub, list);
     if (r) goto fail;
 
     /* ignore failures here - the subs file gets deleted soon anyway */
