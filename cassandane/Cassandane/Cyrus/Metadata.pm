bc3ed7da5f202c813ed85fb45931e69648430256
Greg Banks <gnb@fastmail.fm>
2011-09-16 14:08:48 +1000

    Fix test Metadata.size to use right folder names
    It turns out this test was failing because it used the folder name
    "inbox", where it should have used either "INBOX" when doing operations
    as user cassandane or "user.cassandane" when user admin.

--- cassandane/Cassandane/Cyrus/Metadata.pm
+++ cassandane/Cassandane/Cyrus/Metadata.pm
@@ -405,24 +405,26 @@ sub test_size
 
     my $imaptalk = $self->{store}->get_client();
     my $res;
-    my $folder = $self->{store}->{folder};
+    my $folder_cass = 'INBOX';
+    my $folder_admin = 'user.cassandane';
+    $self->{store}->set_folder($folder_cass);
     my $entry = '/shared/vendor/cmu/cyrus-imapd/size';
 
     xlog "initial value is numeric zero";
-    $res = $imaptalk->getmetadata($folder, $entry);
+    $res = $imaptalk->getmetadata($folder_cass, $entry);
     $self->assert_str_equals('ok', $imaptalk->get_last_completion_response());
     $self->assert_deep_equals({
-	$folder => { $entry => "0" }
+	$folder_cass => { $entry => "0" }
     }, $res);
 
     xlog "cannot set the value as ordinary user";
-    $imaptalk->setmetadata($folder, $entry, '123');
+    $imaptalk->setmetadata($folder_cass, $entry, '123');
     $self->assert_str_equals('no', $imaptalk->get_last_completion_response());
     $self->assert($imaptalk->get_last_error() =~ m/permission denied/i);
 
     xlog "cannot set the value as admin either";
     my $admintalk = $self->{adminstore}->get_client();
-    $admintalk->setmetadata($folder, $entry, '123');
+    $admintalk->setmetadata($folder_admin, $entry, '123');
     $self->assert_str_equals('no', $admintalk->get_last_completion_response());
     $self->assert($admintalk->get_last_error() =~ m/permission denied/i);
 
@@ -432,20 +434,20 @@ sub test_size
     $msg{A} = $self->make_message('Message A');
     $expected += length($msg{A}->as_string());
 
-    $res = $imaptalk->getmetadata($folder, $entry);
+    $res = $imaptalk->getmetadata($folder_cass, $entry);
     $self->assert_str_equals('ok', $imaptalk->get_last_completion_response());
     $self->assert_deep_equals({
-	$folder => { $entry => "" . $expected }
+	$folder_cass => { $entry => "" . $expected }
     }, $res);
 
     xlog "adding a 2nd message bumps the value by the message's size";
     $msg{B} = $self->make_message('Message B');
     $expected += length($msg{B}->as_string());
 
-    $res = $imaptalk->getmetadata($folder, $entry);
+    $res = $imaptalk->getmetadata($folder_cass, $entry);
     $self->assert_str_equals('ok', $imaptalk->get_last_completion_response());
     $self->assert_deep_equals({
-	$folder => { $entry => "" . $expected }
+	$folder_cass => { $entry => "" . $expected }
     }, $res);
 
     # TODO: removing a message doesn't reduce the value until (possibly delayed) expunge
