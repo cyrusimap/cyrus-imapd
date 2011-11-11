f12deeb18476e684a8310c8ae5b1b024d3903dbd
Ken Murchison <murch@andrew.cmu.edu>
2011-11-11 11:40:29 -0500

    httpd: need to always have SASL_CB_PROXY_POLICY otherwise auth_state isn't created

--- imap/httpd.c
+++ imap/httpd.c
@@ -206,7 +206,7 @@ static struct {
 
 static struct sasl_callback mysasl_cb[] = {
     { SASL_CB_GETOPT, &mysasl_config, NULL },
-/*    { SASL_CB_PROXY_POLICY, &mysasl_proxy_policy, (void*) &httpd_proxyctx },*/
+    { SASL_CB_PROXY_POLICY, &mysasl_proxy_policy, (void*) &httpd_proxyctx },
     { SASL_CB_CANON_USER, &mysasl_canon_user, NULL },
     { SASL_CB_LIST_END, NULL, NULL }
 };
