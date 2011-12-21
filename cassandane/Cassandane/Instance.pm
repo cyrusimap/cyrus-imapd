a1eb0631f0a3a7ad7e0838ae287555c1a578189e
Greg Banks <gnb@fastmail.fm>
2011-12-21 17:16:33 +1100

    Fail the test if core files are discovered

--- cassandane/Cassandane/Instance.pm
+++ cassandane/Cassandane/Instance.pm
@@ -491,7 +491,7 @@ sub start
     }
 }
 
-sub _emit_valgrind_logs
+sub _check_valgrind_logs
 {
     my ($self) = @_;
 
@@ -523,6 +523,30 @@ sub _emit_valgrind_logs
     closedir VGLOGS;
 
     die "Valgrind found errors" if $nerrs;
+}
+
+sub _check_cores
+{
+    my ($self) = @_;
+
+    my $coredir = $self->{basedir} . '/conf/cores';
+    my $ncores = 0;
+
+    opendir CORES, $coredir
+	or die "Cannot open directory $coredir for reading: $!";
+    while (my $_ = readdir CORES)
+    {
+	next if m/^\./;
+	next unless m/^core(\.\d+)?$/;
+	my $core = "$coredir/$_";
+	next if -z $core;
+	$ncores++;
+
+	xlog "Found core file $core";
+    }
+    closedir CORES;
+
+    die "Core files found in $coredir" if $ncores;
 }
 
 # Stop a given PID.  Returns 1 if the process died
@@ -579,7 +603,8 @@ sub stop
     }
     # Note: no need to reap this daemon which is not our child anymore
 
-    $self->_emit_valgrind_logs();
+    $self->_check_valgrind_logs();
+    $self->_check_cores();
 
 #     return if ($self->{persistent});
 #     rmtree $self->{basedir};
