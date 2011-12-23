678b1d64a626dc2f9618a20be264ebf1ada8cd0a
Bron Gondwana <brong@opera.com>
2011-12-23 23:04:28 +0100

    Revert "[IRIS-1081] don't refresh if highestmodseq is unchanged"
    This reverts commit 3a188287b8393de929ac66e529bd1b47fa130a96.
    
    This doesn't work correctly in 2.4.  Doh.

--- imap/index.c
+++ imap/index.c
@@ -660,9 +660,7 @@ int index_check(struct index_state *state, int usinguid, int printuid)
 	return IMAP_MAILBOX_NONEXISTENT;
     }
 
-    /* if highestmodseq has changed, read updates */
-    if (state->highestmodseq != mailbox->i.highestmodseq)
-	index_refresh(state);
+    index_refresh(state);
 
     /* any updates? */
     index_tellchanges(state, usinguid, printuid);
@@ -1171,13 +1169,8 @@ int index_getuidsequence(struct index_state *state,
 int index_lock(struct index_state *state)
 {
     int r = mailbox_lock_index(state->mailbox, LOCK_EXCLUSIVE);
-    if (r) return r;
-
-    /* if highestmodseq has changed, read updates */
-    if (state->highestmodseq != state->mailbox->i.highestmodseq)
-	index_refresh(state);
-
-    return 0;
+    if (!r) index_refresh(state);
+    return r;
 }
 
 int index_status(struct index_state *state, struct statusdata *sdata)
