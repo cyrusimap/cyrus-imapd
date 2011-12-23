6e45c471162e4866c4f5131c2314144ad11f7184
Bron Gondwana <brong@opera.com>
2011-12-23 13:49:33 +0100

    Bug #3595 - fix handling of errors in repack_commit
    Thanks David Carter <dpc22@cam.ac.uk> for the report and
    example patch.

--- imap/mailbox.c
+++ imap/mailbox.c
@@ -2503,7 +2503,7 @@ int mailbox_repack_commit(struct mailbox_repack **repackptr)
     unsigned char *buf = ibuf.buf;
     struct mailbox_repack *repack = *repackptr;
     int n;
-    int r = 0;
+    int r = IMAP_IOERROR;
 
     assert(repack);
 
@@ -2511,22 +2511,19 @@ int mailbox_repack_commit(struct mailbox_repack **repackptr)
 
     /* rewrite the header with updated details */
     mailbox_index_header_to_buf(&repack->i, buf);
-    n = lseek(repack->newindex_fd, 0, SEEK_SET);
-    if (n == -1) {
-	r = IMAP_IOERROR;
+
+    if (lseek(repack->newindex_fd, 0, SEEK_SET) < 0)
 	goto fail;
-    }
-    n = retry_write(repack->newindex_fd, buf, INDEX_HEADER_SIZE);
-    if (n == -1) {
-	r = IMAP_IOERROR;
+
+    if (retry_write(repack->newindex_fd, buf, INDEX_HEADER_SIZE) < 0)
 	goto fail;
-    }
 
     /* ensure everything is committed to disk */
-    if (fsync(repack->newindex_fd) || fsync(repack->newcache_fd)) {
-	r = IMAP_IOERROR;
+    if (fsync(repack->newindex_fd) < 0)
 	goto fail;
-    }
+
+    if (fsync(repack->newcache_fd) < 0)
+	goto fail;
 
     close(repack->newcache_fd);
     repack->newcache_fd = -1;
