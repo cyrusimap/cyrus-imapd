7496cd8607dc2f522bd7011ac906f89c17ef734a
Ken Murchison <murch@andrew.cmu.edu>
2011-12-27 20:39:57 -0500

    mailbox.c: remove unused variable from mailbox_repack_commit()

--- imap/mailbox.c
+++ imap/mailbox.c
@@ -2438,7 +2438,6 @@ int mailbox_repack_commit(struct mailbox_repack **repackptr)
     indexbuffer_t ibuf;
     unsigned char *buf = ibuf.buf;
     struct mailbox_repack *repack = *repackptr;
-    int n;
     int r = IMAP_IOERROR;
 
     assert(repack);
