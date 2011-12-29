557ab511b7f576f829b78baa674ec822ceba70c0
Bron Gondwana <brong@opera.com>
2011-12-29 12:20:17 +0100

    Bug #3603 - don't kick_mupdate on standard backends

--- imap/imapd.c
+++ imap/imapd.c
@@ -5123,15 +5123,13 @@ void cmd_create(char *tag, char *name, char *partition, int localonly)
 	prot_printf(imapd_out, "%s NO %s\r\n", tag, error_message(r));
     }
     else {
-	if (config_mupdate_server &&
-	    (config_mupdate_config != IMAP_ENUM_MUPDATE_CONFIG_STANDARD)) {
+	if (config_mupdate_server)
 	    kick_mupdate();
-	}
 
 	prot_printf(imapd_out, "%s OK %s\r\n", tag,
 		    error_message(IMAP_OK_COMPLETED));
     }
-}	
+}
 
 /* Callback for use by cmd_delete */
 static int delmbox(char *name,
@@ -5276,10 +5274,8 @@ void cmd_delete(char *tag, char *name, int localonly, int force)
 	prot_printf(imapd_out, "%s NO %s\r\n", tag, error_message(r));
     }
     else {
-	if (config_mupdate_server &&
-	    (config_mupdate_config != IMAP_ENUM_MUPDATE_CONFIG_STANDARD)) {
+	if (config_mupdate_server)
 	    kick_mupdate();
-	}
 
 	prot_printf(imapd_out, "%s OK %s\r\n", tag,
 		    error_message(IMAP_OK_COMPLETED));
@@ -5722,10 +5718,8 @@ submboxes:
     if (r) {
 	prot_printf(imapd_out, "%s NO %s\r\n", tag, error_message(r));
     } else {
-	if (config_mupdate_server &&
-	    (config_mupdate_config != IMAP_ENUM_MUPDATE_CONFIG_STANDARD)) {
+	if (config_mupdate_server)
 	    kick_mupdate();
-	}
 
 	prot_printf(imapd_out, "%s OK %s\r\n", tag,
 		    error_message(IMAP_OK_COMPLETED));
@@ -6412,10 +6406,8 @@ void cmd_setacl(char *tag, const char *name,
     if (r) {
 	prot_printf(imapd_out, "%s NO %s\r\n", tag, error_message(r));
     } else {
-	if (config_mupdate_server &&
-	    (config_mupdate_config != IMAP_ENUM_MUPDATE_CONFIG_STANDARD)) {
+	if (config_mupdate_server)
 	    kick_mupdate();
-	}
 
 	prot_printf(imapd_out, "%s OK %s\r\n", tag,
 		    error_message(IMAP_OK_COMPLETED));
