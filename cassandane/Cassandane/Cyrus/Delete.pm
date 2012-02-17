25f2170447ef514502b240b3e9c6cd78061c6148
Greg Banks <gnb@fmeh.org>
2012-02-17 14:17:30 +1100

    Add basic test of DELETE INBOX
    i.e. test that it isn't allowed and doesn't lose any messages, but that
    DELETE of an ordinary subfolder is allowed and the subfolder is gone.

---
+++ cassandane/Cassandane/Cyrus/Delete.pm
@@ -0,0 +1,122 @@
+#!/usr/bin/perl
+#
+#  Copyright (c) 2011-2012 Opera Software Australia Pty. Ltd.  All rights
+#  reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#
+#  3. The name "Opera Software Australia" must not be used to
+#     endorse or promote products derived from this software without
+#     prior written permission. For permission or any legal
+#     details, please contact
+# 	Opera Software Australia Pty. Ltd.
+# 	Level 50, 120 Collins St
+# 	Melbourne 3000
+# 	Victoria
+# 	Australia
+#
+#  4. Redistributions of any form whatsoever must retain the following
+#     acknowledgment:
+#     "This product includes software developed by Opera Software
+#     Australia Pty. Ltd."
+#
+#  OPERA SOFTWARE AUSTRALIA DISCLAIMS ALL WARRANTIES WITH REGARD TO
+#  THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+#  AND FITNESS, IN NO EVENT SHALL OPERA SOFTWARE AUSTRALIA BE LIABLE
+#  FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+#  AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+#  OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+use strict;
+use warnings;
+package Cassandane::Cyrus::Delete;
+use base qw(Cassandane::Cyrus::TestCase);
+use Cassandane::Util::Log;
+
+sub new
+{
+    my ($class, @args) = @_;
+    return $class->SUPER::new({}, @args);
+}
+
+sub set_up
+{
+    my ($self) = @_;
+    $self->SUPER::set_up();
+}
+
+sub tear_down
+{
+    my ($self) = @_;
+    $self->SUPER::tear_down();
+}
+
+sub test_self_inbox
+{
+    my ($self) = @_;
+
+    xlog "Testing that a non-admin can delete an a subfolder";
+    xlog "but cannot delete their own INBOX";
+
+    my $store = $self->{store};
+    my $talk = $store->get_client();
+    my $subfolder = 'INBOX.foo';
+
+    xlog "First create a sub folder";
+    $talk->create($subfolder)
+	or die "Cannot create folder $subfolder: $@";
+    $self->assert_str_equals('ok', $talk->get_last_completion_response());
+
+    xlog "Generate a message in INBOX";
+    my %exp_inbox;
+    $exp_inbox{A} = $self->make_message("Message INBOX A");
+    $self->check_messages(\%exp_inbox);
+
+    xlog "Generate a message in $subfolder";
+    my %exp_sub;
+    $store->set_folder($subfolder);
+    $store->_select();
+    $self->{gen}->set_next_uid(1);
+    $exp_sub{A} = $self->make_message("Message $subfolder A");
+    $self->check_messages(\%exp_sub);
+
+    xlog "can delete the subfolder";
+    $talk->unselect();
+    $talk->delete($subfolder)
+	or die "Cannot delete folder $subfolder: $@";
+    $self->assert_str_equals('ok', $talk->get_last_completion_response());
+
+    xlog "Cannot select the subfolder anymore";
+    $talk->select($subfolder);
+    $self->assert_str_equals('no', $talk->get_last_completion_response());
+    $self->assert_matches(qr/Mailbox does not exist/i, $talk->get_last_error());
+
+    xlog "But the message in INBOX is still there";
+    $store->set_folder('INBOX');
+    $store->_select();
+    $self->check_messages(\%exp_inbox);
+
+    xlog "cannot delete our own INBOX";
+    $talk->delete('INBOX');
+    $self->assert_str_equals('no', $talk->get_last_completion_response());
+    $self->assert_matches(qr/Operation is not supported/i, $talk->get_last_error());
+
+    xlog "And the message in INBOX is still there";
+    $store->set_folder('INBOX');
+    $store->_select();
+    $self->check_messages(\%exp_inbox);
+}
+
+1;
