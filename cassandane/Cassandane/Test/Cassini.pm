370bc60a4e5e605de1130ec2745b3839a9fb6c5e
Greg Banks <gnb@fastmail.fm>
2012-04-05 10:30:10 +1000

    Add a self-test for the Cassini module

---
+++ cassandane/Cassandane/Test/Cassini.pm
@@ -0,0 +1,126 @@
+#!/usr/bin/perl
+#
+#  Copyright (c) 2011-2012 Opera Software Australia Pty. Ltd.  All rights
+#  reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#
+#  3. The name "Opera Software Australia" must not be used to
+#     endorse or promote products derived from this software without
+#     prior written permission. For permission or any legal
+#     details, please contact
+# 	Opera Software Australia Pty. Ltd.
+# 	Level 50, 120 Collins St
+# 	Melbourne 3000
+# 	Victoria
+# 	Australia
+#
+#  4. Redistributions of any form whatsoever must retain the following
+#     acknowledgment:
+#     "This product includes software developed by Opera Software
+#     Australia Pty. Ltd."
+#
+#  OPERA SOFTWARE AUSTRALIA DISCLAIMS ALL WARRANTIES WITH REGARD TO
+#  THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+#  AND FITNESS, IN NO EVENT SHALL OPERA SOFTWARE AUSTRALIA BE LIABLE
+#  FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+#  AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+#  OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+use strict;
+use warnings;
+package Cassandane::Test::Cassini;
+use base qw(Cassandane::Unit::TestCase);
+use File::chdir;
+use File::Temp qw(tempdir);
+use Cassandane::Cassini;
+use Cassandane::Util::Log;
+
+sub new
+{
+    my $class = shift;
+    return $class->SUPER::new(@_);
+}
+
+sub write_inifile
+{
+    my ($options, %contents) = @_;
+
+    my $filename = $options->{filename} || 'cassandane.ini';
+
+    my %sections;
+    foreach my $k (keys %contents)
+    {
+	my ($sec, $param) = split(/\./, $k);
+	$sections{$sec} ||= {};
+	$sections{$sec}->{$param} = $contents{$k};
+    }
+
+    open INIFILE, '>', $filename
+	or die "Cannot open file $filename for writing: $!";
+    foreach my $sec (keys %sections)
+    {
+	printf INIFILE "[%s]\n", $sec;
+	foreach my $param (keys %{$sections{$sec}})
+	{
+	    printf INIFILE "%s=%s\n", $sec, $sections{$sec}->{$param};
+	}
+    }
+    close INIFILE;
+}
+
+sub test_basic
+{
+    my ($self) = @_;
+
+    local $CWD = tempdir(CLEANUP => 1);
+
+    xlog "Working in temporary directory $CWD";
+    # data thanks to hipsteripsum.me
+    write_inifile({},
+	'helvetica.blog' => 'ethical',
+    );
+
+    my $cassini = Cassandane::Cassini->instance();
+
+    # Don't find non-existant param in non-existant section
+    $self->assert_null($cassini->val('swag', 'quinoa'));
+    # or return the default
+    $self->assert_str_equals('whatever',
+			     $cassini->val('swag', 'quinoa', 'whatever'));
+
+    # Don't find non-existant param in existant section
+    $self->assert_null($cassini->val('helvetica', 'quinoa'));
+    # or return the default
+    $self->assert_str_equals('whatever',
+			     $cassini->val('helvetica', 'quinoa', 'whatever'));
+
+    # Don't find param in non-existant section where the
+    # param does exist in another section
+    $self->assert_null($cassini->val('swag', 'blog'));
+    # or return the default
+    $self->assert_str_equals('whatever',
+			     $cassini->val('swag', 'blog', 'whatever'));
+
+    # Don't find case aliases for existant param
+    $self->assert_null($cassini->val('Helvetica', 'blog'));
+    $self->assert_null($cassini->val('helvetica', 'Blog'));
+    $self->assert_null($cassini->val('HELvEtIca', 'blOG'));
+
+    # Do find exact match for existant param
+    $self->assert_str_equals('ethical', $cassini->val('helvetica', 'blog'));
+}
+
+1;
