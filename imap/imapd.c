43419bf484f56056aec715af9bdd0cddc7668bdd
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2012-04-05 13:31:48 +0100

    Cherry-pick [master 368d4c45] for #3613.

--- imap/imapd.c
+++ imap/imapd.c
@@ -3079,7 +3079,15 @@ static int catenate_url(const char *s, const char *cur_name, FILE *f,
 	    }
 
 	    /* local mailbox */
-	    if (!r) r = index_open(mailboxname, NULL, &state);
+	    if (!r) {
+		struct index_init init;
+		memset(&init, 0, sizeof(init));
+		init.qresync = imapd_client_capa & CAPA_QRESYNC;
+		init.userid = imapd_userid;
+		init.authstate = imapd_authstate;
+		init.out = imapd_out;
+		r = index_open(mailboxname, &init, &state);
+	    }
 	    if (!r) doclose = 1;
 
 	    if (!r && !(state->myrights & ACL_READ))
