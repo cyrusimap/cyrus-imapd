6f30ad2bc80905a9b46478cfda698e6e642eb40f
Дилян Палаузов <git-dpa@aegee.org>
2012-04-19 17:56:48 +0000

    master/master does not need COM_ERR_LIBS
    lib/libconfig.c contained:
    
          if (!infile)
              infile = fopen(filename, "r");
    
          if (!infile) {
              snprintf(buf, bufsize, "can't open configuration file %s: %s",
    
    meaning, that if there is an error, then it is from fopen().  To print
    the error message from fopen(), the function libc:strerror() shall be
    used in place of error_message().
    
    In turn libcyrus_min.a does not need to link with COM_ERR_LIBS.

--- lib/libconfig.c
+++ lib/libconfig.c
@@ -384,7 +384,7 @@ void config_read_file(const char *filename)
 
     if (!infile) {
 	snprintf(buf, bufsize, "can't open configuration file %s: %s",
-		 filename, error_message(errno));
+		 filename, strerror(errno));
 	fatal(buf, EC_CONFIG);
     }
 
