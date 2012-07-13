f22df10643223e23d6b465637b92be6fd38af9d0
Greg Banks <gnb@fastmail.fm>
2012-07-13 16:17:43 +1000

    sync: remove algorithm.setup
    Since the sync_crc_algorithm abstraction code was implemented, it has
    been changed so that all the ->setup ->begin and ->end function pointers
    point at the very same function.  So we shall remove them all those
    function pointers and inline the trivial code they contain at the one
    callsite each, starting with ->setup.

--- imap/sync_support.c
+++ imap/sync_support.c
@@ -1819,7 +1819,6 @@ out:
 struct sync_crc_algorithm {
     const char *name;
     int preference;
-    int (*setup)(int);
     void (*begin)(void);
     void (*addrecord)(const struct mailbox *, const struct index_record *, int);
     void (*addannot)(const struct sync_annot *);
@@ -1829,11 +1828,6 @@ struct sync_crc_algorithm {
 
 static bit32 sync_crc32;
 static struct buf sync_crc32_buf;
-
-static int sync_crc32_setup(int cflags __attribute__((unused)))
-{
-    return 0;
-}
 
 static void sync_crc32_begin(void)
 {
@@ -2028,26 +2022,23 @@ static int sync_crc32_end(char *buf, int maxlen)
 static const struct sync_crc_algorithm sync_crc_algorithms[] = {
     { "CRC32",
 	1,
-	sync_crc32_setup,
 	sync_crc32_begin,
 	sync_crc32_addrecord_xor,
 	sync_crc32_addannot_xor,
 	sync_crc32_end },
     { "CRC32M", /* modulo arithmetic */
 	2,
-	sync_crc32_setup,
 	sync_crc32_begin,
 	sync_crc32_addrecord_plus,
 	sync_crc32_addannot_plus,
 	sync_crc32_end },
     { "MD5",  /* XOR the first 16 bytes of md5s instead */
 	3,
-	sync_crc32_setup,
 	sync_crc32_begin,
 	sync_md5_addrecord_xor,
 	sync_md5_addannot_xor,
 	sync_crc32_end },
-    { NULL, 0, NULL, NULL, NULL, NULL, NULL }
+    { NULL, 0, NULL, NULL, NULL, NULL }
 };
 
 static const struct sync_crc_algorithm *find_algorithm(const char *string)
@@ -2140,7 +2131,6 @@ int sync_crc_setup(const char *algorithm, const char *covers,
 {
     const struct sync_crc_algorithm *alg;
     int cflags;
-    int r;
 
     if (!algorithm || !*algorithm) {
 	/* default is first one */
@@ -2167,9 +2157,8 @@ int sync_crc_setup(const char *algorithm, const char *covers,
 	}
     }
 
-    r = alg->setup(cflags);
-    if (r < 0)
-	return r;
+    if (!(cflags & SYNC_CRC_BASIC))
+	return IMAP_INVALID_IDENTIFIER;
 
     sync_crc_algorithm = alg;
     sync_crc_covers = cflags;
