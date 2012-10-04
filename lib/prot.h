9bef24325bce38c0884a7659cd9e73085c70bb85
Ken Murchison <murch@andrew.cmu.edu>
2012-10-04 17:05:57 -0400

    prot: added prot_readbuf()

--- lib/prot.h
+++ lib/prot.h
@@ -262,6 +262,7 @@ extern int prot_printliteral(struct protstream *out, const char *s,
 extern int prot_printstring(struct protstream *out, const char *s);
 extern int prot_printastring(struct protstream *out, const char *s);
 extern int prot_read(struct protstream *s, char *buf, unsigned size);
+extern int prot_readbuf(struct protstream *s, struct buf *buf, unsigned size);
 extern char *prot_fgets(char *buf, unsigned size, struct protstream *s);
 
 /* select() for protstreams */
