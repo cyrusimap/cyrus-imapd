c03209731478c06f539e71aee2b0bc2632747377
Greg Banks <gnb@fastmail.fm>
2012-10-16 17:17:53 +1100

    Add command_popen() and friends.
    This makes the whole fork-and-pipe-and-exec dance a little bit simpler.
    
    v2: merged in
    fbec395 Actually add the command unit tests

--- lib/command.c
+++ lib/command.c
@@ -43,7 +43,6 @@
 #include <config.h>
 
 #include <sys/types.h>
-#include <stdio.h>
 #include <syslog.h>
 #include <stdlib.h>
 #ifdef HAVE_UNISTD_H
@@ -51,10 +50,15 @@
 #endif
 #include <errno.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #include "imap/imap_err.h"
+#include "xmalloc.h"
+#include "command.h"
 #include "signals.h"
 #include "strarray.h"
+
+static int wait_for_child(const char *argv0, pid_t pid);
 
 EXPORTED int run_command(const char *argv0, ...)
 {
@@ -86,6 +90,146 @@ EXPORTED int run_command(const char *argv0, ...)
     }
     else {
 	/* in parent */
+	r = wait_for_child(argv0, pid);
+    }
+
+out:
+    strarray_fini(&argv);
+    return r;
+}
+
+#define PIPE_READ	0
+#define PIPE_WRITE	1
+
+EXPORTED int command_popen(struct command **cmdp, const char *mode, const char *argv0, ...)
+{
+    va_list va;
+    const char *p;
+    strarray_t argv = STRARRAY_INITIALIZER;
+    pid_t pid;
+    int r = 0;
+    struct command *cmd;
+    int do_stdin = (strchr(mode, 'w') != NULL);
+    int do_stdout = (strchr(mode, 'r') != NULL);
+    int stdin_pipe[2] = { -1, -1 };
+    int stdout_pipe[2] = { -1, -1 };
+
+    strarray_append(&argv, argv0);
+
+    va_start(va, argv0);
+    while ((p = va_arg(va, const char *)))
+	strarray_append(&argv, p);
+    va_end(va);
+
+    if (do_stdin) {
+	r = pipe(stdin_pipe);
+	if (r) {
+	    syslog(LOG_ERR, "Failed to pipe(): %m");
+	    r = IMAP_SYS_ERROR;
+	    goto out;
+	}
+    }
+
+    if (do_stdout) {
+	r = pipe(stdout_pipe);
+	if (r) {
+	    syslog(LOG_ERR, "Failed to pipe(): %m");
+	    r = IMAP_SYS_ERROR;
+	    goto out;
+	}
+    }
+
+    pid = fork();
+    if (pid < 0) {
+	syslog(LOG_ERR, "Failed to fork: %m");
+	r = IMAP_SYS_ERROR;
+	goto out;
+    }
+
+    if (!pid) {
+	/* in child */
+	if (do_stdin) {
+	    close(stdin_pipe[PIPE_WRITE]);
+	    dup2(stdin_pipe[PIPE_READ], STDIN_FILENO);
+	    close(stdin_pipe[PIPE_READ]);
+	}
+	if (do_stdout) {
+	    close(stdout_pipe[PIPE_READ]);
+	    dup2(stdout_pipe[PIPE_WRITE], STDOUT_FILENO);
+	    close(stdout_pipe[PIPE_WRITE]);
+	}
+
+	r = execv(argv0, argv.data);
+	syslog(LOG_ERR, "Failed to execute %s: %m", argv0);
+	exit(1);
+    }
+
+    /* in parent */
+    cmd = xzmalloc(sizeof(struct command));
+    cmd->argv0 = xstrdup(argv0);
+    cmd->pid = pid;
+    if (do_stdin)
+	cmd->stdin_prot = prot_new(stdin_pipe[PIPE_WRITE], /*write*/1);
+    if (do_stdout)
+	cmd->stdout_prot = prot_new(stdout_pipe[PIPE_READ], /*write*/0);
+    *cmdp = cmd;
+
+out:
+    if (stdin_pipe[PIPE_READ] >= 0) close(stdin_pipe[PIPE_READ]);
+    if (stdout_pipe[PIPE_WRITE] >= 0) close(stdout_pipe[PIPE_WRITE]);
+    if (r) {
+	if (stdin_pipe[PIPE_WRITE] >= 0) close(stdin_pipe[PIPE_WRITE]);
+	if (stdout_pipe[PIPE_READ] >= 0) close(stdout_pipe[PIPE_READ]);
+    }
+    strarray_fini(&argv);
+    return r;
+}
+
+EXPORTED int command_pclose(struct command **cmdp)
+{
+    struct command *cmd = (cmdp ? *cmdp : NULL);
+    int r;
+
+    if (!cmd) return 0;
+
+    if (cmd->stdin_prot) {
+	prot_flush(cmd->stdin_prot);
+	close(cmd->stdin_prot->fd);
+	prot_free(cmd->stdin_prot);
+    }
+
+    if (cmd->stdout_prot) {
+	close(cmd->stdout_prot->fd);
+	prot_free(cmd->stdout_prot);
+    }
+
+    r = wait_for_child(cmd->argv0, cmd->pid);
+
+    free(cmd->argv0);
+    free(cmd);
+    *cmdp = NULL;
+
+    return r;
+}
+
+EXPORTED int command_done_stdin(struct command *cmd)
+{
+    int r = 0;
+
+    if (cmd->stdin_prot) {
+	r = prot_flush(cmd->stdin_prot);
+	close(cmd->stdin_prot->fd);
+	prot_free(cmd->stdin_prot);
+	cmd->stdin_prot = NULL;
+    }
+    return r;
+}
+
+static int wait_for_child(const char *argv0, pid_t pid)
+{
+    int r = 0;
+
+    if (pid) {
 	for (;;) {
 	    int status;
 	    pid_t pr = waitpid(pid, &status, 0);
@@ -122,7 +266,5 @@ EXPORTED int run_command(const char *argv0, ...)
 	}
     }
 
-out:
-    strarray_fini(&argv);
     return r;
 }
