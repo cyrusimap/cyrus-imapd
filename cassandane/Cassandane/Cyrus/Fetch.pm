94073e0c32418c87631fef74424c914d950a26cd
Bron Gondwana <brong@opera.com>
2012-10-23 10:28:09 +0200

    Fetch: ensure header limitation returns just the correct response

---
+++ cassandane/Cassandane/Cyrus/Fetch.pm
@@ -0,0 +1,91 @@
+#!/usr/bin/perl
+#
+#  Copyright (c) 2011 Opera Software Australia Pty. Ltd.  All rights
+#  reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#
+#  3. The name "Opera Software Australia" must not be used to
+#     endorse or promote products derived from this software without
+#     prior written permission. For permission or any legal
+#     details, please contact
+# 	Opera Software Australia Pty. Ltd.
+# 	Level 50, 120 Collins St
+# 	Melbourne 3000
+# 	Victoria
+# 	Australia
+#
+#  4. Redistributions of any form whatsoever must retain the following
+#     acknowledgment:
+#     "This product includes software developed by Opera Software
+#     Australia Pty. Ltd."
+#
+#  OPERA SOFTWARE AUSTRALIA DISCLAIMS ALL WARRANTIES WITH REGARD TO
+#  THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+#  AND FITNESS, IN NO EVENT SHALL OPERA SOFTWARE AUSTRALIA BE LIABLE
+#  FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+#  AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+#  OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+use strict;
+use warnings;
+package Cassandane::Cyrus::Fetch;
+use base qw(Cassandane::Cyrus::TestCase);
+use DateTime;
+use Cassandane::Util::Log;
+use IO::Scalar;
+
+sub new
+{
+    my $class = shift;
+    return $class->SUPER::new({}, @_);
+}
+
+sub set_up
+{
+    my ($self) = @_;
+    $self->SUPER::set_up();
+}
+
+sub tear_down
+{
+    my ($self) = @_;
+    $self->SUPER::tear_down();
+}
+
+#
+# Test COPY behaviour with a very long sequence set
+#
+sub test_fetch_header
+{
+    my ($self) = @_;
+
+    my $imaptalk = $self->{store}->get_client();
+
+    $imaptalk->create("INBOX.dest");
+    $self->make_message("Test Message");
+
+    # unfortunately, you can't see the data that went over the wire very easily
+    # in IMAPTalk - but we know the headers will be in a literal, so..
+    my $body = "";
+    $imaptalk->literal_handle_control(new IO::Scalar \$body);
+    my $res = $imaptalk->fetch('1', '(UID FLAGS BODY.PEEK[HEADER.FIELDS (MESSAGE-ID)])');
+    $imaptalk->literal_handle_control(0);
+
+    $self->assert(defined $res, "Fetch feturned a response");
+    $self->assert_matches(qr/^Message-ID: <[^>]+>\s+$/, $body);
+}
+
+1;
