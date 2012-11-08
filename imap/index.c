d6cac98b09fef67c2fee122194f2e2aa0faf49e9
Greg Banks <gnb@fastmail.fm>
2012-11-08 15:20:25 +1100

    Stop pretending index_calcsearchflags can fail

--- imap/index.c
+++ imap/index.c
@@ -1809,7 +1809,7 @@ static void index_unlock(struct index_state *state)
     mailbox_unlock_index(state->mailbox, NULL);
 }
 
-static int index_calcsearchflags(struct index_state *state, struct searchargs *searchargs)
+static void index_calcsearchflags(struct index_state *state, struct searchargs *searchargs)
 {
     struct strlist *l;
     int flag;
@@ -1838,8 +1838,6 @@ static int index_calcsearchflags(struct index_state *state, struct searchargs *s
 	}
 	searchargs->user_flags_unset[flag/32] |= 1<<(flag&31);
     }
-
-    return 0;
 }
 
 /*
@@ -1858,8 +1856,7 @@ EXPORTED int index_search(struct index_state *state, struct searchargs *searchar
 	return 0;
 
     /* calculate the user flags */
-    if (index_calcsearchflags(state, searchargs))
-	return 0;
+    index_calcsearchflags(state, searchargs);
 
     /* now do the search */
     n = _index_search(&list, state, searchargs, 
@@ -1941,8 +1938,7 @@ EXPORTED int index_sort(struct index_state *state,
 	return 0;
 
     /* calculate the user flags */
-    if (index_calcsearchflags(state, searchargs))
-	return 0;
+    index_calcsearchflags(state, searchargs);
 
     if (searchargs->modseq) modseq = 1;
     else {
@@ -2282,8 +2278,7 @@ EXPORTED int index_convsort(struct index_state *state,
     }
 
     /* calculate the user flags */
-    if (index_calcsearchflags(state, searchargs))
-	return 0;
+    index_calcsearchflags(state, searchargs);
 
     /* this works both with and without conversations */
     total = search_predict_total(state, cstate, searchargs,
@@ -2697,8 +2692,7 @@ static struct multisort_result *multisort_run(struct index_state *state,
 	searchargs2 = index_copy_search(state, searchargs, state2);
 
 	/* calculate the user flags */
-	if (index_calcsearchflags(state2, searchargs2))
-	    continue;
+	index_calcsearchflags(state2, searchargs2);
 
 	/* One pass through the folder's message list */
 	for (msgno = 1 ; msgno <= state2->exists ; msgno++) {
@@ -3556,8 +3550,7 @@ EXPORTED int index_convupdates(struct index_state *state,
 	return IMAP_INTERNAL;
 
     /* calculate the user flags */
-    if (index_calcsearchflags(state, searchargs))
-	return 0;
+    index_calcsearchflags(state, searchargs);
 
     total = search_predict_total(state, cstate, searchargs,
 				windowargs->conversations,
@@ -3737,8 +3730,7 @@ EXPORTED int index_thread(struct index_state *state, int algorithm,
 	return 0;
 
     /* calculate the user flags */
-    if (index_calcsearchflags(state, searchargs))
-	return 0;
+    index_calcsearchflags(state, searchargs);
     
     if(CONFIG_TIMING_VERBOSE)
 	start = clock();
