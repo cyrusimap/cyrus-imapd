1587dbcb784e80daf5e72d725f3355bf102c4a0b
Bron Gondwana <brong@opera.com>
2012-11-15 14:22:26 +0100

    gnb review: add version string to sortcache keys
    It's in the key rather than the value because it fits the cleanup
    paradigm nicely this way.

--- imap/index.c
+++ imap/index.c
@@ -3061,6 +3061,8 @@ static char *multisort_cachekey(struct sortcrit *sortcrit,
     return buf_release(&b);
 }
 
+#define SORTCACHE_VERSION 0
+
 static struct multisort_result *multisort_cache_load(struct db *db,
 						     modseq_t hms,
 						     const char *cachekey)
@@ -3077,7 +3079,7 @@ static struct multisort_result *multisort_cache_load(struct db *db,
 
     if (!db) goto done;
 
-    buf_printf(&prefix, MODSEQ_FMT " ", hms);
+    buf_printf(&prefix, MODSEQ_FMT " %d ", hms, SORTCACHE_VERSION);
 
     memset(&rock, 0, sizeof(struct sortcache_cleanup_rock));
     rock.db = db;
@@ -3145,7 +3147,7 @@ static void multisort_cache_save(struct db *db,
 
     if (!db) goto done;
 
-    buf_printf(&prefix, MODSEQ_FMT " %s", hms, cachekey);
+    buf_printf(&prefix, MODSEQ_FMT " %d %s", hms, SORTCACHE_VERSION, cachekey);
 
     dl = dlist_newkvlist(NULL, NULL);
     dlist_setnum32(dl, "NFOLDERS", sortres->nfolders);
