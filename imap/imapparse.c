42880e00f40132e32c059d994f0c9df971e77eeb
Greg Banks <gnb@fastmail.fm>
2012-12-08 16:36:20 +1100

    Regression fix: MODSEQ is a >= test

--- imap/imapparse.c
+++ imap/imapparse.c
@@ -844,7 +844,7 @@ static int get_search_criterion(struct protstream *pin,
 	    if (c != ' ') goto missingarg;
 	    c = getmodseq(pin, &ms);
 	    if (c == EOF) goto badnumber;
-	    e = search_expr_new(parent, SEOP_MATCH);
+	    e = search_expr_new(parent, SEOP_GE);
 	    e->attr = search_attr_find("convmodseq");
 	    e->value.u = ms;
 	}
@@ -959,7 +959,7 @@ static int get_search_criterion(struct protstream *pin,
 	    }
 	    c = getmodseq(pin, &modseq);
 	    if (c == EOF) goto badnumber;
-	    e = search_expr_new(parent, SEOP_MATCH);
+	    e = search_expr_new(parent, SEOP_GE);
 	    e->attr = search_attr_find("modseq");
 	    e->value.u = modseq;
 	}
