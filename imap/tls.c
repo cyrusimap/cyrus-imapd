d7b36d4f56ae77f97f76cfd0adad909dc9f69ee6
Ken Murchison <murch@andrew.cmu.edu>
2012-12-14 15:22:33 -0500

    tls.c: only use servername_callback on version 1.0.0+

--- imap/tls.c
+++ imap/tls.c
@@ -314,6 +314,7 @@ static int verify_callback(int ok, X509_STORE_CTX * ctx)
 }
 
 
+#if (OPENSSL_VERSION_NUMBER >= 0x10000000fL)
 static int servername_callback(SSL *ssl, int *ad __attribute__((unused)),
 			       void *arg __attribute__((unused)))
 {
@@ -326,6 +327,7 @@ static int servername_callback(SSL *ssl, int *ad __attribute__((unused)),
 
     return SSL_TLSEXT_ERR_OK;
 }
+#endif
 
 
 /*
@@ -771,7 +773,9 @@ int     tls_init_serverengine(const char *ident,
 	    | SSL_VERIFY_CLIENT_ONCE;
     SSL_CTX_set_verify(s_ctx, verify_flags, verify_callback);
 
+#if (OPENSSL_VERSION_NUMBER >= 0x10000000fL)
     SSL_CTX_set_tlsext_servername_callback(s_ctx, servername_callback);
+#endif
 
     if (askcert || requirecert) {
       if (CAfile == NULL) {
