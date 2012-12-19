7d6e10cb9eeafd57725dde9cb4cd78ee752d125a
Greg Banks <gnb@fastmail.fm>
2012-12-19 17:19:12 +1100

    Add comments on RFC origins of sort criteria

--- imap/imapd.h
+++ imap/imapd.h
@@ -239,30 +239,30 @@ struct sortcrit {
 /* Values for sort keys */
 enum {
     SORT_SEQUENCE = 0,
-    SORT_ARRIVAL,
-    SORT_CC,
-    SORT_DATE,
-    SORT_DISPLAYFROM,
-    SORT_DISPLAYTO,
-    SORT_FROM,
-    SORT_SIZE,
-    SORT_SUBJECT,
-    SORT_TO,
-    SORT_ANNOTATION,
-    SORT_MODSEQ,
-    SORT_UID,
-    SORT_HASFLAG,
-    SORT_CONVMODSEQ,
-    SORT_CONVEXISTS,
-    SORT_CONVSIZE,
-    SORT_HASCONVFLAG,
-    SORT_FOLDER,
+    SORT_ARRIVAL,	/* RFC 5256 */
+    SORT_CC,		/* RFC 5256 */
+    SORT_DATE,		/* RFC 5256 */
+    SORT_DISPLAYFROM,	/* RFC 5957 */
+    SORT_DISPLAYTO,	/* RFC 5957 */
+    SORT_FROM,		/* RFC 5256 */
+    SORT_SIZE,		/* RFC 5256 */
+    SORT_SUBJECT,	/* RFC 5256 */
+    SORT_TO,		/* RFC 5256 */
+    SORT_ANNOTATION,	/* RFC 5257 */
+    SORT_MODSEQ,	/* nonstandard */
+    SORT_UID,		/* nonstandard */
+    SORT_HASFLAG,	/* nonstandard */
+    SORT_CONVMODSEQ,	/* nonstandard */
+    SORT_CONVEXISTS,	/* nonstandard */
+    SORT_CONVSIZE,	/* nonstandard */
+    SORT_HASCONVFLAG,	/* nonstandard */
+    SORT_FOLDER,	/* nonstandard */
     SORT_RELEVANCY	/* RFC 6203 */
     /* values > 255 are reserved for internal use */
 };
 
 /* Sort key modifier flag bits */
-#define SORT_REVERSE		(1<<0)
+#define SORT_REVERSE		(1<<0)	    /* RFC 5256 */
 
 /* Windowing arguments for the XCONVSORT command */
 struct windowargs {
