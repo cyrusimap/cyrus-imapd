5bc0a70384925abd5dfffe1b09ed1cf18eb463e1
Greg Banks <gnb@fastmail.fm>
2012-12-19 17:03:54 +1100

    SORT command prints MSNs when asked to
    and not just always UIDs.

--- imap/index.c
+++ imap/index.c
@@ -1832,8 +1832,6 @@ EXPORTED int index_sort(struct index_state *state,
     folder = search_query_find_folder(query, state->mailbox->name);
 
     if (folder) {
-	if (!usinguid)
-	    search_folder_use_msn(folder, state);
 	if (highestmodseq)
 	    highestmodseq = search_folder_get_highest_modseq(folder);
 	nmsg = search_folder_get_count(folder);
@@ -1845,7 +1843,8 @@ EXPORTED int index_sort(struct index_state *state,
 	/* Output the sorted messages */
 	for (i = 0 ; i < query->merged_msgdata.count ; i++) {
 	    MsgData *md = ptrarray_nth(&query->merged_msgdata, i);
-	    prot_printf(state->out, " %u", md->uid);
+	    prot_printf(state->out, " %u",
+			(usinguid ? md->uid : md->msgno));
 	}
     }
 
