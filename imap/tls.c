0dc96cd7603eaa25b99a7d5037271438029b280c
Ken Murchison <murch@andrew.cmu.edu>
2013-02-01 11:42:25 -0500

    tls.c: make note about client-side context for iSchedule

--- imap/tls.c
+++ imap/tls.c
@@ -1228,7 +1228,8 @@ int tls_init_clientengine(int verifydepth,
 	printf("TLS client engine: cannot seed PRNG\n");
 	return -1;
     }
-    
+
+    /* XXX  May need to use only SSLv3 for iSchedule */
     c_ctx = SSL_CTX_new(TLSv1_client_method());
     if (c_ctx == NULL) {
 	return (-1);
