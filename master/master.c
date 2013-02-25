cyrusimap/cyrus-imapd

commit 36d59247e889059e204183e9af41ff7075a44516

fixed bug #2582 - friendly handling of removed services (via SIGHUP)

master/master.c
@@ -39,7 +39,7 @@
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: master.c,v 1.99 2004/12/08 18:05:05 ken3 Exp $ */
+/* $Id: master.c,v 1.100 2004/12/17 16:32:24 ken3 Exp $ */
 
 #include <config.h>
 
@@ -1313,6 +1313,7 @@ void add_service(const char *name, struct entry *e, void *rock)
     char *proto = xstrdup(masterconf_getstring(e, "proto", "tcp"));
     char *max = xstrdup(masterconf_getstring(e, "maxchild", "-1"));
     rlim_t maxfds = (rlim_t) masterconf_getint(e, "maxfds", 256);
+    int reconfig = 0;
     int i, j;
 
     if(babysit && prefork == 0) prefork = 1;
@@ -1331,11 +1332,16 @@ void add_service(const char *name, struct entry *e, void *rock)
 	fatal(buf, EX_CONFIG);
     }
 
-    /* see if we have an existing entry for this service */
+    /* see if we have an existing entry that can be reused */
     for (i = 0; i < nservices; i++) {
+	/* skip non-primary instances */
 	if (Services[i].associate > 0)
 	    continue;
-	if (Services[i].name && !strcmp(Services[i].name, name)) break;
+	/* must have empty/same service name, listen and proto */
+	if ((!Services[i].name || !strcmp(Services[i].name, name)) &&
+	    (!Services[i].listen || !strcmp(Services[i].listen, listen)) &&
+	    (!Services[i].proto || !strcmp(Services[i].proto, proto)))
+	    break;
     }
 
     /* we have duplicate service names in the config file */
@@ -1350,63 +1356,8 @@ void add_service(const char *name, struct entry *e, void *rock)
 
 	fatal(buf, EX_CONFIG);
     }
-
-    if ((i < nservices) &&
-	!strcmp(Services[i].listen, listen) &&
-	!strcmp(Services[i].proto, proto)) {
-
-	/* we found an existing entry and the port paramters are the same */
-	Services[i].exec = tokenize(cmd);
-	if (!Services[i].exec) fatal("out of memory", EX_UNAVAILABLE);
-
-	/* is this service actually there? */
-	if (!verify_service_file(Services[i].exec)) {
-	    char buf[1024];
-	    snprintf(buf, sizeof(buf),
-		     "cannot find executable for service '%s'", name);
-
-	    /* if it is not, we're misconfigured, die. */
-	    fatal(buf, EX_CONFIG);
-	}
-
-	Services[i].maxforkrate = maxforkrate;
- 	Services[i].maxfds = maxfds;
-
-	if (!strcmp(Services[i].proto, "tcp") ||
-	    !strcmp(Services[i].proto, "tcp4") ||
-	    !strcmp(Services[i].proto, "tcp6")) {
-	    Services[i].desired_workers = prefork;
-	    Services[i].babysit = babysit;
-	    Services[i].max_workers = atoi(max);
-	    if (Services[i].max_workers == -1) {
-		Services[i].max_workers = INT_MAX;
-	    }
-	} else {
-	    /* udp */
-	    if (prefork > 1) prefork = 1;
-	    Services[i].desired_workers = prefork;
-	    Services[i].max_workers = 1;
-	}
  
-	for (j = 0; j < nservices; j++) {
-	    if (Services[j].associate > 0 &&
-		Services[j].name && !strcmp(Services[j].name, name)) {
-		Services[j].maxforkrate = Services[i].maxforkrate;
-		Services[j].exec = Services[i].exec;
-		Services[j].desired_workers = Services[i].desired_workers;
-		Services[j].babysit = Services[i].babysit;
-		Services[j].max_workers = Services[i].max_workers;
-	    }
-	}
-
-	if (verbose > 2)
-	    syslog(LOG_DEBUG, "reconfig: service '%s' (%s, %s:%s, %d, %d)",
-		   Services[i].name, cmd,
-		   Services[i].proto, Services[i].listen,
-		   Services[i].desired_workers,
-		   Services[i].max_workers);
-    }
-    else {
+    if (i == nservices) {
 	/* either we don't have an existing entry or we are changing
 	 * the port parameters, so create a new service
 	 */
@@ -1416,69 +1367,73 @@ void add_service(const char *name, struct entry *e, void *rock)
 	    Services = xrealloc(Services, 
 			       (allocservices+=5) * sizeof(struct service));
 	}
+	memset(&Services[nservices++], 0, sizeof(struct service));
 
-	Services[nservices].name = xstrdup(name);
-	Services[nservices].listen = listen;
-	Services[nservices].proto = proto;
-	Services[nservices].exec = tokenize(cmd);
-	if (!Services[nservices].exec) fatal("out of memory", EX_UNAVAILABLE);
+	Services[i].last_interval_start = time(NULL);
+    }
+    else if (Services[i].listen) reconfig = 1;
 
-	/* is this service actually there? */
-	if (!verify_service_file(Services[i].exec)) {
-	    char buf[1024];
-	    snprintf(buf, sizeof(buf),
-		     "cannot find executable for service '%s'", name);
+    if (!Services[i].name) Services[i].name = xstrdup(name);
+    if (Services[i].listen) free(Services[i].listen);
+    Services[i].listen = listen;
+    if (Services[i].proto) free(Services[i].proto);
+    Services[i].proto = proto;
 
-	    /* if it is not, we're misconfigured, die. */
-	    fatal(buf, EX_CONFIG);
-	}
+    Services[i].exec = tokenize(cmd);
+    if (!Services[i].exec) fatal("out of memory", EX_UNAVAILABLE);
 
-	Services[nservices].socket = 0;
-	Services[nservices].saddr = NULL;
-
-	Services[nservices].ready_workers = 0;
-
- 	Services[nservices].maxfds = maxfds;
-	Services[nservices].maxforkrate = maxforkrate;
-
-	if(!strcmp(Services[nservices].proto, "tcp") ||
-	   !strcmp(Services[nservices].proto, "tcp4") ||
-	   !strcmp(Services[nservices].proto, "tcp6")) {
-	    Services[nservices].desired_workers = prefork;
-	    Services[nservices].babysit = babysit;
-	    Services[nservices].max_workers = atoi(max);
-	    if (Services[nservices].max_workers == -1) {
-		Services[nservices].max_workers = INT_MAX;
-	    }
-	} else {
-	    if (prefork > 1) prefork = 1;
-	    Services[nservices].desired_workers = prefork;
-	    Services[nservices].max_workers = 1;
-	}
+    /* is this service actually there? */
+    if (!verify_service_file(Services[i].exec)) {
+	char buf[1024];
+	snprintf(buf, sizeof(buf),
+		 "cannot find executable for service '%s'", name);
 	
-	memset(Services[nservices].stat, 0, sizeof(Services[nservices].stat));
-
-	Services[nservices].last_interval_start = time(NULL);
-	Services[nservices].interval_forks = 0;
-	Services[nservices].forkrate = 0;
-	
-	Services[nservices].nforks = 0;
-	Services[nservices].nactive = 0;
-	Services[nservices].nconnections = 0;
-	Services[nservices].associate = 0;
-	
-	if (verbose > 2)
-	    syslog(LOG_DEBUG, "add: service '%s' (%s, %s:%s, %d, %d, %d)",
-		   Services[nservices].name, cmd,
-		   Services[nservices].proto, Services[nservices].listen,
-		   Services[nservices].desired_workers,
-		   Services[nservices].max_workers,
-		   (int) Services[nservices].maxfds);
-
-	nservices++;
+	/* if it is not, we're misconfigured, die. */
+	fatal(buf, EX_CONFIG);
     }
 
+    Services[i].maxforkrate = maxforkrate;
+    Services[i].maxfds = maxfds;
+
+    if (!strcmp(Services[i].proto, "tcp") ||
+	!strcmp(Services[i].proto, "tcp4") ||
+	!strcmp(Services[i].proto, "tcp6")) {
+	Services[i].desired_workers = prefork;
+	Services[i].babysit = babysit;
+	Services[i].max_workers = atoi(max);
+	if (Services[i].max_workers == -1) {
+	    Services[i].max_workers = INT_MAX;
+	}
+    } else {
+	/* udp */
+	if (prefork > 1) prefork = 1;
+	Services[i].desired_workers = prefork;
+	Services[i].max_workers = 1;
+    }
     free(max);
+ 
+    if (reconfig) {
+	/* reconfiguring an existing service, update any other instances */
+	for (j = 0; j < nservices; j++) {
+	    if (Services[j].associate > 0 && Services[j].listen &&
+		Services[j].name && !strcmp(Services[j].name, name)) {
+		Services[j].maxforkrate = Services[i].maxforkrate;
+		Services[j].exec = Services[i].exec;
+		Services[j].desired_workers = Services[i].desired_workers;
+		Services[j].babysit = Services[i].babysit;
+		Services[j].max_workers = Services[i].max_workers;
+	    }
+	}
+    }
+
+    if (verbose > 2)
+	syslog(LOG_DEBUG, "%s: service '%s' (%s, %s:%s, %d, %d, %d)",
+	       reconfig ? "reconfig" : "add",
+	       Services[i].name, cmd,
+	       Services[i].proto, Services[i].listen,
+	       Services[i].desired_workers,
+	       Services[i].max_workers,
+	       (int) Services[i].maxfds);
 }
 
 void add_event(const char *name, struct entry *e, void *rock)
@@ -1599,17 +1554,26 @@ void reread_conf(void)
 		       Services[i].name, Services[i].socket,
 		       Services[i].stat[0], Services[i].stat[1]);
 
-	    /* Only free the service info once */
+	    /* Only free the service info on the primary */
 	    if(Services[i].associate == 0) {
-		free(Services[i].name);
 		free(Services[i].listen);
 		free(Services[i].proto);
 	    }
-	    Services[i].name = NULL;
+	    Services[i].listen = NULL;
+	    Services[i].proto = NULL;
 	    Services[i].desired_workers = 0;
-	    Services[i].nforks = 0;
-	    Services[i].nactive = 0;
-	    Services[i].nconnections = 0;
+
+	    /* send SIGHUP to all children */
+	    for (j = 0 ; j < child_table_size ; j++ ) {
+		c = ctable[j];
+		while (c != NULL) {
+		    if ((c->si == i) &&
+			(c->service_state != SERVICE_STATE_DEAD)) {
+			kill(c->pid, SIGHUP);
+		    }
+		    c = c->next;
+		}
+	    }
 
 	    /* close all listeners */
 	    if (Services[i].socket > 0) {
@@ -1617,20 +1581,6 @@ void reread_conf(void)
 		close(Services[i].socket);
 	    }
 	    Services[i].socket = 0;
-	    Services[i].saddr = NULL;
-
-	    if (Services[i].stat[0] > 0) close(Services[i].stat[0]);
-	    if (Services[i].stat[1] > 0) close(Services[i].stat[1]);
-	    memset(Services[i].stat, 0, sizeof(Services[i].stat));
-
-	    /* remove service from all children */
-	    for (j = 0 ; j < child_table_size ; j++ ) {
-		c = ctable[j];
-		while (c != NULL) {
-		    if (c->si == i) c->si = SERVICE_NONE;
-		    c = c->next;
-		}
-	    }
 	}
 	else if (Services[i].exec && !Services[i].socket) {
 	    /* initialize new services */
@@ -2008,6 +1958,27 @@ int main(int argc, char **argv)
 		       "Applying babysitter.",
 		       Services[i].name);
 		spawn_service(i);
+	    } else if (!Services[i].exec /* disabled */ &&
+		       Services[i].name /* not yet removed */ &&
+		       Services[i].nactive == 0) {
+		if (verbose > 2)
+		    syslog(LOG_DEBUG, "remove: service %s pipe %d %d",
+			   Services[i].name,
+			   Services[i].stat[0], Services[i].stat[1]);
+
+		/* Only free the service info on the primary */
+		if (Services[i].associate == 0) {
+		    free(Services[i].name);
+		}
+		Services[i].name = NULL;
+		Services[i].nforks = 0;
+		Services[i].nactive = 0;
+		Services[i].nconnections = 0;
+		Services[i].associate = 0;
+
+		if (Services[i].stat[0] > 0) close(Services[i].stat[0]);
+		if (Services[i].stat[1] > 0) close(Services[i].stat[1]);
+		memset(Services[i].stat, 0, sizeof(Services[i].stat));
 	    }
 	}
 

imap/fud.c
@@ -42,7 +42,7 @@
 
 #include <config.h>
 
-/* $Id: fud.c,v 1.51 2004/12/03 16:24:15 ken3 Exp $ */
+/* $Id: fud.c,v 1.52 2004/12/17 16:32:07 ken3 Exp $ */
 
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
@@ -123,7 +123,10 @@ int begin_handling(void)
             memset(mbox,'\0',MAX_MAILBOX_NAME+1);
             memset(buf, '\0', MAXLOGNAME + MAX_MAILBOX_NAME + 1);
 
-	    signals_poll();
+	    if (signals_poll() == SIGHUP) {
+		/* caught a SIGHUP, return */
+		return 0;
+	    }
             r = recvfrom(soc, buf, 511, 0, 
 			 (struct sockaddr *) &sfrom, &sfromsiz);
             if (r == -1) {
@@ -170,7 +173,6 @@ int service_init(int argc, char **argv, char **envp)
     setproctitle_init(argc, argv, envp);
 
     signals_set_shutdown(&shut_down);
-    signals_add_handlers();
 
     mboxlist_init(0);
     mboxlist_open(NULL);

imap/global.h
@@ -1,5 +1,5 @@
 /* global.h -- Header for global/shared variables & functions.
- * $Id: global.h,v 1.5 2004/01/26 17:46:56 ken3 Exp $
+ * $Id: global.h,v 1.6 2004/12/17 16:32:08 ken3 Exp $
  * Copyright (c) 1998-2003 Carnegie Mellon University.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -47,6 +47,7 @@
 #include "libconfig.h"
 #include "auth.h"
 #include "mboxname.h"
+#include "signals.h"
 
 /* Flags for cyrus_init() */
 enum {
@@ -97,13 +98,6 @@ extern int mysasl_proxy_policy(sasl_conn_t *conn,
 /* check if `authstate' is a valid member of class */
 extern int global_authisa(struct auth_state *authstate, 
 			  enum imapopt opt);
-
-/* signal handling (signals.c) */
-typedef void shutdownfn(int);
-
-void signals_add_handlers(void);
-void signals_set_shutdown(shutdownfn *s);
-void signals_poll(void);
 
 /* useful types */
 struct protstream;

imap/imapd.c
@@ -38,7 +38,7 @@
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: imapd.c,v 1.488 2004/11/17 22:29:03 shadow Exp $ */
+/* $Id: imapd.c,v 1.489 2004/12/17 16:32:08 ken3 Exp $ */
 
 #include <config.h>
 
@@ -533,7 +533,6 @@ int service_init(int argc, char **argv, char **envp)
 
     /* set signal handlers */
     signals_set_shutdown(&shut_down);
-    signals_add_handlers();
     signal(SIGPIPE, SIG_IGN);
 
     /* load the SASL plugins */

imap/lmtpd.c
@@ -1,6 +1,6 @@
 /* lmtpd.c -- Program to deliver mail to a mailbox
  *
- * $Id: lmtpd.c,v 1.139 2004/09/09 16:21:26 shadow Exp $
+ * $Id: lmtpd.c,v 1.140 2004/12/17 16:32:15 ken3 Exp $
  * Copyright (c) 1998-2003 Carnegie Mellon University.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -148,7 +148,6 @@ int service_init(int argc __attribute__((unused)),
     if (geteuid() == 0) return 1;
     
     signals_set_shutdown(&shut_down);
-    signals_add_handlers();
     signal(SIGPIPE, SIG_IGN);
 
     singleinstance = config_getswitch(IMAPOPT_SINGLEINSTANCESTORE);

imap/lmtpproxyd.c
@@ -1,6 +1,6 @@
 /* lmtpproxyd.c -- Program to proxy mail delivery
  *
- * $Id: lmtpproxyd.c,v 1.66 2004/10/27 16:53:36 shadow Exp $
+ * $Id: lmtpproxyd.c,v 1.67 2004/12/17 16:32:16 ken3 Exp $
  * Copyright (c) 1998-2003 Carnegie Mellon University.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -179,7 +179,6 @@ int service_init(int argc __attribute__((unused)),
     if (geteuid() == 0) return 1;
     
     signals_set_shutdown(&shut_down);
-    signals_add_handlers();
     signal(SIGPIPE, SIG_IGN);
 
     BB = config_getstring(IMAPOPT_POSTUSER);

imap/mbexamine.c
@@ -39,7 +39,7 @@
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: mbexamine.c,v 1.10 2004/12/03 16:24:15 ken3 Exp $ */
+/* $Id: mbexamine.c,v 1.11 2004/12/17 16:32:16 ken3 Exp $ */
 
 #include <config.h>
 
@@ -158,7 +158,7 @@ int main(int argc, char **argv)
     mboxlist_open(NULL);
 
     signals_set_shutdown(&shut_down);
-    signals_add_handlers();
+    signals_add_handlers(0);
 
     if (optind == argc) {
 	strlcpy(buf, "*", sizeof(buf));

imap/mupdate.c
@@ -1,6 +1,6 @@
 /* mupdate.c -- cyrus murder database master 
  *
- * $Id: mupdate.c,v 1.88 2004/05/29 05:18:21 ken3 Exp $
+ * $Id: mupdate.c,v 1.89 2004/12/17 16:32:16 ken3 Exp $
  * Copyright (c) 1998-2003 Carnegie Mellon University.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -509,7 +509,6 @@ int service_init(int argc, char **argv,
 
     /* set signal handlers */
     signals_set_shutdown(&shut_down);
-    signals_add_handlers();
     signal(SIGPIPE, SIG_IGN);
 
     global_sasl_init(1, 1, mysasl_cb);

commit aa434fd62976162b2aa9cd1f9873d03d7e94f284

Bug #2897 - fix check for duplicate service names
Things go horribly wrong if two separate SERVICES entries in cyrus.conf
have the same service name.  In particular, the accept() call that
allows service processes to receive a connection from a client are
locked using a fcntl lock on a filename (this isn't strictly necessary
on Linux but the code is there for other OSes).  That filename is built
using the name of the entry in SERVICES.  If two or more services have
the same name, and the prefork= option is used, one but one of them will
block in fcntl() instead of in accept(), and no client will be able to
connect to them.

When master reads cyrus.conf, it can and should check for this.  And in
fact it used to, there is code in there designed to detect duplicate
names, issue a complaint to syslog, and fail to start master.  This
could be better; for example, it might be capable of detecting a
*second* mistake in the file before exiting.  But at least it's clear.

Except...that check was broken in commit

  36d5924 fixed bug #2582 - friendly handling of removed services (via SIGHUP)

about 2 years after it was added in commit

  cb95f9f abort if we get two identical service names

which is by the same author.

master/master.c
@@ -1552,24 +1552,25 @@ static void add_service(const char *name, struct entry *e, void *rock)
 	/* skip non-primary instances */
 	if (Services[i].associate > 0)
 	    continue;
+
+	if (!strcmpsafe(Services[i].name, name) && Services[i].exec) {
+	    /* we have duplicate service names in the config file */
+	    char buf[256];
+	    snprintf(buf, sizeof(buf), "multiple entries for service '%s'", name);
+
+	    if (ignore_err) {
+		syslog(LOG_WARNING, "WARNING: %s -- ignored", buf);
+		goto done;
+	    }
+
+	    fatal(buf, EX_CONFIG);
+	}
+
 	/* must have empty/same service name, listen and proto */
 	if ((!Services[i].name || !strcmp(Services[i].name, name)) &&
 	    (!Services[i].listen || !strcmp(Services[i].listen, listen)) &&
 	    (!Services[i].proto || !strcmp(Services[i].proto, proto)))
 	    break;
-    }
-
-    /* we have duplicate service names in the config file */
-    if ((i < nservices) && Services[i].exec) {
-	char buf[256];
-	snprintf(buf, sizeof(buf), "multiple entries for service '%s'", name);
-
-	if (ignore_err) {
-	    syslog(LOG_WARNING, "WARNING: %s -- ignored", buf);
-	    goto done;
-	}
-
-	fatal(buf, EX_CONFIG);
     }
 
     if (i == nservices) {