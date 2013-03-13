2ad21cc7fcbfbf50281afa4f0cc936a69e7a6d81
Bron Gondwana <brong@opera.com>
2013-03-13 14:32:17 +1100

    search_test become_cyrus

--- imap/search_test.c
+++ imap/search_test.c
@@ -255,7 +255,7 @@ int main(int argc, char **argv)
     int multiple = 0;
     int r = 0;
 
-    if ((geteuid()) == 0 && (become_cyrus() != 0)) {
+    if ((geteuid()) == 0 && (become_cyrus(/*is_master*/0) != 0)) {
 	fatal("must run as the Cyrus user", EC_USAGE);
     }
 
