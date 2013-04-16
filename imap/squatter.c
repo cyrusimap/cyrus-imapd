c4656cc708a8779bfb658669938693edb420ed86
Bron Gondwana <brong@fastmail.fm>
2013-04-16 11:55:38 +1000

    squatter: add -F filter mode

--- imap/squatter.c
+++ imap/squatter.c
@@ -806,10 +806,14 @@ int main(int argc, char **argv)
 
     setbuf(stdout, NULL);
 
-    while ((opt = getopt(argc, argv, "C:I:RT:S:c:de:f:mn:rsiavz:t:ou")) != EOF) {
+    while ((opt = getopt(argc, argv, "C:I:RT:S:Fc:de:f:mn:rsiavz:t:ou")) != EOF) {
 	switch (opt) {
 	case 'C':		/* alt config file */
 	    alt_config = optarg;
+	    break;
+
+	case 'F':
+	    compact_flags |= SEARCH_COMPACT_FILTER;
 	    break;
 
 	case 'I':		/* indexer, using specified mbox/uids in file */
