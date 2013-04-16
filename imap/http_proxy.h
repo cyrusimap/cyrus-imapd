7ce7b8a91746c8540245c9d02970904ccb478e6b
Ken Murchison <murch@andrew.cmu.edu>
2013-04-16 13:56:40 -0400

    http_proxy.h: fix some line-wrapping

--- imap/http_proxy.h
+++ imap/http_proxy.h
@@ -49,14 +49,14 @@
 
 extern struct protocol_t http_protocol;
 
-extern int http_mlookup(const char *name, char **server, char **aclp, void *tid);
+extern int http_mlookup(const char *name,
+			char **server, char **aclp, void *tid);
 extern int http_pipe_req_resp(struct backend *be, struct transaction_t *txn);
 extern int http_proxy_copy(struct backend *src_be, struct backend *dest_be,
 			   struct transaction_t *txn);
-extern int http_read_response(struct backend *be, unsigned meth, unsigned decode,
-			      unsigned *code, unsigned *close,
-			      const char **statline,
-			      hdrcache_t *hdrs, struct buf *body,
-			      const char **errstr);
+extern int http_read_response(struct backend *be, unsigned meth,
+			      unsigned decode, unsigned *code, unsigned *close,
+			      const char **statline, hdrcache_t *hdrs,
+			      struct buf *body, const char **errstr);
 
 #endif /* _HTTP_PROXY_H */
