204ad0cd819c0b8a50d2c3ea39dc91d7cd3556a9
Bron Gondwana <brong@opera.com>
2013-11-22 11:56:14 +1100

    status: fix status response for non-conversations folders

--- imap/imapd.c
+++ imap/imapd.c
@@ -8502,34 +8502,32 @@ static int imapd_statusdata(const char *mailboxname, unsigned statusitems,
 			    struct statusdata *sd)
 {
     int r;
+    struct conversations_state *state = NULL;
 
-    if (statusitems & STATUS_CONVITEMS) {
-	struct conversations_state *state = NULL;
+    if (!(statusitems & STATUS_CONVITEMS)) goto nonconv;
+    statusitems &= ~STATUS_CONVITEMS; /* strip them for the regular lookup */
 
-	/* use the existing state if possible */
-	state = conversations_get_mbox(mailboxname);
+    /* use the existing state if possible */
+    state = conversations_get_mbox(mailboxname);
 
-	/* otherwise fetch a new one and just use for this one thing! */
-	if (!state) {
-	    if (global_conversations) {
-		conversations_abort(&global_conversations);
-		global_conversations = NULL;
-	    }
-	    r = conversations_open_mbox(mailboxname, &state);
-	    if (r) {
-		/* maybe the mailbox doesn't even have conversations - just ignore */
-		r = 0;
-		goto out;
-	    }
-	    global_conversations = state;
+    /* otherwise fetch a new one! */
+    if (!state) {
+	if (global_conversations) {
+	    conversations_abort(&global_conversations);
+	    global_conversations = NULL;
 	}
-
-	r = conversation_getstatus(state, mailboxname, &sd->xconv);
-	if (r) goto out;
-
-	statusitems &= ~STATUS_CONVITEMS; /* got them */
+	r = conversations_open_mbox(mailboxname, &state);
+	if (r) {
+	    /* maybe the mailbox doesn't even have conversations - just ignore */
+	    goto nonconv;
+	}
+	global_conversations = state;
     }
 
+    r = conversation_getstatus(state, mailboxname, &sd->xconv);
+    if (r) return r;
+
+nonconv:
     /* use the index status if we can so we get the 'alive' Recent count */
     if (!strcmpsafe(mailboxname, index_mboxname(imapd_index)))
 	r = index_status(imapd_index, sd);
@@ -8537,7 +8535,6 @@ static int imapd_statusdata(const char *mailboxname, unsigned statusitems,
     else
 	r = status_lookup(mailboxname, imapd_userid, statusitems, sd);
 
-out:
     return r;
 }
 
