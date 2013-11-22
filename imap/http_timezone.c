55b419dd798bb738b18e2e245893de2b04bffbec
Ken Murchison <murch@andrew.cmu.edu>
2013-11-22 14:20:06 -0500

    http_timezone.c: create a single json_response() function for use in outputting responses and errors

--- imap/http_timezone.c
+++ imap/http_timezone.c
@@ -85,6 +85,9 @@ static int meth_get(struct transaction_t *txn, void *params);
 static int action_capa(struct transaction_t *txn, struct hash_table *params);
 static int action_list(struct transaction_t *txn, struct hash_table *params);
 static int action_get(struct transaction_t *txn, struct hash_table *params);
+static int json_response(int code, struct transaction_t *txn, json_t *root,
+			 char **resp);
+static int json_error_response(struct transaction_t *txn, const char *err);
 
 static const struct action_t {
     const char *name;
@@ -212,7 +215,8 @@ static int action_capa(struct transaction_t *txn,
     struct message_guid guid;
     const char *etag;
     static time_t lastmod = 0;
-    static char *buf = NULL;
+    static char *resp = NULL;
+    json_t *root = NULL;
 
     /* Generate ETag based on compile date/time of this source file.
      * Extend this to include config file size/mtime if we add run-time options.
@@ -244,8 +248,6 @@ static int action_capa(struct transaction_t *txn,
     }
 
     if (txn->resp_body.lastmod > lastmod) {
-	size_t flags = JSON_PRESERVE_ORDER;
-	json_t *root;
 	struct zoneinfo info;
 	int r;
 
@@ -316,25 +318,12 @@ static int action_capa(struct transaction_t *txn,
 	    return HTTP_SERVER_ERROR;
 	}
 
-	/* Dump JSON object into a text buffer */
-	if (buf) free(buf);
-	flags |= (config_httpprettytelemetry ? JSON_INDENT(2) : JSON_COMPACT);
-	buf = json_dumps(root, flags);
-	json_decref(root);
-
-	if (!buf) {
-	    txn->error.desc = "Error dumping JSON object";
-	    return HTTP_SERVER_ERROR;
-	}
-
+	/* Update lastmod */
 	lastmod = txn->resp_body.lastmod;
     }
 
     /* Output the JSON object */
-    txn->resp_body.type = "application/json; charset=utf-8";
-    write_body(precond, txn, buf, strlen(buf));
-
-    return 0;
+    return json_response(precond, txn, root, &resp);
 }
 
 
@@ -367,18 +356,17 @@ static int list_cb(const char *tzid, int tzidlen,
 /* Perform a list action */
 static int action_list(struct transaction_t *txn, struct hash_table *params)
 {
-    int ret = 0, r, precond = HTTP_OK;
+    int r, precond = HTTP_OK;
     struct resp_body_t *resp_body = &txn->resp_body;
-    char *buf = NULL;
-    unsigned long buflen = 0;
     struct zoneinfo info;
     time_t lastmod, changedsince = 0;
     const char *name = NULL;
+    json_t *root = NULL;
 
     /* Sanity check the parameters */
     if (!strcmp("find", hash_lookup("action", params))) {
 	name = hash_lookup("name", params);
-	if (!name) return HTTP_BAD_REQUEST;
+	if (!name) return json_error_response(txn, "invalid-name");
     }
     else {
 	const char *cs = hash_lookup("changedsince", params);
@@ -387,10 +375,11 @@ static int action_list(struct transaction_t *txn, struct hash_table *params)
 	if (name && !strcmp(name, "*")) name = NULL;
 
 	if (cs) {
-	    if (name) return HTTP_BAD_REQUEST;
+	    if (name) return json_error_response(txn, "invalid-tzid");
 
 	    changedsince = icaltime_as_timet(icaltime_from_string(cs));
-	    if (!changedsince) return HTTP_BAD_REQUEST;
+	    if (!changedsince)
+		return json_error_response(txn, "invalid-changedsince");
 	}
     }
 
@@ -429,8 +418,6 @@ static int action_list(struct transaction_t *txn, struct hash_table *params)
 
 
     if (txn->meth == METH_GET) {
-	size_t flags = JSON_PRESERVE_ORDER;
-	json_t *root;
 	char dtstamp[21];
 
 	/* Start constructing our response */
@@ -444,26 +431,10 @@ static int action_list(struct transaction_t *txn, struct hash_table *params)
 	/* Add timezones to array */
 	zoneinfo_find((char *) name, changedsince, &list_cb,
 		      json_object_get(root, "timezones"));
-
-	/* Dump JSON object into a text buffer */
-	flags |= (config_httpprettytelemetry ? JSON_INDENT(2) : JSON_COMPACT);
-	buf = json_dumps(root, flags);
-	json_decref(root);
-
-	if (buf) buflen = strlen(buf);
-	else {
-	    txn->error.desc = "Error dumping JSON object";
-	    return HTTP_SERVER_ERROR;
-	}
     }
 
     /* Output the JSON object */
-    resp_body->type = "application/json; charset=utf-8";
-    write_body(precond, txn, buf, buflen);
-
-    if (buf) free(buf);
-
-    return ret;
+    return json_response(precond, txn, root, NULL);
 }
 
 
@@ -479,8 +450,10 @@ static int action_get(struct transaction_t *txn, struct hash_table *params)
     struct resp_body_t *resp_body = &txn->resp_body;
     struct mime_type_t *mime;
 
+    /* Sanity check the parameters */
     tzid = hash_lookup("tzid", params);
-    if (!tzid || strchr(tzid, '.')) return HTTP_BAD_REQUEST;
+    if (!tzid || strchr(tzid, '.'))
+	return json_error_response(txn, "invalid-tzid");
 
     /* Check/find requested MIME type */
     format = hash_lookup("format", params);
@@ -556,4 +529,51 @@ static int action_get(struct transaction_t *txn, struct hash_table *params)
     }
 
     return ret;
+}
+
+
+static int json_response(int code, struct transaction_t *txn, json_t *root,
+			 char **resp)
+{
+    size_t flags = JSON_PRESERVE_ORDER;
+    char *buf = NULL;
+
+    if (root) {
+	/* Dump JSON object into a text buffer */
+	flags |= (config_httpprettytelemetry ? JSON_INDENT(2) : JSON_COMPACT);
+	buf = json_dumps(root, flags);
+	json_decref(root);
+
+	if (!buf) {
+	    txn->error.desc = "Error dumping JSON object";
+	    return HTTP_SERVER_ERROR;
+	}
+	else if (resp) {
+	    if (*resp) free(*resp);
+	    *resp = buf;
+	}
+    }
+    else if (resp) buf = *resp;
+
+    /* Output the JSON object */
+    txn->resp_body.type = "application/json; charset=utf-8";
+    write_body(code, txn, buf, buf ? strlen(buf) : 0);
+
+    if (!resp && buf) free(buf);
+
+    return 0;
+}
+
+
+static int json_error_response(struct transaction_t *txn, const char *err)
+{
+    json_t *root;
+
+    root = json_pack("{s:s}", "error", err);
+    if (!root) {
+	txn->error.desc = "Unable to create JSON response";
+	return HTTP_SERVER_ERROR;
+    }
+
+    return json_response(HTTP_BAD_REQUEST, txn, root, NULL);
 }
