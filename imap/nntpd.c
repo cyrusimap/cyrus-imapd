5631334b6f33b734cc2d0d4e8acde558188b53bb
Ken Murchison <murch@andrew.cmu.edu>
2014-01-06 18:30:29 -0500

    Revert "Bug #3415 - fix nntp access"
    This reverts commit 5fe468b7acf5bcb614c2a359df125b3e2bf6e075.

--- imap/nntpd.c
+++ imap/nntpd.c
@@ -1069,7 +1069,7 @@ static void cmdloop(void)
 		be = backend_current;
 		if (arg1.len &&
 		    (!is_newsgroup(arg1.s) ||
-		     (r = open_group(arg1.s, 1, &be, NULL)))) goto nogroup;
+		     (r = open_group(arg1.s, 0, &be, NULL)))) goto nogroup;
 		else if (be) {
 		    prot_printf(be->out, "%s", cmd.s);
 		    if (arg1.len) {
@@ -2548,7 +2548,7 @@ int do_active(char *name, void *rock)
 	}
     }
     else {
-	prot_printf(nntp_out, "%s %u %u %c\r\n", name,
+	prot_printf(nntp_out, "%s %u %u %c\r\n", name+strlen(newsprefix),
 		    group_state->exists ? index_getuid(group_state, group_state->exists) :
 		    group_state->mailbox->i.last_uid,
 		    group_state->exists ? index_getuid(group_state, 1) :
@@ -3004,7 +3004,7 @@ static int parse_groups(const char *groups, message_data_t *msg)
 	if (!rcpt) return -1;
 
 	/* construct the mailbox name */
-	sprintf(rcpt, "%.*s", (int) n, p);
+	sprintf(rcpt, "%s%.*s", newsprefix, (int) n, p);
 
 	/* skip mailboxes that we don't serve as newsgroups */
 	if (!is_newsgroup(rcpt)) continue;
