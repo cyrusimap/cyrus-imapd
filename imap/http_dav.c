25ecbb17f82b0f5930df6c8618d53a7c87ff9d3f
Ken Murchison <murch@andrew.cmu.edu>
2014-02-07 11:31:11 -0500

    http_dav.c: MKCALENDAR/MKCOL that fails to set properties MUST result in a 207 <multistatus> response

--- imap/http_dav.c
+++ imap/http_dav.c
@@ -3487,8 +3487,12 @@ int meth_mkcol(struct transaction_t *txn, void *params)
 	    annotatemore_abort(pctx.tid);
 
 	    if (!ret) {
+		/* Error response MUST be a multistatus */
+		xmlNodeSetName(root, BAD_CAST "multistatus");
+		xmlSetNs(root, ns[NS_DAV]);
+
 		/* Output the XML response */
-		xml_response(HTTP_FORBIDDEN, txn, outdoc);
+		xml_response(HTTP_MULTI_STATUS, txn, outdoc);
 		ret = 0;
 	    }
 
