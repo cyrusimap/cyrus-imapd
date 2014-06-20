b1c5f4d1b7d9702d72c6809bd96de8843bca3ace
Ken Murchison <murch@andrew.cmu.edu>
2014-06-20 16:29:53 -0400

    Revert "dav: don't spit out negative quota for no quota"
    This reverts commit 6759607391029d86b3beeb8ab8f51ca501918fc4.

--- imap/http_dav.c
+++ imap/http_dav.c
@@ -1684,13 +1684,9 @@ int propfind_quota(const xmlChar *name, xmlNsPtr ns,
     buf_reset(&fctx->buf);
     if (!xmlStrcmp(name, BAD_CAST "quota-available-bytes")) {
 	/* Calculate limit in bytes and subtract usage */
-	if (fctx->quota.limits[QUOTA_STORAGE] < 0) { // unlimited
-	    buf_printf(&fctx->buf, QUOTA_T_FMT, (quota_t)INT64_MAX);
-	}
-	else {
-	    quota_t limit = fctx->quota.limits[QUOTA_STORAGE] * quota_units[QUOTA_STORAGE];
-	    buf_printf(&fctx->buf, QUOTA_T_FMT, limit - fctx->quota.useds[QUOTA_STORAGE]);
-	}
+	uquota_t limit = fctx->quota.limit * QUOTA_UNITS;
+
+	buf_printf(&fctx->buf, UQUOTA_T_FMT, limit - fctx->quota.used);
     }
     else if (fctx->record) {
 	/* Bytes used by resource */
