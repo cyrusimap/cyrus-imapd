36fad29d9faa1d96001a34fa24ce188eec3d87c2
James Cassell <code@james.cassell.me>
2014-07-21 21:03:25 -0400

    [sieve] bc_emit.c: add comments for B_FILEINTO
    make sure the order of parameters is well-defined

--- sieve/bc_emit.c
+++ sieve/bc_emit.c
@@ -641,17 +641,20 @@ static int bc_action_emit(int fd, int codep, int stopcodep,
 		return -1;
 	    filelen += ret;
 
+	    /* Write Copy */
 	    if(write_int(fd,bc->data[codep++].value) == -1)
 		return -1;
 
 	    filelen += sizeof(int);
 
+	    /* Write string length of Folder */
 	    len = bc->data[codep++].len;
 	    if(write_int(fd,len) == -1)
 		return -1;
 
 	    filelen+=sizeof(int);
 
+	    /* Write Folder */
 	    if(write(fd,bc->data[codep++].str,len) == -1)
 		return -1;
 
