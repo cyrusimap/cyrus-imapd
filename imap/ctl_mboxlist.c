03692f293390cc43ae61de7827f36fe779b501f6
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2014-08-13 12:52:37 +0200

    Prevent another potential segfault on standalone systems

--- imap/ctl_mboxlist.c
+++ imap/ctl_mboxlist.c
@@ -215,12 +215,10 @@ static int dump_cb(void *rockp,
 		    } // if (r = dlist_getatom())
 
 		    if (dl_ace->name && tmp) {
-			const char *ace = strconcat(xstrdup(dl_ace->name), "\t", xstrdup(tmp));
-
 			if (acl) {
-			    acl = strconcat(xstrdup(acl), "\t", ace);
+			    acl = strconcat(xstrdup(acl), "\t", dl_ace->name, "\t", tmp);
 			} else {
-			    acl = xstrdup(ace);
+			    acl = strconcat(dl_ace->name, "\t", tmp);
 			}
 		    }
 		}
