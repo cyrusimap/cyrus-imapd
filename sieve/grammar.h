b5b841fbe4e53577a710387534107ba30ab10314
James Cassell <code@james.cassell.me>
2014-11-06 16:24:56 -0500

    [sieve] implement parse_string() to resolve variables in strings
    also moved logic for testing identifier from sieve.y to grammar.c

---
+++ sieve/grammar.h
@@ -0,0 +1,17 @@
+/*
+ * grammar.h
+ *
+ *  Created on: Nov 6, 2014
+ *      Author: James Cassell
+ */
+
+#ifndef SIEVE_GRAMMAR_H_
+#define SIEVE_GRAMMAR_H_
+
+#include "strarray.h"
+#include "varlist.h"
+
+int is_identifier(char *s);
+char *parse_string(const char *s, variable_list_t *vars);
+
+#endif /* SIEVE_GRAMMAR_H_ */
