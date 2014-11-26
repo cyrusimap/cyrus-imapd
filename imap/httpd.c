6c302a5758c3e843d7ef6c806ca478cc4c831cb6
Ken Murchison <murch@andrew.cmu.edu>
2014-11-26 12:43:06 -0500

    httpd.c: no reason to prot_printf() a constant string

--- imap/httpd.c
+++ imap/httpd.c
@@ -1972,8 +1972,7 @@ EXPORTED void response_header(long code, struct transaction_t *txn)
 			(ALLOW_CAL_AVAIL | ALLOW_CAL_SCHED)) {
 			if ((hdr = spool_getheader(txn->req_hdrs, "User-Agent"))
 			    && strstr(hdr[0], "CalendarAgent/")) {
-			    prot_printf(httpd_out,
-					"DAV: inbox-availability\r\n");
+			    prot_puts(httpd_out, "DAV: inbox-availability\r\n");
 			}
 		    }
 		}
