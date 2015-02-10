7319353bf47cd4bc2db4ff130ed9785233a4d884
Bron Gondwana <brong@fastmail.fm>
2015-02-11 05:03:47 +1100

    caldav: don't crash on missing propname error

--- imap/http_caldav.c
+++ imap/http_caldav.c
@@ -2532,25 +2532,18 @@ const char *get_icalcomponent_errstr(icalcomponent *ical)
 	     prop = icalcomponent_get_next_property(comp, ICAL_ANY_PROPERTY)) {
 
 	    if (icalproperty_isa(prop) == ICAL_XLICERROR_PROPERTY) {
-		icalparameter *param;
 		const char *errstr = icalproperty_get_xlicerror(prop);
+		char propname[256];
 
 		if (!errstr) return "Unknown iCal parsing error";
 
-		param = icalproperty_get_first_parameter(
-		    prop, ICAL_XLICERRORTYPE_PARAMETER);
-
-		if (icalparameter_get_xlicerrortype(param) ==
-		    ICAL_XLICERRORTYPE_VALUEPARSEERROR) {
-		    /* Check if this is an empty property error */
-		    char propname[256];
-		    if (sscanf(errstr,
-			       "No value for %s property", propname) == 1) {
-			/* Empty LOCATION is OK */
-			if (!strcasecmp(propname, "LOCATION")) continue;
-			if (!strcasecmp(propname, "COMMENT")) continue;
-			if (!strcasecmp(propname, "DESCRIPTION")) continue;
-		    }
+		/* Check if this is an empty property error */
+		if (sscanf(errstr,
+			   "No value for %s property", propname) == 1) {
+		    /* Empty LOCATION is OK */
+		    if (!strcasecmp(propname, "LOCATION")) continue;
+		    if (!strcasecmp(propname, "COMMENT")) continue;
+		    if (!strcasecmp(propname, "DESCRIPTION")) continue;
 		}
 	    }
 	}
