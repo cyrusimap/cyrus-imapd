b25c896f6ae9b09dc7e4edc5d6b68ce52e62687b
Bron Gondwana <brong@fastmail.fm>
2015-02-25 10:17:04 +1100

    mailbox: update find_index_record API usage for sync

--- imap/mailbox.c
+++ imap/mailbox.c
@@ -2401,7 +2401,7 @@ EXPORTED void mailbox_annot_changed(struct mailbox *mailbox,
     if (uid) {
 	/* check that the record isn't already expunged */
 	struct index_record record;
-	int r = mailbox_find_index_record(mailbox, uid, &record);
+	int r = mailbox_find_index_record(mailbox, uid, &record, NULL);
 	if (r || record.system_flags & FLAG_EXPUNGED)
 	    return;
 	if (oldval->len)
