d92958ec6ea7486844ee93a6a53fa6ff1a2072d7
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-03-04 01:56:33 +0100

    Update scripts, check build without openssl

---
+++ contrib/drydock/without-openssl.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# This script tests the fix applied in D7 / T26:
+#
+#   - https://git.cyrus.foundation/D7
+#   - https://git.cyrus.foundation/T26
+
+git clean -d -f -x || exit 2
+
+if [ ! -z "${commit}" ]; then
+    git checkout -f ${commit} || exit 2
+fi
+
+autoreconf -vi || exit 3
+
+./configure --enable-maintainer-mode || exit 124
+
+# Once normally
+./configure --with-openssl=no || exit 4
+
+# Work around a broken lex (??)
+make sieve/sieve-lex.c && \
+    perl -p -i -e "s/int yyl;/yy_size_t yyl;/" sieve/sieve-lex.c
+
+make -j4 || exit 5
+
+make clean
+
+# Once with -Werror
+CFLAGS="-g -fPIC -W -Wall -Wextra -Werror"
+export CFLAGS
+./configure --with-openssl=no || exit 6
+
+# Work around a broken lex (??)
+make sieve/sieve-lex.c && \
+    perl -p -i -e "s/int yyl;/yy_size_t yyl;/" sieve/sieve-lex.c
+
+make -j4 || exit 7
+
