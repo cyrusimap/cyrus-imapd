49310c536491e35e2e08e9792fadf56c4ae370e5
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-03-08 18:29:12 +0100

    It is extremely confusing to have stdout and stderr be split in DryDock

--- contrib/drydock.sh
+++ contrib/drydock.sh
@@ -11,7 +11,7 @@ fi
 
 for script in `find contrib/drydock-tests/ -type f -name "*.sh" | sort`; do
     if [ -x $script ]; then
-        ./$script || exit $?
+        ./$script 2>&1 || exit $?
     else
         echo "Skipping $script"
     fi
