brong/cyrus-imapd-legacy

Add xstats: cheap & nasty metrics for Cyrus search
New command XSTATS reports a number of 32b counter metrics in an
untagged response.  Various points in the search stack are instrumented.
This is basically just a nasty hack to be able to measure various
performance-related parameters of the search code from Cassandane
without actually getting Cyrus' statistics working again.

imap/index.c
@@ -80,6 +80,7 @@
 #include "statuscache.h"
 #include "strhash.h"
 #include "util.h"
+#include "xstats.h"
 #include "ptrarray.h"
 #include "xmalloc.h"
 #include "xstrlcpy.h"
@@ -2328,6 +2329,7 @@ EXPORTED int index_convmultisort(struct index_state *state,
 	free(usermbox);
 	free(pattern);
 	r = 0;
+	xstats_inc(SEARCH_TRIVIAL);
     }
     if (r) goto out;
 
@@ -2489,6 +2491,7 @@ EXPORTED int index_convmultisort(struct index_state *state,
 
     /* Print the resulting list */
 
+    xstats_add(SEARCH_RESULT, results.count);
     if (results.count) {
 	/* The untagged reponse would be XCONVMULTISORT but
 	 * Mail::IMAPTalk has an undocumented hack whereby any untagged
@@ -4684,6 +4687,7 @@ static int index_search_evaluate(struct index_state *state,
     struct buf localmap = BUF_INITIALIZER;
     int retval = 0;
 
+    if (!msgfile) xstats_inc(SEARCH_EVALUATE);
     if (!msgfile) msgfile = &localmap;
 
     if ((searchargs->flags & SEARCH_RECENT_SET) && !im->isrecent)
@@ -4937,6 +4941,7 @@ static int index_searchmsg(struct index_record *record,
 {
     struct searchmsg_rock sr;
 
+    xstats_inc(SEARCH_BODY);
     sr.substr = substr;
     sr.pat = pat;
     sr.skipheader = skipheader;
@@ -4954,6 +4959,8 @@ static int index_searchheader(char *name,
 {
     char *p;
     strarray_t header = STRARRAY_INITIALIZER;
+
+    xstats_inc(SEARCH_HEADER);
 
     strarray_append(&header, name);
 
@@ -4980,6 +4987,8 @@ static int index_searchcacheheader(struct index_state *state, uint32_t msgno,
     int r;
     struct mailbox *mailbox = state->mailbox;
     struct index_map *im = &state->map[msgno-1];
+
+    xstats_inc(SEARCH_CACHE_HEADER);
 
     r = mailbox_cacherecord(mailbox, &im->record);
     if (r) return 0;
@@ -5186,6 +5195,7 @@ static MsgData **index_msgdata_load(struct index_state *state,
     /* create an array of MsgData */
     ptrs = (MsgData **) xzmalloc(n * sizeof(MsgData *) + n * sizeof(MsgData));
     md = (MsgData *)(ptrs + n);
+    xstats_add(MSGDATA_LOAD, n);
 
     if (found_anchor)
 	*found_anchor = 0;

cyrusimap/cyrus-imapd

imap/index.c
@@ -76,6 +76,7 @@
 #include "statuscache.h"
 #include "strhash.h"
 #include "util.h"
+#include "xstats.h"
 #include "ptrarray.h"
 #include "xmalloc.h"
 #include "xstrlcpy.h"
@@ -2562,6 +2563,7 @@ EXPORTED int index_convmultisort(struct index_state *state,
 	free(usermbox);
 	free(pattern);
 	r = 0;
+	xstats_inc(SEARCH_TRIVIAL);
     }
     if (r) goto out;
 
@@ -2723,6 +2725,7 @@ EXPORTED int index_convmultisort(struct index_state *state,
 
     /* Print the resulting list */
 
+    xstats_add(SEARCH_RESULT, results.count);
     if (results.count) {
 	/* The untagged reponse would be XCONVMULTISORT but
 	 * Mail::IMAPTalk has an undocumented hack whereby any untagged
@@ -5065,6 +5068,7 @@ static int index_search_evaluate(struct index_state *state,
     if (index_reload_record(state, msgno, &record))
 	goto zero;
 
+    if (!buf) xstats_inc(SEARCH_EVALUATE);
     if (!buf) buf = &localbuf;
 
     if ((searchargs->flags & SEARCH_RECENT_SET) && !im->isrecent)
@@ -5317,6 +5321,7 @@ static int index_searchmsg(struct index_record *record,
 {
     struct searchmsg_rock sr;
 
+    xstats_inc(SEARCH_BODY);
     sr.substr = substr;
     sr.pat = pat;
     sr.skipheader = skipheader;
@@ -5334,6 +5339,8 @@ static int index_searchheader(char *name,
 {
     char *p;
     strarray_t header = STRARRAY_INITIALIZER;
+
+    xstats_inc(SEARCH_HEADER);
 
     strarray_append(&header, name);
 
@@ -5362,6 +5369,11 @@ static int index_searchcacheheader(struct index_state *state, uint32_t msgno,
     struct index_record record;
 
     r = index_reload_record(state, msgno, &record);
+    if (r) return 0;
+
+    xstats_inc(SEARCH_CACHE_HEADER);
+
+    r = mailbox_cacherecord(mailbox, &im->record);
     if (r) return 0;
 
     r = mailbox_cacherecord(mailbox, &record);
@@ -5574,6 +5586,7 @@ static MsgData **index_msgdata_load(struct index_state *state,
     /* create an array of MsgData */
     ptrs = (MsgData **) xzmalloc(n * sizeof(MsgData *) + n * sizeof(MsgData));
     md = (MsgData *)(ptrs + n);
+    xstats_add(MSGDATA_LOAD, n);
 
     if (found_anchor)
 	*found_anchor = 0;