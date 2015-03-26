ace7f45d38d51ada4ca3a222f92da8a43c4b3982
Ken Murchison <murch@andrew.cmu.edu>
2015-03-26 12:34:57 -0400

    ctl_zoneinfo.c: squash size_t compiler complaints in map_refresh() and map_free()

--- imap/ctl_zoneinfo.c
+++ imap/ctl_zoneinfo.c
@@ -412,7 +412,7 @@ void do_zonedir(const char *dir, struct hash_table *tzentries,
 	    /* Path is a regular file (zone) */
 	    int fd;
 	    const char *base = NULL;
-	    unsigned long len = 0;
+	    size_t len = 0;
 	    icalcomponent *ical, *comp;
 	    icalproperty *prop;
 
