ae93e9fba49d10a9714f05337e2520ff5ab02bec
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-03-26 17:26:16 +0100

    Ensure we have at least the name of the script that failed the build

--- contrib/drydock.sh
+++ contrib/drydock.sh
@@ -11,8 +11,14 @@ fi
 
 for script in `find contrib/drydock-tests/ -type f -name "*.sh" | sort`; do
     if [ -x $script ]; then
-        ./$script 2>&1 || exit $?
+        echo "Running script $script"
+        ./$script 2>&1; retval=$?
     else
         echo "Skipping $script"
+    fi
+
+    if [ ${retval} -ne 0 ]; then
+        echo "Script ./$script FAILED"
+        exit ${retval}
     fi
 done
