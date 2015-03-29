d93241359eb95c6b816a7c4f6913d66c92ae1f88
Bron Gondwana <brong@fastmail.fm>
2015-03-30 00:05:20 +1100

    list_response: only magic 'inbox.*' if no ! in name
    Resolves T116

--- imap/imapd.c
+++ imap/imapd.c
@@ -9784,7 +9784,7 @@ static void list_response(char *name, int attributes,
     if (!name) return;
 
     /* first convert "INBOX" to "user.<userid>" */
-    if (!strncasecmp(name, "inbox", 5)
+    if (!strchr(name, '!') && !strncasecmp(name, "inbox", 5)
 	&& (!name[5] || name[5] == '.') ) {
 	(*imapd_namespace.mboxname_tointernal)(&imapd_namespace, "INBOX",
 					       imapd_userid, internal_name);
