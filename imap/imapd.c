9abe1708b79dbc013ee74df9af5b3fbadf0f2d73
ellie timoney <elliefm@users.noreply.github.com>
2015-03-31 12:16:35 +1100

    cmd_syncapply: initialise local_only to 0

--- imap/imapd.c
+++ imap/imapd.c
@@ -12862,7 +12862,7 @@ static void cmd_syncapply(const char *tag, struct dlist *kin, struct sync_reserv
     int r;
     struct sync_state sync_state = {
 	imapd_userid, imapd_userisadmin, imapd_authstate,
-	&imapd_namespace, imapd_out
+	&imapd_namespace, imapd_out, 0,
     };
 
     if (!strcmp(kin->name, "MESSAGE"))
