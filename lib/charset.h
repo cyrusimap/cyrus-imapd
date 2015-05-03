b72ebfcaf45ec0de9bd9cf4476af5f8037ae3dcf
Ken Murchison <murch@andrew.cmu.edu>
2015-05-03 16:33:27 -0400

    charset.c: add charset_qpencode_mimebody()

--- lib/charset.h
+++ lib/charset.h
@@ -84,6 +84,8 @@ extern const char *charset_decode_mimebody(const char *msg_base, size_t len,
 extern char *charset_encode_mimebody(const char *msg_base, size_t len,
 				     char *retval, size_t *outlen, 
 				     int *outlines);
+extern char *charset_qpencode_mimebody(const char *msg_base, size_t len,
+				       size_t *outlen);
 extern char *charset_to_utf8(const char *msg_base, size_t len, charset_index charset, int encoding);
 extern int charset_search_mimeheader(const char *substr, comp_pat *pat, const char *s, int flags);
 
