66be30df0ebfee1de7f3c4a5ea12da4bc0000030
Ken Murchison <murch@andrew.cmu.edu>
2015-05-07 19:03:04 -0400

    httpd.c: we no longer have to split query params on ';' (this was a bad hack for RSS module)

--- imap/httpd.c
+++ imap/httpd.c
@@ -1376,7 +1376,7 @@ static void cmdloop(void)
 
 	    assert(!buf_len(&txn.buf));  /* Unescape buffer */
 
-	    tok_init(&tok, (char *) query, ";&",
+	    tok_init(&tok, (char *) query, "&",
 		     TOK_TRIMLEFT|TOK_TRIMRIGHT|TOK_EMPTY);
 	    while ((param = tok_next(&tok))) {
 		struct strlist *vals;
