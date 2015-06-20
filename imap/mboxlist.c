6dd615eeac37df683e03370e52ce9d4e21bb4d68
Bron Gondwana <brong@fastmail.fm>
2015-06-21 05:14:54 +1000

    mboxlist: iterate DELETED names properly

--- imap/mboxlist.c
+++ imap/mboxlist.c
@@ -2312,9 +2312,18 @@ EXPORTED int mboxlist_mboxtree(const char *mboxname, mboxlist_cb *proc, void *ro
     }
 
     if ((flags & MBOXTREE_DELETED)) {
-        char *prefix = strconcat(mboxname, ".", (char *)NULL);
+        struct buf buf = BUF_INITIALIZER;
+        const char *p = strchr(mboxname, '!');
+        const char *dp = config_getstring(IMAPOPT_DELETEDPREFIX);
+        if (p) {
+            buf_printf(&buf, "%.*s!%s.%s", (int)(p-mboxname), mboxname, dp, p+1);
+        }
+        else {
+            buf_printf(&buf, "%s.%s", dp, mboxname);
+        }
+        const char *prefix = buf_cstring(&buf);
         r = cyrusdb_foreach(mbdb, prefix, strlen(prefix), allmbox_p, allmbox_cb, &mbrock, 0);
-        free(prefix);
+        buf_free(&buf);
         if (r) goto done;
     }
 
