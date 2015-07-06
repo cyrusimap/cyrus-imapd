7727b819031a486eb51cfec540643202af834018
ellie timoney <elliefm@users.noreply.github.com>
2015-07-06 13:28:38 +1000

    add release notes for 2.4.18

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1095,8 +1095,8 @@ rst_prolog = """
 .. |imap_last_stable_version| replace:: 2.4.17
 .. |imap_last_stable_branch| replace:: `cyrus-imapd-2.4`
 .. |imap_last_stable_next_version| replace:: 2.4.17 + patches
-.. |imap_current_stable_version| replace:: 2.5.3
-.. |imap_current_stable_next_version| replace:: 2.5.3 + patches
+.. |imap_current_stable_version| replace:: 2.5.4
+.. |imap_current_stable_next_version| replace:: 2.5.4 + patches
 .. |imap_current_stable_branch| replace:: `cyrus-imapd-2.5`
 .. |imap_latest_development_version| replace:: 3.0
 .. |imap_latest_development_branch| replace:: master
