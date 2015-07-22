368e8c528aceababe85f6ba90ddff32de60b6622
Thomas Jarosch <thomas.jarosch@intra2net.com>
2015-07-23 09:58:30 +1000

    _metadata_to_annotate(): Reserve space for terminating zero
    From:
        Thomas Jarosch <thomas.jarosch@intra2net.com>
    To:
        cyrus-devel@lists.andrew.cmu.edu
    Subject:
        [PATCH] _metadata_to_annotate(): Reserve space for terminating zero
    Date:
        Tuesday, July 21, 2015 8:03 PM
    List-Id:
        List for developers of Cyrus SASL and Cyrus IMAPd <cyrus-devel.lists.andrew.cmu.edu>
    Size:
        6 KB
    
    Affects 2.5 and git HEAD, patch against git HEAD.
    
    cppcheck reported:
    Dangerous usage of strncat - 3rd parameter is the maximum number of characters to append.

--- imap/imapd.c
+++ imap/imapd.c
@@ -9628,7 +9628,7 @@ static int _metadata_to_annotate(const strarray_t *entries,
      * the old annotation system works. */
     for (i = 0 ; i < entries->count ; i++) {
         char *ent = entries->data[i];
-        char entry[MAX_MAILBOX_NAME];
+        char entry[MAX_MAILBOX_NAME+1];
 
         lcase(ent);
         /* there's no way to perfect this - unfortunately - the old style
