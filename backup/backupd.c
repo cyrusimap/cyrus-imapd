afa2c880213b2de61dacb7ccd9672fffe9e761f5
ellie timoney <ellie@fastmail.com>
2015-07-23 10:37:05 +1000

    backupd: minimal service_init

--- backup/backupd.c
+++ backup/backupd.c
@@ -43,15 +43,44 @@
 
 #include <config.h>
 
+#include <signal.h>
 #include <stdlib.h>
 #include <syslog.h>
+#include <unistd.h>
 
-/* for config.c */
+#include <sys/types.h>
+
+#include "exitcodes.h"
+#include "signals.h"
+
+#include "imap/global.h"
+#include "imap/proc.h"
+
 const int config_need_data = 0;
+static sasl_ssf_t extprops_ssf = 0;
+
+static struct auth_state *backupd_authstate = 0;
+static int backupd_userisadmin = 0;
+
+static struct proxy_context backupd_proxyctx = {
+    0, 1, &backupd_authstate, &backupd_userisadmin, NULL
+};
+
+static struct sasl_callback mysasl_cb[] = {
+    { SASL_CB_GETOPT, (mysasl_cb_ft *) &mysasl_config, NULL },
+    { SASL_CB_PROXY_POLICY, (mysasl_cb_ft *) &mysasl_proxy_policy, (void*) &backupd_proxyctx },
+    { SASL_CB_CANON_USER, (mysasl_cb_ft *) &mysasl_canon_user, NULL },
+    { SASL_CB_LIST_END, NULL, NULL }
+};
 
 static void shut_down(int code)
 {
     exit(code);
+}
+
+static void usage(void)
+{
+    // FIXME
 }
 
 EXPORTED void fatal(const char* s, int code)
@@ -80,7 +109,29 @@ EXPORTED int service_init(int argc __attribute__((unused)),
                  char **argv __attribute__((unused)),
                  char **envp __attribute__((unused)))
 {
-    return -1;
+    // FIXME should this be calling fatal? fatal exits directly
+    if (geteuid() == 0) fatal("must run as the Cyrus user", EC_USAGE);
+    setproctitle_init(argc, argv, envp);
+
+    /* set signal handlers */
+    signals_set_shutdown(&shut_down);
+    signal(SIGPIPE, SIG_IGN);
+
+    /* load the SASL plugins */
+    global_sasl_init(1, 1, mysasl_cb);
+
+    int opt;
+    while ((opt = getopt(argc, argv, "p:f")) != EOF) {
+        switch(opt) {
+        case 'p': /* external protection */
+            extprops_ssf = atoi(optarg);
+            break;
+        default:
+            usage();
+        }
+    }
+
+    return 0;
 }
 
 /* Called by service API to shut down the service */
