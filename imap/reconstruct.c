16734740e86e148fc5be2afb83ce8f852c79f6ea
Marty Lee <marty@maui-systems.co.uk>
2015-08-24 10:33:43 +1000

    fix segfault on reconstruct failure (Solaris 10)

--- imap/reconstruct.c
+++ imap/reconstruct.c
@@ -465,7 +465,7 @@ static int do_reconstruct(const char *name,
     r = mailbox_reconstruct(lastname, reconstruct_flags);
     if (r) {
 	com_err(lastname, r, "%s",
-		(r == IMAP_IOERROR) ? error_message(errno) : NULL);
+		(r == IMAP_IOERROR) ? error_message(errno) : "Failed to reconstruct mailbox");
 	return 0;
     }
 
