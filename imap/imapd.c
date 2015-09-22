be75ebc6e9f46f6304e48616e50e38b92f029b29
Bron Gondwana <brong@fastmail.fm>
2015-09-23 09:55:41 +1000

    imapd: fix error paths where quota root is uninitialised

--- imap/imapd.c
+++ imap/imapd.c
@@ -7956,58 +7956,46 @@ static void cmd_getquota(const char *tag, const char *name)
     struct quota q;
     char *intname = mboxname_from_external(name, &imapd_namespace, imapd_userid);
 
+    quota_init(&q, intname);
+
     imapd_check(NULL, 0);
 
     if (!imapd_userisadmin && !imapd_userisproxyadmin) {
         r = IMAP_PERMISSION_DENIED;
-        goto err;
+        goto done;
     }
 
     r = mlookup(NULL, NULL, intname, &mbentry);
-
     if (!r && (mbentry->mbtype & MBTYPE_REMOTE)) {
         /* remote mailbox */
 
         snprintf(quotarootbuf, sizeof(quotarootbuf), "%s.", intname);
 
         r = mboxlist_allmbox(quotarootbuf, quota_cb, (void *)mbentry->server, 0);
+        if (r) goto done;
 
-        if (!r) {
-            struct backend *s;
-
-            s = proxy_findserver(mbentry->server, &imap_protocol,
-                                 proxy_userid, &backend_cached,
-                                 &backend_current, &backend_inbox, imapd_in);
-            if (!s) r = IMAP_SERVER_UNAVAILABLE;
-
-            imapd_check(s, 0);
-
-            if (!r) {
-                prot_printf(s->out, "%s Getquota {" SIZE_T_FMT "+}\r\n%s\r\n",
-                            tag, strlen(name), name);
-                pipe_including_tag(s, tag, 0);
-            }
+        struct backend *s;
+        s = proxy_findserver(mbentry->server, &imap_protocol,
+                             proxy_userid, &backend_cached,
+                             &backend_current, &backend_inbox, imapd_in);
+        if (!s) {
+            r = IMAP_SERVER_UNAVAILABLE;
+            goto done;
         }
 
-        if (r) prot_printf(imapd_out, "%s NO %s\r\n", tag, error_message(r));
+        imapd_check(s, 0);
 
-        mboxlist_entry_free(&mbentry);
-        free(intname);
+        prot_printf(s->out, "%s Getquota {" SIZE_T_FMT "+}\r\n%s\r\n",
+                    tag, strlen(name), name);
+        pipe_including_tag(s, tag, 0);
 
-        return;
+        goto done;
     }
-
-    mboxlist_entry_free(&mbentry);
 
     /* local mailbox */
 
-    quota_init(&q, intname);
     r = quota_read(&q, NULL, 0);
-    if (r) {
-    err:
-        prot_printf(imapd_out, "%s NO %s\r\n", tag, error_message(r));
-        goto done;
-    }
+    if (r) goto done;
 
     prot_printf(imapd_out, "* QUOTA ");
     prot_printastring(imapd_out, name);
@@ -8019,6 +8007,8 @@ static void cmd_getquota(const char *tag, const char *name)
                 error_message(IMAP_OK_COMPLETED));
 
  done:
+    if (r) prot_printf(imapd_out, "%s NO %s\r\n", tag, error_message(r));
+    mboxlist_entry_free(&mbentry);
     quota_free(&q);
     free(intname);
 }
