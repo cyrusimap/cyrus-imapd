fb6677102ff12bac69597b10249a1d99b42d2c76
Robert Stepanek <rsto@paranoia.at>
2015-10-14 13:31:24 +0200

    jmap: setCalendarEvents: rewrite LocalDates for recurrence

--- cassandane/Cassandane/Cyrus/JMAP.pm
+++ cassandane/Cassandane/Cyrus/JMAP.pm
@@ -1050,4 +1050,122 @@ sub test_setcalendarevents {
     $self->assert_str_equals($res->[0][1]{notFound}[1], "foo");
 }
 
+sub test_setcalendarevents_update_recurrence {
+    my ($self) = @_;
+
+    my $jmap = $self->{jmap};
+    my $caldav = $self->{caldav};
+
+    xlog "create calendar";
+    my $res = $jmap->Request([
+            ['setCalendars', { create => { "#1" => {
+                            name => "foo", color => "coral", sortOrder => 1, isVisible => \1
+             }}}, "R1"]
+    ]);
+    my $calid = $res->[0][1]{created}{"#1"}{id};
+
+    xlog "create event";
+    $res = $jmap->Request([['setCalendarEvents', { create => {
+                        "#1" => {
+                            "calendarId" => $calid,
+                            "summary" => "foo",
+                            "description" => "foo's description",
+                            "location" => "foo's location",
+                            "showAsFree" => JSON::false,
+                            "isAllDay" => JSON::false,
+                            "start" => "2015-10-06T16:45:00",
+                            "startTimeZone" => "Australia/Melbourne",
+                            "end" => "2015-10-06T17:15:00",
+                            "endTimeZone" => "Australia/Melbourne",
+                            "recurrence" => {
+                                "frequency" => "daily",
+                                "byDay" => [-21, -10, -1, 2, 8, 15],
+                                "byMonth" => [2, 8],
+                                "until" => "2015-10-08T16:45:00"
+                            }
+                        }
+                    }}, "R1"]]);
+    my $id = $res->[0][1]{created}{"#1"}{id};
+
+    xlog "get calendar event $id";
+    $res = $jmap->Request([['getCalendarEvents', {ids => [$id]}, "R1"]]);
+    my $event = $res->[0][1]{list}[0];
+    $self->assert_str_equals($event->{id}, $id);
+    $self->assert_str_equals($event->{recurrence}{frequency}, "daily");
+    $self->assert_deep_equals($event->{recurrence}{byDay}, [-21, -10, -1, 2, 8, 15]);
+    $self->assert_deep_equals($event->{recurrence}{byMonth}, [2, 8]);
+    $self->assert_str_equals($event->{recurrence}{until}, "2015-10-08T16:45:00");
+
+    xlog "update recurrence of event $id";
+    $res = $jmap->Request([['setCalendarEvents', { update => {
+                        $id => {
+                            "recurrence" => {
+                                "frequency" => "weekly",
+                                "until" => "2016-10-08T16:45:00"
+                            }
+                        }
+                    }}, "R1"]]);
+
+    $self->assert_str_equals($res->[0][1]{updated}[0], $id);
+
+    xlog "get calendar event $id";
+    $res = $jmap->Request([['getCalendarEvents', {ids => [$id]}, "R1"]]);
+
+    my $x = Dumper($res);
+    xlog "$x";
+
+    $event = $res->[0][1]{list}[0];
+    $self->assert_str_equals($event->{id}, $id);
+    $self->assert_str_equals($event->{recurrence}{frequency}, "weekly");
+    $self->assert_null($event->{recurrence}{byDay});
+    $self->assert_null($event->{recurrence}{byMonth});
+    $self->assert_str_equals($event->{recurrence}{until}, "2016-10-08T16:45:00");
+
+    xlog "do not touch recurrence of event $id";
+    $res = $jmap->Request([['setCalendarEvents', { update => {
+                        $id => {
+                            "summary" => "baz"
+                        }
+                    }}, "R1"]]);
+
+    $self->assert_str_equals($res->[0][1]{updated}[0], $id);
+
+    xlog "get calendar $id";
+    $res = $jmap->Request([['getCalendarEvents', {ids => [$id]}, "R1"]]);
+    $event = $res->[0][1]{list}[0];
+    $self->assert_str_equals($event->{id}, $id);
+    $self->assert_str_equals($event->{recurrence}{frequency}, "weekly");
+    $self->assert_null($event->{recurrence}{byDay});
+    $self->assert_null($event->{recurrence}{byMonth});
+    $self->assert_str_equals($event->{recurrence}{until}, "2016-10-08T16:45:00");
+
+    xlog "update startTimeZone of event $id";
+    $res = $jmap->Request([['setCalendarEvents', { update => {
+                        $id => {
+                            "startTimeZone" => "Asia/Bangkok"
+                        }
+                    }}, "R1"]]);
+    $self->assert_str_equals($res->[0][1]{updated}[0], $id);
+
+    xlog "get calendar $id";
+    $res = $jmap->Request([['getCalendarEvents', {ids => [$id]}, "R1"]]);
+    $event = $res->[0][1]{list}[0];
+    $self->assert_str_equals($event->{id}, $id);
+    $self->assert_str_equals($event->{recurrence}{until}, "2016-10-08T16:45:00");
+    xlog "remove recurrence of event $id";
+    $res = $jmap->Request([['setCalendarEvents', { update => {
+                        $id => {
+                            "recurrence" => undef
+                        }
+                    }}, "R1"]]);
+
+    $self->assert_str_equals($res->[0][1]{updated}[0], $id);
+
+    xlog "get calendar $id";
+    $res = $jmap->Request([['getCalendarEvents', {ids => [$id]}, "R1"]]);
+    $event = $res->[0][1]{list}[0];
+    $self->assert_str_equals($event->{id}, $id);
+    $self->assert_null($event->{recurrence});
+}
+
 1;
