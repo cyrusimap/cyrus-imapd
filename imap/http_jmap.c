4340a43f5b2b990b132816c9a5866d568813d940
Robert Norris <rob@eatenbyagrue.org>
2015-11-12 11:18:42 +1100

    jmap: store jobTitle in TITLE vcard field
    Also read it from ORG[2], so we don't need to rewrite all our stored
    cards (yet).

--- imap/http_jmap.c
+++ imap/http_jmap.c
@@ -1352,10 +1352,16 @@ static int getcontacts_cb(void *rock, struct carddav_data *cdata)
     if (_wantprop(crock->props, "department"))
         json_object_set_new(obj, "department",
                             json_string(strarray_safenth(org, 1)));
-    if (_wantprop(crock->props, "jobTitle"))
-        json_object_set_new(obj, "jobTitle",
-                            json_string(strarray_safenth(org, 2)));
-    /* XXX - position? */
+
+    if (_wantprop(crock->props, "jobTitle")) {
+        /* we used to store jobTitle in ORG[2] instead of TITLE, which confused
+         * CardDAV clients. that's fixed, but there's now lots of cards with it
+         * stored in the wrong place, so check both */
+        const char *item = vparse_stringval(card, "title");
+        if (!item)
+            item = strarray_safenth(org, 2);
+        json_object_set_new(obj, "jobTitle", json_string(item));
+    }
 
     /* address - we need to open code this, because it's repeated */
     if (_wantprop(crock->props, "addresses")) {
@@ -2094,6 +2100,11 @@ static int _json_to_card(struct vparse_card *card,
             if (r) return r;
             record_is_dirty = 1;
         }
+        else if (!strcmp(key, "jobTitle")) {
+            int r = _kv_to_card(card, "title", jval);
+            if (r) return r;
+            record_is_dirty = 1;
+        }
         else if (!strcmp(key, "company")) {
             const char *val = json_string_value(jval);
             if (!val)
@@ -2108,14 +2119,6 @@ static int _json_to_card(struct vparse_card *card,
                 return -1;
             struct vparse_entry *org = _card_multi(card, "org");
             strarray_set(org->v.values, 1, val);
-            record_is_dirty = 1;
-        }
-        else if (!strcmp(key, "jobTitle")) {
-            const char *val = json_string_value(jval);
-            if (!val)
-                return -1;
-            struct vparse_entry *org = _card_multi(card, "org");
-            strarray_set(org->v.values, 2, val);
             record_is_dirty = 1;
         }
         else if (!strcmp(key, "emails")) {
