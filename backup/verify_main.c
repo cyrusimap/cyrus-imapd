87ad4b089baab2e264f5737ba82524af42e45d57
ellie timoney <ellie@fastmail.com>
2015-11-26 11:57:23 +1100

    verify: update for api change

--- backup/verify_main.c
+++ backup/verify_main.c
@@ -20,16 +20,19 @@ HIDDEN int main (int argc, char **argv) {
     if (argc != 2) usage(argv[0]);
 
     char *backup_name = argv[1];
+    struct backup *backup = NULL;
+    int r;
 
-    struct backup *backup = backup_open_paths(backup_name, NULL);
-    if (!backup) {
+    r = backup_open_paths(&backup, backup_name, NULL,
+                          BACKUP_OPEN_BLOCK);
+    if (r) {
         fprintf(stderr, "couldn't open %s\n", backup_name);
         return -1;
     }
 
     fprintf(stderr, "verifying %s...\n", backup_name);
 
-    int r = backup_verify(backup, BACKUP_VERIFY_FULL);
+    r = backup_verify(backup, BACKUP_VERIFY_FULL);
     backup_close(&backup);
 
     return r;
