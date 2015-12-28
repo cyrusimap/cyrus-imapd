1b9d85923c1f4e6c5eefdc04083369183c555a87
Bron Gondwana <brong@fastmail.fm>
2015-12-29 00:41:34 +1100

    cyr_dbtool: support improved_mboxlist_sort

--- imap/cyr_dbtool.c
+++ imap/cyr_dbtool.c
@@ -250,10 +250,13 @@ int main(int argc, char *argv[])
     struct txn *tid = NULL;
     struct txn **tidp = NULL;
 
-    while ((opt = getopt(argc, argv, "C:ntT")) != EOF) {
+    while ((opt = getopt(argc, argv, "C:MntT")) != EOF) {
         switch (opt) {
         case 'C': /* alt config file */
             alt_config = optarg;
+            break;
+        case 'M': /* use "improved_mboxlist_sort" */
+            db_flags |= CYRUSDB_MBOXSORT;
             break;
         case 'n': /* create new */
             db_flags |= CYRUSDB_CREATE;
