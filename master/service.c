1dc1800ef8530752cc3de74fcef6117c95c56d7a
ellie timoney <ellie@fastmail.com>
2016-02-22 11:43:27 +1100

    services: don't divide by zero when given -T 0
    Thanks Ian Batten and Jens Erat for the patches
    
    Resolves T227

--- master/service.c
+++ master/service.c
@@ -382,10 +382,13 @@ int main(int argc, char **argv, char **envp)
     }
     id = atoi(p);
 
-    /* pick a random timeout between reuse_timeout -> 2*reuse_timeout
-     * to avoid massive IO overload if the network connection goes away */
     srand(time(NULL) * getpid());
-    reuse_timeout = reuse_timeout + (rand() % reuse_timeout);
+
+    /* if timeout is enabled, pick a random timeout between reuse_timeout
+     * and 2*reuse_timeout to avoid massive IO overload if the network
+     * connection goes away */
+    if (reuse_timeout)
+        reuse_timeout = reuse_timeout + (rand() % reuse_timeout);
 
     extern const int config_need_data;
     cyrus_init(alt_config, service, 0, config_need_data);
