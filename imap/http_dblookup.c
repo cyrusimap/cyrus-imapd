0e839dc6bc355f8d81855cb1ed82222cab84b2a8
Bron Gondwana <brong@fastmail.fm>
2016-02-24 12:20:27 +1100

    dblookup: put in the +=2

--- imap/http_dblookup.c
+++ imap/http_dblookup.c
@@ -205,7 +205,7 @@ static int get_uid2groups(struct transaction_t *txn,
     if (!array) goto done;
 
     json = json_pack("{}");
-    for (i = 0; i < strarray_size(array); i++) {
+    for (i = 0; i < strarray_size(array); i += 2) {
         json_object_set_new(json, strarray_nth(array, i), json_string(strarray_nth(array, i+1)));
     }
 
