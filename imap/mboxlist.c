9da831ce4cc945135374fd48d8781be0ba603ddf
Michae Menge <michael.menge@zdv.uni-tuebingen.de>
2016-03-01 10:44:35 +1100

    mboxlist_renamemailbox: rollback db changes on mupdate failure
    https://bugzilla.cyrusimap.org/show_bug.cgi?id=3862

--- imap/mboxlist.c
+++ imap/mboxlist.c
@@ -1327,6 +1327,7 @@ EXPORTED int mboxlist_renamemailbox(const char *oldname, const char *newname,
 			   int local_only, int forceuser, int ignorequota)
 {
     int r;
+    int mupdatecommiterror = 0;
     long myrights;
     int isusermbox = 0; /* Are we renaming someone's inbox */
     int partitionmove = 0;
@@ -1530,6 +1531,7 @@ EXPORTED int mboxlist_renamemailbox(const char *oldname, const char *newname,
 	    syslog(LOG_ERR,
 		   "MUPDATE: can't commit mailbox entry for '%s'",
 		   newname);
+            mupdatecommiterror = r;
 	}
 	if (mupdate_h) mupdate_disconnect(&mupdate_h);
 	free(loc);
@@ -1540,7 +1542,37 @@ EXPORTED int mboxlist_renamemailbox(const char *oldname, const char *newname,
 	r = mailbox_commit(newmailbox);
 
     if (r) {
-	/* XXX - rollback DB changes if it was an mupdate failure */
+	/* rollback DB changes if it was an mupdate failure */
+        if (mupdatecommiterror) {
+            r = 0;
+
+            /* recreate an old entry */
+            if (!isusermbox)
+                r = cyrusdb_store(mbdb, oldname, strlen(oldname),
+				  mboxent, strlen(mboxent), &tid);
+
+            /* delete the new entry */
+            if (!r)
+                r = cyrusdb_delete(mbdb, newname, strlen(newname), &tid, 0);
+
+            /* Commit transaction */
+            if (!r)
+                r = cyrusdb_commit(mbdb, tid);
+
+            tid = NULL;
+            if (r) {
+                /* XXX HOWTO repair this mess! */
+                syslog(LOG_ERR, "DBERROR: failed DB rollback on mailboxrename %s %s: %s",
+                       oldname, newname, cyrusdb_strerror(r));
+                syslog(LOG_ERR, "DBERROR: mailboxdb on mupdate and backend ARE NOT CONSISTENT");
+                syslog(LOG_ERR, "DBERROR: mailboxdb on mupdate has entry for %s, mailboxdb on backend has entry for %s and files are on the old position", oldname, newname);
+                r = IMAP_IOERROR;
+            } else {
+                r = mupdatecommiterror;
+            }
+
+        }
+
 	if (newmailbox) mailbox_delete(&newmailbox);
 	if (partitionmove && newpartition)
 	    mailbox_delete_cleanup(newpartition, newname);
