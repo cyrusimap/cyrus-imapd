d4450ed41d08e9b5d45ea089892095da60e84cb0
Robert Stepanek <rsto@paranoia.at>
2016-03-08 16:33:52 +0100

    xapian: add fuzzy search tests

---
+++ cassandane/Cassandane/Cyrus/SearchFuzzy.pm
@@ -0,0 +1,172 @@
+#!/usr/bin/perl
+#
+#  Copyright (c) 2011 Opera Software Australia Pty. Ltd.  All rights
+#  reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#
+#  3. The name "Opera Software Australia" must not be used to
+#     endorse or promote products derived from this software without
+#     prior written permission. For permission or any legal
+#     details, please contact
+# 	Opera Software Australia Pty. Ltd.
+# 	Level 50, 120 Collins St
+# 	Melbourne 3000
+# 	Victoria
+# 	Australia
+#
+#  4. Redistributions of any form whatsoever must retain the following
+#     acknowledgment:
+#     "This product includes software developed by Opera Software
+#     Australia Pty. Ltd."
+#
+#  OPERA SOFTWARE AUSTRALIA DISCLAIMS ALL WARRANTIES WITH REGARD TO
+#  THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+#  AND FITNESS, IN NO EVENT SHALL OPERA SOFTWARE AUSTRALIA BE LIABLE
+#  FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+#  AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+#  OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+use strict;
+use warnings;
+package Cassandane::Cyrus::SearchFuzzy;
+use base qw(Cassandane::Cyrus::TestCase);
+use Cwd qw(abs_path);
+use DateTime;
+use Cassandane::Util::Log;
+use Data::Dumper;
+
+sub new
+{
+    my $class = shift;
+    return $class->SUPER::new({}, @_);
+}
+
+sub set_up
+{
+    my ($self) = @_;
+    $self->SUPER::set_up();
+
+    # Test if Xapian is enabled. Otherwise we won't run fuzzy search tests.
+    my $engine = $self->{instance}->{config}->get("search_engine");
+    if ($engine ne 'xapian') {
+        xlog "No xapian search_engine in imapd.conf. Skipping tests.";
+        return;
+    }
+    $self->{test_fuzzy_search} = 1;
+
+    xlog "Generate test messages.";
+    # Some subjects with the same verb word stem
+    $self->make_message("I am running") || die;
+    $self->make_message("I run") || die;
+    $self->make_message("He runs") || die;
+ 
+    # Some bodies with the same word stems but different senders. We use 
+    # the "connect" word stem since it it the first example on Xapian's
+    # Stemming documentation (https://xapian.org/docs/stemming.html).
+    # Mails from foo@example.com...
+    my %params;
+    %params = (
+        from => Cassandane::Address->new(
+            localpart => "foo",
+            domain => "example.com"
+        ),
+    );
+    $params{'body'} ="He has connections.",
+    $self->make_message("1", %params) || die;
+    $params{'body'} = "Gonna get myself connected.";
+    $self->make_message("2", %params) || die;
+    # ...as well as from bar@example.com.
+    %params = (
+        from => Cassandane::Address->new(
+            localpart => "bar",
+            domain => "example.com"
+        ),
+        body => "Einstein's gravitational theory resulted in beautiful relations connecting gravitational phenomena with the geometry of space; this was an exciting idea."
+    );
+    $self->make_message("3", %params) || die;
+
+    # Create the search database.
+    xlog "Run squatter";
+    $self->{instance}->run_command({cyrus => 1}, 'squatter');
+}
+
+sub tear_down
+{
+    my ($self) = @_;
+    $self->SUPER::tear_down();
+}
+
+sub test_stem_verbs
+{
+    my ($self) = @_;
+    return if not $self->{test_fuzzy_search};
+
+    my $talk = $self->{store}->get_client();
+
+    xlog "Select INBOX";
+    $talk->select("INBOX") || die;
+
+    my $r;
+    xlog 'SEARCH for subject "runs"';
+    $r = $talk->search('subject', { Quote => "runs" }) || die;
+    $self->assert_num_equals(1, scalar @$r);
+
+    xlog 'SEARCH for FUZZY subject "runs"';
+    $r = $talk->search('fuzzy', ['subject', { Quote => "runs" }]) || die;
+    $self->assert_num_equals(3, scalar @$r);
+}
+
+sub test_stem_any
+{
+    my ($self) = @_;
+    return if not $self->{test_fuzzy_search};
+
+    my $talk = $self->{store}->get_client();
+
+    xlog "Select INBOX";
+    $talk->select("INBOX") || die;
+
+    my $r;
+    xlog 'SEARCH for body "connection"';
+    $r = $talk->search('body', { Quote => "connection" }) || die;
+    $self->assert_num_equals(1, scalar @$r);
+
+    xlog "SEARCH for FUZZY body \"connection\"";
+    $r = $talk->search(
+        "fuzzy", ["body", { Quote => "connection" }],
+    ) || die;
+    $self->assert_num_equals(3, scalar @$r);
+}
+
+sub test_mix_fuzzy_and_nonfuzzy
+{
+    my ($self) = @_;
+    return if not $self->{test_fuzzy_search};
+
+    my $talk = $self->{store}->get_client();
+
+    xlog "Select INBOX";
+    $talk->select("INBOX") || die;
+
+    xlog "SEARCH for from \"foo\@example.com\" with FUZZY body \"connection\"";
+    my $r = $talk->search(
+        "fuzzy", ["body", { Quote => "connection" }],
+        "from", { Quote => "foo\@example.com" }
+    ) || die;
+    $self->assert_num_equals(2, scalar @$r);
+}
+
+1;
