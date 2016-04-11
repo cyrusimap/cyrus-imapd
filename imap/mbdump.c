558bd7d6012dd6522f4048117668b691298fed6d
Wolfgang Breyha <wbreyha@gmx.net>
2016-04-11 11:53:26 +1000

    mbdump.c: reopen mailbox if first annotation is detected (#3927)
    Since dump/undump has a fixed order transferring cyrus.*, message files,
    annotations, ...
    
    If the first annotation is detected the mailbox is closed and opened again to
    reread header and index. The annotate_state_set_mailbox call is moved after the
    reopen.

--- imap/mbdump.c
+++ imap/mbdump.c
@@ -833,6 +833,7 @@ EXPORTED int undump_mailbox(const char *mbname,
     const char *sieve_path = NULL;
     int sieve_usehomedir = config_getswitch(IMAPOPT_SIEVEUSEHOMEDIR);
     const char *userid = NULL;
+    int first_annotation = 1;
     char *annotation = NULL;
     struct buf content = BUF_INITIALIZER;
     char *seen_file = NULL;
@@ -905,8 +906,6 @@ EXPORTED int undump_mailbox(const char *mbname,
     mailbox_get_usage(mailbox, old_quota_usage);
 
     astate = annotate_state_new();
-    r = annotate_state_set_mailbox(astate, mailbox);
-    if (r) goto done;
 
     while(1) {
         char fnamebuf[MAX_MAILBOX_PATH + 1024];
@@ -929,6 +928,16 @@ EXPORTED int undump_mailbox(const char *mbname,
             /* Annotation */
             int i;
             char *tmpuserid;
+
+            /* is this the first annotation? re-read cyrus.header/index */
+            if (first_annotation) {
+                first_annotation = 0;
+                mailbox_close(&mailbox);
+                r = mailbox_open_exclusive(mbname, &mailbox);
+                if (r) goto done;
+                r = annotate_state_set_mailbox(astate, mailbox);
+                if (r) goto done;
+            }
 
             for(i=2; file.s[i]; i++) {
                 if(file.s[i] == '/') break;
