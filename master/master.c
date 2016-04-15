0a76ed8e22bc444d18d9e002b8f326dad2e8083d
ellie timoney <ellie@fastmail.com>
2016-04-15 12:47:02 +1000

    master: don't loop myselect while in_shutdown

--- master/master.c
+++ master/master.c
@@ -2504,7 +2504,7 @@ int main(int argc, char **argv)
 		    fatalf(1, "select failed: %m");
 		}
 	    }
-	} while (r == -1);
+	} while (!in_shutdown && r == -1);
 
 #if defined(HAVE_UCDSNMP) || defined(HAVE_NETSNMP)
 	/* check for SNMP queries */
