7fc89f78b28b1e15edf2288678a56681d304fb8e
ellie timoney <ellie@fastmail.com>
2016-05-20 13:23:15 +1000

    master: make better use of select() return value semantics

--- master/master.c
+++ master/master.c
@@ -2387,7 +2387,7 @@ int main(int argc, char **argv)
     syslog(LOG_DEBUG, "ready for work");
 
     for (;;) {
-        int r, i, maxfd, total_children = 0;
+        int i, maxfd, ready_fds, total_children = 0;
         struct timeval tv, *tvptr;
         struct notify_message msg;
 #if defined(HAVE_UCDSNMP) || defined(HAVE_NETSNMP)
@@ -2529,9 +2529,9 @@ int main(int argc, char **argv)
             snmp_select_info(&maxfd, &rfds, tvptr, &blockp);
 #endif
             errno = 0;
-            r = myselect(maxfd, &rfds, NULL, NULL, tvptr);
+            ready_fds = myselect(maxfd, &rfds, NULL, NULL, tvptr);
 
-            if (r == -1) {
+            if (ready_fds < 0) {
                 switch (errno) {
                 case EAGAIN:
                 case EINTR:
@@ -2551,47 +2551,54 @@ int main(int argc, char **argv)
                     fatalf(1, "select failed: %m");
                 }
             }
-        } while (!in_shutdown && r == -1);
+        } while (!in_shutdown && ready_fds < 0);
 
 #if defined(HAVE_UCDSNMP) || defined(HAVE_NETSNMP)
         /* check for SNMP queries */
-        snmp_read(&rfds);
-        snmp_timeout();
+        if (ready_fds > 0)
+            snmp_read(&rfds);
+        if (ready_fds == 0)
+            snmp_timeout();
 #endif
-        for (i = 0; i < nservices; i++) {
-            int x = Services[i].stat[0];
-            int y = Services[i].socket;
 
-            if ((x >= 0) && FD_ISSET(x, &rfds)) {
-                while ((r = read_msg(x, &msg)) == 0)
-                    process_msg(i, &msg);
+        if (ready_fds > 0) {
+            for (i = 0; i < nservices; i++) {
+                int x = Services[i].stat[0];
+                int y = Services[i].socket;
 
-                if (r == 2) {
-                    syslog(LOG_ERR,
-                        "got incorrectly sized response from child: %x", i);
-                    continue;
+                if ((x >= 0) && FD_ISSET(x, &rfds)) {
+                    while ((r = read_msg(x, &msg)) == 0)
+                        process_msg(i, &msg);
+
+                    if (r == 2) {
+                        syslog(LOG_ERR,
+                            "got incorrectly sized response from child: %x", i);
+                        continue;
+                    }
+                    if (r < 0) {
+                        syslog(LOG_ERR,
+                            "error while receiving message from child %x: %m", i);
+                        continue;
+                    }
                 }
-                if (r < 0) {
-                    syslog(LOG_ERR,
-                        "error while receiving message from child %x: %m", i);
-                    continue;
-                }
-            }
 
-            if (!in_shutdown && Services[i].exec &&
-                Services[i].nactive < Services[i].max_workers &&
-                Services[i].ready_workers == 0 &&
-                y >= 0 && FD_ISSET(y, &rfds))
-            {
-                /* huh, someone wants to talk to us */
-                spawn_service(i);
+                if (!in_shutdown && Services[i].exec &&
+                    Services[i].nactive < Services[i].max_workers &&
+                    Services[i].ready_workers == 0 &&
+                    y >= 0 && FD_ISSET(y, &rfds))
+                {
+                    /* huh, someone wants to talk to us */
+                    spawn_service(i);
+                }
             }
         }
+
         gettimeofday(&now, 0);
         child_janitor(now);
 
 #ifdef HAVE_NETSNMP
-        run_alarms();
+        if (ready_fds == 0)
+            run_alarms();
 #endif
     }
 
