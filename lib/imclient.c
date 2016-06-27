d606fbe48472ad8d6c625307e358e994d6ca04cb
ellie timoney <ellie@fastmail.com>
2016-06-28 09:39:43 +1000

    tls: no ephemeral RSA support in newer OpenSSL
    (likewise for imtest and imclient)

--- lib/imclient.c
+++ lib/imclient.c
@@ -1575,6 +1575,7 @@ static int verify_callback(int ok, X509_STORE_CTX * ctx)
 }
 
 
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
 /* taken from OpenSSL apps/s_cb.c */
 static RSA *tmp_rsa_cb(SSL *s __attribute__((unused)),
 		       int export __attribute__((unused)),
@@ -1587,6 +1588,7 @@ static RSA *tmp_rsa_cb(SSL *s __attribute__((unused)),
     }
     return (rsa_tmp);
 }
+#endif
 
 /*
  * Seed the random number generator.
@@ -1675,7 +1677,10 @@ static int tls_init_clientengine(struct imclient *imclient,
             printf("[ TLS engine: cannot load cert/key data, may be a cert/key mismatch]\n");
 	    return -1;
 	}
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
     SSL_CTX_set_tmp_rsa_callback(imclient->tls_ctx, tmp_rsa_cb);
+#endif
 
     verify_depth = verifydepth;
     SSL_CTX_set_verify(imclient->tls_ctx, verify_flags, verify_callback);
