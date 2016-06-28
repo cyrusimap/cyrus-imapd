a42b500d237bfb7a4f50b91e9752b0d616753214
Bron Gondwana <brong@fastmail.fm>
2016-06-28 13:18:11 +1000

    build-with-cyruslibs - need to export some env

--- tools/build-with-cyruslibs.sh
+++ tools/build-with-cyruslibs.sh
@@ -2,9 +2,9 @@
 
 CYRUSLIBS=cyruslibs
 PREFIX=/usr/local/$CYRUSLIBS
-LDFLAGS="-L$PREFIX/lib/x86_64-linux-gnu -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib/x86_64-linux-gnu -Wl,-rpath,$PREFIX/lib"
-PKG_CONFIG_PATH="$PREFIX/lib/x86_64-linux-gnu/pkgconfig:$PREFIX/lib/pkgconfig:\$PKG_CONFIG_PATH"
-CFLAGS="-g -fPIC -W -Wall -Wextra"
+export LDFLAGS="-L$PREFIX/lib/x86_64-linux-gnu -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib/x86_64-linux-gnu -Wl,-rpath,$PREFIX/lib"
+export PKG_CONFIG_PATH="$PREFIX/lib/x86_64-linux-gnu/pkgconfig:$PREFIX/lib/pkgconfig:\$PKG_CONFIG_PATH"
+export CFLAGS="-g -fPIC -W -Wall -Wextra"
 autoreconf -v -i
 ./configure --prefix=/usr/cyrus --with-cyrus-prefix=/usr/cyrus --enable-http --enable-calalarmd --enable-unit-tests --enable-replication --enable-nntp --enable-murder --enable-idled --enable-xapian XAPIAN_CONFIG=$PREFIX/bin/xapian-config-1.3
 make lex-fix
