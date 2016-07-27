dbca58fc1eccb42d0287164f635440c16bc9f0d5
Bron Gondwana <brong@fastmail.fm>
2016-07-27 14:44:18 +1000

    build-with-cyruslibs: allow overridden configopts

--- tools/build-with-cyruslibs.sh
+++ tools/build-with-cyruslibs.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-CYRUSLIBS=cyruslibs
-PREFIX=/usr/local/$CYRUSLIBS
-export LDFLAGS="-L$PREFIX/lib/x86_64-linux-gnu -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib/x86_64-linux-gnu -Wl,-rpath,$PREFIX/lib"
-export PKG_CONFIG_PATH="$PREFIX/lib/x86_64-linux-gnu/pkgconfig:$PREFIX/lib/pkgconfig:\$PKG_CONFIG_PATH"
+: ${CYRUSLIBS:=cyruslibs}
+: ${LIBSDIR:=/usr/local/$CYRUSLIBS}
+: ${TARGET:=/usr/cyrus}
+: ${CONFIGOPTS:="--enable-http --enable-calalarmd --enable-unit-tests --enable-replication --enable-nntp --enable-murder --enable-idled --enable-xapian"}
+export LDFLAGS="-L$LIBSDIR/lib/x86_64-linux-gnu -L$LIBSDIR/lib -Wl,-rpath,$LIBSDIR/lib/x86_64-linux-gnu -Wl,-rpath,$LIBSDIR/lib"
+export PKG_CONFIG_PATH="$LIBSDIR/lib/x86_64-linux-gnu/pkgconfig:$LIBSDIR/lib/pkgconfig:\$PKG_CONFIG_PATH"
 export CFLAGS="-g -fPIC -W -Wall -Wextra"
 autoreconf -v -i
-./configure --prefix=/usr/cyrus --with-cyrus-prefix=/usr/cyrus --enable-http --enable-calalarmd --enable-unit-tests --enable-replication --enable-nntp --enable-murder --enable-idled --enable-xapian XAPIAN_CONFIG=$PREFIX/bin/xapian-config-1.3
+echo "./configure --prefix=$TARGET $CONFIGOPTS XAPIAN_CONFIG=$LIBSDIR/bin/xapian-config-1.3"
+./configure --prefix=$TARGET $CONFIGOPTS XAPIAN_CONFIG=$LIBSDIR/bin/xapian-config-1.3
 make lex-fix
 make -j 8
 make -j 8 check
