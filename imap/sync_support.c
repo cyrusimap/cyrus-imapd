elliefm/cyrus-imapd-old

sync_support: compile sieve scripts on upload
Silently ignore attempts to upload compiled bytecode (in case
master server is running older version that still does this).

We now need to link programs against libcyrus_sieve if they use
the sync_support sources.

imap/sync_support.c
@@ -78,6 +78,10 @@
 #include "dlist.h"
 #include "xstrlcat.h"
 
+#ifdef USE_SIEVE
+#include "sieve/sieve_interface.h"
+#endif
+
 /* generated headers are not necessarily in current directory */
 #include "imap/imap_err.h"
 
@@ -1003,12 +1007,19 @@ int sync_sieve_upload(const char *userid, const char *name,
                       size_t len)
 {
     const char *sieve_path = user_sieve_path(userid);
-    char tmpname[2048];
-    char newname[2048];
+    char tmpscriptname[2048];
+    char newscriptname[2048];
+    char *ext;
     FILE *file;
     int   r = 0;
     struct stat sbuf;
     struct utimbuf utimbuf;
+
+    ext = strrchr(name, '.');
+    if (ext && !strcmp(ext, ".bc")) {
+        /* silently ignore attempts to upload compiled bytecode */
+        return 0;
+    }
 
     if (stat(sieve_path, &sbuf) == -1 && errno == ENOENT) {
         if (cyrus_mkdir(sieve_path, 0755) == -1) return IMAP_IOERROR;
@@ -1018,12 +1029,12 @@ int sync_sieve_upload(const char *userid, const char *name,
         }
     }
 
-    snprintf(tmpname, sizeof(tmpname), "%s/sync_tmp-%lu",
+    snprintf(tmpscriptname, sizeof(tmpscriptname), "%s/sync_tmp-%lu.script",
              sieve_path, (unsigned long)getpid());
-    snprintf(newname, sizeof(newname), "%s/%s", sieve_path, name);
+    snprintf(newscriptname, sizeof(newscriptname), "%s/%s", sieve_path, name);
 
-    if ((file=fopen(tmpname, "w")) == NULL) {
-        return(IMAP_IOERROR);
+    if ((file=fopen(tmpscriptname, "w")) == NULL) {
+        return IMAP_IOERROR;
     }
 
     /* XXX - error handling */
@@ -1037,11 +1048,23 @@ int sync_sieve_upload(const char *userid, const char *name,
     utimbuf.actime  = time(NULL);
     utimbuf.modtime = last_update;
 
-    if (!r && (utime(tmpname, &utimbuf) < 0))
+    if (!r && (utime(tmpscriptname, &utimbuf) < 0))
         r = IMAP_IOERROR;
 
-    if (!r && (rename(tmpname, newname) < 0))
+    if (!r && (rename(tmpscriptname, newscriptname) < 0))
         r = IMAP_IOERROR;
+
+#ifdef USE_SIEVE
+    if (!r) {
+        char *bcname = sieve_getbcfname(newscriptname);
+        if (bcname) {
+            r = sieve_rebuild(newscriptname, bcname, /*force*/ 1, NULL);
+            if (r == SIEVE_PARSE_ERROR || r == SIEVE_FAIL)
+                r = IMAP_SYNC_BADSIEVE;
+            free(bcname);
+        }
+    }
+#endif
 
     sync_log_sieve(userid);
 

cyrusimap/cyrus-imapd

imap/sync_support.c
@@ -78,6 +78,10 @@
 #include "dlist.h"
 #include "xstrlcat.h"
 
+#ifdef USE_SIEVE
+#include "sieve/sieve_interface.h"
+#endif
+
 /* generated headers are not necessarily in current directory */
 #include "imap/imap_err.h"
 
@@ -1005,10 +1009,17 @@ int sync_sieve_upload(const char *userid, const char *name,
     const char *sieve_path = user_sieve_path(userid);
     char tmpname[2048];
     char newname[2048];
+    char *ext;
     FILE *file;
     int   r = 0;
     struct stat sbuf;
     struct utimbuf utimbuf;
+
+    ext = strrchr(name, '.');
+    if (ext && !strcmp(ext, ".bc")) {
+        /* silently ignore attempts to upload compiled bytecode */
+        return 0;
+    }
 
     if (stat(sieve_path, &sbuf) == -1 && errno == ENOENT) {
         if (cyrus_mkdir(sieve_path, 0755) == -1) return IMAP_IOERROR;
@@ -1023,7 +1034,7 @@ int sync_sieve_upload(const char *userid, const char *name,
     snprintf(newname, sizeof(newname), "%s/%s", sieve_path, name);
 
     if ((file=fopen(tmpname, "w")) == NULL) {
-        return(IMAP_IOERROR);
+        return IMAP_IOERROR;
     }
 
     /* XXX - error handling */
@@ -1042,6 +1053,18 @@ int sync_sieve_upload(const char *userid, const char *name,
 
     if (!r && (rename(tmpname, newname) < 0))
         r = IMAP_IOERROR;
+
+#ifdef USE_SIEVE
+    if (!r) {
+        char *bcname = sieve_getbcfname(newname);
+        if (bcname) {
+            r = sieve_rebuild(newname, bcname, /*force*/ 1, NULL);
+            if (r == SIEVE_PARSE_ERROR || r == SIEVE_FAIL)
+                r = IMAP_SYNC_BADSIEVE;
+            free(bcname);
+        }
+    }
+#endif
 
     sync_log_sieve(userid);
 