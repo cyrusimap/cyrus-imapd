fef01d1d4949f0a405b1b3bae1f1c307de254f49
Bron Gondwana <brong@fastmail.fm>
2016-10-04 14:27:51 +1100

    dbtool: -c => convert to named DB type

--- imap/cyr_dbtool.c
+++ imap/cyr_dbtool.c
@@ -250,10 +250,13 @@ int main(int argc, char *argv[])
     struct txn *tid = NULL;
     struct txn **tidp = NULL;
 
-    while ((opt = getopt(argc, argv, "C:MntT")) != EOF) {
+    while ((opt = getopt(argc, argv, "C:MntTc")) != EOF) {
         switch (opt) {
         case 'C': /* alt config file */
             alt_config = optarg;
+            break;
+        case 'c':
+            db_flags |= CYRUSDB_CONVERT;
             break;
         case 'M': /* use "improved_mboxlist_sort" */
             db_flags |= CYRUSDB_MBOXSORT;
