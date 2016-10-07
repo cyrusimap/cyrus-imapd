a16e918ca44fe92796bb6f269bf970fc8d1a15ca
Bron Gondwana <brong@fastmail.fm>
2016-10-07 17:06:04 +1100

    caldav: fix up x-schedule-user-address to return an href
    based on review by kmurchison

--- imap/http_caldav.c
+++ imap/http_caldav.c
@@ -5194,7 +5194,10 @@ static int propfind_scheduser(const xmlChar *name, xmlNsPtr ns,
                             void *rock __attribute__((unused)))
 {
     struct buf buf = BUF_INITIALIZER;
-    int r = 0;
+    xmlNodePtr node;
+
+    node = xml_add_prop(HTTP_OK, fctx->ns[NS_DAV], &propstat[PROPSTAT_OK],
+                        name, ns, NULL, 0);
 
     if (propstat) {
         if (!fctx->record || !fctx->mailbox) return HTTP_NOT_FOUND;
@@ -5208,11 +5211,14 @@ static int propfind_scheduser(const xmlChar *name, xmlNsPtr ns,
     }
 
     if (buf.len) {
-        r = propfind_getdata(name, ns, fctx, prop, propstat, caldav_mime_types,
-                             CALDAV_SUPP_DATA, buf.s, buf.len);
+        buf_reset(&fctx->buf);
+        buf_printf(&fctx->buf, "mailto:%s", buf_cstring(&buf));
+        xml_add_href(node, fctx->ns[NS_DAV], buf_cstring(&fctx->buf));
     }
 
-    return r;
+    buf_free(&buf);
+
+    return 0;
 }
 
 /* Callback to prescreen/fetch CALDAV:calendar-data */
