d155b863f7363f428bdab0c49d73de3c2b4eba42
Robert Stepanek <rsto@paranoia.at>
2016-11-03 01:38:48 +0100

    JMAPMail.pm: add further message move tests

--- cassandane/Cassandane/Cyrus/JMAPMail.pm
+++ cassandane/Cassandane/Cyrus/JMAPMail.pm
@@ -864,7 +864,6 @@ sub test_getmessages_multimailboxes
     xlog "get message";
     $res = $jmap->Request([['getMessageList', {fetchMessages => JSON::true}, "R1"]]);
     $msg = $res->[1][1]{list}[0];
-    xlog Dumper($res);
     $self->assert_num_equals(1, scalar @{$res->[0][1]{messageIds}});
     $self->assert_num_equals(2, scalar @{$msg->{mailboxIds}});
 }
@@ -1287,17 +1286,32 @@ sub test_setmessages_move
     my @mboxids = $msg->{mailboxIds};
     $self->assert_num_equals(1, scalar @mboxids);
 
-    xlog "move message from $inbox to mailboxes $a and $b";
+    xlog "move message to [$a,$b]";
     $msg->{mailboxIds} = [$a, $b];
     $res = $jmap->Request([['setMessages', { update => { $id => $msg }}, "R1"]]);
     $self->assert_str_equals($res->[0][1]{updated}[0], $id);
 
     $res = $jmap->Request([['getMessages', { ids => [$id] }, "R1"]]);
     $msg = $res->[0][1]->{list}[0];
-    xlog Dumper($res);
     $self->assert_deep_equals(sort [$a, $b], sort $msg->{mailboxIds});
 
-    # FIXME - continue from here
+    xlog "move message to [$a,$b,$c]";
+    $msg->{mailboxIds} = [$a, $b, $c];
+    $res = $jmap->Request([['setMessages', { update => { $id => $msg }}, "R1"]]);
+    $self->assert_str_equals($res->[0][1]{updated}[0], $id);
+
+    $res = $jmap->Request([['getMessages', { ids => [$id] }, "R1"]]);
+    $msg = $res->[0][1]->{list}[0];
+    $self->assert_deep_equals(sort [$a, $b, $c], sort $msg->{mailboxIds});
+
+    xlog "move message to [$d]";
+    $msg->{mailboxIds} = [$d];
+    $res = $jmap->Request([['setMessages', { update => { $id => $msg }}, "R1"]]);
+    $self->assert_str_equals($res->[0][1]{updated}[0], $id);
+
+    $res = $jmap->Request([['getMessages', { ids => [$id] }, "R1"]]);
+    $msg = $res->[0][1]->{list}[0];
+    $self->assert_deep_equals(sort [$d], sort $msg->{mailboxIds});
 }
 
 sub test_setmessages_update
