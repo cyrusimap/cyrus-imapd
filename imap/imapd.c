173dac009ca543009dcaee695828e06d839dc06c
Ken Murchison <murch@andrew.cmu.edu>
2016-11-03 11:52:56 -0400

    imapd.c: suppress DAV mailboxes for LIST (subscribed recursivematch) unless explicitly requested

--- imap/imapd.c
+++ imap/imapd.c
@@ -12669,6 +12669,27 @@ static int recursivematch_cb(struct findall_data *data, void *rockp)
 
     const char *extname = data->extname;
 
+    /* skip non-responsive mailboxes early, so they don't break sub folder detection */
+    if (!(imapd_userisadmin || (rock->listargs->sel & LIST_SEL_DAV))) {
+        mbname_t *mbname = (mbname_t *) data->mbname;
+        const char *intname;
+        int r;
+
+        if (!mbname) {
+            mbname = mbname_from_extname(extname, &imapd_namespace, imapd_userid);
+        }
+
+        intname = mbname_intname(mbname);
+        r = mboxname_iscalendarmailbox(intname, 0) ||
+            mboxname_isaddressbookmailbox(intname, 0) ||
+            mboxname_isdavdrivemailbox(intname, 0) ||
+            mboxname_isdavnotificationsmailbox(intname, 0);
+
+        if (!data->mbname) mbname_free(&mbname);
+
+        if (r) return 0;
+    }
+
     int *list_info = hash_lookup(extname, &rock->table);
     if (!list_info) {
         list_info = xzmalloc(sizeof(int));
