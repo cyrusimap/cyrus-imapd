c09b67edabb0733e4a2ca2b155623324056f1472
ellie timoney <ellie@fastmail.com>
2016-11-14 17:35:34 +1100

    imapd: fix whitespace in cmd_xfer

--- imap/imapd.c
+++ imap/imapd.c
@@ -10903,7 +10903,7 @@ static int xfer_initialsync(struct xfer_header *xfer)
         xfer->items = NULL;
 
         r = mboxlist_usermboxtree(xfer->userid, xfer_addusermbox,
-				  xfer, MBOXTREE_DELETED);
+                                  xfer, MBOXTREE_DELETED);
     }
     else {
         struct sync_name_list *mboxname_list = sync_name_list_create();
@@ -11431,140 +11431,140 @@ static void cmd_xfer(const char *tag, const char *name,
 
     /* Build list of users/mailboxes to transfer */
     if (config_partitiondir(name)) {
-	/* entire partition */
-	list.part = name;
-	mboxlist_findall(NULL, "*", 1, NULL, NULL, xfer_addmbox, &list);
+        /* entire partition */
+        list.part = name;
+        mboxlist_findall(NULL, "*", 1, NULL, NULL, xfer_addmbox, &list);
     } else {
-	/* mailbox pattern */
-	mbname_t *mbname;
+        /* mailbox pattern */
+        mbname_t *mbname;
 
-	intname = mboxname_from_external(name, &imapd_namespace, imapd_userid);
+        intname = mboxname_from_external(name, &imapd_namespace, imapd_userid);
 
-	mbname = mbname_from_intname(intname);
-	if (mbname_localpart(mbname) &&
-	    (mbname_isdeleted(mbname) || strarray_size(mbname_boxes(mbname)))) {
-	    /* targeted a user submailbox */
-	    list.allow_usersubs = 1;
-	}
-	mbname_free(&mbname);
+        mbname = mbname_from_intname(intname);
+        if (mbname_localpart(mbname) &&
+            (mbname_isdeleted(mbname) || strarray_size(mbname_boxes(mbname)))) {
+            /* targeted a user submailbox */
+            list.allow_usersubs = 1;
+        }
+        mbname_free(&mbname);
 
-	mboxlist_findall(NULL, intname, 1, NULL, NULL, xfer_addmbox, &list);
-	free(intname);
+        mboxlist_findall(NULL, intname, 1, NULL, NULL, xfer_addmbox, &list);
+        free(intname);
     }
 
     r = xfer_init(toserver, &xfer);
     if (r) goto done;
 
     for (item = list.mboxes; item; item = next) {
-	mbentry_t *mbentry = item->mbentry;
+        mbentry_t *mbentry = item->mbentry;
 
-	/* NOTE: Since XFER can only be used by an admin, and we always connect
-	 * to the destination backend as an admin, we take advantage of the fact
-	 * that admins *always* use a consistent mailbox naming scheme.
-	 * So, 'name' should be used in any command we send to a backend, and
-	 * 'intname' is the internal name to be used for mupdate and findall.
-	 */
+        /* NOTE: Since XFER can only be used by an admin, and we always connect
+         * to the destination backend as an admin, we take advantage of the fact
+         * that admins *always* use a consistent mailbox naming scheme.
+         * So, 'name' should be used in any command we send to a backend, and
+         * 'intname' is the internal name to be used for mupdate and findall.
+         */
 
-	r = 0;
-	intname = mbentry->name;
-	xfer->topart = xstrdup(topart ? topart : mbentry->partition);
+        r = 0;
+        intname = mbentry->name;
+        xfer->topart = xstrdup(topart ? topart : mbentry->partition);
 
-	/* if we are not moving a user, just move the one mailbox */
-	if (item->state != XFER_MOVING_USER) {
+        /* if we are not moving a user, just move the one mailbox */
+        if (item->state != XFER_MOVING_USER) {
 
-	    syslog(LOG_INFO, "XFER: mailbox '%s' -> %s!%s",
-		   mbentry->name, xfer->toserver, xfer->topart);
+            syslog(LOG_INFO, "XFER: mailbox '%s' -> %s!%s",
+                   mbentry->name, xfer->toserver, xfer->topart);
 
-	    /* is the selected mailbox the one we're moving? */
-	    if (!strcmpsafe(intname, index_mboxname(imapd_index))) {
-		r = IMAP_MAILBOX_LOCKED;
-		goto next;
-	    }
+            /* is the selected mailbox the one we're moving? */
+            if (!strcmpsafe(intname, index_mboxname(imapd_index))) {
+                r = IMAP_MAILBOX_LOCKED;
+                goto next;
+            }
 
-	    /* we're moving this mailbox */
-	    xfer_addusermbox(mbentry, xfer);
-	    mbox_count++;
+            /* we're moving this mailbox */
+            xfer_addusermbox(mbentry, xfer);
+            mbox_count++;
 
-	    r = do_xfer(xfer);
-	} else {
-	    xfer->userid = mboxname_to_userid(intname);
+            r = do_xfer(xfer);
+        } else {
+            xfer->userid = mboxname_to_userid(intname);
 
-	    syslog(LOG_INFO, "XFER: user '%s' -> %s!%s",
-		   xfer->userid, xfer->toserver, xfer->topart);
+            syslog(LOG_INFO, "XFER: user '%s' -> %s!%s",
+                   xfer->userid, xfer->toserver, xfer->topart);
 
-	    if (!config_getswitch(IMAPOPT_ALLOWUSERMOVES)) {
-		/* not configured to allow user moves */
-		r = IMAP_MAILBOX_NOTSUPPORTED;
-	    } else if (!strcmp(xfer->userid, imapd_userid)) {
-		/* don't move your own inbox, that could be troublesome */
-		r = IMAP_MAILBOX_NOTSUPPORTED;
-	    } else if (!strncmpsafe(intname, index_mboxname(imapd_index),
-			     strlen(intname))) {
-		/* selected mailbox is in the namespace we're moving */
-		r = IMAP_MAILBOX_LOCKED;
-	    }
-	    if (r) goto next;
+            if (!config_getswitch(IMAPOPT_ALLOWUSERMOVES)) {
+                /* not configured to allow user moves */
+                r = IMAP_MAILBOX_NOTSUPPORTED;
+            } else if (!strcmp(xfer->userid, imapd_userid)) {
+                /* don't move your own inbox, that could be troublesome */
+                r = IMAP_MAILBOX_NOTSUPPORTED;
+            } else if (!strncmpsafe(intname, index_mboxname(imapd_index),
+                             strlen(intname))) {
+                /* selected mailbox is in the namespace we're moving */
+                r = IMAP_MAILBOX_LOCKED;
+            }
+            if (r) goto next;
 
-	    if (!xfer->use_replication) {
-		/* set the quotaroot if needed */
-		r = xfer_setquotaroot(xfer, intname);
-		if (r) goto next;
+            if (!xfer->use_replication) {
+                /* set the quotaroot if needed */
+                r = xfer_setquotaroot(xfer, intname);
+                if (r) goto next;
 
-		/* backport the seen file if needed */
-		if (xfer->remoteversion < 12) {
-		    r = seen_open(xfer->userid, SEEN_CREATE, &xfer->seendb);
-		    if (r) goto next;
-		}
-	    }
+                /* backport the seen file if needed */
+                if (xfer->remoteversion < 12) {
+                    r = seen_open(xfer->userid, SEEN_CREATE, &xfer->seendb);
+                    if (r) goto next;
+                }
+            }
 
-	    r = mboxlist_usermboxtree(xfer->userid, xfer_addusermbox,
-				      xfer, MBOXTREE_DELETED);
+            r = mboxlist_usermboxtree(xfer->userid, xfer_addusermbox,
+                                      xfer, MBOXTREE_DELETED);
 
-	    /* NOTE: mailboxes were added in reverse, so the inbox is
-	     * done last */
-	    r = do_xfer(xfer);
-	    if (r) goto next;
+            /* NOTE: mailboxes were added in reverse, so the inbox is
+             * done last */
+            r = do_xfer(xfer);
+            if (r) goto next;
 
-	    /* this was a successful user move, and we need to delete
-	       certain user meta-data (but not seen state!) */
-	    syslog(LOG_INFO, "XFER: deleting user metadata");
-	    user_deletedata(xfer->userid, 0);
-	}
+            /* this was a successful user move, and we need to delete
+               certain user meta-data (but not seen state!) */
+            syslog(LOG_INFO, "XFER: deleting user metadata");
+            user_deletedata(xfer->userid, 0);
+        }
 
       next:
-	if (r) {
-	    if (xfer->userid)
-		prot_printf(imapd_out, "* NO USER %s (%s)\r\n",
-			    xfer->userid, error_message(r));
-	    else
-		prot_printf(imapd_out, "* NO MAILBOX \"%s\" (%s)\r\n",
-			    item->extname, error_message(r));
-	} else {
-	    partial_success = 1;
+        if (r) {
+            if (xfer->userid)
+                prot_printf(imapd_out, "* NO USER %s (%s)\r\n",
+                            xfer->userid, error_message(r));
+            else
+                prot_printf(imapd_out, "* NO MAILBOX \"%s\" (%s)\r\n",
+                            item->extname, error_message(r));
+        } else {
+            partial_success = 1;
 
-	    if (xfer->userid)
-		prot_printf(imapd_out, "* OK USER %s\r\n", xfer->userid);
-	    else
-		prot_printf(imapd_out, "* OK MAILBOX \"%s\"\r\n", item->extname);
-	}
-	prot_flush(imapd_out);
+            if (xfer->userid)
+                prot_printf(imapd_out, "* OK USER %s\r\n", xfer->userid);
+            else
+                prot_printf(imapd_out, "* OK MAILBOX \"%s\"\r\n", item->extname);
+        }
+        prot_flush(imapd_out);
 
-	mboxlist_entry_free(&mbentry);
-	next = item->next;
-	free(item);
+        mboxlist_entry_free(&mbentry);
+        next = item->next;
+        free(item);
 
-	if (xfer->userid || mbox_count > 1000) {
-	    /* RESTART after each user or after every 1000 mailboxes */
-	    mbox_count = 0;
+        if (xfer->userid || mbox_count > 1000) {
+            /* RESTART after each user or after every 1000 mailboxes */
+            mbox_count = 0;
 
-	    sync_send_restart(xfer->be->out);
-	    r = sync_parse_response("RESTART", xfer->be->in, NULL);
-	    if (r) goto done;
-	}
+            sync_send_restart(xfer->be->out);
+            r = sync_parse_response("RESTART", xfer->be->in, NULL);
+            if (r) goto done;
+        }
 
-	xfer_cleanup(xfer);
+        xfer_cleanup(xfer);
 
-	if (partial_success) r = 0;
+        if (partial_success) r = 0;
     }
 
 done:
@@ -11574,7 +11574,7 @@ done:
 
     if (r) {
         prot_printf(imapd_out, "%s NO %s\r\n", tag,
-		    error_message(r));
+                    error_message(r));
     } else {
         prot_printf(imapd_out, "%s OK %s\r\n", tag,
                     error_message(IMAP_OK_COMPLETED));
