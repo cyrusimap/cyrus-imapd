fecf201b504b32e182826aadc41a350d698faad4
Robert Stepanek <rsto@paranoia.at>
2016-11-14 11:27:16 +0100

    JMAPMail.pm: add test for getMessageList

--- cassandane/Cassandane/Cyrus/JMAPMail.pm
+++ cassandane/Cassandane/Cyrus/JMAPMail.pm
@@ -1679,4 +1679,264 @@ sub test_setmessages_destroy
     $self->assert_num_equals(0, scalar @{$res->[0][1]->{messageIds}});
 }
 
+sub test_getmessagelist
+    :min_version_3_0
+{
+    my ($self) = @_;
+    my $jmap = $self->{jmap};
+
+    my $store = $self->{store};
+    my $talk = $store->get_client();
+
+    my $res = $jmap->Request([['getMailboxes', { }, "R1"]]);
+    my $inboxid = $res->[0][1]{list}[0]{id};
+
+
+    xlog "create mailboxes";
+    $talk->create("INBOX.A") || die;
+    $talk->create("INBOX.B") || die;
+    $talk->create("INBOX.C") || die;
+    $res = $jmap->Request([['getMailboxes', {}, "R1"]]);
+    my %m = map { $_->{name} => $_ } @{$res->[0][1]{list}};
+    my $mboxa = $m{"A"}->{id};
+    my $mboxb = $m{"B"}->{id};
+    my $mboxc = $m{"C"}->{id};
+    $self->assert_not_null($mboxa);
+    $self->assert_not_null($mboxb);
+    $self->assert_not_null($mboxc);
+
+    xlog "create messages";
+    my %params;
+    $store->set_folder("INBOX.A");
+    my $dtfoo = DateTime->new(
+        year       => 2016,
+        month      => 11,
+        day        => 1,
+        hour       => 7,
+        time_zone  => 'Etc/UTC',
+    );
+    my $bodyfoo = "A rather short message";
+    %params = (
+        date => $dtfoo,
+        body => $bodyfoo,
+    );
+    $res = $self->make_message("foo", %params) || die;
+    $talk->copy($res->{attrs}->{uid}, "INBOX.C");
+
+    $store->set_folder("INBOX.B");
+    my $dtbar = DateTime->new(
+        year       => 2016,
+        month      => 3,
+        day        => 1,
+        hour       => 19,
+        time_zone  => 'Etc/UTC',
+    );
+    my $bodybar = ""
+        . "In the context of electronic mail, messages are viewed as having an\r\n"
+        . "envelope and contents.  The envelope contains whatever information is\r\n"
+        . "needed to accomplish transmission and delivery.  (See [RFC5321] for a\r\n"
+        . "discussion of the envelope.)  The contents comprise the object to be\r\n"
+        . "delivered to the recipient.  This specification applies only to the\r\n"
+        . "format and some of the semantics of message contents.  It contains no\r\n"
+        . "specification of the information in the envelope.i\r\n"
+        . "\r\n"
+        . "However, some message systems may use information from the contents\r\n"
+        . "to create the envelope.  It is intended that this specification\r\n"
+        . "facilitate the acquisition of such information by programs.\r\n"
+        . "\r\n"
+        . "This specification is intended as a definition of what message\r\n"
+        . "content format is to be passed between systems.  Though some message\r\n"
+        . "systems locally store messages in this format (which eliminates the\r\n"
+        . "need for translation between formats) and others use formats that\r\n"
+        . "differ from the one specified in this specification, local storage is\r\n"
+        . "outside of the scope of this specification.\r\n";
+
+    %params = (
+        date => $dtbar,
+        body => $bodybar,
+        extra_headers => [
+            ['X-Tra', "baz"],
+        ],
+    );
+    $self->make_message("bar", %params) || die;
+
+    xlog "run squatter";
+    $self->{instance}->run_command({cyrus => 1}, 'squatter');
+
+    xlog "fetch messages without filter";
+    $res = $jmap->Request([['getMessageList', { fetchMessages => JSON::true }, "R1"]]);
+    $self->assert_num_equals(2, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_num_equals(2, scalar @{$res->[1][1]->{list}});
+
+    %m = map { $_->{subject} => $_ } @{$res->[1][1]{list}};
+    my $foo = $m{"foo"}->{id};
+    my $bar = $m{"bar"}->{id};
+    $self->assert_not_null($foo);
+    $self->assert_not_null($bar);
+
+    xlog "filter text";
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            text => "foo",
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(1, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($foo, $res->[0][1]->{messageIds}[0]);
+
+    xlog "filter NOT text";
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            operator => "NOT",
+            conditions => [ {text => "foo"} ],
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(1, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($bar, $res->[0][1]->{messageIds}[0]);
+
+    xlog "filter mailbox A";
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            inMailboxes => [ $mboxa ],
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(1, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($foo, $res->[0][1]->{messageIds}[0]);
+
+    xlog "filter mailboxes";
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            inMailboxes => [ $mboxa, $mboxc ]
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(1, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($foo, $res->[0][1]->{messageIds}[0]);
+
+    xlog "filter mailboxes";
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            inMailboxes => [ $mboxa, $mboxb, $mboxc ]
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(0, scalar @{$res->[0][1]->{messageIds}});
+
+    xlog "filter not in mailbox A";
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            notInMailboxes => [ $mboxa ],
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(1, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($bar, $res->[0][1]->{messageIds}[0]);
+
+    xlog "filter by before";
+    my $dtbefore = $dtfoo->clone()->subtract(seconds => 1);
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            before => $dtbefore->strftime('%Y-%m-%dT%TZ'),
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(1, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($bar, $res->[0][1]->{messageIds}[0]);
+
+    xlog "filter by after",
+    my $dtafter = $dtbar->clone()->add(seconds => 1);
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            after => $dtafter->strftime('%Y-%m-%dT%TZ'),
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(1, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($foo, $res->[0][1]->{messageIds}[0]);
+
+    xlog "filter by after and before",
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            after => $dtafter->strftime('%Y-%m-%dT%TZ'),
+            before => $dtbefore->strftime('%Y-%m-%dT%TZ'),
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(0, scalar @{$res->[0][1]->{messageIds}});
+
+    xlog "filter by minSize";
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            minSize => length($bodybar),
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(1, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($bar, $res->[0][1]->{messageIds}[0]);
+
+    xlog "filter by maxSize";
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            maxSize => length($bodybar),
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(1, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($foo, $res->[0][1]->{messageIds}[0]);
+
+    xlog "filter by header";
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            header => [ "X-Tra" ],
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(1, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($bar, $res->[0][1]->{messageIds}[0]);
+
+    xlog "filter by header and value";
+    $res = $jmap->Request([['getMessageList', {
+        filter => {
+            header => [ "X-Tra", "bam" ],
+        },
+    }, "R1"]]);
+    $self->assert_num_equals(0, scalar @{$res->[0][1]->{messageIds}});
+
+    xlog "sort by ascending date";
+    $res = $jmap->Request([['getMessageList', {
+        sort => [ "date asc" ],
+    }, "R1"]]);
+    $self->assert_num_equals(2, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($bar, $res->[0][1]->{messageIds}[0]);
+    $self->assert_str_equals($foo, $res->[0][1]->{messageIds}[1]);
+
+    xlog "sort by descending date";
+    $res = $jmap->Request([['getMessageList', {
+        sort => [ "date desc" ],
+    }, "R1"]]);
+    $self->assert_num_equals(2, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($foo, $res->[0][1]->{messageIds}[0]);
+    $self->assert_str_equals($bar, $res->[0][1]->{messageIds}[1]);
+
+    xlog "sort by ascending size";
+    $res = $jmap->Request([['getMessageList', {
+        sort => [ "size asc" ],
+    }, "R1"]]);
+    $self->assert_num_equals(2, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($foo, $res->[0][1]->{messageIds}[0]);
+    $self->assert_str_equals($bar, $res->[0][1]->{messageIds}[1]);
+
+    xlog "sort by descending size";
+    $res = $jmap->Request([['getMessageList', {
+        sort => [ "size desc" ],
+    }, "R1"]]);
+    $self->assert_num_equals(2, scalar @{$res->[0][1]->{messageIds}});
+    $self->assert_str_equals($bar, $res->[0][1]->{messageIds}[0]);
+    $self->assert_str_equals($foo, $res->[0][1]->{messageIds}[1]);
+
+    xlog "sort by ascending id";
+    $res = $jmap->Request([['getMessageList', {
+        sort => [ "id asc" ],
+    }, "R1"]]);
+    my @ids = sort ($foo, $bar);
+    $self->assert_deep_equals(\@ids, $res->[0][1]->{messageIds});
+
+    xlog "sort by descending id";
+    $res = $jmap->Request([['getMessageList', {
+        sort => [ "id desc" ],
+    }, "R1"]]);
+    @ids = reverse sort ($foo, $bar);
+    $self->assert_deep_equals(\@ids, $res->[0][1]->{messageIds});
+}
+
 1;
