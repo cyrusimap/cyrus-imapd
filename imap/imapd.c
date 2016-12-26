90a563f8b9d80bb9ee2c147ff401233a11f70f27
Javier Torres <jtorres@carto.com>
2016-12-26 12:22:11 +0100

    Revert "||, not &&, because only one of either needs to appear"
    This reverts commit 0ca3a923e5f85ed544e8c6b843345b00c0938f84.

--- imap/imapd.c
+++ imap/imapd.c
@@ -8204,7 +8204,7 @@ static int parse_metadata_string_or_list(const char *tag,
 	strarray_append(entries, arg.s);
 
 	// It is a list if there are wildcards
-	if (!strchr(arg.s, '*') || !strchr(arg.s, '%')) {
+	if (!strchr(arg.s, '*') && !strchr(arg.s, '%')) {
 	    // No wildcards; Not a list
 	    *is_list = 0;
 	}
