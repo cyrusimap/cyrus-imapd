7f47564867cc72fe2966f533634b66f549d223ad
Josh Soref <jsoref@users.noreply.github.com>
2017-05-08 02:28:57 +0000

    spelling: humongous

--- imap/backend.c
+++ imap/backend.c
@@ -329,7 +329,7 @@ static int parse_capability(struct backend *s, const char *str)
 
     } else {
         /*
-         * IMAP style: one humungous line with a list of atoms
+         * IMAP style: one humongous line with a list of atoms
          * of the form NAME or NAME=PARAM, preceeded by the atom
          * CAPABILITY, and either surrounded by [] or being an
          * untagged response like "* CAPABILITY ...atoms... CRLF"
