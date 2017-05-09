2d30071b9a1c7c50145120db0ec1330d1a3e2998
ellie timoney <ellie@fastmail.com>
2017-05-09 14:09:49 +1000

    docs: released 2.5.11

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -1181,7 +1181,7 @@ epub_exclude_files = ['search.html']
 # When this is updated, you may also need to update the version and release
 # definitions listed above to stay up to date.
 rst_prolog = """
-.. |imap_last_stable_version| replace:: 2.5.10
+.. |imap_last_stable_version| replace:: 2.5.11
 .. |imap_last_stable_branch| replace:: `cyrus-imapd-2.5`
 .. |imap_current_stable_version| replace:: 3.0.1
 .. |imap_current_stable_branch| replace:: `cyrus-imapd-3.0`
