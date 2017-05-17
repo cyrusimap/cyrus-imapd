34d294d97f1afa107dce9863d7871126324558c8
ellie timoney <ellie@fastmail.com>
2017-05-17 13:08:03 +1000

    prometheus: implement register/unregister and inc/decrement

--- lib/prometheus.c
+++ lib/prometheus.c
@@ -42,7 +42,100 @@
 
 #include <config.h>
 
+#include <sys/types.h>
+
+#include <unistd.h>
+
+#include "lib/assert.h"
+#include "lib/exitcodes.h"
+#include "lib/libconfig.h"
 #include "lib/prometheus.h"
+#include "lib/util.h"
+
+EXPORTED struct prometheus_handle *prometheus_register(void)
+{
+    char fname[PATH_MAX];
+    struct prometheus_handle *handle = NULL;
+    struct prom_stats buf = PROM_STATS_INITIALIZER;
+    int r;
+
+    buf.pid = getpid();
+
+    r = snprintf(fname, sizeof(fname), "%s/stats/%jd", config_dir, (intmax_t) buf.pid);
+    if (r < 0 || (size_t) r >= sizeof(fname))
+        fatal("unable to register stats for prometheus", EC_CONFIG);
+
+    r = cyrus_mkdir(fname, 0755);
+    if (r) return NULL;
+
+    handle = xzmalloc(sizeof(*handle));
+    r = mappedfile_open(&handle->mf, fname, MAPPEDFILE_CREATE | MAPPEDFILE_RW);
+    if (r) goto error;
+
+    r = mappedfile_writelock(handle->mf);
+    if (r) goto error;
+
+    r = mappedfile_pwrite(handle->mf, &buf, sizeof(buf), 0);
+    if (r) goto error;
+
+    r = mappedfile_commit(handle->mf);
+    if (r) goto error;
+
+    r = mappedfile_unlock(handle->mf);
+    if (r) goto error;
+
+    return handle;
+
+error:
+    if (handle) free(handle);
+    return NULL;
+}
+
+EXPORTED void prometheus_unregister(struct prometheus_handle **handlep)
+{
+    struct prometheus_handle *handle = *handlep;
+
+    *handlep = NULL;
+
+    if (!handle) return; /* make double-call safe */
+
+    mappedfile_writelock(handle->mf);
+    unlink(mappedfile_fname(handle->mf));
+    mappedfile_unlock(handle->mf);
+    mappedfile_close(&handle->mf);
+
+    free(handle);
+}
+
+/* do not call this directly! use the prometheus_increment() and
+ * prometheus_decrement() wrapper macros.
+ */
+EXPORTED int prometheus_adjust_at_offset(struct prometheus_handle *handle,
+                                         size_t offset, double delta)
+{
+    int r;
+
+    if (!handle) return 0;
+
+    assert(offset < sizeof(struct prom_stats));
+
+    r = mappedfile_writelock(handle->mf);
+    if (r) return r;
+
+    double v = *(double *)(mappedfile_base(handle->mf) + offset);
+    v += delta;
+
+    r = mappedfile_pwrite(handle->mf, &v, sizeof(v), offset);
+    if (r) return r;
+
+    r = mappedfile_commit(handle->mf);
+    if (r) return r;
+
+    r = mappedfile_unlock(handle->mf);
+    if (r) return r;
+
+    return 0;
+}
 
 EXPORTED int prometheus_text_report(struct buf *buf, const char **mimetype)
 {
