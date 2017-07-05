9908250d2c8bfe1a8efcbf396bce960fb7fa35a5
Partha Susarla <mail@spartha.org>
2017-07-05 16:18:51 +1000

    [lib] RFC5322 date time parser.
    This commit adds support for RFC5322 date time parsing as per this[1].
    
    We should eventually nuke the `time_from_rfc822()` and `time_from_rfc3501()`
    functions.
    
    [1] - https://tools.ietf.org/html/rfc5322#section-3.3

--- lib/times.c
+++ lib/times.c
@@ -51,13 +51,14 @@
 #include "util.h"
 #include "gmtoff.h"
 #include "mkgmtime.h"
+#include "times-private.h"
 
 static const char * const monthname[12] = {
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+    "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
 };
 static const char * const wday[7] = {
-    "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+    "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"
 };
 
 
@@ -74,7 +75,6 @@ static int monthdays(int year/*since 1900*/, int month/*0-based*/)
     return mdays[month] + leapday;
 #undef isleap
 }
-
 
 /* 'buf' must be at least 80 characters */
 EXPORTED int time_to_rfc822(time_t t, char *buf, size_t len)
@@ -854,3 +854,440 @@ baddate:
     return -1;
 }
 
+/**
+ ** Support functions for time_from_rfc5322()
+ **/
+static inline int get_next_char(struct rfc822dtbuf *buf)
+{
+    int c;
+
+    if (buf->offset < buf->len) {
+        buf->offset++;
+        c = buf->str[buf->offset];
+        return c;
+    }
+
+    return EOB;
+}
+
+static inline int get_current_char(struct rfc822dtbuf *buf)
+{
+    int offset = buf->offset;
+
+    if (offset < buf->len)
+        return buf->str[offset];
+    else
+        return EOB;
+}
+
+static inline int get_previous_char(struct rfc822dtbuf *buf)
+{
+    int offset = buf->offset;
+
+    offset--;
+    if (offset >= 0)
+        return buf->str[offset];
+    else
+        return EOB;
+}
+
+/*
+  TODO: Support comments as per RFC.
+*/
+static int skip_ws(struct rfc822dtbuf *buf, int skipcomment)
+{
+    int c = buf->str[buf->offset];
+
+    while (c != EOB) {
+        if (special[c]) {
+            c = get_next_char(buf);
+            continue;
+        }
+
+        break;
+    }
+
+    return 1;
+}
+
+static int get_next_token(struct rfc822dtbuf *buf, char **str, int *len)
+{
+    int c, ret = 1;
+    long ch;
+    static char cache[RFC5322_DATETIME_MAX];
+
+    memset(cache, 1, RFC5322_DATETIME_MAX);
+
+    c = get_current_char(buf);
+    if (c == EOB) {
+        ret = 0;
+        goto failed;
+    }
+
+    *len = 0;
+    for (;;) {
+        if (special[c] || separators[c])
+            break;
+
+        ch = charset[c + 1];
+        if (!(ch & (Alpha | Digit)))
+            break;
+
+        if (*len >= RFC5322_DATETIME_MAX)
+            break;
+
+        cache[*len] = c;
+        *len += 1;
+
+        c = get_next_char(buf);
+        if (c == EOB) {
+            ret = 0;
+            break;
+        }
+    }
+
+ failed:
+    *str = cache;
+
+    return ret;
+}
+
+static inline int to_int(char *str, int len)
+{
+    int i, num = 0;
+
+    for (i = 0; i<len; i++) {
+        if (charset[str[i] + 1] & Digit)
+            num = num * 10 + (str[i] - '0');
+        else {
+            num = -9999;
+            break;
+        }
+    }
+
+    return num;
+}
+
+static inline int to_upper_str_in_place(char **str, int len)
+{
+    int i;
+
+    for (i=0; i<len; i++) {
+        int c = str[0][i];
+        if (charset[c + 1] & LAlpha)
+            str[0][i] = str[0][i] - 32;
+    }
+
+    return 1;
+}
+
+static inline int to_upper(char ch)
+{
+    if (charset[ch + 1] & LAlpha)
+        ch =  ch - 32;
+
+    return ch;
+}
+
+static inline int to_lower(char ch)
+{
+    if (charset[ch + 1] & UAlpha)
+        ch = ch + 32;
+
+    return ch;
+}
+
+static int compute_tzoffset(char *str, int len, int sign)
+{
+    int offset = 0;
+
+    if (len == 1) {         /* Military timezone */
+        int ch;
+        ch = to_upper(str[0]);
+        if (ch < 'J')
+            return (str[0] - 'A' + 1) * 60;
+        if (ch == 'J')
+            return 0;
+        if (ch <= 'M')
+            return (str[0] - 'A') * 60;;
+        if (ch < 'Z')
+            return ('M' - str[0]) * 60;
+
+        return 0;
+    }
+
+    if (len == 2 &&
+        to_upper(str[0]) == 'U' &&
+        to_upper(str[1]) == 'T') {         /* Universal Time zone (UT) */
+        return 0;
+    }
+
+    if (len == 3) {
+        char *p;
+
+        if (to_upper(str[2]) != 'T')
+            return 0;
+
+        p = strchr("AECMPYHB", to_upper(str[0]));
+        if (!p)
+            return 0;
+        offset = (strlen(p) - 12) *  60;
+
+        if (to_upper(str[1]) == 'D')
+            return offset + 60;
+        if (to_upper(str[1]) == 'S')
+            return offset;
+    }
+
+    if (len == 4) {         /* The number timezone offset */
+        int i;
+
+        for (i = 0; i<len; i++) {
+            if (!(charset[str[i] + 1] & Digit))
+                return 0;
+        }
+
+        offset = ((str[0] - '0') * 10 + (str[1] - '0')) * 60 +
+            (str[2] - '0') * 10 +
+            (str[3] - '0');
+
+        return (sign == '+') ? offset : -offset;
+    }
+
+    return 0;
+}
+
+/*
+ *  Date Format as per https://tools.ietf.org/html/rfc5322#section-3.3:
+ *
+ * date-time = [ ([FWS] day-name) "," ]
+ *             ([FWS] 1*2DIGIT FWS)
+ *             month
+ *             (FWS 4*DIGIT FWS)
+ *             2DIGIT ":" 2DIGIT [ ":" 2DIGIT ]
+ *             (FWS ( "+" / "-" ) 4DIGIT)
+ *             [CFWS]
+ *
+ * day-name = "Mon" / "Tue" / "Wed" / "Thu" / "Fri" / "Sat" / "Sun"
+ * month = "Jan" / "Feb" / "Mar" / "Apr" / "May" / "Jun" / "Jul" / "Aug" /
+ *         "Sep" / "Oct" / "Nov" / "Dec"
+ *
+ */
+
+static int tokenise_str_and_create_tm(struct rfc822dtbuf *buf,
+                                      struct tm *tm,
+                                      int *tz_offset)
+{
+    long ch;
+    int c, i, len;
+    char *str_token = NULL;
+
+    /* Skip leading WS, if any */
+    skip_ws(buf, 0);
+
+    c = get_current_char(buf);
+    if (c == EOB)
+        goto failed;
+
+    ch = charset[c + 1];
+    if (ch & Alpha) {       /* Most likely a weekday at the start. */
+        if (!get_next_token(buf, &str_token, &len))
+            goto failed;
+
+        /* We might have a weekday token here, which we should skip*/
+        if (len != 3)
+            goto failed;
+
+        /* The weekday is foll wed by a ',', consume that. */
+        if (get_current_char(buf) == ',')
+            get_next_char(buf);
+        else
+            goto failed;
+
+        skip_ws(buf, 0);
+    }
+
+    /** DATE **/
+    /* date (1 or 2 digits) */
+    if (!get_next_token(buf, &str_token, &len))
+        goto failed;
+
+    if (len < 1 || len > 2 || !(charset[str_token[0] + 1] & Digit))
+        goto failed;
+
+    tm->tm_mday = to_int(str_token, len);
+    if (tm->tm_mday == -9999)
+        goto failed;
+
+    /* month name */
+    get_next_char(buf);     /* Consume a character, either a '-' or ' ' */
+
+    if (!get_next_token(buf, &str_token, &len) ||
+        len != 3 ||
+        !(charset[str_token[0] + 1] & Alpha))
+        goto failed;
+
+    to_upper_str_in_place(&str_token, len);
+    for (i = 0; i < 12; i++) {
+        if (memcmp(monthname[i], str_token, 3) == 0) {
+            tm->tm_mon = i;
+            break;
+        }
+    }
+    if (i == 12)
+        goto failed;
+
+    /* year 2, 4 or >4 digits */
+    get_next_char(buf);     /* Consume a character, either a '-' or ' ' */
+
+    if (!get_next_token(buf, &str_token, &len))
+        goto failed;
+
+    tm->tm_year = to_int(str_token, len);
+    if (tm->tm_year == -9999)
+        goto failed;
+
+    if (len == 2) {
+        /* A 2 digit year */
+        if (tm->tm_year < 70)
+            tm->tm_year += 100;
+    } else {
+        if (tm->tm_year < 1900)
+            goto failed;
+        tm->tm_year -= 1900;
+    }
+
+    /** TIME **/
+    skip_ws(buf, 0);
+    /* hour */
+    if (!get_next_token(buf, &str_token, &len))
+        goto failed;
+
+    if (len < 1 || len > 2 || !(charset[str_token[0] + 1] & Digit))
+        goto failed;
+
+    tm->tm_hour = to_int(str_token, len);
+    if (tm->tm_hour == -9999)
+        goto failed;
+
+    /* minutes */
+    if (get_current_char(buf) == ':' ||
+        get_current_char(buf) == '.')
+        get_next_char(buf); /* Consume ':'/'.' */
+    else
+        goto failed;    /* Something is broken */
+
+    if (!get_next_token(buf, &str_token, &len))
+        goto failed;
+
+    if (len < 1 || len > 2 || !(charset[str_token[0] + 1] & Digit))
+        goto failed;
+
+    tm->tm_min = to_int(str_token, len);
+    if (tm->tm_min == -9999)
+        goto failed;
+
+
+    /* seconds[optional] */
+    if (get_current_char(buf) == ':' ||
+        get_current_char(buf) == '.') {
+        get_next_char(buf); /* Consume ':'/'.' */
+
+        if (!get_next_token(buf, &str_token, &len))
+            goto failed;
+
+        if (len < 1 || len > 2 || !(charset[str_token[0] + 1] & Digit))
+            goto failed;
+
+        tm->tm_sec = to_int(str_token, len);
+        if (tm->tm_sec == -9999)
+            goto failed;
+
+    }
+
+    /* timezone */
+    skip_ws(buf, 0);
+    c = get_current_char(buf); /* the '+' or '-' in the timezone */
+    get_next_char(buf);        /* consume '+' or '-' */
+
+    if (!get_next_token(buf, &str_token, &len)) {
+        *tz_offset = 0;
+    } else {
+        *tz_offset = compute_tzoffset(str_token, len, c);
+    }
+
+    /* dst */
+    tm->tm_isdst = -1;
+    return buf->offset;
+ failed:
+    return -1;
+
+}
+
+
+/*
+ * time_from_rfc5322()
+ * This is meant to be the replacement function for time_from_rfc822() and
+ * time_from_rfc3501() functions.
+ * Returns: Number of characters consumed from @s on success,
+ *          or -1 on error.
+ */
+EXPORTED int time_from_rfc5322(const char *s, time_t *date)
+{
+    struct rfc822dtbuf buf;
+    struct tm tm;
+    time_t tmp_gmtime;
+    int tzone_offset = 0;
+
+    if (!s)
+        goto baddate;
+
+    memset(&tm, 0, sizeof(struct tm));
+    *date = 0;
+
+    buf.str = s;
+    buf.len = strlen(s);
+    buf.offset = 0;
+
+    if (tokenise_str_and_create_tm(&buf, &tm, &tzone_offset) == -1)
+        goto baddate;
+
+    tmp_gmtime = mkgmtime(&tm);
+    if (tmp_gmtime == -1)
+        goto baddate;
+
+    *date = tmp_gmtime - tzone_offset * 60;
+
+    return buf.offset;
+
+ baddate:
+    return -1;
+}
+
+/*
+ * time_to_rfc5322()
+ * Convert a time_t date to an IMAP-style date.
+ * `buf` which is the buffer this function is going to write into, needs to
+ * be atleast RFC5322_DATETIME_MAX (32), if not more.
+ *
+ * This function is exactly like time_to_rfc3501().
+ */
+EXPORTED int time_to_rfc5322(time_t date, char *buf, size_t len)
+{
+    struct tm *tm = localtime(&date);
+    long gmtoff = gmtoff_of(tm, date);
+    int gmtnegative = 0;
+
+    if (gmtoff < 0) {
+        gmtoff = -gmtoff;
+        gmtnegative = 1;
+    }
+
+    gmtoff /= 60;
+
+    return snprintf(buf, len,
+            "%2u-%s-%u %.2u:%.2u:%.2u %c%.2lu%.2lu",
+            tm->tm_mday, monthname[tm->tm_mon], tm->tm_year+1900,
+            tm->tm_hour, tm->tm_min, tm->tm_sec,
+            gmtnegative ? '-' : '+', gmtoff/60, gmtoff%60);
+}
