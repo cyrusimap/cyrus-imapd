cyrusimap/cyrus-imapd

commit 7ae6fee12e09bb171438f3dd7624b1c0b9511e41

http_ischedule.c: consolidate iSchedule-Capabilities calculation into isched_capa_hdr() and also use it for OPTIONS request

imap/http_ischedule.c
@@ -87,6 +87,7 @@ static void isched_init(struct buf *serverinfo);
 static void isched_shutdown(void);
 
 static int meth_get_isched(struct transaction_t *txn, void *params);
+static int meth_options_isched(struct transaction_t *txn, void *params);
 static int meth_post_isched(struct transaction_t *txn, void *params);
 static int dkim_auth(struct transaction_t *txn);
 static int meth_get_domainkey(struct transaction_t *txn, void *params);
@@ -128,7 +129,7 @@ struct namespace_t namespace_ischedule = {
 	{ NULL,			NULL },	/* MKCALENDAR	*/
 	{ NULL,			NULL },	/* MKCOL	*/
 	{ NULL,			NULL },	/* MOVE		*/
-	{ &meth_options,	NULL },	/* OPTIONS	*/
+	{ &meth_options_isched,	NULL },	/* OPTIONS	*/
 	{ &meth_post_isched,	NULL },	/* POST		*/
 	{ NULL,			NULL },	/* PROPFIND	*/
 	{ NULL,			NULL },	/* PROPPATCH	*/
@@ -164,6 +165,16 @@ struct namespace_t namespace_domainkey = {
 };
 
 
+void isched_capa_hdr(struct transaction_t *txn)
+{
+    struct stat sbuf;
+
+    stat(config_filename, &sbuf);
+    txn->resp_body.iserial = MAX(compile_time, sbuf.st_mtime) +
+	rscale_calendars->num_elements;
+}
+
+
 /* iSchedule Receiver Capabilities */
 static int meth_get_isched(struct transaction_t *txn,
 			   void *params __attribute__((unused)))
@@ -182,9 +193,8 @@ static int meth_get_isched(struct transaction_t *txn,
     if (!lastmod) message_guid_set_null(&prev_guid);
 
     /* Fill in iSchedule-Capabilities */
-    stat(config_filename, &sbuf);
-    lastmod = MAX(compile_time, sbuf.st_mtime);
-    txn->resp_body.iserial = lastmod + rscale_calendars->num_elements;
+    isched_capa_hdr(txn);
+    lastmod = txn->resp_body.iserial - rscale_calendars->num_elements;
 
     /* We don't handle GET on a anything other than ?action=capabilities */
     action = hash_lookup("action", &txn->req_qparams);
@@ -356,6 +366,15 @@ static int meth_get_isched(struct transaction_t *txn,
 }
 
 
+static int meth_options_isched(struct transaction_t *txn, void *params)
+{
+    /* Fill in iSchedule-Capabilities */
+    isched_capa_hdr(txn);
+
+    return meth_options(txn, params);
+}
+
+
 /* iSchedule Receiver */
 static int meth_post_isched(struct transaction_t *txn,
 			    void *params __attribute__((unused)))
@@ -368,12 +387,9 @@ static int meth_post_isched(struct transaction_t *txn,
     icalproperty_method meth = 0;
     icalproperty *prop = NULL;
     const char *uid = NULL;
-    struct stat sbuf;
 
     /* Fill in iSchedule-Capabilities */
-    stat(config_filename, &sbuf);
-    txn->resp_body.iserial = MAX(compile_time, sbuf.st_mtime) +
-	rscale_calendars->num_elements;
+    isched_capa_hdr(txn);
 
     /* Response should not be cached */
     txn->flags.cc |= CC_NOCACHE;

commit 3893b0d3ddae05b63fa448e67384a04c60859d75

[ischedule] Use a valid struct stat buf.
In `meth_get_ischedule()` a `struct stat` buffer is used to generate the ETag.
But the stat buffer used is never initialised. This patch fixes that.

Possibly broken because of changes in 7ae6fee12e09bb171438f3dd7624b1c0b9511e41

imap/http_ischedule.c
@@ -175,7 +175,7 @@ struct namespace_t namespace_domainkey = {
 };
 
 
-void isched_capa_hdr(struct transaction_t *txn, time_t *lastmod)
+void isched_capa_hdr(struct transaction_t *txn, time_t *lastmod, struct stat *sb)
 {
     struct stat sbuf;
     time_t mtime;
@@ -185,6 +185,7 @@ void isched_capa_hdr(struct transaction_t *txn, time_t *lastmod)
     txn->resp_body.iserial = mtime +
         (rscale_calendars ? rscale_calendars->num_elements : 0);
     if (lastmod) *lastmod = mtime;
+    if (sb) *sb = sbuf;
 }
 
 
@@ -206,7 +207,7 @@ static int meth_get_isched(struct transaction_t *txn,
     if (!lastmod) message_guid_set_null(&prev_guid);
 
     /* Fill in iSchedule-Capabilities */
-    isched_capa_hdr(txn, &lastmod);
+    isched_capa_hdr(txn, &lastmod, &sbuf);
 
     /* We don't handle GET on a anything other than ?action=capabilities */
     action = hash_lookup("action", &txn->req_qparams);
@@ -383,7 +384,7 @@ static int meth_get_isched(struct transaction_t *txn,
 static int meth_options_isched(struct transaction_t *txn, void *params)
 {
     /* Fill in iSchedule-Capabilities */
-    isched_capa_hdr(txn, NULL);
+    isched_capa_hdr(txn, NULL, NULL);
 
     return meth_options(txn, params);
 }
@@ -403,7 +404,7 @@ static int meth_post_isched(struct transaction_t *txn,
     const char *uid = NULL;
 
     /* Fill in iSchedule-Capabilities */
-    isched_capa_hdr(txn, NULL);
+    isched_capa_hdr(txn, NULL, NULL);
 
     /* Response should not be cached */
     txn->flags.cc |= CC_NOCACHE;