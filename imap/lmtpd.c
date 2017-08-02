c3f14c28cb87dafeac0653083b167563e043e7c8
Bron Gondwana <brong@fastmail.fm>
2017-08-02 15:45:20 +1000

    Revert "idle.c: register as cyrus module and simplify initialization"
    Actually idle needs to be registered by every process, not just those
    that are idling, so that lmtpd will talk to idled and tell it of
    changes

--- imap/lmtpd.c
+++ imap/lmtpd.c
@@ -221,6 +221,9 @@ int service_init(int argc __attribute__((unused)),
                       EC_SOFTWARE);
             }
         }
+
+        /* setup for sending IMAP IDLE notifications */
+        idle_init();
     }
 
     /* Set namespace */
@@ -902,6 +905,8 @@ void shut_down(int code)
     if (!isproxy) {
         if (dupelim)
             duplicate_done();
+
+        idle_done();
     }
 
 #ifdef HAVE_SSL
