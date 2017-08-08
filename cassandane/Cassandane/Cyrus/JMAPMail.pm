1b181ea0adbaec3e1ef3ba1b3f4464951dfa84b5
Bron Gondwana <brong@fastmail.fm>
2017-08-08 16:54:44 +1000

    JMAPMail: require Mail::JMAPTalk 0.08 to get account support for blob uploads

--- cassandane/Cassandane/Cyrus/JMAPMail.pm
+++ cassandane/Cassandane/Cyrus/JMAPMail.pm
@@ -44,7 +44,7 @@ use DateTime;
 use JSON::XS;
 use Net::CalDAVTalk 0.09;
 use Net::CardDAVTalk 0.03;
-use Mail::JMAPTalk 0.06;
+use Mail::JMAPTalk 0.08;
 use Data::Dumper;
 use Storable 'dclone';
 use MIME::Base64 qw(encode_base64);
