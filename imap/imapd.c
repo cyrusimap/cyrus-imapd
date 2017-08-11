6a203665a4c813aca88ba66b0e21f1bfdc870600
Bron Gondwana <brong@fastmail.fm>
2017-08-11 11:58:37 +1000

    imapd: check for isadmin BEFORE parsing sync lines

--- imap/imapd.c
+++ imap/imapd.c
@@ -2007,6 +2007,8 @@ static void cmdloop(void)
                 snmp_increment(SCAN_COUNT, 1);
             }
             else if (!strcmp(cmd.s, "Syncapply")) {
+                if (!imapd_userisadmin) goto badcmd;
+
                 struct dlist *kl = sync_parseline(imapd_in);
 
                 if (kl) {
@@ -2016,6 +2018,8 @@ static void cmdloop(void)
                 else goto extraargs;
             }
             else if (!strcmp(cmd.s, "Syncget")) {
+                if (!imapd_userisadmin) goto badcmd;
+
                 struct dlist *kl = sync_parseline(imapd_in);
 
                 if (kl) {
@@ -2025,6 +2029,8 @@ static void cmdloop(void)
                 else goto extraargs;
             }
             else if (!strcmp(cmd.s, "Syncrestart")) {
+                if (!imapd_userisadmin) goto badcmd;
+
                 if (c == '\r') c = prot_getc(imapd_in);
                 if (c != '\n') goto extraargs;
 
@@ -2032,6 +2038,8 @@ static void cmdloop(void)
                 cmd_syncrestart(tag.s, &reserve_list, 1);
             }
             else if (!strcmp(cmd.s, "Syncrestore")) {
+                if (!imapd_userisadmin) goto badcmd;
+
                 struct dlist *kl = sync_parseline(imapd_in);
 
                 if (kl) {
