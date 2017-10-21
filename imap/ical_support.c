1966c22fc4249a1157a4d4c1224138ce78653514
Ken Murchison <murch@andrew.cmu.edu>
2017-10-21 19:04:08 -0400

    Account for the removal of icaltimetype.is_utc field in upcoming libical

--- imap/ical_support.c
+++ imap/ical_support.c
@@ -833,6 +833,15 @@ icalrecurrenceset_get_utc_timespan(icalcomponent *ical,
     return span;
 }
 
+EXPORTED void icaltime_set_utc(struct icaltimetype *t, int set)
+{
+#ifdef ICALTIME_HAS_IS_UTC
+    t->is_utc = set;
+#else
+    icaltime_set_timezone(t, set ? icaltimezone_get_utc_timezone() : NULL);
+#endif
+}
+
 
 #ifndef HAVE_TZDIST_PROPS
 
