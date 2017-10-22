86e06ac64479afd4efb9f7bc44b2fade6356fedc
Ken Murchison <murch@andrew.cmu.edu>
2017-10-22 18:45:12 -0400

    http_dav.c: don't leak request_target fields when multiget returns something other than 200 for an href

--- imap/http_dav.c
+++ imap/http_dav.c
@@ -7398,7 +7398,7 @@ int report_multiget(struct transaction_t *txn, struct meth_params *rparams,
                     xml_add_response(fctx, HTTP_MOVED, 0, NULL, resultstr);
                 else
                     xml_add_response(fctx, r, 0, resultstr, NULL);
-                continue;
+                goto next;
             }
 
             fctx->req_tgt = &tgt;
@@ -7415,7 +7415,7 @@ int report_multiget(struct transaction_t *txn, struct meth_params *rparams,
                            tgt.mbentry->name, error_message(r));
                     xml_add_response(fctx, HTTP_SERVER_ERROR,
                                      0, error_message(r), NULL);
-                    continue;
+                    goto next;
                 }
 
                 fctx->mailbox = mailbox;
@@ -7424,7 +7424,7 @@ int report_multiget(struct transaction_t *txn, struct meth_params *rparams,
             if (!fctx->mailbox || !tgt.resource) {
                 /* Add response for missing target */
                 xml_add_response(fctx, HTTP_NOT_FOUND, 0, NULL, NULL);
-                continue;
+                goto next;
             }
 
             /* Open the DAV DB corresponding to the mailbox */
@@ -7438,11 +7438,12 @@ int report_multiget(struct transaction_t *txn, struct meth_params *rparams,
 
             fctx->proc_by_resource(fctx, ddata);
 
+            rparams->davdb.close_db(fctx->davdb);
+
+        next:
             /* XXX - split this into a req_tgt cleanup */
             free(tgt.userid);
             mboxlist_entry_free(&tgt.mbentry);
-
-            rparams->davdb.close_db(fctx->davdb);
         }
     }
 
