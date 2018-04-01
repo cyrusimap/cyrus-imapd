1bd6d632eca7c83bfe339cdd01f620210069c616
Bernard Spil <brnrd@FreeBSD.org>
2018-04-01 22:55:49 +0200

    Fix build failure with LibreSSL 2.7
    LibreSSL 2.7 adds OpenSSL 1.1 API leading to conflicts in method names
    
    Patch also applies to 2.4 and 2.5
    
    See also: https://bugs.freebsd.org/227166
    Signed-off-by: Bernard Spil <brnrd@FreeBSD.org>

--- imap/tls.c
+++ imap/tls.c
@@ -225,7 +225,7 @@ static RSA *tmp_rsa_cb(SSL * s __attribute__((unused)),
 }
 #endif
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L)
 /* replacements for new 1.1 API accessors */
 /* XXX probably put these somewhere central */
 static int DH_set0_pqg(DH *dh, BIGNUM *p, BIGNUM *q, BIGNUM *g)
