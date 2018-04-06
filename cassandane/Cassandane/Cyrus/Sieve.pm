b9b826d518ee984c85adf347bab6a21955f1924c
Ken Murchison <murch@fastmail.com>
2018-04-06 12:06:52 -0400

    Sieve.pm: add test to check nesting of Sieve tests and discard functionality

--- cassandane/Cassandane/Cyrus/Sieve.pm
+++ cassandane/Cassandane/Cyrus/Sieve.pm
@@ -1128,4 +1128,30 @@ EOF
     $self->assert_num_equals(1, $imaptalk->get_response_code('exists'));
 }
 
+sub test_nested_tests_and_discard
+{
+    my ($self) = @_;
+
+    xlog "Install a sieve script discarding all mail";
+    $self->{instance}->install_sieve_script(<<EOF
+if anyof (false,
+          allof (not false,
+                 true)
+          ) {
+  discard;
+  stop;
+}
+EOF
+    );
+
+    xlog "Attempt to deliver a message";
+    my $msg1 = $self->{gen}->generate(subject => "Message 1");
+    $self->{instance}->deliver($msg1);
+
+    # should fail to deliver and NOT appear in INBOX
+    my $imaptalk = $self->{store}->get_client();
+    $imaptalk->select("INBOX");
+    $self->assert_num_equals(0, $imaptalk->get_response_code('exists'));
+}
+
 1;
