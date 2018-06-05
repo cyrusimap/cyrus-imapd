7422508344e529c1d663e9a7cb13b74a07364849
Bron Gondwana <brong@fastmail.fm>
2018-06-06 01:18:09 +1000

    jmap_mail: only say canCalculateChanges if not is_mutable

--- imap/jmap_mail.c
+++ imap/jmap_mail.c
@@ -5024,7 +5024,7 @@ static void _email_query(jmap_req_t *req, struct jmap_query *query,
         *err = json_pack("{s:s}", "type", "serverError");
         goto done;
     }
-    query->can_calculate_changes = 1;
+    query->can_calculate_changes = search->is_mutable ? 0 : 1;
 
     // TODO cache emailId -> threadId on the request context
     // TODO support negative positions
