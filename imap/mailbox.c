cyrusimap/cyrus-imapd

commit bbe312e6501f3a8778ab032d1620e7d1764aea7e

mailbox: store one modseq per call to dirty (aka, new modseq per UID changed in bulk changes)
The cost is negligable, because:

a) if we're not in conversations mode it's all NOOPs anyway
b) we already have a lock on the conversations DB, so the value won't change anyway,
   so the total extra cost is one more read of the counters file per mailbox change.
c) we have 64 bits to play with, extra numbers won't hurt any.

But this does guarantee that we can always calculate an ordered set of changes.  Every
single change has its own MODSEQ

imap/mailbox.c
@@ -1058,15 +1058,16 @@ EXPORTED modseq_t mailbox_modseq_dirty(struct mailbox *mailbox)
 {
     assert(mailbox_index_islocked(mailbox, 1));
 
-    if (mailbox->modseq_dirty)
-        return mailbox->i.highestmodseq;
+    if (!mailbox->modseq_dirty) {
+        mailbox->i.highestmodseq = mboxname_setmodseq(mailbox->name,
+                                   mailbox->i.highestmodseq,
+                                   mailbox->mbtype, /*dofolder*/0);
+        mailbox->last_updated = time(0);
+        mailbox->modseq_dirty = 1;
+        mailbox_index_dirty(mailbox);
+    }
 
-    mailbox->i.highestmodseq = mboxname_nextmodseq(mailbox->name,
-                               mailbox->i.highestmodseq,
-                               mailbox->mbtype, /*dofolder*/0);
-    mailbox->last_updated = time(0);
-    mailbox->modseq_dirty = 1;
-    mailbox_index_dirty(mailbox);
+    mailbox->i.highestmodseq++;
 
     return mailbox->i.highestmodseq;
 }
@@ -2487,6 +2488,10 @@ EXPORTED int mailbox_commit(struct mailbox *mailbox)
 
     if (!mailbox->i.dirty)
         return 0;
+
+    mboxname_setmodseq(mailbox->name,
+                       mailbox->i.highestmodseq,
+                       mailbox->mbtype, /*dofolder*/0);
 
     assert(mailbox_index_islocked(mailbox, 1));
 

commit e9eed8c0e8c2d55592a659af259974a45922bdcc

mailbox: bump the counters when we bump the modseq!
This fixes a bug introduced in bbe312e6501f3a8778ab032d1620e7d1764aea7e
where mboxevents would have a counter value which had the earlier
values than the modseq on the actual records.

imap/mailbox.c
@@ -1064,14 +1064,12 @@ EXPORTED modseq_t mailbox_modseq_dirty(struct mailbox *mailbox)
     if (mailbox->silentchanges)
         return mailbox->i.highestmodseq;
 
-    if (!mailbox->modseq_dirty) {
-        mailbox->i.highestmodseq = mboxname_setmodseq(mailbox->name,
+    mailbox->i.highestmodseq = mboxname_nextmodseq(mailbox->name,
                                    mailbox->i.highestmodseq,
                                    mailbox->mbtype, /*dofolder*/0);
-        mailbox->last_updated = time(0);
-        mailbox->modseq_dirty = 1;
-        mailbox_index_dirty(mailbox);
-    }
+    mailbox->last_updated = time(0);
+    mailbox->modseq_dirty = 1;
+    mailbox_index_dirty(mailbox);
 
     mailbox->i.highestmodseq++;
 
@@ -2640,10 +2638,6 @@ EXPORTED int mailbox_commit(struct mailbox *mailbox)
 
     if (!mailbox->i.dirty)
         return 0;
-
-    mboxname_setmodseq(mailbox->name,
-                       mailbox->i.highestmodseq,
-                       mailbox->mbtype, /*dofolder*/0);
 
     assert(mailbox_index_islocked(mailbox, 1));
 