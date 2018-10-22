6d11e8d5c513561080019909fa6001b4bc751f44
Ken Murchison <murch@fastmail.com>
2018-10-22 19:53:48 -0400

    mboxlist.c: change $RACL$ keys to 'R' keys

--- imap/mboxlist.c
+++ imap/mboxlist.c
@@ -89,6 +89,7 @@
 
 #define KEY_TYPE_NAME 'N'
 #define KEY_TYPE_ID   'I'
+#define KEY_TYPE_RACL 'R'
 
 #define DB_DOMAINSEP_STR    "\x1D"  /* group separator (GS) */
 #define DB_DOMAINSEP_CHAR   DB_DOMAINSEP_STR[0]
@@ -777,15 +778,19 @@ HIDDEN int mboxlist_findstage(const char *name, char *stagedir, size_t sd_len)
 static void mboxlist_racl_key(int isuser, const char *keyuser,
                               const char *dbname, struct buf *buf)
 {
-    buf_setcstr(buf, "$RACL$");
-    buf_putc(buf, isuser ? 'U' : 'S');
-    buf_putc(buf, '$');
-    if (keyuser) {
-        buf_appendcstr(buf, keyuser);
+    buf_reset(buf);
+    buf_putc(buf, KEY_TYPE_RACL);
+    if (keyuser || dbname) {
+        buf_putc(buf, isuser ? 'U' : 'S');
         buf_putc(buf, '$');
-    }
-    if (dbname) {
-        buf_appendcstr(buf, dbname);
+
+        if (keyuser) {
+            buf_appendcstr(buf, keyuser);
+            buf_putc(buf, '$');
+        }
+        if (dbname) {
+            buf_appendcstr(buf, dbname);
+        }
     }
 }
 
@@ -931,15 +936,18 @@ static int mboxlist_update_entry(const char *name,
                                  const mbentry_t *mbentry, struct txn **txn)
 {
     char *dbname = mboxname_to_dbname(name);
+    struct buf key = BUF_INITIALIZER;
     mbentry_t *old = NULL;
     int r = 0;
 
     mboxlist_mylookup(dbname, &old, txn, 0, 1); // ignore errors, it will be NULL
 
-    if (!cyrusdb_fetch(mbdb, "$RACL", 5, NULL, NULL, txn)) {
+    mboxlist_racl_key(0, NULL, NULL, &key);
+    if (!cyrusdb_fetch(mbdb, buf_base(&key), buf_len(&key), NULL, NULL, txn)) {
         r = mboxlist_update_racl(dbname, old, mbentry, txn);
         /* XXX return value here is discarded? */
     }
+    buf_free(&key);
 
     r = mboxlist_update_name(dbname, mbentry, txn);
     if (!r) {
@@ -3446,15 +3454,19 @@ static int racls_add_cb(const mbentry_t *mbentry, void *rock)
 
 EXPORTED int mboxlist_set_racls(int enabled)
 {
+    struct buf key = BUF_INITIALIZER;
     struct txn *tid = NULL;
     int r = 0;
+
+    mboxlist_racl_key(0, NULL, NULL, &key);
 
     init_internal();
 
     if (have_racl && !enabled) {
         syslog(LOG_NOTICE, "removing reverse acl support");
         /* remove */
-        r = cyrusdb_foreach(mbdb, "$RACL", 5, NULL, racls_del_cb, &tid, &tid);
+        r = cyrusdb_foreach(mbdb, buf_base(&key), buf_len(&key),
+                            NULL, racls_del_cb, &tid, &tid);
         if (!r) have_racl = 0;
     }
     if (enabled && !have_racl) {
@@ -3467,7 +3479,7 @@ EXPORTED int mboxlist_set_racls(int enabled)
             syslog(LOG_ERR, "ERROR: failed to add reverse acl support %s", error_message(r));
         }
         mboxlist_entry_free(&mbrock.mbentry);
-        if (!r) r = cyrusdb_store(mbdb, "$RACL", 5, "", 0, &tid);
+        if (!r) r = cyrusdb_store(mbdb, buf_base(&key), buf_len(&key), "", 0, &tid);
         if (!r) have_racl = 1;
     }
 
@@ -3476,6 +3488,7 @@ EXPORTED int mboxlist_set_racls(int enabled)
     else
         cyrusdb_commit(mbdb, tid);
 
+    buf_free(&key);
     return r;
 }
 
@@ -3644,7 +3657,9 @@ static int mboxlist_find_category(struct find_rock *rock, const char *prefix, si
 
     init_internal();
 
-    if (!rock->issubs && !rock->isadmin && have_racl) {
+    mboxlist_racl_key(0, NULL, NULL, &key);
+    if (!rock->issubs && !rock->isadmin && have_racl &&
+        !cyrusdb_fetch(rock->db, buf_base(&key), buf_len(&key), NULL, NULL, NULL)) {
         /* we're using reverse ACLs */
         strarray_t matches = STRARRAY_INITIALIZER;
         int i;
