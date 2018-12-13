e02dd05e1620cdbb46b0922ebda4733774bd17d8
Adam Gołębiowski <adamg@pld-linux.org>
2018-12-13 09:51:53 +0100

    imap/cyr_virusscan: support clamav-0.101.0
    Cisco-Talos/clamav-devel@048a88e reworked number of internal structures,
    including way of specifing options in cl_scanfile().
    
    Surround the call to cl_scanfile() with ifdef macro to support
    both pre-0.101.0 as well as 0.101.0+ versions (based on value
    of LIBCLAMAV_MAJORVER)

--- imap/cyr_virusscan.c
+++ imap/cyr_virusscan.c
@@ -193,8 +193,17 @@ int clamav_scanfile(void *state, const char *fname,
     int r;
 
     /* scan file */
+#if LIBCLAMAV_MAJORVER < 9
     r = cl_scanfile(fname, virname, NULL, st->av_engine,
                     CL_SCAN_STDOPT);
+#else
+    static struct cl_scan_options options;
+
+    memset(&options, 0, sizeof(struct cl_scan_options));
+    options.parse |= ~0; /* enable all parsers */
+
+    r = cl_scanfile(fname, virname, NULL, st->av_engine, &options);
+#endif
 
     switch (r) {
     case CL_CLEAN:
