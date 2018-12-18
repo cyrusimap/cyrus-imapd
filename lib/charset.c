f987a1e215754a65fb77baa3129862d215e22ba4
Robert Stepanek <rsto@fastmailteam.com>
2018-12-18 11:23:20 +0100

    charset: reset base64 decoder state for '=' padding character

--- lib/charset.c
+++ lib/charset.c
@@ -247,7 +247,7 @@ static const char index_64[256] = {
     XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
     XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
     XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,62, XX,XX,XX,63,
-    52,53,54,55, 56,57,58,59, 60,61,XX,XX, XX,XX,XX,XX,
+    52,53,54,55, 56,57,58,59, 60,61,XX,XX, XX,64,XX,XX,
     XX, 0, 1, 2,  3, 4, 5, 6,  7, 8, 9,10, 11,12,13,14,
     15,16,17,18, 19,20,21,22, 23,24,25,XX, XX,XX,XX,XX,
     XX,26,27,28, 29,30,31,32, 33,34,35,36, 37,38,39,40,
@@ -430,6 +430,13 @@ static void b64_2byte(struct convert_rock *rock, uint32_t c)
 
     /* could just be whitespace, ignore it */
     if (b == XX) return;
+
+    /* the padding character, reset state */
+    if (b == 64) {
+        s->codepoint = 0;
+        s->bytesleft = 0;
+        return;
+    }
 
     switch (s->bytesleft) {
     case 0:
