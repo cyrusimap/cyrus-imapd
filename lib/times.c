bf3a126d9e0485ed0e49021dc0b1b9d5e04b7d40
Robert Stepanek <rsto@fastmailteam.com>
2019-02-20 09:56:33 +0100

    times: pad RFC3339 years with leading zeros

--- lib/times.c
+++ lib/times.c
@@ -552,7 +552,7 @@ EXPORTED int time_to_rfc3339(time_t t, char *buf, size_t len)
 {
     struct tm *tm = gmtime(&t);
 
-    return snprintf(buf, len, "%4d-%02d-%02dT%02d:%02d:%02dZ",
+    return snprintf(buf, len, "%04d-%02d-%02dT%02d:%02d:%02dZ",
                     tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
                     tm->tm_hour, tm->tm_min, tm->tm_sec);
 }
