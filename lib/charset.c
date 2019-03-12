29218f4758b54bd97d1065b37b2f1ef329d33854
Robert Stepanek <rsto@fastmailteam.com>
2019-03-12 10:05:25 +0100

    charset: don't split multi-octet UTF-8 words in Q-encoded MIME headers (backport)
    Backports
    https://github.com/cyrusimap/cyrus-imapd/commit/3ca31fe0ee1ce3cdf71a062eae39b20b1ecb74d1
    on the 3.0 branch.

--- lib/charset.c
+++ lib/charset.c
@@ -2766,19 +2766,25 @@ static char *qp_encode(const char *data, size_t len, int isheader,
             unsigned char this = data[n];
             unsigned char next = (n < len - 1) ? data[n+1] : '\0';
 
-            if (cnt >= BASE64_MAX_LINE_LEN) {
-                if (isheader) {
-                    /* split encoded token with fold */
-                    buf_appendcstr(&buf, "?=");
-                    buf_appendcstr(&buf, "\r\n ");
-                    buf_appendcstr(&buf, "=?UTF-8?Q?");
+            /* Insert line break before exceeding line length limits */
+            if (isheader) {
+                /* RFC2047 forbids splitting multi-octet characters */
+                int needbytes;
+                if (this < 0x80) needbytes = 0;
+                else if (this < 0xc0) needbytes = 0; // UTF-8 continuation
+                else if (this < 0xe0) needbytes = 3;
+                else if (this < 0xf0) needbytes = 6;
+                else if (this < 0xf8) needbytes = 9;
+                else needbytes = 0; // impossible UTF-8 encoding
+                if (cnt + needbytes >= BASE64_MAX_LINE_LEN) {
+                    buf_appendcstr(&buf, "?=\r\n =?UTF-8?Q?");
                     cnt = 11;
                 }
-                else {
-                    /* add soft line break to body */
-                    buf_appendcstr(&buf, "=\r\n");
-                    cnt = 0;
-                }
+            }
+            else if (cnt >= BASE64_MAX_LINE_LEN) {
+                /* add soft line break to body */
+                buf_appendcstr(&buf, "=\r\n");
+                cnt = 0;
             }
 
             if ((QPSAFECHAR[this]
