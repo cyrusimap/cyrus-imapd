b16a28c212907d3f61f2e61e464681057b2feb16
Ken Murchison <murch@fastmail.com>
2019-04-02 15:57:05 -0400

    Added mboxlist_upgrade() to upgrade mailboxes.db to mailboxes-by-id format

--- imap/ctl_cyrusdb.c
+++ imap/ctl_cyrusdb.c
@@ -177,6 +177,9 @@ static int fixmbox(const mbentry_t *mbentry,
 
 static void process_mboxlist(void)
 {
+    /* upgrade database to new mailboxes-by-id records */
+    mboxlist_upgrade(NULL);
+
     /* build a list of mailboxes - we're using internal names here */
     mboxlist_allmbox(NULL, fixmbox, NULL, MBOXTREE_INTERMEDIATES);
 
