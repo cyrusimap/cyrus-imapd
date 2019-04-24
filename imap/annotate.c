0014659df30bed300a6e6cffd690ce69ec1bf62f
Bron Gondwana <brong@fastmail.fm>
2019-04-24 15:58:27 +1000

    annotate: add dumpentryatt API call for debugging

--- imap/annotate.c
+++ imap/annotate.c
@@ -412,6 +412,32 @@ EXPORTED void setentryatt(struct entryattlist **l, const char *entry,
     }
 }
 
+EXPORTED char *dumpentryatt(const struct entryattlist *l)
+{
+    struct buf buf = BUF_INITIALIZER;
+
+    const struct entryattlist *ee;
+    buf_printf(&buf, "(");
+    const char *sp = "";
+    const struct attvaluelist *av;
+    for (ee = l ; ee ; ee = ee->next) {
+        buf_printf(&buf, "%s%s (", sp, ee->entry);
+        const char *insp = "";
+        for (av = ee->attvalues ; av ; av = av->next) {
+            buf_printf(&buf, "%s%s %s", insp, av->attrib, buf_cstring(&av->value));
+            insp = " ";
+        }
+        buf_printf(&buf, ")");
+        sp = " ";
+    }
+    buf_printf(&buf, ")");
+
+    char *res = buf_release(&buf);
+    buf_free(&buf);
+
+    return res;
+}
+
 EXPORTED void clearentryatt(struct entryattlist **l, const char *entry,
                    const char *attrib)
 {
