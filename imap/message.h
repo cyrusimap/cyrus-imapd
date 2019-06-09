2e4aa80ae73ecb5b1f28b3281da5c68fa06148e2
Дилян Палаузов <git-dpa@aegee.org>
2019-06-09 11:22:05 +0000

    imap/message.h: remove declaration for message_get_header() — there is no such function

--- imap/message.h
+++ imap/message.h
@@ -292,7 +292,6 @@ extern void message_unref(message_t **m);
 
 extern int message_get_field(message_t *m, const char *name,
                              int format, struct buf *buf);
-extern int message_get_header(message_t *m, int format, struct buf *buf);
 extern int message_get_cachebody(message_t *m, const struct body **bodyp);
 extern int message_get_body(message_t *m, struct buf *buf);
 extern int message_get_type(message_t *m, const char **strp);
