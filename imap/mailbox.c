266b5b7d05ff94ad55a3f8accef91d81d752b1f9
Ken Murchison <murch@fastmail.com>
2019-07-01 14:28:18 -0400

    replication: add "silent" flag to mailbox_rename_nocopy

--- imap/mailbox.c
+++ imap/mailbox.c
@@ -6042,6 +6042,9 @@ HIDDEN int mailbox_rename_nocopy(struct mailbox *oldmailbox,
         }
     }
 
+    /* unless on a replica, bump the modseq */
+    if (!silent) mailbox_modseq_dirty(oldmailbox);
+
     return r;
 }
 
