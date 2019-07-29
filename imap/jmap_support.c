02cb328e236168ded53414fe12d68fdcf6b6aced
Ken Murchison <murch@fastmail.com>
2019-07-29 10:13:18 -0400

    jmap_support.c: fix filename in comment

--- imap/jmap_support.c
+++ imap/jmap_support.c
@@ -1,6 +1,6 @@
-/* jmap_mail_submission.c -- Routines for handling JMAP mail submission
+/* jmap_support.c -- Support routines for handling JMAP requests
  *
- * Copyright (c) 1994-2018 Carnegie Mellon University.  All rights reserved.
+ * Copyright (c) 1994-2019 Carnegie Mellon University.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
