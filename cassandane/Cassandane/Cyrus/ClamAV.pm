321fdd367f61d394510cadf49bb27fc4390ce3c7
ellie timoney <ellie@fastmail.com>
2019-09-04 17:26:43 +1000

    ClamAV: test custom notifications

--- cassandane/Cassandane/Cyrus/ClamAV.pm
+++ cassandane/Cassandane/Cyrus/ClamAV.pm
@@ -40,6 +40,7 @@
 package Cassandane::Cyrus::ClamAV;
 use strict;
 use warnings;
+use Cwd qw(abs_path);
 use Data::Dumper;
 
 use lib '.';
@@ -79,11 +80,6 @@ sub set_up
 {
     my ($self) = @_;
     $self->SUPER::set_up();
-
-    # set up a shared folder that's easy to write to
-    my $admintalk = $self->{adminstore}->get_client();
-    $admintalk->create('shared.folder');
-    $admintalk->setacl('shared.folder', 'cassandane' => 'lrswipkxtecd');
 }
 
 sub tear_down
@@ -105,6 +101,11 @@ sub test_remove_infected
     :needs_dependency_clamav
 {
     my ($self) = @_;
+
+    # set up a shared folder that's easy to write to
+    my $admintalk = $self->{adminstore}->get_client();
+    $admintalk->create('shared.folder');
+    $admintalk->setacl('shared.folder', 'cassandane' => 'lrswipkxtecd');
 
     $self->{store}->set_fetch_attributes(qw(uid flags));
 
@@ -222,6 +223,70 @@ sub test_notify_deleted
 
             # make sure the virus was identified correctly
             $self->assert_matches(qr/Eicar-Test-Signature/, $body);
+        }
+    }
+    $self->{store}->read_end();
+
+    # finally, there should've been exactly one notification email sent
+    $self->assert_num_equals(1, $found_notifications);
+}
+
+sub test_custom_notify_deleted
+    :needs_dependency_clamav :NoStartInstances
+{
+    my ($self) = @_;
+
+    # set up a custom notification template
+    $self->{instance}->{config}->set(
+        virusscan_notification_template =>
+            abs_path('data/custom-notification-template'),
+    );
+    $self->_start_instances();
+
+    $self->{store}->set_fetch_attributes(qw(uid flags));
+
+    # put some test messages in INBOX (and verify)
+    $self->{store}->set_folder("INBOX");
+    my %cass_exp;
+    $cass_exp{1} = $self->make_message("eicar attached 1", uid => 1, %eicar_attached);
+    $cass_exp{2} = $self->make_message("eicar attached 2", uid => 2, %eicar_attached);
+    $self->check_messages(\%cass_exp, ( keyed_on => 'uid' ));
+
+    # run cyr_virusscan
+    $self->{instance}->run_command({ cyrus => 1, },
+                                   'cyr_virusscan', '-r', '-n');
+
+    # let's see what's in there now
+    my $found_notifications = 0;
+    $self->{store}->read_begin();
+    while (my $msg = $self->{store}->read_message()) {
+        # should not be any of our test messages remaining
+        $self->assert_null($msg->get_header('x-cassandane-unique'));
+
+        # if we find something that looks like a notification, check it
+		if ($msg->get_header('message-id') =~ m{^<cmu-cyrus-\d+-\d+-\d+\@}) {
+            $found_notifications ++;
+
+            my $body = $msg->get_body();
+#            xlog "body: $body";
+
+            # make sure report body includes all our infected tests
+            foreach my $exp (values %cass_exp) {
+                my $message_id = $exp->get_header('message-id');
+                $self->assert_matches(qr/Message-ID: $message_id/, $body);
+
+                my $subject = $exp->get_header('subject');
+                $self->assert_matches(qr/Subject: $subject/, $body);
+
+                my $uid = $exp->get_attribute('uid');
+                $self->assert_matches(qr/IMAP UID: $uid/, $body);
+            }
+
+            # make sure the virus was identified correctly
+            $self->assert_matches(qr/Eicar-Test-Signature/, $body);
+
+            # make sure our custom notification template was used
+            $self->assert_matches(qr/^custom notification!/, $body);
         }
     }
     $self->{store}->read_end();
