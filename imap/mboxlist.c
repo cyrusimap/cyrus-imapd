66c695a31397c2e46b675fc32efa3ea0b651bf56
Bron Gondwana <brong@fastmail.fm>
2019-10-02 09:47:02 +1000

    mboxlist: old records may not include uniqueids, don't crash when hitting them

--- imap/mboxlist.c
+++ imap/mboxlist.c
@@ -628,7 +628,7 @@ struct _find_uniqueid_data {
 static int _find_uniqueid(const mbentry_t *mbentry, void *rock) {
     struct _find_uniqueid_data *d = (struct _find_uniqueid_data *) rock;
     int r = 0;
-    if (!strcmp(d->uniqueid, mbentry->uniqueid)) {
+    if (!strcmpsafe(d->uniqueid, mbentry->uniqueid)) {
         d->mboxname = xstrdup(mbentry->name);
         r = CYRUSDB_DONE;
     }
