25f18517b4d1cc99ef212d7d10e97932859f6425
ellie timoney <ellie@fastmail.com>
2019-10-14 17:08:16 +1100

    mboxname: describe set of printables not in GOODCHARS

--- imap/mboxname.c
+++ imap/mboxname.c
@@ -1512,6 +1512,9 @@ EXPORTED int mboxname_same_userid(const char *name1, const char *name2)
  * Apply site policy restrictions on mailbox names.
  * Restrictions are hardwired for now.
  * NOTE: '^' is '.' externally in unixhs, and invalid in unixhs
+ *
+ * The set of printable chars that are not in GOODCHARS are:
+ *    !"%&/;<>\`{|}
  */
 #define GOODCHARS " #$'()*+,-.0123456789:=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz~"
 HIDDEN int mboxname_policycheck(const char *name)
