a632ace321785713bc94b8d981560a48c36f55b1
Ken Murchison <murch@fastmail.com>
2019-11-05 12:09:59 -0500

    httpd.c: only allow reuse of auth creds on a persistent connection against a backend server in a Murder

--- imap/httpd.c
+++ imap/httpd.c
@@ -1231,6 +1231,22 @@ static void cmdloop(void)
 	    txn.auth_chal.scheme = NULL;
 	}
 
+	/* Drop auth credentials, if not a backend in a Murder */
+	else if (!config_mupdate_server || !config_getstring(IMAPOPT_PROXYSERVERS)) {
+	    syslog(LOG_DEBUG, "drop auth creds");
+
+	    free(httpd_userid);
+	    httpd_userid = NULL;
+
+	    free(proxy_userid);
+	    proxy_userid = NULL;
+
+	    if (httpd_authstate) {
+		auth_freestate(httpd_authstate);
+		httpd_authstate = NULL;
+	    }
+	}
+
 	/* Perform proxy authorization, if necessary */
 	else if (saslprops.authid &&
 		 (hdr = spool_getheader(txn.req_hdrs, "Authorize-As")) &&
