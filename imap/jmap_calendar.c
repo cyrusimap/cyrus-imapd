aabe1b79c99358f08d2ef7a8361f16a0659ee53b
Robert Stepanek <rsto@paranoia.at>
2019-12-05 11:37:41 +0100

    jmap_calendar: fix typo

--- imap/jmap_calendar.c
+++ imap/jmap_calendar.c
@@ -1278,7 +1278,7 @@ static int jmap_calendar_set(struct jmap_req *req)
             }
         }
 
-        /* The myRights property is server-sete and MUST NOT be set. */
+        /* The myRights property is server-set and MUST NOT be set. */
         pe = jmap_readprop(arg, "myRights", 0,  invalid, "o", &myrights);
         if (pe > 0) {
             json_array_append_new(invalid, json_string("myRights"));
@@ -1477,7 +1477,7 @@ static int jmap_calendar_set(struct jmap_req *req)
 
         jmap_readprop(arg, "scheduleAddressSet", 0,  invalid, "o", &scheduleAddressSet);
 
-        /* The myRights property is server-sete and MUST NOT be set. */
+        /* The myRights property is server-set and MUST NOT be set. */
         pe = jmap_readprop(arg, "myRights", 0,  invalid, "o", &myrights);
         if (pe > 0) {
             json_array_append_new(invalid, json_string("myRights"));
