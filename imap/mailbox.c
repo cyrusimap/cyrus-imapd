90ee8b5fc08866fe3e59b2eb0f33314371ebdb33
Robert Stepanek <rsto@paranoia.at>
2019-12-19 16:01:15 +0100

    mailbox: use guid as DAV attachment filename if not set by user

--- imap/mailbox.c
+++ imap/mailbox.c
@@ -3528,6 +3528,7 @@ static int mailbox_update_webdav(struct mailbox *mailbox,
             resource = param->value;
         }
     }
+    if (!resource) resource = message_guid_encode(&new->guid);
 
     webdavdb = mailbox_open_webdav(mailbox);
 
