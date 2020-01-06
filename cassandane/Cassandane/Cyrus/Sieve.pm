1b3502f7d17a9a56e116dab3ddaf55bab87cd1e7
ellie timoney <ellie@fastmail.com>
2020-01-06 14:47:20 +1100

    Sieve: test vacation response to msg with unfoldable subject

--- cassandane/Cassandane/Cyrus/Sieve.pm
+++ cassandane/Cassandane/Cyrus/Sieve.pm
@@ -2126,6 +2126,85 @@ EOF
     $self->assert_matches(qr/\r\n\r\nI am out of the office today./, $msg2);
 }
 
+sub test_vacation_with_nonfoldable_origsubject
+    :min_version_3_5
+    :needs_component_sieve
+    :NoAltNameSpace
+{
+    my ($self) = @_;
+
+    my $target = 'INBOX.Sent';
+
+    xlog $self, "Install a sieve script with vacation action that uses :fcc";
+    $self->{instance}->install_sieve_script(<<"EOF"
+require ["vacation", "fcc"];
+
+vacation :fcc "$target" :days 1 :addresses ["cassandane\@example.com"] text:
+I am out of the office today. I will answer your email as soon as I can.
+.
+;
+EOF
+    );
+
+    xlog $self, "Create the target folder";
+    my $talk = $self->{store}->get_client();
+    $talk->create($target, "(USE (\\Sent))");
+
+    xlog $self, "Deliver a message";
+    # runs more than 75c without whitespace!
+    my $subject = "volutpatdiamutvenenatistellusinmetus"
+                . "vulputateeuscelerisquefelisimperdietproin"
+                . "fermentumleovelorciportanonpulvinarneque"
+                . "laoreetsuspendisseinterdumconsectetur";
+
+    my $msg1 = $self->{gen}->generate(
+        subject => $subject,
+        to => Cassandane::Address->new(localpart => 'cassandane',
+                                       domain => 'example.com'));
+    $self->{instance}->deliver($msg1);
+
+    xlog $self, "Check that a copy of the auto-reply message made it";
+    $talk->select($target);
+    $self->assert_num_equals(1, $talk->get_response_code('exists'));
+
+    xlog $self, "Check that the message is an auto-reply";
+    my $res = $talk->fetch(1, 'rfc822');
+    my $msg2 = $res->{1}->{rfc822};
+
+    # check we folded a reasonable number of times
+    my $actual_subject;
+    if ($msg2 =~ m/^(Subject:.*?\r\n)(?!\s)/ms) {
+        $actual_subject = $1;
+    }
+    $self->assert_matches(qr/^Subject:/, $actual_subject);
+    my $fold_count = () = $actual_subject =~ m/\r\n /g;
+    xlog "fold count: $fold_count";
+    $self->assert_num_gte(2, $fold_count);
+    $self->assert_num_lte(4, $fold_count);
+
+    # subject should be the original subject plus "Auto: " and CRLF
+    if (version->parse($Encode::MIME::Header::VERSION)
+        < version->parse("2.28")) {
+        # XXX Work around a bug in older Encode::MIME::Header
+        # XXX (https://rt.cpan.org/Public/Bug/Display.html?id=42902)
+        # XXX that loses the space between 'Subject:' and 'Auto:',
+        # XXX by allowing it to be optional
+        my $subjpat = "Auto: " . decode("MIME-Header", $subject) . "\r\n";
+        my $subjre = qr/Subject:\s?$subjpat/;
+        $self->assert_matches($subjre, decode("MIME-Header", $actual_subject));
+    }
+    else {
+        my $subjpat = "Subject: Auto: "
+                    . decode("MIME-Header", $subject) . "\r\n";
+        $self->assert_str_equals($subjpat,
+                                 decode("MIME-Header", $actual_subject));
+    }
+
+    # check for auto-submitted header
+    $self->assert_matches(qr/Auto-Submitted: auto-replied \(vacation\)\r\n/, $msg2);
+    $self->assert_matches(qr/\r\n\r\nI am out of the office today./, $msg2);
+}
+
 sub test_github_issue_complex_variables
     :min_version_3_1
     :needs_component_sieve
