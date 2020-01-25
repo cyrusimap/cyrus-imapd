6fb2ebc881fb1ad8a4fce8e358797bd4c3c85e65
Ken Murchison <murch@fastmail.com>
2020-01-24 20:12:30 -0500

    Sieve.pm: augmented editheader test with a fileinto :copy and a deleteheader :regex

--- cassandane/Cassandane/Cyrus/Sieve.pm
+++ cassandane/Cassandane/Cyrus/Sieve.pm
@@ -1464,9 +1464,12 @@ sub test_editheader
 {
     my ($self) = @_;
 
+    my $target = "INBOX.target";
+
     xlog $self, "Install a sieve script with editheader actions";
     $self->{instance}->install_sieve_script(<<EOF
-require ["editheader", "index"];
+require ["editheader", "index", "regex", "fileinto", "copy"];
+fileinto :copy "$target";
 addheader "X-Cassandane-Test" "prepend1";
 addheader "X-Cassandane-Test2" "prepend2";
 addheader "X-Cassandane-Test2" "prepend3";
@@ -1474,24 +1477,40 @@ addheader :last "X-Cassandane-Test" "append1";
 addheader :last "X-Cassandane-Test" "append2";
 addheader :last "X-Cassandane-Test" "append3";
 addheader :last "X-Cassandane-Test" "append4";
+addheader :last "X-Cassandane-Test" "append5";
+addheader :last "X-Cassandane-Test" "append6";
 deleteheader :index 3 :contains "X-Cassandane-Test" "append";
+deleteheader :regex "X-Cassandane-Test" "append4";
 deleteheader :index 1 :last "X-Cassandane-Test";
 deleteheader "X-Cassandane-Test2";
 EOF
     );
 
+    xlog $self, "Create the target folder";
+    my $imaptalk = $self->{store}->get_client();
+
+    $imaptalk->create($target)
+         or die "Cannot create $target: $@";
+
     xlog $self, "Deliver a message";
     my $msg1 = $self->{gen}->generate(subject => "Message 1");
     $self->{instance}->deliver($msg1);
 
-    my $imaptalk = $self->{store}->get_client();
     $imaptalk->select("INBOX");
     my $res = $imaptalk->fetch(1, 'rfc822');
 
     $msg1 = $res->{1}->{rfc822};
 
     $self->assert_matches(qr/^X-Cassandane-Test: prepend1/, $msg1);
-    $self->assert_matches(qr/X-Cassandane-Test: append3\r\n\r\n/, $msg1);
+    $self->assert_matches(qr/X-Cassandane-Test: append1\r\nX-Cassandane-Test: append3\r\nX-Cassandane-Test: append5\r\n\r\n/, $msg1);
+
+    $imaptalk->select($target);
+    $res = $imaptalk->fetch(1, 'rfc822');
+
+    $msg1 = $res->{1}->{rfc822};
+
+    $self->assert_matches(qr/^Return-Path: /, $msg1);
+    $self->assert_matches(qr/X-Cassandane-Unique: .*\r\n\r\n/, $msg1);
 }
 
 sub test_duplicate
