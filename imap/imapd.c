44899799142a2b03fdbf36341324da764422592c
Ken Murchison <murch@fastmail.com>
2020-03-27 12:27:18 -0400

    Don't need improved_mboxlist_sort anymore since we changed the internal hierarchy separator

--- imap/imapd.c
+++ imap/imapd.c
@@ -13462,7 +13462,7 @@ static int list_entry_comparator(const void *p1, const void *p2) {
     const struct list_entry *e1 = (struct list_entry *)p1;
     const struct list_entry *e2 = (struct list_entry *)p2;
 
-    return bsearch_compare_mbox(e1->extname, e2->extname);
+    return strcmp(e1->extname, e2->extname);
 }
 
 static void free_list_entry(void *rock)
