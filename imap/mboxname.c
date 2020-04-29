3a240bb4183f6cb003471bebcd68a18647f86ed0
Robert Stepanek <rsto@paranoia.at>
2020-04-29 16:11:08 +0200

    mboxname: add mboxname_isjmapuploadmailbox function

--- imap/mboxname.c
+++ imap/mboxname.c
@@ -1489,6 +1489,25 @@ EXPORTED int mboxname_ispushsubscriptionmailbox(const char *name, int mbtype)
     return res;
 }
 
+/*
+ * If (internal) mailbox 'name' is a user's #jmap upload mailbox
+ * returns boolean
+ */
+EXPORTED int mboxname_isjmapuploadmailbox(const char *name, int mbtype __attribute__((unused)))
+{
+    int res = 0;
+
+    mbname_t *mbname = mbname_from_intname(name);
+    const strarray_t *boxes = mbname_boxes(mbname);
+    const char *prefix = config_getstring(IMAPOPT_JMAPUPLOADFOLDER);
+
+    if (strarray_size(boxes) && !strcmpsafe(prefix, strarray_nth(boxes, 0)))
+        res = 1;
+
+    mbname_free(&mbname);
+    return res;
+}
+
 EXPORTED char *mboxname_user_mbox(const char *userid, const char *subfolder)
 {
     if (!userid) return NULL;
