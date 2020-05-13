7936ff6ef5b22c4dd8e99c9e89faf750477beb4f
Ken Murchison <murch@fastmail.com>
2020-05-13 07:22:47 -0400

    Revert "jmap_backup.c: when recreating a mailbox use the uniqueid of the deleted one"
    This reverts commit cbe2731b5a89e2c5491bfe190c1763ec2efdab69.

--- imap/jmap_backup.c
+++ imap/jmap_backup.c
@@ -1572,26 +1572,16 @@ static void restore_mailbox_cb(const char *mboxname, void *data, void *rock)
                 }
             }
             else {
-                /* Create the mailbox with same ID as the deleted one */
+                /* Create the mailbox */
                 struct mboxlock *namespacelock =
                     user_namespacelock(req->accountid);
-                mbentry_t *mbentry = NULL;
 
-                r = mboxlist_lookup_allow_all(mboxname, &mbentry, NULL);
-
-                if (!r) {
-                    r = mboxlist_createmailbox_unq(newmboxname, MBTYPE_EMAIL,
-                                                   /*partition*/NULL,
-                                                   /*isadmin*/0,
-                                                   req->accountid,
-                                                   req->authstate,
-                                                   /*localonly*/0,
-                                                   /*forceuser*/0,
-                                                   /*dbonly*/0, /*notify*/0,
-                                                   mbentry->uniqueid,
-                                                   &newmailbox);
-                }
-                mboxlist_entry_free(&mbentry);
+                r = mboxlist_createmailbox(newmboxname, MBTYPE_EMAIL,
+                                           /*partition*/NULL, /*isadmin*/0,
+                                           req->accountid, req->authstate,
+                                           /*localonly*/0, /*forceuser*/0,
+                                           /*dbonly*/0, /*notify*/0,
+                                           &newmailbox);
                 mboxname_release(&namespacelock);
 
                 if (r) {
