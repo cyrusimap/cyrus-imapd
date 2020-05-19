f7e9d1e507725bb925bf9fa563e32df646b9aa22
Robert Stepanek <rsto@paranoia.at>
2020-05-19 11:26:02 +0200

    matchmime: index mail into temporary directory instead in-memory

--- imap/xapian_wrap.h
+++ imap/xapian_wrap.h
@@ -62,7 +62,6 @@ extern void xapian_check_if_needs_reindex(const strarray_t *sources, strarray_t 
 #define XAPIAN_DBW_CONVINDEXED 0
 #define XAPIAN_DBW_XAPINDEXED 1
 extern int xapian_dbw_open(const char **paths, xapian_dbw_t **dbwp, int mode);
-extern int xapian_dbw_openmem(xapian_dbw_t **dbwp);
 extern void xapian_dbw_close(xapian_dbw_t *dbw);
 extern int xapian_dbw_begin_txn(xapian_dbw_t *dbw);
 extern int xapian_dbw_commit_txn(xapian_dbw_t *dbw);
