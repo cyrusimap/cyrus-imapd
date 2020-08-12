b6a68e9003a845251a3b67ced8900905f90b083b
ellie timoney <ellie@fastmail.com>
2020-08-12 13:30:21 +1000

    imapd: fix bug in 2.3.x version detection

--- imap/imapd.c
+++ imap/imapd.c
@@ -8492,6 +8492,7 @@ static int backend_version(struct backend *be)
 
     minor = strstr(be->banner, "v2.3.");
     if (!minor) return 6;
+    minor += strlen("v2.3.");
 
     /* at least version 2.3.10 */
     if (minor[1] != ' ') {
