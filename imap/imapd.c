70ff67dee5cf4c848ec7c5c860635f7dba56d0c3
ellie timoney <ellie@fastmail.com>
2020-08-12 13:35:25 +1000

    imapd: syslog when downgrading index to v6
    due to unrecognised backend version

--- imap/imapd.c
+++ imap/imapd.c
@@ -10171,7 +10171,7 @@ static int backend_version(struct backend *be)
     }
 
     minor = strstr(be->banner, "v2.3.");
-    if (!minor) return 6;
+    if (!minor) goto unrecognised;
     minor += strlen("v2.3.");
 
     /* at least version 2.3.10 */
@@ -10200,7 +10200,11 @@ static int backend_version(struct backend *be)
 	break;
     }
 
+unrecognised:
     /* fallthrough, shouldn't happen */
+    syslog(LOG_WARNING, "%s: did not recognise remote Cyrus version from "
+                        "banner \"%s\".  Assuming index version 6!",
+                        __func__, be->banner);
     return 6;
 }
 
