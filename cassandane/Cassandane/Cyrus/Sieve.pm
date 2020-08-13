5beb080d59ac33eed248b185379a0383452d24ef
Ken Murchison <murch@fastmail.com>
2020-08-13 11:30:23 -0400

    Sieve.pm: added test_variable_modifiers

--- cassandane/Cassandane/Cyrus/Sieve.pm
+++ cassandane/Cassandane/Cyrus/Sieve.pm
@@ -3295,4 +3295,59 @@ EOF
     $self->assert_not_null($updatecal);
 }
 
+sub test_variable_modifiers
+    :needs_component_sieve
+{
+    my ($self) = @_;
+
+    $self->{instance}->install_sieve_script(<<'EOF'
+require ["variables", "editheader", "regex", "enotify"];
+
+set                    "a" "juMBlEd?lETteRS.*";  #  => "juMBlEd?lETteRS.*"
+set :length            "b" "${a}";               #  => "17"
+set :lower             "c" "${a}";               #  => "jumbled?letters.*"
+set :upper             "d" "${a}";               #  => "JUMBLED?LETTERS.*"
+set :lowerfirst        "e" "${a}";               #  => "juMBlEd?lETteRS.*"
+set :lowerfirst :upper "f" "${a}";               #  => "jUMBLED?LETTERS.*"
+set :upperfirst        "g" "${a}";               #  => "JuMBlEd?lETteRS.*"
+set :upperfirst :lower "h" "${a}";               #  => "Jumbled?letters.*"
+set :quotewildcard     "i" "${a}";               #  => "juMBlEd\?lETteRS.\*"
+set :quoteregex        "j" "${a}";               #  => "juMBlEd\?lETteRS\.\*"
+set :encodeurl         "k" "${a}";               #  => "juMBlEd%26lETteRS.%2A"
+
+addheader "X-Cassandane-Test" "len = \"${b}\"";
+addheader "X-Cassandane-Test" "lower = \"${c}\"";
+addheader "X-Cassandane-Test" "upper = \"${d}\"";
+addheader "X-Cassandane-Test" "lowerfirst = \"${e}\"";
+addheader "X-Cassandane-Test" "lowerfirst+upper = \"${f}\"";
+addheader "X-Cassandane-Test" "upperfirst = \"${g}\"";
+addheader "X-Cassandane-Test" "upperfirst+lower = \"${h}\"";
+addheader "X-Cassandane-Test" "wild = \"${i}\"";
+addheader "X-Cassandane-Test" "regex = \"${j}\"";
+addheader "X-Cassandane-Test" "url = \"${k}\"";
+EOF
+        );
+
+    xlog $self, "Deliver a message";
+    my $msg1 = $self->{gen}->generate(subject => "Message 1");
+    $self->{instance}->deliver($msg1);
+
+    my $imaptalk = $self->{store}->get_client();
+    $imaptalk->select("INBOX");
+    my $res = $imaptalk->fetch(1, 'rfc822');
+
+    $msg1 = $res->{1}->{rfc822};
+
+    $self->assert_matches(qr/X-Cassandane-Test: len = "17"\r\n/, $msg1);
+    $self->assert_matches(qr/X-Cassandane-Test: lower = "jumbled\?letters\.\*"\r\n/, $msg1);
+    $self->assert_matches(qr/X-Cassandane-Test: upper = "JUMBLED\?LETTERS\.\*"\r\n/, $msg1);
+    $self->assert_matches(qr/X-Cassandane-Test: lowerfirst = "juMBlEd\?lETteRS\.\*"\r\n/, $msg1);
+    $self->assert_matches(qr/X-Cassandane-Test: lowerfirst\+upper = "jUMBLED\?LETTERS\.\*"\r\n/, $msg1);
+    $self->assert_matches(qr/X-Cassandane-Test: upperfirst = "JuMBlEd\?lETteRS\.\*"\r\n/, $msg1);
+    $self->assert_matches(qr/X-Cassandane-Test: upperfirst\+lower = "Jumbled\?letters\.\*"\r\n/, $msg1);
+    $self->assert_matches(qr/X-Cassandane-Test: wild = "juMBlEd\\\?lETteRS\.\\\*"\r\n/, $msg1);
+    $self->assert_matches(qr/X-Cassandane-Test: regex = "juMBlEd\\\?lETteRS\\\.\\\*"\r\n/, $msg1);
+    $self->assert_matches(qr/X-Cassandane-Test: url = "juMBlEd%3FlETteRS\.%2A"\r\n/, $msg1);
+}
+
 1;
