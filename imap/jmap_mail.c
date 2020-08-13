e302c2cff6286b22b0dc10efb4aed01b48b18ee5
Ken Murchison <murch@fastmail.com>
2020-08-13 15:26:11 -0400

    jmap_mail.c: inMailbox* query should only check "real" mailboxes and NOT intermediates

--- imap/jmap_mail.c
+++ imap/jmap_mail.c
@@ -1822,6 +1822,7 @@ static search_expr_t *_email_buildsearchexpr(jmap_req_t *req, json_t *filter,
             const char *mboxid = json_string_value(val);
             const mbentry_t *mbentry = jmap_mbentry_by_uniqueid(req, mboxid);
             if (mbentry && mbtype_isa(mbentry->mbtype) == MBTYPE_EMAIL &&
+                !(mbentry->mbtype & MBTYPE_INTERMEDIATE) &&
                     jmap_hasrights_mbentry(req, mbentry, JACL_LOOKUP)) {
                 strarray_append(folders, mbentry->name);
             }
@@ -1845,6 +1846,7 @@ static search_expr_t *_email_buildsearchexpr(jmap_req_t *req, json_t *filter,
                 const char *mboxid = json_string_value(jmboxid);
                 const mbentry_t *mbentry = jmap_mbentry_by_uniqueid(req, mboxid);
                 if (mbentry && mbtype_isa(mbentry->mbtype) == MBTYPE_EMAIL &&
+                    !(mbentry->mbtype & MBTYPE_INTERMEDIATE) &&
                         jmap_hasrights_mbentry(req, mbentry, JACL_LOOKUP)) {
                     strarray_append(folders, mbentry->name);
                 }
