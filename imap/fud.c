eb795584e9e1d5ffd15ae1f3a5b0088dc48b9955
OBATA Akio <obache@users.noreply.github.com>
2020-08-21 18:04:32 +0900

    Fix to declare missing MAXLOGNAME properly
    Use LOGIN_NAME_MAX if it is available for the condition.

--- imap/fud.c
+++ imap/fud.c
@@ -60,6 +60,7 @@
 #include <netdb.h>
 #include <errno.h>
 #include <pwd.h>
+#include <limits.h>
 
 #include "acl.h"
 #include "mboxlist.h"
@@ -99,7 +100,11 @@ static void send_reply(struct sockaddr *sfrom, socklen_t sfromsiz, int status,
 static int soc = 0; /* inetd (master) has handed us the port as stdin */
 
 #ifndef MAXLOGNAME
+#  ifdef LOGIN_NAME_MAX
+#define MAXLOGNAME (LOGIN_NAME_MAX - 1)
+#  else
 #define MAXLOGNAME 16           /* should find out for real */
+#  endif
 #endif
 #ifndef MAXDOMNAME
 #define MAXDOMNAME 20           /* should find out for real */
