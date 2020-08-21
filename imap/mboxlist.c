493069fed7e019939f712002832e7c01c88701a5
Ken Murchison <murch@fastmail.com>
2020-08-21 14:35:17 -0400

    Revert "mboxlist.c: flag all new mailboxes withthe legacy flag (mostly for Cassandane testing)"
    This reverts commit e70c73c6623c6ba61dee4ca3d3e32c528458fd6a.

--- imap/mboxlist.c
+++ imap/mboxlist.c
@@ -1670,7 +1670,7 @@ static int mboxlist_createmailbox_full(const char *mboxname, int mbtype,
     /* all is well - activate the mailbox */
     newmbentry = mboxlist_entry_create();
     newmbentry->acl = xstrdupnull(acl);
-    newmbentry->mbtype = mbtype | MBTYPE_LEGACY_DIRS;
+    newmbentry->mbtype = mbtype;
     newmbentry->partition = xstrdupnull(newpartition);
     if (newmailbox) {
         newmbentry->uniqueid = xstrdupnull(newmailbox->uniqueid);
