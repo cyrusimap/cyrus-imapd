4911f33274f2760163a4c67f9b0967be336757b7
Robert Stepanek <rsto@paranoia.at>
2020-09-07 17:50:30 +0200

    httpd: log auth headers without credentials as raw values

--- imap/httpd.c
+++ imap/httpd.c
@@ -3743,7 +3743,7 @@ HIDDEN void log_cachehdr(const char *name, const char *contents,
     /* Ignore private headers in our cache */
     if (name[0] == ':') return;
 
-    if (!strcasecmp(name, "authorization")) {
+    if (!strcasecmp(name, "authorization") && strchr(contents, ' ')) {
         /* Replace authorization credentials with an ellipsis */
         const char *creds = strchr(contents, ' ') + 1;
         buf_printf(buf, "%c%s: %.*s%-*s\r\n", toupper(name[0]), name+1,
