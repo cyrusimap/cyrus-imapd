1be6fbf97e73569a1200390c3c21b8b812591fa9
Ken Murchison <murch@fastmail.com>
2020-12-01 15:04:24 -0500

    sync_support.c: rewritten to use #sieve mailbox

--- imap/imapd.c
+++ imap/imapd.c
@@ -11765,7 +11765,6 @@ static int xfer_finalsync(struct xfer_header *xfer)
     if (!r && xfer->userid) {
         r = sync_do_user_seen(&xfer->sync_cs, xfer->userid, replica_seen);
         if (!r) r = sync_do_user_sub(&xfer->sync_cs, xfer->userid, replica_subs);
-        if (!r) r = sync_do_user_sieve(&xfer->sync_cs, xfer->userid, replica_sieve);
     }
 
   done:
