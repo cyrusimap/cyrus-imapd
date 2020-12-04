de5872e5f7a166d0f472382bfb225acf104d8f91
Robert Stepanek <rsto@fastmailteam.com>
2020-12-04 14:59:58 +0100

    seen_db: return IMAP_NOTFOUND if no seen database exists

--- imap/seen_db.c
+++ imap/seen_db.c
@@ -123,7 +123,7 @@ EXPORTED int seen_open(const char *user,
             syslog(level, "DBERROR: opening %s: %s", fname,
                    cyrusdb_strerror(r));
         }
-        r = IMAP_IOERROR;
+        r = r == CYRUSDB_NOTFOUND ? IMAP_NOTFOUND : IMAP_IOERROR;
         free(seendb);
         free(fname);
         return r;
