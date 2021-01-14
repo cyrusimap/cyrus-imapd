01b4863c6b53912e5b7dd348f2e003ad5ad53e8b
ellie timoney <ellie@fastmail.com>
2021-01-15 09:55:12 +1100

    JMAPEmail: fix up another min_version

--- cassandane/Cassandane/Cyrus/JMAPEmail.pm
+++ cassandane/Cassandane/Cyrus/JMAPEmail.pm
@@ -20214,7 +20214,7 @@ EOF
 }
 
 sub test_email_query_mailbox_andor
-    :min_version_3_1 :needs_component_jmap :JMAPExtensions
+    :min_version_3_5 :needs_component_jmap :JMAPExtensions
 {
     my ($self) = @_;
     my $jmap = $self->{jmap};
