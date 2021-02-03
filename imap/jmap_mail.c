ace7c7d515f8bc9fa61499160cac113875a9bdf2
Robert Stepanek <rsto@fastmailteam.com>
2021-02-03 10:45:51 +0100

    jmap_mail: add JMAP error description for slow searches

--- imap/jmap_mail.c
+++ imap/jmap_mail.c
@@ -3941,8 +3941,11 @@ static int _email_query_uidsearch(jmap_req_t *req,
     if (r) {
         switch (r) {
             case IMAP_SEARCH_NOT_SUPPORTED:
-            case IMAP_SEARCH_SLOW:
                 *err = json_pack("{s:s}", "type", "unsupportedFilter");
+                break;
+            case IMAP_SEARCH_SLOW:
+                *err = json_pack("{s:s s:s}", "type", "unsupportedFilter",
+                        "description", "search too slow");
                 break;
             default:
                 *err = jmap_server_error(r);
@@ -4226,7 +4229,8 @@ static void _email_query(jmap_req_t *req, struct jmap_emailquery *q,
 done:
     if (r && *err == NULL) {
         if (r == IMAP_SEARCH_SLOW) {
-            *err = json_pack("{s:s}", "type", "unsupportedFilter");
+            *err = json_pack("{s:s s:s}", "type", "unsupportedFilter",
+                    "description", "search too slow");
         }
         else *err = jmap_server_error(r);
     }
