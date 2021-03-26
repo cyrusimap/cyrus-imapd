4e077d4c92a14edf0c681d9ee6b6ad2b9280f261
ellie timoney <ellie@fastmail.com>
2021-03-26 11:03:23 +1100

    JMAPEmail: fix backported to 3.4

--- cassandane/Cassandane/Cyrus/JMAPEmail.pm
+++ cassandane/Cassandane/Cyrus/JMAPEmail.pm
@@ -20816,7 +20816,7 @@ sub test_email_set_update_no_id
 }
 
 sub test_email_query_guidsearch_mixedfilter2
-    :min_version_3_5 :needs_component_jmap :JMAPExtensions
+    :min_version_3_4 :needs_component_jmap :JMAPExtensions
 {
     my ($self) = @_;
     my $jmap = $self->{jmap};
