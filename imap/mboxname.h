da0dce3321163faa7cb655ec0e095c466a6864e1
Ken Murchison <murch@fastmail.com>
2021-05-07 13:10:46 -0400

    mboxname.c: add mbname_from_extnameUTF8()

--- imap/mboxname.h
+++ imap/mboxname.h
@@ -106,6 +106,7 @@ mbname_t *mbname_from_userid(const char *userid);
 mbname_t *mbname_from_localdom(const char *localpart, const char *domain);
 mbname_t *mbname_from_intname(const char *intname);
 mbname_t *mbname_from_extname(const char *extname, const struct namespace *ns, const char *userid);
+mbname_t *mbname_from_extnameUTF8(const char *extname, const struct namespace *ns, const char *userid);
 mbname_t *mbname_from_extsub(const char *extsub, const struct namespace *ns, const char *userid);
 mbname_t *mbname_from_recipient(const char *recip, const struct namespace *ns);
 mbname_t *mbname_from_path(const char *path);
