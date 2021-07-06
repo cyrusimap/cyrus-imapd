eb727b81c41ad8ae7303ab1803775f11b2a2ae5e
Robert Stepanek <rsto@fastmailteam.com>
2021-07-06 14:49:12 +0200

    http_caldav: do not persist SCHEDULE-FORCE-SEND parameter

--- imap/http_caldav.c
+++ imap/http_caldav.c
@@ -8838,6 +8838,32 @@ static void strip_vtimezones(icalcomponent *ical)
     free_hash_table(&tzid_table, free);
 }
 
+static void strip_schedule_params(icalcomponent *ical)
+{
+    icalcomponent *comp = icalcomponent_get_first_real_component(ical);
+    if (!comp) return;
+
+    /* Only remove SCHEDULE-FORCE-SEND */
+
+    icalcomponent_kind kind = icalcomponent_isa(comp);
+    for (comp = icalcomponent_get_first_component(ical, kind);
+         comp;
+         comp = icalcomponent_get_next_component(ical, kind)) {
+
+        /* Grab the organizer */
+        icalproperty *prop = icalcomponent_get_first_property(comp, ICAL_ORGANIZER_PROPERTY);
+
+        /* Remove CalDAV Scheduling parameters from organizer */
+        icalproperty_remove_parameter_by_name(prop, "SCHEDULE-FORCE-SEND");
+
+        /* Remove CalDAV Scheduling parameters from attendees */
+        for (prop = icalcomponent_get_first_invitee(comp);
+             prop;
+             prop = icalcomponent_get_next_invitee(comp)) {
+            icalproperty_remove_parameter_by_name(prop, "SCHEDULE-FORCE-SEND");
+        }
+    }
+}
 
 /* Store the iCal data in the specified calendar/resource */
 int caldav_store_resource(struct transaction_t *txn, icalcomponent *ical,
@@ -8936,6 +8962,9 @@ int caldav_store_resource(struct transaction_t *txn, icalcomponent *ical,
         strip_vtimezones(ical);
         tzbyref = 1;
     }
+
+    /* Remove schedule parameters */
+    strip_schedule_params(ical);
 
     /* Set Schedule-Tag, if any */
     if (flags & NEW_STAG) {
