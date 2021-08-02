cf77e77b64988c303cb56e0e67c83f3da859c52d
Ken Murchison <murch@fastmail.com>
2021-08-02 10:07:35 -0400

    httpd.c: need to actually set a compression function pointer for xfer-encoding

--- imap/httpd.c
+++ imap/httpd.c
@@ -1838,6 +1838,7 @@ static void postauth_check_hdrs(struct transaction_t *txn)
                 (!strcasecmp(e->token, "gzip") ||
                  !strcasecmp(e->token, "x-gzip"))) {
                 txn->flags.te = TE_GZIP;
+                txn->resp_body.enc.proc = &zlib_compress;
             }
             free(e->token);
         }
@@ -3240,7 +3241,7 @@ EXPORTED void response_header(long code, struct transaction_t *txn)
             }
             free(encfname);
         }
-        if (txn->resp_body.enc.proc) {
+        if (txn->resp_body.enc.type) {
             /* Construct Content-Encoding header */
             comma_list_hdr(txn, "Content-Encoding", ce_strings, txn->resp_body.enc.type);
         }
@@ -3504,7 +3505,7 @@ EXPORTED void write_body(long code, struct transaction_t *txn,
     }
 
     /* Compress data */
-    if (txn->resp_body.enc.proc || txn->flags.te & ~TE_CHUNKED) {
+    if (txn->resp_body.enc.proc) {
         unsigned flags = 0;
 
         if (code) flags |= COMPRESS_START;
