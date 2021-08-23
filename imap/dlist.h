a6114e21ac23a18880e4561add4143ae2a787c9a
Bron Gondwana <brong@fastmail.fm>
2021-08-24 01:14:08 +1000

    dlist: add a 'buf' to the sax callback
    This allows us to read binary values in sax callbacks too

--- imap/dlist.h
+++ imap/dlist.h
@@ -25,7 +25,8 @@ enum dlistsax_t {
 
 struct dlistsax_data {
     const struct buf kbuf;
-    const char *data;
+    const struct buf buf;
+    const char *data; // cstring buffer or NULL for NIL
     void *rock;
 };
 
