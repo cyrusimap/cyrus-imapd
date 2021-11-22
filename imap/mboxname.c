867ca9d24c16efab1620fc4b8106f70723ef69e3
Ken Murchison <murch@fastmail.com>
2021-11-22 13:10:11 -0500

    mboxname.c: fix mboxname_isdavdrivemailbox() logic

--- imap/mboxname.c
+++ imap/mboxname.c
@@ -1543,7 +1543,7 @@ EXPORTED int mboxname_isaddressbookmailbox(const char *name, int mbtype)
  */
 EXPORTED int mboxname_isdavdrivemailbox(const char *name, int mbtype)
 {
-    if (mbtype_isa(mbtype) == MBTYPE_COLLECTION) return 1;  /* Only works on backends */
+    if (mbtype_isa(mbtype) != MBTYPE_COLLECTION) return 0;  /* Only works on backends */
     int res = 0;
 
     mbname_t *mbname = mbname_from_intname(name);
