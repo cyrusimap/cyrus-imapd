7cafb260bc61170e19d53cae4ea34313d1e19f95
Ken Murchison <murch@fastmail.com>
2021-12-14 13:42:07 -0500

    http_caldav.c: use makeuuid() as attachment uid

--- imap/http_caldav.c
+++ imap/http_caldav.c
@@ -2858,9 +2858,8 @@ static int caldav_post_attach(struct transaction_t *txn, int rights)
 
     if (op == ATTACH_REMOVE) aprop = NULL;
     else {
-        /* SHA1 of content used as resource UID, resource name, & managed-id */
-        static char uid[2*MESSAGE_GUID_SIZE+1];
-        struct message_guid guid;
+        /* UUID used as resource UID, resource name, & managed-id */
+        const char *uid = makeuuid();
 
         /* Read body */
         txn->req_body.flags |= BODY_DECODE;
@@ -2876,11 +2875,6 @@ static int caldav_post_attach(struct transaction_t *txn, int rights)
             ret = HTTP_BAD_REQUEST;
             goto done;
         }
-
-        /* Generate UID of body content */
-        message_guid_generate(&guid, buf_base(&txn->req_body.payload),
-                              buf_len(&txn->req_body.payload));
-        strcpy(uid, message_guid_encode(&guid));
 
         /* Store the new/updated attachment using WebDAV callback */
         ret = webdav_params.put.proc(txn, &txn->req_body.payload,
