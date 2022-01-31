01531856d3e75b9ee4f9580621d509945ce6e9ba
Christian Walther <walther@indel.ch>
2022-01-31 10:42:11 +0100

    imap: fix compile error on macOS: unknown type name 'ulong'
    This appears to be a Linux-specific thing, coming from /usr/include/x86_64-linux-gnu/sys/types.h:
    
      #ifdef __USE_MISC
      /* Old compatibility names for C types.  */
      typedef unsigned long int ulong;
    
    Signed-off-by: Christian Walther <walther@indel.ch>

--- imap/http_client.h
+++ imap/http_client.h
@@ -57,7 +57,7 @@ struct body_t {
     unsigned char framing;              /* Message framing   */
     unsigned char te;                   /* Transfer-Encoding */
     unsigned max;                       /* Max allowed len   */
-    ulong len;                          /* Content-Length    */
+    unsigned long len;                  /* Content-Length    */
     struct buf payload;                 /* Payload           */
 };
 
