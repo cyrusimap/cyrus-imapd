7ebd0c9abbadf024d35db61222c744b34d06cd17
Ricardo Signes <rjbs@semiotic.systems>
2022-03-11 21:01:11 -0500

    utils/tiny-test-splitter.pl: to split a big pile of tests into files
    Signed-off-by: Ricardo Signes <rjbs@semiotic.systems>

---
+++ cassandane/utils/tiny-test-splitter.pl
@@ -0,0 +1,31 @@
+use v5.20.0;
+use warnings;
+
+my $file;
+my @buffer;
+
+my @lines = <<>>;
+
+LINE: while ($_ = shift @lines) {
+  if (/^sub test_(\S+)/) {
+    $file = $1;
+    $file =~ s/_/-/g;
+    say $file;
+  }
+
+  if (/^}$/ && (!@lines || $lines[0] =~ /^$/)) {
+    push @buffer, $_;
+    open my $fh, '>', "tiny-tests/$file"
+      or die "can't open $file: $!";
+    print {$fh} "#!perl\nuse Cassandane::Tiny;\n\n", @buffer;
+    close $fh;
+
+    undef $file;
+    @buffer = ();
+    next LINE;
+  }
+
+  next if ! $file && /^\s*$/;
+
+  push @buffer, $_;
+}
