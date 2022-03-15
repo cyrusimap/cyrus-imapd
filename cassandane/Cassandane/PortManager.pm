c317675bd73991839acadb4afe57d668b8594681
Bron Gondwana <brong@fastmail.fm>
2022-03-15 09:43:21 -0400

    PortManager: start at port 29100
    This is higher and less likely to clash with other things

--- cassandane/Cassandane/PortManager.pm
+++ cassandane/Cassandane/PortManager.pm
@@ -61,9 +61,8 @@ sub alloc
         die "Invalid TEST_UNIT_WORKER_ID - code not run in Worker context"
             if (defined($workerid) && $workerid eq 'invalid');
         my $cassini = Cassandane::Cassini->instance();
-        my $cassini_base_port = $cassini->val('cassandane', 'base_port') // 0;
-        $base_port = 0 + $cassini_base_port || 9100;
-        $base_port += $max_ports * ($workerid-1);
+        my $cassandane_base_port = 0 + $cassini->val('cassandane', 'base_port', '29100');
+        $base_port = $cassandane_base_port + $max_ports * ($workerid-1);
     }
     for (my $i = 0 ; $i < $max_ports ; $i++)
     {
