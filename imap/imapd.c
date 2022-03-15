a672d5774a135e4cbfdc4977a1b0721f7ff2a713
Ken Murchison <murch@fastmail.com>
2022-03-15 17:04:09 -0400

    imapd.c: need an index_init so index_check() has a protstream Fixes a bug in RENAME INBOX INBOX.foo if INBOX hasn't been selected

--- imap/imapd.c
+++ imap/imapd.c
@@ -7780,7 +7780,13 @@ static void cmd_rename(char *tag, char *oldname, char *newname, char *location)
             state = imapd_index;
         }
         else {
-            r = index_open(oldmailboxname, NULL, &state);
+            struct index_init init;
+            memset(&init, 0, sizeof(init));
+            init.userid = imapd_userid;
+            init.authstate = imapd_authstate;
+            init.out = imapd_out;
+            r = index_open(oldmailboxname, &init, &state);
+            seqset_free(&init.vanishedlist);
         }
 
         /* move all the emails to the new folder */
