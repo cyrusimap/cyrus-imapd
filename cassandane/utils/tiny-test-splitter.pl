6b5c2da62c88895a078b44df52471e9375ec995e
Ricardo Signes <rjbs@semiotic.systems>
2022-06-06 08:00:57 -0400

    Cassandane: make test splitter cope with interleaved tests and not
    Signed-off-by: Ricardo Signes <rjbs@semiotic.systems>

--- cassandane/utils/tiny-test-splitter.pl
+++ cassandane/utils/tiny-test-splitter.pl
@@ -1,37 +1,54 @@
 use v5.20.0;
 use warnings;
 
+use File::Basename qw(fileparse);
+
 my $file;
 my @buffer;
 
-my $prefix = shift @ARGV;
+my $filename = shift @ARGV;
+
+my $prefix = fileparse($filename, '.pm');
 
 unless (-d "tiny-tests/$prefix") {
   system "mkdir -p tiny-tests/$prefix";
 }
 
-my @lines = <<>>;
+open my $infile, '<', $filename
+  or die "can't open $filename for reading: $!";
+
+my @lines = <$infile>;
+
+close $infile or die "error reading $infile: $!";
 
 LINE: while ($_ = shift @lines) {
   if (/^sub test_(\S+)/) {
-    $file = $1;
-    $file =~ s/_/-/g;
+    my $file = $1 =~ s/_/-/gr;
     say $file;
+
+    my @test_buffer;
+
+    while ($_ = shift @lines) {
+      push @test_buffer, $_;
+
+      if (/^}$/ && (!@lines || $lines[0] =~ /^$/)) {
+        open my $fh, '>', "tiny-tests/$prefix/$file"
+          or die "can't open $file: $!";
+        print {$fh} "#!perl\nuse Cassandane::Tiny;\n\n", @test_buffer;
+        close $fh;
+        next LINE;
+      }
+    }
   }
 
-  if (/^}$/ && (!@lines || $lines[0] =~ /^$/)) {
-    push @buffer, $_;
-    open my $fh, '>', "tiny-tests/$prefix/$file"
-      or die "can't open $file: $!";
-    print {$fh} "#!perl\nuse Cassandane::Tiny;\n\n", @buffer;
-    close $fh;
-
-    undef $file;
-    @buffer = ();
-    next LINE;
-  }
-
-  next if ! $file && /^\s*$/;
+  # Skip double blanks lines.
+  next LINE if @buffer && $buffer[-1] =~ /^$/ && /^$/;
 
   push @buffer, $_;
 }
+
+open my $outfile, '>', $filename
+  or die "can't open $filename for writing: $!";
+
+print {$outfile} @buffer;
+close $outfile or die "error writing to $filename and closing: $!";
