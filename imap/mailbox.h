c76a43e1c970bdbb0f5697f45e2acbfee030f3df
Robert Stepanek <rsto@fastmailteam.com>
2022-06-24 11:04:43 +0200

    mailbox: remove unimplemented mailbox_cleanup declaration
    Signed-off-by: Robert Stepanek <rsto@fastmailteam.com>

--- imap/mailbox.h
+++ imap/mailbox.h
@@ -640,8 +640,6 @@ extern int mailbox_expunge(struct mailbox *mailbox,
 extern void mailbox_archive(struct mailbox *mailbox,
                             mailbox_decideproc_t *decideproc, void *deciderock, unsigned flags);
 extern void mailbox_remove_files_from_object_storage(struct mailbox *mailbox, unsigned flags);
-extern int mailbox_cleanup(struct mailbox *mailbox, int iscurrentdir,
-                           mailbox_decideproc_t *decideproc, void *deciderock);
 extern void mailbox_unlock_index(struct mailbox *mailbox, struct statusdata *sd);
 
 extern int mailbox_create(const char *name, uint32_t mbtype, const char *part, const char *acl,
