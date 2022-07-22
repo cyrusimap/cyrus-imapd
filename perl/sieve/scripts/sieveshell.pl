cyrusimap/cyrus-imapd

commit 4f3d911e5188a976084b9af7d97732e3ca1a9a2a

sieveshell: add --password and --execfile options
based on patch from Debian in #3281

perl/sieve/scripts/sieveshell.pl
@@ -62,7 +62,9 @@ my $deletehelp =     "delete <name>    - delete script.\n";
 my $username = $ENV{USER};
 my $authname = $ENV{USER};
 my $realm = "";
+my $password;
 my $ex = "";
+my $exfile = "";
 my $help = 0;
 my $man = 0;
 my $ret;
@@ -70,7 +72,9 @@ my $ret;
 GetOptions("a|authname:s" => \$authname,
     "u|username:s" => \$username,
     "r|realm:s" => \$realm,
+    "p|password:s" => \$password,
     "e|exec:s" => \$ex,
+    "f|execfile:s" => \$exfile,
     "help|?" => \$help,
     man => \$man) or pod2usage(2);
 pod2usage(1) if $help;
@@ -85,7 +89,11 @@ my $acapserver = $ARGV[0];
 my $filehandle;
 my $interactive;
 
-if (! $ex eq "") {
+if (! $exfile eq "") {
+    open(FILEH,"<$exfile") || die "unable to open file: $?";
+    $filehandle = *FILEH;
+    $interactive = 0;
+} elsif (! $ex eq "") {
     $filehandle = tempfile();
 
     if (!$filehandle) { die "unable to open tmp file: $?"; }
@@ -97,8 +105,6 @@ if (! $ex eq "") {
     $filehandle = *STDIN;
     $interactive = 1;
 }
-
-
 
 sub list_cb {
 
@@ -123,6 +129,8 @@ sub prompt {
       return $authname;
   } elsif (($type eq "realm") && (defined $realm)) {
       return $realm;
+  } elsif (($type eq "password") && (defined $password)) {
+      return $password;
   }
 
   my $ostty;
@@ -173,6 +181,8 @@ if (!defined $obj) {
 
 my $term = Term::ReadLine->new("sieveshell");
 
+my $exitcode = 0;
+
 $term->ornaments(0);
 
 while(defined($_  = ($interactive ? $term->readline('> ') : <$filehandle>))){
@@ -199,6 +209,7 @@ while(defined($_  = ($interactive ? $term->readline('> ') : <$filehandle>))){
         my $errstr = sieve_get_error($obj);
         $errstr = "unknown error" if(!defined($errstr));
         print "upload failed: $errstr\n";
+        $exitcode = 1;
       }
     } elsif (($words[0] eq "list") ||
              ($words[0] eq "l") ||
@@ -208,6 +219,7 @@ while(defined($_  = ($interactive ? $term->readline('> ') : <$filehandle>))){
             my $errstr = sieve_get_error($obj);
             $errstr = "unknown error" if(!defined($errstr));
             print "list failed: $errstr\n";
+            $exitcode = 1;
         }
     } elsif (($words[0] eq "activate") ||
              ($words[0] eq "a")) {
@@ -220,6 +232,7 @@ while(defined($_  = ($interactive ? $term->readline('> ') : <$filehandle>))){
             my $errstr = sieve_get_error($obj);
             $errstr = "unknown error" if(!defined($errstr));
             print "activate failed: $errstr\n";
+            $exitcode = 1;
         }
     } elsif (($words[0] eq "deactivate") ||
              ($words[0] eq "da")) {
@@ -232,6 +245,7 @@ while(defined($_  = ($interactive ? $term->readline('> ') : <$filehandle>))){
             my $errstr = sieve_get_error($obj);
             $errstr = "unknown error" if(!defined($errstr));
             print "deactivate failed: $errstr\n";
+            $exitcode = 1;
         }
     } elsif (($words[0] eq "delete") ||
              ($words[0] eq "d")) {
@@ -244,6 +258,7 @@ while(defined($_  = ($interactive ? $term->readline('> ') : <$filehandle>))){
             my $errstr = sieve_get_error($obj);
             $errstr = "unknown error" if(!defined($errstr));
             print "delete failed: $errstr\n";
+            $exitcode = 1;
         }
     } elsif (($words[0] eq "get") ||
              ($words[0] eq "g")) {
@@ -257,6 +272,7 @@ while(defined($_  = ($interactive ? $term->readline('> ') : <$filehandle>))){
             my $errstr = sieve_get_error($obj);
             $errstr = "unknown error" if(!defined($errstr));
             print "get failed: $errstr\n";
+            $exitcode = 1;
         } else {
             if ($words[2]) {
                 open (OUTPUT,">$words[2]") || die "Unable to open $words[2]";
@@ -268,13 +284,15 @@ while(defined($_  = ($interactive ? $term->readline('> ') : <$filehandle>))){
         }
     } elsif (($words[0] eq "quit") || ($words[0] eq "q")) {
         sieve_logout($obj);
-        exit 0;
     } elsif (($words[0] eq "help") || ($words[0] eq "?")) {
         show_help();
     } else {
         print "Invalid command: $words[0]\n";
+        $exitcode = 1;
     }
 }
+
+exit $exitcode
 
 __END__
 
@@ -291,7 +309,8 @@ sieveshell - remotely manipulate sieve scripts
 =head1 SYNOPSIS
 
 sieveshell [B<--user>=I<user>] [B<--authname>=I<authname>]
-[B<--realm>=I<realm>] [B<--exec>=I<script>] I<server>[B<:>I<port>]
+[B<--realm>=I<realm>] [B<--password>=I<password>]
+[B<--exec>=I<script>] [B<--execfile>=I<file>] I<server>[B<:>I<port>]
 
 sieveshell B<--help>
 
@@ -336,9 +355,19 @@ current login user.
 
 The realm to attempt authentication in.
 
+=item B<-p> I<password>, B<--password>=I<password>
+
+The password to use when authenticating to server. Note that this
+parameter can be seen in the process list. B<Use with caution!>
+
 =item B<-e> I<script>, B<--exec>=I<script>
 
 Instead of working interactively, run commands from I<script>, and
+exit when done.
+
+=item B<-f> I<file>, B<--execfile>=I<file>
+
+Instead of working interactively, run commands from file I<file> and
 exit when done.
 
 =back

perl/imap/IMAP/Shell.pm
@@ -483,7 +483,7 @@ sub shell {
                           -mechanism => $mech, -password => $pw,
                           -tlskey => $tlskey, -notls => $notls,
                           -cafile => $cacert, -capath => $capath)
-      or die "cyradm: cannot authenticate to server with $mech as $auth\n";
+      or die "cyradm: cannot authenticate to server" . (defined($mech)?" with $mech":"") . " as $auth\n";
   }
   my $fstk = [*STDIN, *STDOUT, *STDERR];
   if ($dorc && $systemrc ne '' && -f $systemrc) {

commit 408bcc51a0a03c24d9383c96b505c6f28a511c98

sieveshell: fix segfault after quit
Exit after processing the 'quit' command, otherwise further commands
will segfault because sieve_logout() invalidates the object.

Broken in 4f3d911, see also https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=962362

Co-authored-by: Valentin Vidic <vvidic@debian.org>
Signed-off-by: Christian Walther <walther@indel.ch>

perl/sieve/scripts/sieveshell.pl
@@ -284,6 +284,7 @@ while(defined($_  = ($interactive ? $term->readline('> ') : <$filehandle>))){
         }
     } elsif (($words[0] eq "quit") || ($words[0] eq "q")) {
         sieve_logout($obj);
+        last;
     } elsif (($words[0] eq "help") || ($words[0] eq "?")) {
         show_help();
     } else {