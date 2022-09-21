f54379a18c757158cd19c5567accc335675c7065
Bron Gondwana <brong@fastmail.fm>
2022-09-21 16:08:07 +1000

    index: track changes for modseq bump when setting seen on shared folders

--- imap/index.c
+++ imap/index.c
@@ -4830,6 +4830,7 @@ static int index_storeflag(struct index_state *state,
     int dirty = 0;
     modseq_t oldmodseq;
     struct index_map *im = &state->map[msgno-1];
+    int seen_dirty = 0;
     int r;
 
     memset(modified_flags, 0, sizeof(struct index_modified_flags));
@@ -4855,6 +4856,7 @@ static int index_storeflag(struct index_state *state,
             im->isseen = new;
             state->seen_dirty = 1;
             dirty++;
+            seen_dirty = 1;
         }
     }
 
@@ -4977,6 +4979,7 @@ static int index_storeflag(struct index_state *state,
         else
             system_flags &= ~FLAG_SEEN;
     }
+
     /* add back the internal tracking flags */
     system_flags |= keep;
 
@@ -4993,6 +4996,18 @@ static int index_storeflag(struct index_state *state,
     if (r) return r;
     r = msgrecord_set_userflags(msgrec, user_flags);
     if (r) return r;
+
+    // patch back in seen state for non-internal-seen
+    if (seen_dirty && !state->internalseen) {
+        if (im->isseen) {
+            modified_flags->added_system_flags |= FLAG_SEEN;
+            modified_flags->added_flags++;
+        }
+        else {
+            modified_flags->removed_system_flags |= FLAG_SEEN;
+            modified_flags->removed_flags++;
+        }
+    }
 
     /* if it's silent and unchanged, update the seen value, but
      * not if qresync is enabled - RFC 4551 says that the MODSEQ
