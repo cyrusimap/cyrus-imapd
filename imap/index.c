91654403987af752af5492b9ee925b4b71233412
ellie timoney <ellie@fastmail.com>
2022-09-26 15:07:19 +1000

    various: mem leak fixes
    Fixes #4247
    Fixes #4248
    Fixes #4249
    Fixes #4250
    Fixes #4252

--- imap/index.c
+++ imap/index.c
@@ -2981,7 +2981,7 @@ index_copy(struct index_state *state,
     struct mailbox *destmailbox = NULL;
     struct index_map *im;
     int is_same_user;
-    ptrarray_t *msgrecs = ptrarray_new();
+    ptrarray_t *msgrecs = NULL;
 
     *copyuidp = NULL;
 
@@ -3042,6 +3042,7 @@ index_copy(struct index_state *state,
     if (r) goto done;
 
     docopyuid = (appendstate.myrights & ACL_READ);
+    msgrecs = ptrarray_new();
 
     for (i = 0; i < copyargs.nummsg; i++) {
         msgrecord_t *mr = msgrecord_from_index_record(srcmailbox, &copyargs.records[i]);
@@ -3102,11 +3103,13 @@ index_copy(struct index_state *state,
 done:
     free(copyargs.records);
     mailbox_close(&destmailbox);
-    for (i = 0; i < msgrecs->count; i++) {
-        msgrecord_t *mr = ptrarray_nth(msgrecs, i);
-        msgrecord_unref(&mr);
+    if (msgrecs) {
+        for (i = 0; i < msgrecs->count; i++) {
+            msgrecord_t *mr = ptrarray_nth(msgrecs, i);
+            msgrecord_unref(&mr);
+        }
+        ptrarray_free(msgrecs);
     }
-    ptrarray_free(msgrecs);
 
     return r;
 }
