905d5d70632c2ce8627c102ee6fb68a22d2b2d55
Ken Murchison <murch@fastmail.com>
2022-10-11 10:30:06 -0400

    bc_eval.c: do case-insensitive URI comparisons

--- sieve/bc_eval.c
+++ sieve/bc_eval.c
@@ -2058,11 +2058,12 @@ int sieve_eval_bc(sieve_execute_t *exe, int *impl_keep_p, sieve_interp_t *i,
 
             if (op == B_ENOTIFY) {
                 /* Parse/split the method URI */
-                if (!strncmp(method, "mailto:", 7)) {
+                if (!strncasecmp(method, "mailto:", 7)) {
                     strarray_insert(cmd.u.n.options, 0, method+7);
                     method = "mailto";
                 }
-                else if (!strncmp(method, "https://cyrusimap.org/notifiers/", 32)) {
+                else if (!strncasecmp(method,
+                                      "https://cyrusimap.org/notifiers/", 32)) {
                     method = method+32;
                 }
             }
