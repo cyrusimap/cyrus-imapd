4c185bbf79795758817a34aed5fbd967503ecd19
ellie timoney <ellie@fastmail.com>
2022-10-24 11:11:53 +1100

    Generator: include monotonic counter in Message-ID
    Prevents collisions without overthinking the random generation.
    Might be useful in postmortem test debugging too?

--- cassandane/Cassandane/Generator.pm
+++ cassandane/Cassandane/Generator.pm
@@ -40,6 +40,7 @@
 package Cassandane::Generator;
 use strict;
 use warnings;
+use feature qw(state);
 use Digest::MD5 qw(md5_hex);
 
 use lib '.';
@@ -187,10 +188,12 @@ sub _generate_to
 sub _generate_messageid
 {
     my ($self, $params) = @_;
+    state $counter = 0;
     my $idsalt = int(rand(65536));
 
-    return sprintf 'fake.%d.%d@%s',
+    return sprintf 'fake.%d.%d.%d@%s',
                    $params->{date}->epoch(),
+                   ++ $counter,
                    $idsalt,
                    $params->{from}->domain();
 }
