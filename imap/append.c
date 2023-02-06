86d5949f23e0e61f61e34d29813ed07ca7b8427a
Bron Gondwana <brong@fastmail.fm>
2023-02-07 08:21:49 +1100

    append: don't replace existing stagefile, check GUID and use direct file

--- imap/append.c
+++ imap/append.c
@@ -872,7 +872,8 @@ out:
 
 struct findstage_cb_rock {
     const char *partition;
-    const char *stagefile;
+    const char *guid;
+    char *fname;
 };
 
 static int findstage_cb(const conv_guidrec_t *rec, void *vrock)
@@ -891,19 +892,25 @@ static int findstage_cb(const conv_guidrec_t *rec, void *vrock)
         struct stat sbuf;
         const char *msgpath = mbentry_datapath(mbentry, rec->uid);
         if (msgpath && !stat(msgpath, &sbuf)) {
-            /* link the first stage part to the existing message file */
-            r = cyrus_copyfile(msgpath, rock->stagefile, 0/*flags*/);
-            if (r) {
-                /* don't fail - worst case, we will use existing stage */
-                r = 0;
+            FILE *file = fopen(msgpath, "r");
+            // errors just mean "skip this file"
+            if (file) {
+                struct body *body = NULL;
+                r = message_parse_file(file, NULL, NULL, &body, msgpath);
+                if (!r && !strcmp(rock->guid, message_guid_encode(&body->guid)))
+                    rock->fname = xstrdup(msgpath);
+                if (body) {
+                    message_free_body(body);
+                    free(body);
+                }
+                fclose(file);
             }
-            else r = CYRUSDB_DONE;
         }
     }
 
     mboxlist_entry_free(&mbentry);
 
-    return r;
+    return rock->fname ? CYRUSDB_DONE : 0;
 }
 
 /*
@@ -934,6 +941,7 @@ EXPORTED int append_fromstage_full(struct appendstate *as, struct body **body,
 
     /* for staging */
     char stagefile[MAX_MAILBOX_PATH+1] = "";
+    char *linkfile = NULL;
 
     assert(stage != NULL && stage->parts.count);
 
@@ -959,17 +967,23 @@ EXPORTED int append_fromstage_full(struct appendstate *as, struct body **body,
         struct conversations_state *cstate = mailbox_get_cstate(mailbox);
 
         if (cstate) {
-            struct findstage_cb_rock rock = { mailbox_partition(mailbox), stagefile };
+            char *guid = xstrdup(message_guid_encode(&(*body)->guid));
+            struct findstage_cb_rock rock = { mailbox_partition(mailbox), guid, NULL };
 
-            r = conversations_guid_foreach(cstate,
-                                           message_guid_encode(&(*body)->guid),
-                                           findstage_cb, &rock);
-            if (r && r != CYRUSDB_DONE) {
-                r = IMAP_IOERROR;
-                goto out;
+            // ignore errors, it's OK for this to fail
+            conversations_guid_foreach(cstate, guid, findstage_cb, &rock);
+
+            // if we found a file, remember it
+            if (rock.fname) {
+                syslog(LOG_NOTICE, "found existing file %s for %s; linking", guid, rock.fname);
+                linkfile = rock.fname;
             }
+
+            free(guid);
         }
     }
+
+    if (linkfile) goto havefile;
 
     for (i = 0 ; i < stage->parts.count ; i++) {
         /* ok, we've successfully created the file */
@@ -1011,7 +1025,9 @@ EXPORTED int append_fromstage_full(struct appendstate *as, struct body **body,
         strarray_append(&stage->parts, stagefile);
     }
 
-    /* 'stagefile' contains the message and is on the same partition
+havefile:
+
+    /* 'linkfile' or 'stagefile' contains the message and is on the same partition
        as the mailbox we're looking at */
 
     /* Setup */
@@ -1063,7 +1079,7 @@ EXPORTED int append_fromstage_full(struct appendstate *as, struct body **body,
     r = msgrecord_get_fname(msgrec, &fname);
     if (r) goto out;
 
-    r = mailbox_copyfile(stagefile, fname, nolink);
+    r = mailbox_copyfile(linkfile ? linkfile : stagefile, fname, nolink);
     if (r) goto out;
 
     FILE *destfile = fopen(fname, "r");
