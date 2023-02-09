569b141f6f6b82c0f5fe171691d1949f901614bc
ellie timoney <ellie@fastmail.com>
2023-02-10 10:33:49 +1100

    docsrc/conf.py: release 3.6.1, 3.4.5, 3.2.11, 3.0.18

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -431,7 +431,7 @@ epub_exclude_files = ['search.html']
 # When this is updated, you may also need to update the version and release
 # definitions listed above to stay up to date.
 rst_prolog = """
-.. |imap_last_stable_version| replace:: 3.6.0
+.. |imap_last_stable_version| replace:: 3.6.1
 .. |imap_last_stable_branch| replace:: `cyrus-imapd-3.6`
 .. |imap_current_stable_version| replace:: 3.8.0-alpha0
 .. |imap_current_stable_branch| replace:: `cyrus-imapd-3.8`
