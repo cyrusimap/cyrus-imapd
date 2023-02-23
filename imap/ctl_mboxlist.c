1521cde6513eb619ca10c7bbf509232fb153c24b
Ken Murchison <murch@fastmail.com>
2023-02-23 09:59:55 -0500

    ctl_mboxlist.c: don't free static memory

--- imap/ctl_mboxlist.c
+++ imap/ctl_mboxlist.c
@@ -991,8 +991,6 @@ static void add_part(ptrarray_t *found,
             /* found it */
             if (override) {
                 /* replace the path with the one containing cyrus.header */
-                if (entry->path)
-                    free(entry->path);
                 strcpy(entry->path, path);
             }
 
