a9083377f7ad132b9e4b13a85ead9ff9dde464d9
Ken Murchison <murch@fastmail.com>
2023-04-03 09:50:38 -0400

    Revert "Merge pull request #4468 from dilyanpalauzov/relocate_by_id_static_free"
    This reverts commit 4fc6ddda282dcf75409973632868d0b4a172428b, reversing
    changes made to c894666bed8dfb7dc11b18a5e753a11b1b33243e.

--- imap/relocate_by_id.c
+++ imap/relocate_by_id.c
@@ -298,7 +298,7 @@ int main(int argc, char **argv)
                 /* Add sieve path */
                 path = user_sieve_path(userid);
                 if (*path) {
-                    strarray_append(oldpaths, xstrdup(path));
+                    strarray_append(oldpaths, path);
 
                     buf_setcstr(&buf, config_getstring(IMAPOPT_SIEVEDIR));
                     buf_printf(&buf, "/%s", userpath);
