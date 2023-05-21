27d688b48b3247a76efff462de6dc2f9b86a0caf
ellie timoney <ellie@fastmail.com>
2023-05-22 09:50:08 +1000

    docs: 3.8.0 released

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -434,7 +434,7 @@ epub_exclude_files = ['search.html']
 rst_prolog = """
 .. |imap_last_stable_version| replace:: 3.6.1
 .. |imap_last_stable_branch| replace:: `cyrus-imapd-3.6`
-.. |imap_current_stable_version| replace:: 3.8.0-rc1
+.. |imap_current_stable_version| replace:: 3.8.0
 .. |imap_current_stable_branch| replace:: `cyrus-imapd-3.8`
 .. |imap_latest_development_version| replace:: 3.9.0-alpha0
 .. |imap_latest_development_branch| replace:: master
@@ -449,7 +449,7 @@ rst_prolog = """
 .. |sasl_current_stable_version| replace:: 2.1.28
 .. |imap_stable_release_notes| raw:: html
 
-    <a href="3.8/x/3.8.0-rc1.html">3.8.0-rc1</a>
+    <a href="3.8/x/3.8.0.html">3.8.0</a>
 
 .. |imap_development_release_notes| raw:: html
 
