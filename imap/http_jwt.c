bb42c136a1b1407654edb2641084cfb03734713a
Bruno Thomas <bruno@barreverte.fr>
2023-06-02 11:22:54 +0200

    feat: add exp/nbf claims validation for JWT

--- imap/http_jwt.c
+++ imap/http_jwt.c
@@ -68,6 +68,7 @@
 static int is_enabled = 0;
 
 static ptrarray_t pkeys = PTRARRAY_INITIALIZER;
+static const size_t MAX_JWT_PAYLOAD_FIELD_NB = 5;
 
 static time_t max_age = 0;
 
@@ -492,7 +493,7 @@ static int validate_payload(struct jwt *jwt, char *out, size_t outlen)
     int ret = 0;
 
     json_t *jws = json_loads(buf_cstring(&jwt->buf), JSON_REJECT_DUPLICATES, NULL);
-    if (!json_object_size(jws) || json_object_size(jws) > 2) {
+    if (!json_object_size(jws) || json_object_size(jws) > MAX_JWT_PAYLOAD_FIELD_NB) {
         xsyslog(LOG_ERR, "Unexpected JWS payload structure", NULL);
         goto done;
     }
@@ -504,30 +505,50 @@ static int validate_payload(struct jwt *jwt, char *out, size_t outlen)
     }
 
     if (json_object_size(jws) >= 2) {
-        json_t *jiat = json_object_get(jws, "iat");
-        if (!json_is_integer(jiat)) {
-            if (jiat) {
-                char *val = json_dumps(jiat, JSON_COMPACT|JSON_ENCODE_ANY);
-                xsyslog(LOG_ERR, "Invalid \"iat\" claim", "iat=<%s>", val);
-                free(val);
-            }
-            else xsyslog(LOG_ERR, "JWT contains unsupported claims", NULL);
-            goto done;
-        }
-
-        if (max_age) {
-            time_t iat = json_integer_value(jiat);
-            time_t now = time(NULL);
-            if (iat + max_age <= now || iat > now) {
-                char *val = json_dumps(jiat, JSON_COMPACT|JSON_ENCODE_ANY);
-                xsyslog(LOG_ERR, "Out-of-range \"iat\" claim", "iat=<%s>", val);
-                free(val);
+        time_t now = time(NULL);
+        json_t *jnbf = json_object_get(jws, "nbf");
+        if (json_is_integer(jnbf)) {
+            time_t nbf = json_integer_value(jnbf);
+            if (!(now >= nbf)) {
+                xsyslog(LOG_ERR, "Out-of-range \"nbf\" claim", NULL);
                 goto done;
             }
         }
-    }
-    else if (max_age) {
-        xsyslog(LOG_ERR, "Missing \"iat\" claim", NULL);
+
+        json_t *jexp = json_object_get(jws, "exp");
+        if (json_is_integer(jexp)) {
+            time_t exp = json_integer_value(jexp);
+            if (!(now < exp)) {
+                xsyslog(LOG_ERR, "Out-of-range \"exp\" claim", NULL);
+                goto done;
+            }
+        } else {
+            json_t *jiat = json_object_get(jws, "iat");
+            if (json_is_integer(jiat)) {
+                if (max_age) {
+                    time_t iat = json_integer_value(jiat);
+                    if (iat + max_age <= now || iat > now) {
+                        char *val = json_dumps(jiat, JSON_COMPACT|JSON_ENCODE_ANY);
+                        xsyslog(LOG_ERR, "Out-of-range \"iat\" claim", "iat=<%s>", val);
+                        free(val);
+                        goto done;
+                    }
+                } else {
+                    xsyslog(LOG_ERR, "Missing \"max_age\" parameter with \"iat\" claim", NULL);
+                    goto done;
+                }
+            } else {
+                if (jiat) {
+                    char *val = json_dumps(jiat, JSON_COMPACT|JSON_ENCODE_ANY);
+                    xsyslog(LOG_ERR, "Invalid \"iat\" claim", "iat=<%s>", val);
+                    free(val);
+                }
+                else xsyslog(LOG_ERR, "JWT contains unsupported claims", NULL);
+                goto done;
+            }
+        }
+    } else {
+        xsyslog(LOG_ERR, "Missing \"iat\" or \"exp\" claim", NULL);
         goto done;
     }
 
