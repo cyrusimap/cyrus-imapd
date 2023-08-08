bffb57138afdd9b0056dd26fcc81dc987b9ab374
Ken Murchison <murch@fastmail.com>
2023-08-08 10:47:06 -0400

    imapd.c: make sure we delete XFERed mailboxes from source (issue #4370)
    this commit also removes an extra write to mailboxes.db,
    since mboxlist_deletemailbox() already sets MBTYPE_DELETED

--- imap/imapd.c
+++ imap/imapd.c
@@ -12143,7 +12143,6 @@ static int xfer_reactivate(struct xfer_header *xfer)
 
 static int xfer_delete(struct xfer_header *xfer)
 {
-    mbentry_t *newentry = NULL;
     struct xfer_item *item;
     int r;
 
@@ -12152,26 +12151,6 @@ static int xfer_delete(struct xfer_header *xfer)
     /* 7) local delete of mailbox
      * & remove local "remote" mailboxlist entry */
     for (item = xfer->items; item; item = item->next) {
-        /* Set mailbox as DELETED on local server
-           (need to also reset to local partition,
-           otherwise mailbox can not be opened for deletion) */
-        /* XXX - this code is awful... need a sane way to manage mbentries */
-        newentry = mboxlist_entry_create();
-        newentry->name = xstrdupnull(item->mbentry->name);
-        newentry->uniqueid = xstrdupnull(item->mbentry->uniqueid);
-        newentry->acl = xstrdupnull(item->mbentry->acl);
-        newentry->server = xstrdupnull(item->mbentry->server);
-        newentry->partition = xstrdupnull(item->mbentry->partition);
-        newentry->mbtype = item->mbentry->mbtype | MBTYPE_DELETED;
-        r = mboxlist_updatelock(newentry, 1);
-        mboxlist_entry_free(&newentry);
-
-        if (r) {
-            syslog(LOG_ERR,
-                   "Could not move mailbox: %s, mboxlist_update failed (%s)",
-                   item->mbentry->name, error_message(r));
-        }
-
         /* Note that we do not check the ACL, and we don't update MUPDATE */
         /* note also that we need to remember to let proxyadmins do this */
         /* On a unified system, the subsequent MUPDATE PUSH on the remote
@@ -12179,11 +12158,11 @@ static int xfer_delete(struct xfer_header *xfer)
         r = mboxlist_deletemailboxlock(item->mbentry->name,
                                    imapd_userisadmin || imapd_userisproxyadmin,
                                    imapd_userid, imapd_authstate, NULL,
-                                   MBOXLIST_DELETE_LOCALONLY);
+                                   MBOXLIST_DELETE_LOCALONLY|MBOXLIST_DELETE_FORCE);
         if (r) {
             syslog(LOG_ERR,
-                   "Could not delete local mailbox during move of %s",
-                   item->mbentry->name);
+                   "Could not delete local mailbox during move of %s: %s",
+                   item->mbentry->name, error_message(r));
             /* can't abort now! */
         }
     }
