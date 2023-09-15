c8e8299005303e8e07dcd99bbba5f4fff260a681
ellie timoney <ellie@fastmail.com>
2023-09-15 11:42:59 +1000

    Info: remove redundant 'info' from test names

--- cassandane/Cassandane/Cyrus/Info.pm
+++ cassandane/Cassandane/Cyrus/Info.pm
@@ -89,7 +89,7 @@ sub run_cyr_info
     return @res;
 }
 
-sub test_info_conf
+sub test_conf
 {
     my ($self) = @_;
 
@@ -126,7 +126,7 @@ sub test_info_conf
     $self->assert_deep_equals(\%imapd_conf, \%cyr_info_conf);
 }
 
-sub test_info_conf_all
+sub test_conf_all
 {
     my ($self) = @_;
 
@@ -168,7 +168,7 @@ sub test_info_conf_all
     $self->assert_deep_equals(\%imapd_conf, \%cyr_info_conf);
 }
 
-sub test_info_conf_default
+sub test_conf_default
 {
     my ($self) = @_;
 
@@ -192,7 +192,7 @@ sub test_info_conf_default
     }
 }
 
-sub test_info_lint
+sub test_lint
 {
     my ($self) = @_;
 
@@ -206,7 +206,7 @@ Cassandane::Cyrus::TestCase::magic(ConfigJunk => sub {
     shift->config_set(trust_fund => 'street art');
 });
 
-sub test_info_lint_junk
+sub test_lint_junk
     :ConfigJunk
 {
     my ($self) = @_;
@@ -217,7 +217,7 @@ sub test_info_lint_junk
     $self->assert_deep_equals(["trust_fund: street art\n"], \@output);
 }
 
-sub test_info_lint_channels
+sub test_lint_channels
     :min_version_3_2 :NoStartInstances
 {
     my ($self) = @_;
@@ -244,7 +244,7 @@ sub test_info_lint_channels
     );
 }
 
-sub test_info_lint_partitions
+sub test_lint_partitions
     :min_version_3_0 :NoStartInstances
 {
     my ($self) = @_;
