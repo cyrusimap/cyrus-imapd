a08c74b2b7ce12db64717c7be6d31c58a3f053b6
Robert Stepanek <rsto@fastmailteam.com>
2023-09-26 15:10:31 +0200

    zoneinfo: only set zoneinfo db handle once
    Signed-off-by: Robert Stepanek <rsto@fastmailteam.com>

--- imap/zoneinfo_db.c
+++ imap/zoneinfo_db.c
@@ -74,6 +74,9 @@ EXPORTED int zoneinfo_open(const char *fname)
     int ret;
     char *tofree = NULL;
 
+    if (zoneinfo_dbopen)
+        return 0;
+
     if (!fname) fname = config_getstring(IMAPOPT_ZONEINFO_DB_PATH);
 
     /* create db file name */
