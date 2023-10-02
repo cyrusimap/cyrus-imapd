b24aabfa7a66de32c33b1550b212bedbb1c71234
ellie timoney <ellie@fastmail.com>
2023-10-02 14:19:04 +1100

    search_squat: fix non-incremental reindexes
    The "incremental" argument became a "flags" argument back in 2.5,
    but search_squat was never updated, so any non-zero set of flags
    were treated as being the incremental flag

--- imap/search_squat.c
+++ imap/search_squat.c
@@ -774,7 +774,7 @@ static int doc_check(void *closure, const SquatListDoc *doc)
 
 static int begin_mailbox(search_text_receiver_t *rx,
                          struct mailbox *mailbox,
-                         int incremental)
+                         int flags)
 {
     SquatReceiverData *d = (SquatReceiverData *)rx;
     SquatOptions options;
@@ -786,6 +786,7 @@ static int begin_mailbox(search_text_receiver_t *rx,
     SquatSearchIndex *old_index = NULL;
     int r = 0;      /* IMAP error code */
     int s = 0;      /* SQUAT error code */
+    int incremental = (flags & SEARCH_UPDATE_INCREMENTAL);
 
     bv_clearall(&d->indexed);
 
