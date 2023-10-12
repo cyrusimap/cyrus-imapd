ded1b77f9bf6fae7ebf2937983e66bd7d1f9749a
ellie timoney <ellie@fastmail.com>
2023-10-13 10:28:50 +1100

    various: use assert_matches rather than bare assert

--- cassandane/Cassandane/Cyrus/Conversations.pm
+++ cassandane/Cassandane/Cyrus/Conversations.pm
@@ -937,7 +937,8 @@ sub test_guid_duplicate_same_folder
 
     if ($self->{instance}->{have_syslog_replacement}) {
         my @lines = $self->{instance}->getsyslog();
-        $self->assert(grep { m/IOERROR: conversations GUID limit/ } @lines);
+        $self->assert_matches(qr{IOERROR: conversations GUID limit},
+                              join("\n", @lines));
     }
 
     $talk->select("INBOX.dest");
@@ -983,7 +984,8 @@ sub test_guid_duplicate_total_count
 
     if ($self->{instance}->{have_syslog_replacement}) {
         my @lines = $self->{instance}->getsyslog();
-        $self->assert(grep { m/IOERROR: conversations GUID limit/ } @lines);
+        $self->assert_matches(qr{IOERROR: conversations GUID limit},
+                              join("\n", @lines));
     }
 }
 
@@ -1021,7 +1023,8 @@ sub test_guid_duplicate_expunges
 
     if ($self->{instance}->{have_syslog_replacement}) {
         my @lines = $self->{instance}->getsyslog();
-        $self->assert(grep { m/IOERROR: conversations GUID limit/ } @lines);
+        $self->assert_matches(qr{IOERROR: conversations GUID limit},
+                              join("\n", @lines));
     }
 }
 
