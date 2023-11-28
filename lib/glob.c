cyrusimap/cyrus-imapd

commit ba43ab2462adca1a6f2dbfc54d967f02bd1dcb33

glob: assert that regcomp succeeded
instead of assuming it succeeded

lib/glob.c
@@ -58,6 +58,7 @@
 EXPORTED glob *glob_init(const char *str, char sep)
 {
     struct buf buf = BUF_INITIALIZER;
+    int r;
 
     buf_appendcstr(&buf, "(^");
     while (*str) {
@@ -109,7 +110,9 @@ EXPORTED glob *glob_init(const char *str, char sep)
     buf_appendcstr(&buf, "]|$)");
 
     glob *g = xmalloc(sizeof(glob));
-    regcomp(&g->regex, buf_cstring(&buf), REG_EXTENDED);
+    r = regcomp(&g->regex, buf_cstring(&buf), REG_EXTENDED);
+    /* XXX handle regex compilation failure properly! */
+    assert(r == 0);
     buf_free(&buf);
 
     return g;

commit c3228b411aa2846673cdbd7878075ef44d734667

glob: fix missing include
broken by ba43ab2462adca1a6f2dbfc54d967f02bd1dcb33,
reported by @yselkowitz in comment on #4741

lib/glob.c
@@ -46,6 +46,7 @@
 #include <config.h>
 #include <stdio.h>
 #include <string.h>
+#include "assert.h"
 #include "util.h"
 #include "glob.h"
 #include "xmalloc.h"