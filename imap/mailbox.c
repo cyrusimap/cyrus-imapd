6202cbdcddb28a394ca569534f56bc53536974d3
Bron Gondwana <brong@fastmailteam.com>
2023-12-02 23:32:07 +1300

    mailbox: make rename_nocopy work correctly
    We need the new mbentry since we're not creating a full mailbox; and
    we also need to syntheticaly make sure we're updating the new
    conversations db.  This mirrors the logic from mailbox_rename_copy.

--- imap/mailbox.c
+++ imap/mailbox.c
@@ -6598,40 +6598,56 @@ EXPORTED int mailbox_copy_files(struct mailbox *mailbox, const char *newpart,
 
 
 HIDDEN int mailbox_rename_nocopy(struct mailbox *oldmailbox,
-                                 const char *newname, int silent)
+                                 mbentry_t *newmbentry, int silent)
 {
+    const char *newname = newmbentry->name;
     char quotaroot[MAX_MAILBOX_BUFFER];
     int hasquota = quota_findroot(quotaroot, sizeof(quotaroot), newname);
+    struct conversations_state *oldcstate = NULL;
+    struct conversations_state *newcstate = NULL;
+    struct conversations_state *localcstate = NULL;
 
     /* Move any quota usage */
     int r = mailbox_changequotaroot(oldmailbox,
-                                    hasquota ? quotaroot: NULL, silent);
+                                    hasquota ? quotaroot : NULL, silent);
+    if (r) goto done;
 
-    if (!r) {
-        /* copy any mailbox annotations */
-        mbentry_t newmbentry = MBENTRY_INITIALIZER;
-        newmbentry.name = (char *) newname;
-        struct mailbox newmailbox = { .mbentry = &newmbentry,
-                                      .index_locktype = LOCK_EXCLUSIVE };
-        r = annotate_rename_mailbox(oldmailbox, &newmailbox);
+    struct mailbox newmailbox = *oldmailbox;
+    newmailbox.mbentry = newmbentry;
+    r = annotate_rename_mailbox(oldmailbox, &newmailbox);
+    if (r) goto done;
+
+    oldcstate = mailbox_get_cstate(oldmailbox);
+    newcstate = conversations_get_mbox(newname);
+    if (!newcstate) {
+        conversations_open_mbox(newname, 0, &localcstate);
+        newcstate = localcstate;
     }
 
-    if (!r && mailbox_has_conversations(oldmailbox)) {
-        struct conversations_state *oldcstate =
-            mailbox_get_cstate(oldmailbox);
-
-        assert(oldcstate);
-
-        if (mboxname_isdeletedmailbox(newname, NULL)) {
-            /* we never store data about deleted mailboxes */
-            r = mailbox_delete_conversations(oldmailbox);
-        }
-        else if (oldcstate->folders_byname) {
-            /* we can just rename within the same user */
+    if (oldcstate && newcstate && !strcmp(oldcstate->path, newcstate->path)) {
+        /* we can just rename within the same user */
+        if (oldcstate->folders_byname) {
             r = conversations_rename_folder(oldcstate, mailbox_name(oldmailbox), newname);
         }
-        // otherwise, we don't need to rename because it's the same uniqueid
+        /* otherwise it's got the same key, so nothing to do */
     }
+    else {
+        /* have to handle each one separately */
+        if (oldcstate)
+            r = mailbox_delete_conversations(oldmailbox);
+        if (!r && newcstate) {
+            // sub in the values for the new location so we write to the
+            // correct location in the new database!
+            struct conversations_state *tempcs = oldmailbox->local_cstate;
+            mbentry_t *tempmb = oldmailbox->mbentry;
+            oldmailbox->local_cstate = newcstate;
+            oldmailbox->mbentry = newmbentry;
+            r = mailbox_add_conversations(oldmailbox, /*silent*/0);
+            oldmailbox->mbentry = tempmb;
+            oldmailbox->local_cstate = tempcs;
+        }
+    }
+    if (r) goto done;
 
     /* unless on a replica, bump the modseq */
     if (!silent) mailbox_modseq_dirty(oldmailbox);
@@ -6641,6 +6657,11 @@ HIDDEN int mailbox_rename_nocopy(struct mailbox *oldmailbox,
     oldmailbox->h.name = xstrdup(newname);
     oldmailbox->header_dirty = 1;
 
+done:
+    if (localcstate) {
+        if (r) conversations_abort(&localcstate);
+        else conversations_commit(&localcstate);
+    }
     return r;
 }
 
