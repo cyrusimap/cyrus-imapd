cyrusimap/cyrus-imapd

commit 0c282379154a776099a83f120311a973c628eb55

xapian_wrap: return NULL query for empty CJK terms
The Xapian wrapper already returned NULL for ASCII query terms that
just consisted of punctuation characters. But a query term containing
Unicode punctuation codepoints such as EN DASH (U+2013) was parsed
as CJK text, returning an empty, always falsible, query instead of NULL.

This issue just showed up for Email/query using uidsearch, but the
new test checks all three codepaths: uidsearch, guidsearch and Sieve.

imap/xapian_wrap.cpp
@@ -1918,7 +1918,7 @@ static Xapian::Query* xapian_query_new_match_word_break(const xapian_db_t *db, c
     return q;
 }
 
-EXPORTED Xapian::Query *
+static Xapian::Query *
 xapian_query_new_match_internal(const xapian_db_t *db, int partnum, const char *str)
 {
     const char *prefix = get_term_prefix(XAPIAN_DB_CURRENT_VERSION, partnum);
@@ -1955,12 +1955,7 @@ xapian_query_new_match_internal(const xapian_db_t *db, int partnum, const char *
         Xapian::TermGenerator::stem_strategy stem_strategy =
             get_stem_strategy(XAPIAN_DB_CURRENT_VERSION, partnum);
 
-        Xapian::Query *qq = query_new_textmatch(db, str, prefix, stem_strategy);
-        if (qq->get_type() == Xapian::Query::LEAF_MATCH_NOTHING) {
-            delete qq;
-            qq = NULL;
-        }
-        return qq;
+        return query_new_textmatch(db, str, prefix, stem_strategy);
 
     } catch (const Xapian::Error &err) {
         xsyslog(LOG_ERR, "IOERROR: caught exception",
@@ -2008,6 +2003,12 @@ xapian_query_new_match(const xapian_db_t *db, int partnum, const char *str)
         free(mystr);
         charset_free(&utf8);
     }
+
+    if (q && q->get_type() == Xapian::Query::LEAF_MATCH_NOTHING) {
+        delete q;
+        q = NULL;
+    }
+
     return (xapian_query_t*) q;
 }
 

cassandane/tiny-tests/JMAPEmail/email_query_utf8punct_term
@@ -1,0 +1,87 @@
+#!perl
+use Cassandane::Tiny;
+use Encode qw(decode encode);
+use JSON qw(encode_json);
+
+sub test_email_query_utf8punct_term
+    :needs_component_jmap :needs_component_sieve :JMAPExtensions
+{
+    my ($self) = @_;
+    my $jmap = $self->{jmap};
+    my $imap = $self->{store}->get_client();
+    $jmap->AddUsing('https://cyrusimap.org/ns/jmap/performance');
+    $jmap->AddUsing('https://cyrusimap.org/ns/jmap/debug');
+    $jmap->AddUsing('https://cyrusimap.org/ns/jmap/mail');
+
+    xlog $self, "Create MIME message containing a non-ASCII punctuation char";
+    my $mime = <<"EOF";
+From: <from\@local>
+To: <to\@local>
+Subject: test
+Date: Mon, 13 Apr 2020 15:34:03 +0200
+MIME-Version: 1.0
+Content-Transfer-Encoding: 8BIT
+Content-Type: text/plain; charset=utf-8
+
+hello \N{U+2013} world
+EOF
+    $mime =~ s/\r?\n/\r\n/gs;
+    $mime = encode('utf-8', $mime);
+    $imap->append('INBOX', $mime) || die $@;
+
+    xlog $self, "run squatter";
+    $self->{instance}->run_command({cyrus => 1}, 'squatter');
+
+    my $filter = {
+        operator => 'AND',
+        conditions => [{
+            body => 'hello'
+        }, {
+            body => "\N{U+2013}",
+        }, {
+            body => 'world',
+        }],
+    };
+
+    xlog $self, "Assert Email/query ignores punctuation character in filter";
+    my $res = $jmap->CallMethods([
+        ['Email/query', {
+            filter => $filter,
+            disableGuidSearch => JSON::true,
+        }, 'R1'],
+        ['Email/query', {
+            filter => $filter,
+        }, 'R2'],
+    ]);
+    $self->assert_equals(JSON::false,
+        $res->[0][1]{performance}{details}{isGuidSearch});
+    $self->assert_num_equals(1, scalar @{$res->[0][1]{ids}});
+    $self->assert_equals(JSON::true,
+        $res->[1][1]{performance}{details}{isGuidSearch});
+    $self->assert_num_equals(1, scalar @{$res->[1][1]{ids}});
+
+    xlog $self, "Assert JMAP Sieve ignores punctuation character in filter";
+    $imap->create("matches") or die;
+    my $filterAsStr = encode_json($filter);
+    $self->{instance}->install_sieve_script(<<"EOF"
+require ["x-cyrus-jmapquery", "x-cyrus-log", "variables", "fileinto"];
+if
+  allof( not string :is "${stop}" "Y",
+    jmapquery text:
+  $filterAsStr
+.
+  )
+{
+  fileinto "matches";
+}
+EOF
+    );
+
+    my $msg = Cassandane::Message->new();
+    $msg->set_lines(split /\n/, $mime);
+    $self->{instance}->deliver($msg);
+
+    $imap->select('matches');
+    $self->assert_num_equals(1, $imap->get_response_code('exists'));
+    $imap->unselect();
+}

commit 419f8077ae0f3a86985ada77426b26ebb8a7ae64

xapian_wrap: ignore punct-only terms only in unstructed search parts
This fixes a regression in 0c282379154a776099a83f120311a973c628eb55
Punctuation-only terms only should be ignored in unstructured search
parts, such as bodies and subjects.

imap/xapian_wrap.cpp
@@ -1945,18 +1945,33 @@ xapian_query_new_match_internal(const xapian_db_t *db, int partnum, const char *
             return query_new_type(db, prefix, str);
         }
 
-        // Don't stem queries for Thaana codepage (0780) or higher.
+        // Match unstructured search parts
+
+        static Xapian::Query *q = NULL;
+
+        int need_word_break = 0;
         for (const unsigned char *p = (const unsigned char *)str; *p; p++) {
-            if (*p > 221) //has highbit
-                return xapian_query_new_match_word_break(db, str, prefix);
+            // Use ICU word break for Thaana codepage (0780) or higher.
+            if (*p > 221) {
+                need_word_break = 1;
+                break;
+            }
         }
 
-        // Stemable codepage.
-        Xapian::TermGenerator::stem_strategy stem_strategy =
-            get_stem_strategy(XAPIAN_DB_CURRENT_VERSION, partnum);
+        if (need_word_break) {
+            q = xapian_query_new_match_word_break(db, str, prefix);
+        }
+        else {
+            Xapian::TermGenerator::stem_strategy stem_strategy =
+                get_stem_strategy(XAPIAN_DB_CURRENT_VERSION, partnum);
+            q = query_new_textmatch(db, str, prefix, stem_strategy);
+        }
+        if (q && q->get_type() == Xapian::Query::LEAF_MATCH_NOTHING) {
+            delete q;
+            q = NULL;
+        }
 
-        return query_new_textmatch(db, str, prefix, stem_strategy);
-
+        return q;
     } catch (const Xapian::Error &err) {
         xsyslog(LOG_ERR, "IOERROR: caught exception",
                          "exception=<%s>",
@@ -2002,11 +2017,6 @@ xapian_query_new_match(const xapian_db_t *db, int partnum, const char *str)
         }
         free(mystr);
         charset_free(&utf8);
-    }
-
-    if (q && q->get_type() == Xapian::Query::LEAF_MATCH_NOTHING) {
-        delete q;
-        q = NULL;
     }
 
     return (xapian_query_t*) q;

cassandane/tiny-tests/JMAPEmail/email_query_punct_no_text
@@ -1,0 +1,56 @@
+#!perl
+use Cassandane::Tiny;
+
+sub test_email_query_punct_no_text
+    :needs_component_sieve :needs_component_jmap :JMAPExtensions
+{
+    my ($self) = @_;
+    my $imap = $self->{store}->get_client();
+
+    $imap->create("matches") or die;
+
+    # Assert that punctuation-only terms in non-text criteria
+    # match nothing. Also see email_query_utf8punct_term.
+
+    $self->{instance}->install_sieve_script(<<'EOF'
+require ["x-cyrus-jmapquery", "x-cyrus-log", "variables", "fileinto"];
+# Search: "from:\"=\""
+if allof(
+  not string :is "${stop}" "Y",
+  jmapquery text:
+  {
+     "conditions" : [
+        {
+           "from" : "\"=\""
+        }
+     ],
+     "operator" : "OR"
+  }
+.
+) {
+  fileinto "matches";
+  set "stop" "Y";
+}
+EOF
+    );
+
+    my $mime = <<'EOF';
+From: from@local
+To: to@local
+Subject: test
+Date: Mon, 13 Apr 2020 15:34:03 +0200
+MIME-Version: 1.0
+Content-Type: text/plain;charset=us-ascii
+Content-Transfer-Encoding: 7bit
+
+hello
+EOF
+    $mime =~ s/\r?\n/\r\n/gs;
+    my $msg = Cassandane::Message->new();
+    $msg->set_lines(split /\n/, $mime);
+    $self->{instance}->deliver($msg);
+
+    xlog "Assert that message did not match";
+    $self->assert_num_equals(0, $imap->message_count('matches'));
+    $self->assert_num_equals(1, $imap->message_count('INBOX'));
+}