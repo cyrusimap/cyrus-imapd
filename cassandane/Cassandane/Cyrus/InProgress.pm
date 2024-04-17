afe18091b00a4fa280f2c38ff3ddb239dcf0c6c2
ellie timoney <ellie@fastmail.com>
2024-04-17 13:21:09 +1000

    InProgress: use debug_slowio for testing INPROGRESS responses

--- cassandane/Cassandane/Cyrus/InProgress.pm
+++ cassandane/Cassandane/Cyrus/InProgress.pm
@@ -85,7 +85,7 @@ sub tear_down
 }
 
 sub test_xrename
-    :NoAltNameSpace :min_version_3_9
+    :NoAltNameSpace
 {
     my ($self) = @_;
 
@@ -122,8 +122,8 @@ sub test_xrename
     $self->assert_str_equals('INBOX.dst.child.grand', $resp[2][5]);
 }
 
-sub test_copy_search_slow
-    :NoAltNameSpace :min_version_3_9
+sub test_copy
+    :SlowIO :NoAltNameSpace
 {
     my ($self) = @_;
 
@@ -137,11 +137,10 @@ sub test_copy_search_slow
         },
     );
 
-    xlog "extract messages into INBOX";
-    $self->{instance}->unpackfile(abs_path('data/cyrus/15k_messages.tar.gz'),
-                                  'data/user/cassandane');
-    $self->{instance}->run_command({ cyrus => 1 },
-                                   'reconstruct', 'q', 'user.cassandane');
+    xlog "generate some test messages";
+    foreach (1..100) {
+        $self->make_message("Message $_", size => 128_000);
+    }
 
     xlog $self, "Create another folder";
     my $talk = $self->{store}->get_client();
@@ -151,70 +150,216 @@ sub test_copy_search_slow
     xlog $self, "copy messages";
     @resp = ();
     $talk->select("INBOX");
-    $talk->_imap_cmd('COPY', 0, \%handlers, '1:15000', 'INBOX.dst');
+    $talk->_imap_cmd('COPY', 0, \%handlers, '1:100', 'INBOX.dst');
     $self->assert_str_equals('ok', $talk->get_last_completion_response());
     $self->assert_str_equals('[INPROGRESS', $resp[0][0]);
     # we don't know what the exact count will be, be we know the total
     $self->assert_matches(qr/^[0-9]+$/, $resp[0][1][1]);
-    $self->assert_str_equals('15000', $resp[0][1][2]);
+    $self->assert_str_equals('100', $resp[0][1][2]);
+}
+
+sub test_search
+    :SlowIO :NoAltNameSpace
+{
+    my ($self) = @_;
+
+    my @resp;
+    my %handlers =
+    (
+        ok => sub
+        {
+            my (undef, $ok) = @_;
+            push(@resp, $ok);
+        },
+    );
+
+    xlog "generate some test messages";
+    foreach (1..100) {
+        $self->make_message("Message $_", size => 128_000);
+    }
 
     xlog $self, "search messages";
+    my $talk = $self->{store}->get_client();
     @resp = ();
-    $talk->_imap_cmd('SEARCH', 0, \%handlers, 'RETURN', '(PARTIAL -1:-500)', '9500:9999', 'BODY', 'needle');
+    $talk->_imap_cmd('SEARCH', 0, \%handlers, 'RETURN', '(PARTIAL -1:-500)', '1:100', 'BODY', 'needle');
     $self->assert_str_equals('ok', $talk->get_last_completion_response());
     $self->assert_str_equals('[INPROGRESS', $resp[0][0]);
     # we don't know what the exact count will be, be we know the total
     $self->assert_matches(qr/^[0-9]+$/, $resp[0][1][1]);
-    $self->assert_str_equals('500', $resp[0][1][2]);
+    $self->assert_str_equals('100', $resp[0][1][2]);
+}
+
+sub test_esearch_selected
+    :SlowIO :NoAltNameSpace
+{
+    my ($self) = @_;
+
+    my @resp;
+    my %handlers =
+    (
+        ok => sub
+        {
+            my (undef, $ok) = @_;
+            push(@resp, $ok);
+        },
+    );
+
+    xlog "generate some test messages";
+    foreach (1..100) {
+        $self->make_message("Message $_", size => 128_000);
+    }
 
     xlog $self, "esearch selected mailbox";
+    my $talk = $self->{store}->get_client();
     @resp = ();
     $talk->_imap_cmd('ESEARCH', 0, \%handlers,
-                     'IN', '(SELECTED)', '9500:9999', 'BODY', 'needle');
+                     'IN', '(SELECTED)', '1:100', 'BODY', 'needle');
     $self->assert_str_equals('ok', $talk->get_last_completion_response());
     $self->assert_str_equals('[INPROGRESS', $resp[0][0]);
     # we don't know what the exact count will be, be we know the total
     $self->assert_matches(qr/^[0-9]+$/, $resp[0][1][1]);
-    $self->assert_str_equals('500', $resp[0][1][2]);
+    $self->assert_str_equals('100', $resp[0][1][2]);
+}
+
+sub test_esearch_multiple
+    :SlowIO :NoAltNameSpace
+{
+    my ($self) = @_;
+
+    my @resp;
+    my %handlers =
+    (
+        ok => sub
+        {
+            my (undef, $ok) = @_;
+            push(@resp, $ok);
+        },
+    );
+
+    xlog "generate some test messages";
+    foreach (1..100) {
+        $self->make_message("Message $_", size => 128_000);
+    }
+
+    xlog $self, "Create another folder";
+    my $talk = $self->{store}->get_client();
+    $talk->create("INBOX.dst");
+    $self->assert_str_equals('ok', $talk->get_last_completion_response());
+
+    xlog $self, "copy messages";
+    @resp = ();
+    $talk->select("INBOX");
+    $talk->_imap_cmd('COPY', 0, \%handlers, '1:100', 'INBOX.dst');
+    $self->assert_str_equals('ok', $talk->get_last_completion_response());
+    $self->assert_str_equals('[INPROGRESS', $resp[0][0]);
+    # we don't know what the exact count will be, be we know the total
+    $self->assert_matches(qr/^[0-9]+$/, $resp[0][1][1]);
+    $self->assert_str_equals('100', $resp[0][1][2]);
 
     xlog $self, "esearch multiple mailboxes";
     @resp = ();
     $talk->_imap_cmd('ESEARCH', 0, \%handlers,
-                     'IN', '(PERSONAL)', '9500:9999', 'BODY', 'needle');
+                     'IN', '(PERSONAL)', '1:100', 'BODY', 'needle');
     $self->assert_str_equals('ok', $talk->get_last_completion_response());
     $self->assert_str_equals('[INPROGRESS', $resp[0][0]);
     # we shouldn't have a count or total
     $self->assert_null($resp[0][1][1]);
     $self->assert_null($resp[0][1][2]);
+}
+
+sub test_sort
+    :SlowIO :NoAltNameSpace
+{
+    my ($self) = @_;
+
+    my @resp;
+    my %handlers =
+    (
+        ok => sub
+        {
+            my (undef, $ok) = @_;
+            push(@resp, $ok);
+        },
+    );
+
+    xlog "generate some test messages";
+    foreach (1..100) {
+        $self->make_message("Message $_", size => 128_000);
+    }
 
     xlog $self, "sort messages";
+    my $talk = $self->{store}->get_client();
     @resp = ();
     $talk->_imap_cmd('SORT', 0, \%handlers,
-                     '(ARRIVAL)', 'US-ASCII', '9500:9999', 'BODY', 'needle');
+                     '(ARRIVAL)', 'US-ASCII', '1:100', 'BODY', 'needle');
     $self->assert_str_equals('ok', $talk->get_last_completion_response());
     $self->assert_str_equals('[INPROGRESS', $resp[0][0]);
     # we don't know what the exact count will be, be we know the total
     $self->assert_matches(qr/^[0-9]+$/, $resp[0][1][1]);
-    $self->assert_str_equals('500', $resp[0][1][2]);
+    $self->assert_str_equals('100', $resp[0][1][2]);
+}
+
+sub test_thread
+    :SlowIO :NoAltNameSpace
+{
+    my ($self) = @_;
+
+    my @resp;
+    my %handlers =
+    (
+        ok => sub
+        {
+            my (undef, $ok) = @_;
+            push(@resp, $ok);
+        },
+    );
+
+    xlog "generate some test messages";
+    foreach (1..100) {
+        $self->make_message("Message $_", size => 128_000);
+    }
 
     xlog $self, "thread messages";
+    my $talk = $self->{store}->get_client();
     @resp = ();
     $talk->_imap_cmd('THREAD', 0, \%handlers,
-                     'REFERENCES', 'US-ASCII', '9500:9999', 'BODY', 'needle');
+                     'REFERENCES', 'US-ASCII', '1:100', 'BODY', 'needle');
     $self->assert_str_equals('ok', $talk->get_last_completion_response());
     $self->assert_str_equals('[INPROGRESS', $resp[0][0]);
     # we don't know what the exact count will be, be we know the total
     $self->assert_matches(qr/^[0-9]+$/, $resp[0][1][1]);
-    $self->assert_str_equals('500', $resp[0][1][2]);
+    $self->assert_str_equals('100', $resp[0][1][2]);
+}
+
+sub test_rename
+    :SlowIO :NoAltNameSpace
+{
+    my ($self) = @_;
+
+    my @resp;
+    my %handlers =
+    (
+        ok => sub
+        {
+            my (undef, $ok) = @_;
+            push(@resp, $ok);
+        },
+    );
+
+    xlog "generate some test messages";
+    foreach (1..100) {
+        $self->make_message("Message $_", size => 128_000);
+    }
 
     xlog $self, "rename INBOX";
+    my $talk = $self->{store}->get_client();
     @resp = ();
     $talk->_imap_cmd('RENAME', 0, \%handlers, 'INBOX', 'INBOX.Archive');
     $self->assert_str_equals('ok', $talk->get_last_completion_response());
     $self->assert_str_equals('[INPROGRESS', $resp[0][0]);
     # we don't know what the exact count will be, be we know the total
     $self->assert_matches(qr/^[0-9]+$/, $resp[0][1][1]);
-    $self->assert_str_equals('15000', $resp[0][1][2]);
+    $self->assert_str_equals('100', $resp[0][1][2]);
 }
 
 1;
