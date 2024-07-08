e85fbaa6991a35dbc7ec8a0df3884f0a62a44947
ellie timoney <ellie@fastmail.com>
2024-07-08 12:08:38 +1000

    Net::SMTPServer: save received messages to disk

--- cassandane/Cassandane/Instance.pm
+++ cassandane/Cassandane/Instance.pm
@@ -681,6 +681,7 @@ sub _build_skeleton
         'data',
         'meta',
         'run',
+        'smtpd',
         'tmp',
     );
     foreach my $sd (@subdirs)
@@ -1125,6 +1126,9 @@ sub _start_smtpd
             port => $port,
             max_servers => 3, # default is 50, yikes
             control_file => "$basedir/conf/smtpd.json",
+            xmtp_tmp_dir => "$basedir/tmp/",
+            store_msg => 1,
+            messages_dir => "$basedir/smtpd/",
         });
         $smtpd->run() or die;
         exit 0; # Never reached
