cyrusimap/cyrus-imapd

commit a2684ec50c63b2e7a965702d4ef83a5387c7a2d5

message.c: handle message/global just like message/rfc822

imap/message.c
@@ -328,6 +328,13 @@ done:
     return r;
 }
 
+static int body_is_rfc822(const struct body *body)
+{
+    return body && !strcasecmpsafe(body->type, "MESSAGE") &&
+           (!strcasecmpsafe(body->subtype, "RFC822") ||
+            !strcasecmpsafe(body->subtype, "GLOBAL"));
+}
+
 EXPORTED int message_parse(const char *fname, struct index_record *record)
 {
     struct body *body = NULL;
@@ -678,8 +685,7 @@ static void message_find_part(struct body *body, const char *section,
                               msg_base, msg_len, parts, n);
         }
     }
-    else if (!strcmp(body->type, "MESSAGE") &&
-             !strcmp(body->subtype, "RFC822")) {
+    else if (body_is_rfc822(body)) {
         snprintf(nextsection, sizeof(nextsection), "%s.1", section);
         message_find_part(body->subpart, nextsection, content_types,
                           msg_base, msg_len, parts, n);
@@ -806,8 +812,7 @@ static int message_parse_body(struct msg *msg, struct body *body,
             message_parse_multipart(msg, body, boundaries, efname);
         }
     }
-    else if (strcmp(body->type, "MESSAGE") == 0 &&
-        strcmp(body->subtype, "RFC822") == 0) {
+    else if (body_is_rfc822(body)) {
         const char *base = msg->base + msg->offset;
         body->subpart = (struct body *)xzmalloc(sizeof(struct body));
 
@@ -1163,7 +1168,8 @@ static void message_parse_charset(const struct body *body,
         message_parse_charset_params(body->params, &charset);
     }
     else if (!strcmp(body->type, "MESSAGE")) {
-        if (!strcmp(body->subtype, "RFC822")) {
+        if (!strcmp(body->subtype, "RFC822") ||
+            !strcmp(body->subtype, "GLOBAL")) {
             charset_free(&charset);
             charset = CHARSET_UNKNOWN_CHARSET;
         }
@@ -2328,8 +2334,7 @@ EXPORTED void message_write_body(struct buf *buf, const struct body *body,
         buf_putc(buf, ' ');
         buf_printf(buf, "%u", body->content_lines);
     }
-    else if (strcmp(body->type, "MESSAGE") == 0
-             && strcmp(body->subtype, "RFC822") == 0) {
+    else if (body_is_rfc822(body)) {
         /* Message/rfc822 gets a body_msg */
         buf_putc(buf, ' ');
         message_write_envelope(buf, body->subpart);
@@ -2518,8 +2523,7 @@ static void message_write_section(struct buf *buf, const struct body *body)
 {
     int part;
 
-    if (strcmp(body->type, "MESSAGE") == 0
-        && strcmp(body->subtype, "RFC822") == 0) {
+    if (body_is_rfc822(body)) {
         if (body->subpart->numparts) {
             /*
              * Part 0 of a message/rfc822 is the message header/text.
@@ -3229,9 +3233,7 @@ static int message_read_body(struct protstream *strm, struct body *body, const c
             c = getuint32(strm, &body->content_lines);
             if (c == EOF) goto done;
         }
-        else if (!strcmp(body->type, "MESSAGE") &&
-                 !strcmp(body->subtype, "RFC822")) {
-
+        else if (body_is_rfc822(body)) {
             body->subpart = (struct body *) xzmalloc(sizeof(struct body));
 
             /* envelope structure */
@@ -3318,8 +3320,7 @@ static void message_read_binarybody(struct body *body, const char **sect,
     p = *sect += CACHE_ITEM_SIZE_SKIP;
     if (!n) return;
 
-    if (!strcmp(body->type, "MESSAGE") && !strcmp(body->subtype, "RFC822") &&
-        body->encoding) {
+    if (body_is_rfc822(body) && body->encoding) {
         // Handle encoded messages.
         if (!body->charset_enc) {
             body->charset_enc = encoding_lookupname(body->encoding);
@@ -3332,8 +3333,7 @@ static void message_read_binarybody(struct body *body, const char **sect,
         }
     }
 
-    if (!strcmp(body->type, "MESSAGE") && !strcmp(body->subtype, "RFC822") &&
-        body->subpart->numparts) {
+    if (body_is_rfc822(body) && body->subpart->numparts) {
         subpart = body->subpart->subpart;
         body = body->subpart;
     }
@@ -4511,8 +4511,7 @@ badformat:
             body->content_lines = atoi(buf_cstring(&buf));
         }
 
-        else if (!strcmpsafe(body->type, "MESSAGE") &&
-                 !strcmpsafe(body->subtype, "RFC822")) {
+        else if (body_is_rfc822(body)) {
             body->numparts = 1;
             body->subpart = xzmalloc(sizeof(struct body));
 
@@ -4585,8 +4584,7 @@ static int parse_bodystructure_sections(const char **cachestrp, const char *cach
         goto done;
     }
 
-    if (strcmp(body->type, "MESSAGE") == 0
-        && strcmp(body->subtype, "RFC822") == 0) {
+    if (body_is_rfc822(body)) {
 
         if (strcmp(body->subpart->type, "MULTIPART") == 0) {
 

cassandane/tiny-tests/JMAPEmail/email_get_encoded_message
@@ -1,0 +1,147 @@
+#!perl
+use Cassandane::Tiny;
+use MIME::Base64 qw(encode_base64);
+use MIME::QuotedPrint qw(encode_qp);
+
+sub encode_qp_strip_crlf
+{
+    # encode_qp encodes CR in CRLF, so remove CR before encoding
+    my ($string, $eol) = @_;
+    (my $stripped_string = $string) =~ s/\r\n/\n/g;
+    return encode_qp($stripped_string, $eol);
+}
+
+sub test_email_get_encoded_message
+    :needs_component_jmap :NoMunge8Bit :RFC2047_UTF8
+{
+    my ($self) = @_;
+    my $imap = $self->{store}->get_client();
+    my $jmap = $self->{jmap};
+    $jmap->AddUsing('https://cyrusimap.org/ns/jmap/mail');
+
+    # Define some MIME message. Its content doesn't matter,
+    # but let's mix an 8bit character in the headers to
+    # make the content transfer encoding actually matter.
+    my $mime8Bit = <<'EOF';
+From: hello@example.com
+To: world@example.com
+Subject: töst
+Date: Thu, 20 May 2004 14:28:51 +0200
+Mime-Version: 1.0
+Content-Type: text/plain; charset=utf-8
+
+täst
+EOF
+    $mime8Bit =~ s/\r?\n/\r\n/gs;
+    my $mime8BitSize = bytes::length($mime8Bit);
+
+    # Define test cases.
+    my @tests = ({
+        type => 'message/rfc822',
+        encoding => '8bit',
+        body => $mime8Bit,
+    }, {
+        type => 'message/global',
+        encoding => '8bit',
+        body => $mime8Bit,
+    }, {
+        type => 'message/rfc822',
+        encoding => 'base64',
+        body => encode_base64($mime8Bit, "\015\012"),
+    }, {
+        type => 'message/global',
+        encoding => 'base64',
+        body => encode_base64($mime8Bit, "\015\012"),
+    }, {
+        type => 'message/rfc822',
+        encoding => 'quoted-printable',
+        body => encode_qp_strip_crlf($mime8Bit, "\015\012"),
+    }, {
+        type => 'message/global',
+        encoding => 'quoted-printable',
+        body => encode_qp_strip_crlf($mime8Bit, "\015\012"),
+    });
+
+    my $res = $jmap->CallMethods([
+        ['Email/get', { ids => [] }, 'R1']
+    ]);
+    my $state = $res->[0][1]{state};
+    $self->assert_not_null($state);
+
+    # Run the tests.
+    while (my ($i, $tc) = each @tests) {
+        my $imapUid = $i + 1;
+        my $testId = "test$imapUid";
+
+        xlog $self, "Testing content-type $tc->{type} and encoding $tc->{encoding}";
+
+        xlog $self, "Delivering test message";
+        my $mime = <<"EOF";
+From: from\@local
+To: to\@local
+Subject: $testId
+Date: Thu, 20 May 2004 14:28:51 +0200
+Content-Type: multipart/mixed; boundary=8c438cf1-a6ac-4d99-b388-b1dfd9550725=_
+Mime-Version: 1.0
+
+--8c438cf1-a6ac-4d99-b388-b1dfd9550725=_
+Content-Type: text/plain; charset=utf-8
+
+test
+
+--8c438cf1-a6ac-4d99-b388-b1dfd9550725=_
+Content-Type: $tc->{type}
+Content-Transfer-Encoding: $tc->{encoding}
+
+$tc->{body}
+--8c438cf1-a6ac-4d99-b388-b1dfd9550725=_--
+EOF
+        $mime =~ s/\r?\n/\r\n/gs;
+
+        my $msg = Cassandane::Message->new();
+        $msg->set_lines(split /\n/, $mime);
+        $self->{instance}->deliver($msg);
+
+        $res = $jmap->CallMethods([
+            ['Email/changes', {
+                sinceState => $state
+            }, 'R1'],
+            ['Email/get', {
+                '#ids' => {
+                    resultOf => 'R1',
+                    name => 'Email/changes',
+                    path => '/created',
+                },
+                properties => ['subject', 'bodyStructure'],
+                bodyProperties => [
+                    'blobId',
+                    'header:content-transfer-encoding:asText',
+                    'partId',
+                    'size',
+                    'type',
+                ],
+            }, 'R2'],
+        ]);
+        $self->assert_str_equals($testId, $res->[1][1]{list}[0]{subject});
+        # Update Email state.
+        $state = $res->[0][1]{newState};
+
+        xlog $self, "Assert Email/get returns expected message";
+        my $bodyPart = $res->[1][1]{list}[0]{bodyStructure}{subParts}[1];
+        $self->assert_str_equals("2", $bodyPart->{partId});
+        $self->assert_str_equals($tc->{type}, $bodyPart->{type});
+        $self->assert_str_equals($tc->{encoding},
+            $bodyPart->{'header:content-transfer-encoding:asText'});
+        $self->assert_num_equals($mime8BitSize, $bodyPart->{size});
+
+        xlog $self, "Assert Blob download";
+        $res = $self->download('cassandane', $bodyPart->{blobId});
+        $self->assert_str_equals($mime8Bit, $res->{content});
+
+        # While we are at it let's check IMAP, too.
+        xlog $self, "Assert IMAP FETCH BINARY";
+        $imap->select('INBOX');
+        $res = $imap->fetch($imapUid, '(BINARY[2])');
+        $self->assert_str_equals($mime8Bit, $res->{$imapUid}{binary});
+    }
+}

imap/index.c
@@ -3599,11 +3599,11 @@ void index_fetchmsg(struct index_state *state, const struct buf *msg,
     if (domain != DOMAIN_7BIT) prot_data_boundary(state->out);
 }
 
-static int body_is_rfc822(struct body *body)
+static int body_is_rfc822(const struct body *body)
 {
-    return body &&
-        !strcasecmpsafe(body->type, "MESSAGE") &&
-        !strcasecmpsafe(body->subtype, "RFC822");
+    return body && !strcasecmpsafe(body->type, "MESSAGE") &&
+           (!strcasecmpsafe(body->subtype, "RFC822") ||
+            !strcasecmpsafe(body->subtype, "GLOBAL"));
 }
 
 static struct body *find_part(struct body *body, int32_t part)
@@ -6009,8 +6009,7 @@ static int find_striphtml_parts(message_t *msg, strarray_t *striphtml)
     ptrarray_push(&work, (void*) root);
     const struct body *body;
     while ((body = ptrarray_pop(&work))) {
-        if (!strcmpsafe(body->type, "MESSAGE") &&
-            !strcmpsafe(body->subtype, "RFC822")) {
+        if (body_is_rfc822(body)) {
             ptrarray_push(&submsgs, (void*)body);
         }
         int i;
@@ -6034,8 +6033,7 @@ static int find_striphtml_parts(message_t *msg, strarray_t *striphtml)
                 has_htmlpart = 1;
                 break;
             }
-            else if (!strcmpsafe(body->type, "MESSAGE") &&
-                     !strcmpsafe(body->subtype, "RFC822")) {
+            else if (body_is_rfc822(body)) {
                 continue;
             }
             else if (body->numparts) {
@@ -6060,8 +6058,7 @@ static int find_striphtml_parts(message_t *msg, strarray_t *striphtml)
                     strarray_push(striphtml, body->part_id);
                 }
             }
-            else if (!strcmpsafe(body->type, "MESSAGE") &&
-                     !strcmpsafe(body->subtype, "RFC822")) {
+            else if (body_is_rfc822(body)) {
                 continue;
             }
             else if (body->numparts) {

imap/jmap_mail.c
@@ -6718,7 +6718,8 @@ static void _cyrusmsg_init_partids(struct body *body, const char *part_id)
         }
 
         if (!strcmp(body->type, "MESSAGE") &&
-            !strcmp(body->subtype, "RFC822")) {
+            (!strcmp(body->subtype, "RFC822") ||
+             !strcmp(body->subtype, "GLOBAL"))) {
             _cyrusmsg_init_partids(body->subpart, body->part_id);
         }
     }

imap/jmap_mail_query.c
@@ -325,8 +325,8 @@ HIDDEN int jmap_email_hasattachment(const struct body *part,
                 !strcmp(part->subtype, "PDF"))) {
         return 1;
     }
-    else if ((!strcmp(part->type, "MESSAGE") &&
-                !strcmp(part->subtype, "RFC822"))) {
+    else if (!strcmp(part->type, "MESSAGE")) {
+        // any message/* is an attachment
         return 1;
     }
     else if ((!strcmp(part->type, "TEXT") &&

commit 58cf14e581d0e2de78ecd8abe9674b326d892232

message.c: only read message/global envelope if cache version >= 13
Fixes a bug introduced by a2684ec50c63b2e7a965702d4ef83a5387c7a2d5
which made the message cache parser errouneously expect a cached
envelope for message/global, causing to an invalid memory read.

Also see 22efff1aa2aa48a71350edd9f926ddff9e109f9a

imap/message.c
@@ -3152,7 +3152,8 @@ static int message_read_envelope(struct protstream *strm, struct body *body)
  * Read cached bodystructure response.
  * Analog to message_write_body()
  */
-static int message_read_body(struct protstream *strm, struct body *body, const char *part_id)
+static int message_read_body(struct protstream *strm, struct body *body, const char *part_id,
+                             uint32_t cache_version)
 {
     int c;
     struct buf buf = BUF_INITIALIZER;
@@ -3174,7 +3175,7 @@ static int message_read_body(struct protstream *strm, struct body *body, const c
             buf_printf(&buf, "%d", body->numparts + 1);
             struct body *subbody = &body->subpart[body->numparts++];
             subbody->part_id = buf_release(&buf);
-            c = message_read_body(strm, subbody, subbody->part_id);
+            c = message_read_body(strm, subbody, subbody->part_id, cache_version);
         } while (((c = prot_getc(strm)) == '(') && prot_ungetc(c, strm));
 
         /* remove the part_id here, you can't address multiparts directly */
@@ -3233,7 +3234,10 @@ static int message_read_body(struct protstream *strm, struct body *body, const c
             c = getuint32(strm, &body->content_lines);
             if (c == EOF) goto done;
         }
-        else if (body_is_rfc822(body)) {
+        else if ((body_is_rfc822(body) && cache_version >= 13) ||
+                 // Cache versions < 13 only handled message/rfc822.
+                 (!strcmp(body->type, "MESSAGE") &&
+                  !strcmp(body->subtype, "RFC822"))) {
             body->subpart = (struct body *) xzmalloc(sizeof(struct body));
 
             /* envelope structure */
@@ -3241,7 +3245,7 @@ static int message_read_body(struct protstream *strm, struct body *body, const c
             if (c == EOF) goto done;
 
             /* body structure */
-            c = message_read_body(strm, body->subpart, body->part_id);
+            c = message_read_body(strm, body->subpart, body->part_id, cache_version);
             if (c == EOF) goto done;
             c = prot_getc(strm); /* trailing SP */
             if (c == EOF) goto done;
@@ -3468,7 +3472,7 @@ EXPORTED void message_read_bodystructure(const struct index_record *record, stru
     strm = prot_readmap(cacheitem_base(record, CACHE_BODYSTRUCTURE),
                         cacheitem_size(record, CACHE_BODYSTRUCTURE));
 
-    message_read_body(strm, *body, NULL);
+    message_read_body(strm, *body, NULL, record->cache_version);
     prot_free(strm);
 
     /* Read binary bodystructure from cache */