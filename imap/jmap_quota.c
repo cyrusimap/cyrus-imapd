593242ed64de5f5775aaba7cb2f67d679924aa9b
Ken Murchison <murch@fastmail.com>
2024-09-13 13:31:25 -0400

    jmap_quota.c: implement Quota/query

--- imap/jmap_quota.c
+++ imap/jmap_quota.c
@@ -45,6 +45,7 @@
 
 #include <syslog.h>
 
+#include "cyr_qsort_r.h"
 #include "http_jmap.h"
 #include "jmap_api.h"
 #include "sieve_db.h"
@@ -58,6 +59,7 @@
 static int jmap_legacy_quota_get(jmap_req_t *req);
 static int jmap_quota_get(jmap_req_t *req);
 static int jmap_quota_changes(jmap_req_t *req);
+static int jmap_quota_query(jmap_req_t *req);
 
 static jmap_method_t jmap_quota_methods_standard[] = {
     {
@@ -70,6 +72,12 @@ static jmap_method_t jmap_quota_methods_standard[] = {
         "Quota/changes",
         JMAP_URN_QUOTA,
         &jmap_quota_changes,
+        JMAP_NEED_CSTATE
+    },
+    {
+        "Quota/query",
+        JMAP_URN_QUOTA,
+        &jmap_quota_query,
         JMAP_NEED_CSTATE
     },
     { NULL, NULL, NULL, 0}
@@ -235,6 +243,7 @@ static const struct jquota_type_t {
 };
 
 struct jquota_root_t {
+    char *id;
     const char *name;
     const char *junits;
     quota_t used;
@@ -660,5 +669,304 @@ static int jmap_quota_changes(jmap_req_t *req)
     free(qrock.inboxname);
     jmap_changes_fini(&changes);
     jmap_parser_fini(&parser);
+    return 0;
+}
+
+static void filter_parse(jmap_req_t *req __attribute__((unused)),
+                         struct jmap_parser *parser,
+                         json_t *filter,
+                         json_t *unsupported __attribute__((unused)),
+                         void *rock __attribute__((unused)),
+                         json_t **err __attribute__((unused)))
+{
+    const char *field;
+    json_t *arg;
+
+    json_object_foreach(filter, field, arg) {
+        if (!strcmp(field, "name")) {
+            if (!json_is_string(arg)) {
+                jmap_parser_invalid(parser, field);
+            }
+        }
+        else if (!strcmp(field, "scope")) {
+            if (!json_is_string(arg)) {
+                jmap_parser_invalid(parser, field);
+            }
+        }
+        else if (!strcmp(field, "resourceType")) {
+            if (!json_is_string(arg)) {
+                jmap_parser_invalid(parser, field);
+            }
+        }
+        else if (!strcmp(field, "type")) {
+            if (!json_is_string(arg)) {
+                jmap_parser_invalid(parser, field);
+            }
+        }
+        else {
+            jmap_parser_invalid(parser, field);
+        }
+    }
+}
+
+
+static int comparator_parse(jmap_req_t *req __attribute__((unused)),
+                            struct jmap_comparator *comp,
+                            void *rock __attribute__((unused)),
+                            json_t **err __attribute__((unused)))
+{
+    if (comp->collation) {
+        return 0;
+    }
+    if (!strcmp(comp->property, "name") ||
+        !strcmp(comp->property, "used")) {
+        return 1;
+    }
+    return 0;
+}
+
+typedef struct filter {
+    const char *name;
+    const char *scope;
+    const char *resourceType;
+    const char *type;
+} filter;
+
+static void *filter_build(json_t *arg)
+{
+    filter *f = (filter *) xzmalloc(sizeof(struct filter));
+
+    /* name */
+    if (JNOTNULL(json_object_get(arg, "name"))) {
+        jmap_readprop(arg, "name", 0, NULL, "s", &f->name);
+    }
+
+    /* scope */
+    if (JNOTNULL(json_object_get(arg, "scope"))) {
+        jmap_readprop(arg, "scope", 0, NULL, "s", &f->scope);
+    }
+
+    /* resourceType */
+    if (JNOTNULL(json_object_get(arg, "resourceType"))) {
+        jmap_readprop(arg, "resourceType", 0, NULL, "s", &f->resourceType);
+    }
+
+    /* type */
+    if (JNOTNULL(json_object_get(arg, "type"))) {
+        jmap_readprop(arg, "type", 0, NULL, "s", &f->type);
+    }
+
+    return f;
+}
+
+/* Match the script in rock against filter. */
+static int filter_match(void *vf, void *rock)
+{
+    filter *f = (filter *) vf;
+    struct jquota_root_t *jroot = (struct jquota_root_t *) rock;
+
+    /* name */
+    if (f->name && !strstr(jroot->name, f->name)) return 0;
+
+    /* scope */
+    if (f->scope && !strstr("account", f->scope)) return 0;
+
+    /* resourceType */
+    if (f->resourceType && !strstr(jroot->junits, f->resourceType)) return 0;
+
+    /* type */
+    if (f->type) {
+        const struct jtype_t *jtype;
+
+        for (jtype = jtypes; jtype->name; jtype++) {
+            if ((jroot->type_mask & jtype->bit) && strstr(jtype->name, f->type)) {
+                break;
+            }
+        }
+        if (!jtype->name) return 0;
+    }
+
+    /* All matched. */
+    return 1;
+}
+
+typedef struct filter_rock {
+    struct jmap_query *query;
+    jmap_filter *parsed_filter;
+    ptrarray_t matches;
+    struct jquota_root_t *anchor;
+} filter_rock;
+
+static void filter_cb(const char *id, void *val, void *rock)
+{
+    struct jquota_root_t *jroot = val;
+    struct filter_rock *frock = (struct filter_rock *) rock;
+    struct jmap_query *query = frock->query;
+
+    if (query->filter &&
+        !jmap_filter_match(frock->parsed_filter, &filter_match, jroot)) {
+        return;
+    }
+
+    jroot->id = xstrdup(id);
+
+    /* Add record of the match to our array */
+    ptrarray_append(&frock->matches, jroot);
+
+    if (query->anchor && !strcmp(query->anchor, jroot->id)) {
+        /* Mark record corresponding to anchor */
+        frock->anchor = jroot;
+    }
+
+    query->total++;
+}
+
+enum quota_sort {
+    QUOTA_SORT_NONE = 0,
+    QUOTA_SORT_NAME,
+    QUOTA_SORT_USED,
+    QUOTA_SORT_DESC = 0x80 /* bit-flag for descending sort */
+};
+
+static int quota_cmp QSORT_R_COMPAR_ARGS(const void *va, const void *vb,
+                                         void *rock)
+{
+    arrayu64_t *sortcrit = (arrayu64_t *) rock;
+    struct jquota_root_t *qa = (struct jquota_root_t *) *(void **) va;
+    struct jquota_root_t *qb = (struct jquota_root_t *) *(void **) vb;
+    size_t i, nsort = arrayu64_size(sortcrit);
+
+    for (i = 0; i < nsort; i++) {
+        enum quota_sort sort = arrayu64_nth(sortcrit, i);
+        int ret = 0;
+
+        switch (sort & ~QUOTA_SORT_DESC) {
+        case QUOTA_SORT_NAME:
+            ret = strcmp(qa->name, qb->name);
+            break;
+
+        case QUOTA_SORT_USED:
+            ret = qa->used - qb->used;
+            break;
+        }
+
+        if (ret) return (sort & QUOTA_SORT_DESC) ? -ret : ret;
+    }
+
+    return 0;
+}
+
+static int jmap_quota_query(jmap_req_t *req)
+{
+    struct jmap_parser parser = JMAP_PARSER_INITIALIZER;
+    struct jmap_query query;
+    jmap_filter *parsed_filter = NULL;
+    arrayu64_t sortcrit = ARRAYU64_INITIALIZER;
+    struct qrock_t qrock = { req, NULL, { 0 }, NULL, HASH_TABLE_INITIALIZER };
+    int r = 0;
+
+    /* Parse request */
+    json_t *err = NULL;
+    jmap_query_parse(req, &parser, NULL, NULL,
+                     filter_parse, NULL, comparator_parse, NULL, &query, &err);
+    if (err) {
+        jmap_error(req, err);
+        goto done;
+    }
+
+    /* Build filter */
+    if (JNOTNULL(query.filter)) {
+        parsed_filter = jmap_buildfilter(query.filter, filter_build);
+    }
+
+    /* Build sort */
+    if (json_array_size(query.sort)) {
+        json_t *jval;
+        size_t i;
+        json_array_foreach(query.sort, i, jval) {
+            const char *prop =
+              json_string_value(json_object_get(jval, "property"));
+            enum quota_sort sort = QUOTA_SORT_NONE;
+
+            if (!strcmp(prop, "name")) {
+                sort = QUOTA_SORT_NAME;
+            } else if (!strcmp(prop, "used")) {
+                sort = QUOTA_SORT_USED;
+            }
+
+            if (json_object_get(jval, "isAscending") == json_false()) {
+                sort |= QUOTA_SORT_DESC;
+            }
+
+            arrayu64_append(&sortcrit, sort);
+        }
+    }
+    qrock.inboxname = mboxname_user_mbox(req->accountid, NULL);
+    construct_hash_table(&qrock.quotas, 20, 0);
+
+    /* Fetch quotaroots for the user */
+    fetch_quotas(&qrock);
+
+    /* Filter the scripts */
+    filter_rock frock = { &query, parsed_filter, PTRARRAY_INITIALIZER, NULL };
+
+    hash_enumerate(&qrock.quotas, &filter_cb, &frock);
+
+    /* Sort results */
+    if (arrayu64_size(&sortcrit)) {
+        cyr_qsort_r(frock.matches.data, frock.matches.count,
+                    sizeof(void *), &quota_cmp, &sortcrit);
+    }
+    arrayu64_fini(&sortcrit);
+
+    /* Process results */
+    if (query.anchor) {
+        query.position = ptrarray_find(&frock.matches, frock.anchor, 0);
+        if (query.position < 0) {
+            query.position = query.total;
+        }
+        else {
+            query.position += query.anchor_offset;
+        }
+    }
+    else if (query.position < 0) {
+        query.position += query.total;
+    }
+    if (query.position < 0) query.position = 0;
+
+    size_t i;
+    for (i = 0; i < query.total; i++) {
+        struct jquota_root_t *match = ptrarray_nth(&frock.matches, i);
+
+        /* Apply position and limit */
+        if (i >= (size_t) query.position &&
+            (!query.limit || query.limit > json_array_size(query.ids))) {
+            /* Add the quota identifier */
+            json_array_append_new(query.ids, json_string(match->id));
+        }
+
+        free(match->id);
+    }
+    ptrarray_fini(&frock.matches);
+
+    if (parsed_filter) jmap_filter_free(parsed_filter, &free);
+
+    /* Build response */
+    modseq_t quotamodseq = mboxname_readquotamodseq(qrock.inboxname);
+    struct buf buf = BUF_INITIALIZER;
+    buf_printf(&buf, MODSEQ_FMT, quotamodseq);
+    query.query_state = buf_release(&buf);
+    query.result_position = query.position;
+    query.can_calculate_changes = 0;
+    jmap_ok(req, jmap_query_reply(&query));
+
+done:
+    if (r) jmap_error(req, jmap_server_error(r));
+    jmap_parser_fini(&parser);
+    jmap_query_fini(&query);
+
+    free_hash_table(&qrock.quotas, &free);
+    free(qrock.inboxname);
+
     return 0;
 }
