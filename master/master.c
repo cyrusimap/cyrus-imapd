e6a417519b16cdef9b0af5950f7efac8cf247901
Bron Gondwana <brong@fastmailteam.com>
2024-09-15 21:10:36 +1000

    master: add the 'ERROR' string to multiple failure errors
    This helps with log monitoring

--- master/master.c
+++ master/master.c
@@ -1426,14 +1426,14 @@ static void reap_child(void)
                         s->lastreadyfail = now;
                         if (++s->nreadyfails >= MAX_READY_FAILS && s->exec) {
                             if (s->babysit) {
-                                syslog(LOG_ERR, "too many failures for"
+                                syslog(LOG_ERR, "ERROR: too many failures for"
                                        "service %s/%s, disabling for %d seconds",
                                        SERVICEPARAM(s->name),
                                        SERVICEPARAM(s->familyname),
                                        MAX_READY_FAIL_INTERVAL);
                             }
                             else {
-                                syslog(LOG_ERR, "too many failures for "
+                                syslog(LOG_ERR, "ERROR: too many failures for "
                                        "service %s/%s, disabling until next SIGHUP",
                                        SERVICEPARAM(s->name),
                                        SERVICEPARAM(s->familyname));
