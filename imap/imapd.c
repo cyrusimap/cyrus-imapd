8402ff062d1c1fbd34024da72d74bf854b3daaed
Ken Murchison <murch@fastmail.com>
2024-11-20 11:19:44 -0500

    imapd.c: fix proxying UID Search (issue #5047)

--- imap/imapd.c
+++ imap/imapd.c
@@ -412,7 +412,7 @@ static int parse_fetch_args(const char *tag, const char *cmd,
                             struct fetchargs *fa);
 static void cmd_fetch(char *tag, char *sequence, int usinguid);
 static void cmd_store(char *tag, char *sequence, int usinguid);
-static void cmd_search(char *tag, char *cmd);
+static void cmd_search(const char *tag, const char *cmd);
 static void cmd_sort(char *tag, int usinguid);
 static void cmd_thread(char *tag, int usinguid);
 static void cmd_copy(char *tag, char *sequence, char *name, int usinguid, int ismove);
@@ -5760,7 +5760,7 @@ static int multisearch_cb(const mbentry_t *mbentry, void *rock)
     return 0;
 }
 
-static void cmd_search(char *tag, char *cmd)
+static void cmd_search(const char *tag, const char *cmd)
 {
     int c;
     struct searchargs *searchargs;
@@ -5771,6 +5771,8 @@ static void cmd_search(char *tag, char *cmd)
 
     if (backend_current) {
         /* remote mailbox */
+        if (cmd[0] == 'U') cmd = "UID Search";
+
         prot_printf(backend_current->out, "%s %s ", tag, cmd);
         if (!pipe_command(backend_current, 65536)) {
             pipe_including_tag(backend_current, tag, 0);
