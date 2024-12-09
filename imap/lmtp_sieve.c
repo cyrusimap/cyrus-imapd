3e45fd3504698215b86a38905344964303b66a11
Ricardo Signes <rjbs@semiotic.systems>
2024-12-09 09:49:21 -0500

    Sieve: allow private Final-Recipient in rejects
    This replaces a longstanding private commit in Fastmail's Cyrus, which
    replaces Final-Recipient with an encrypted version of the original
    recipient.  The goal is to provide a string that can be traced back to a
    recipient by the Cyrus operator, but doesn't disclose the final delivery
    username to the bounce recipient.
    
    Using session_id should accomplish the same result, at least if logs are
    maintained.  It's also a lot less complex than shipping our own little
    RC4 implementation!

--- imap/lmtp_sieve.c
+++ imap/lmtp_sieve.c
@@ -492,7 +492,12 @@ static int send_rejection(const char *userid,
             config_servername, CYRUS_VERSION, SIEVE_VERSION);
     if (origreceip)
         buf_printf(&msgbuf, "Original-Recipient: rfc822; %s\r\n", origreceip);
-    buf_printf(&msgbuf, "Final-Recipient: rfc822; %s\r\n", mailreceip);
+
+    if (config_getswitch(IMAPOPT_SIEVE_MDN_PRIVATE))
+        buf_printf(&msgbuf, "Final-Recipient: rfc822; %s\r\n", session_id());
+    else
+        buf_printf(&msgbuf, "Final-Recipient: rfc822; %s\r\n", mailreceip);
+
     if (origid)
         buf_printf(&msgbuf, "Original-Message-ID: %s\r\n", origid);
     buf_printf(&msgbuf, "Disposition: "
