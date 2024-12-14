f329ab4f5d62f6a180cf2fb9edd9dea8657b9a21
Ricardo Signes <rjbs@semiotic.systems>
2024-12-14 18:06:07 -0500

    Cassandane fakesmtp: use 127.0.0.1, not localhost
    By avoiding ::1, we will avoid weird problems with IO::Socket::IP and
    getaddrinfo on hosts (Docker containers, in my experience) that have no
    inet6 on eth0 but have it on loopback.

--- cassandane/Cassandane/Instance.pm
+++ cassandane/Cassandane/Instance.pm
@@ -1060,7 +1060,7 @@ sub _start_smtpd
 
     my $basedir = $self->{basedir};
 
-    my $host = 'localhost';
+    my $host = '127.0.0.1';
 
     my $port = Cassandane::PortManager::alloc();
 
@@ -1120,7 +1120,7 @@ sub start_httpd {
 
     my $basedir = $self->{basedir};
 
-    my $host = 'localhost';
+    my $host = '127.0.0.1';
     $port ||= Cassandane::PortManager::alloc();
 
     my $httpdpid = fork();
