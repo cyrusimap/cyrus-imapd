58da97380c216db43266bf05f7cc09908630c2b0
Ricardo Signes <rjbs@semiotic.systems>
2025-01-02 20:47:27 -0500

    docs: strip out custom theming and use stock RTD
    This means that the box building the docs will require python3-sphinx-rtd-theme
    (or however else you get that theme).
    
    Why?  Because we don't want to keep having to figure out how our custom
    theming broke across a Sphinx upgrade!

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -144,7 +144,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = 'cyrus'
+html_theme = 'sphinx_rtd_theme'
 
 html_context = {
   "display_github": True, # Add 'Edit on Github' link instead of 'View page source'
