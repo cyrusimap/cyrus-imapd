6c25190dd115698f3865c1ae211c0bc9ffefc840
Ken Murchison <murch@fastmail.com>
2025-01-10 14:05:44 -0500

    mailbox.c: use struct timespec to store internaldate

--- imap/message.c
+++ imap/message.c
@@ -5299,7 +5299,7 @@ EXPORTED int message_get_savedate(message_t *m, time_t *datep)
     int r = message_need(m, M_RECORD);
     if (r) return r;
     *datep = m->record.savedate;
-    if (!*datep) *datep = m->record.internaldate;
+    if (!*datep) *datep = m->record.internaldate.tv_sec;
     return 0;
 }
 
@@ -5331,7 +5331,7 @@ EXPORTED int message_get_internaldate(message_t *m, time_t *datep)
 {
     int r = message_need(m, M_RECORD);
     if (r) return r;
-    *datep = m->record.internaldate;
+    *datep = m->record.internaldate.tv_sec;
     return 0;
 }
 
