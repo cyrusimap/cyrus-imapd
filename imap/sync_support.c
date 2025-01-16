e4a952336b5adcda134f049b55cbef2899de6b2c
Bron Gondwana <brong@fastmailteam.com>
2025-01-16 23:58:36 +1100

    sync_support: uncache BEFORE freeing memory

--- imap/sync_support.c
+++ imap/sync_support.c
@@ -6551,11 +6551,11 @@ int sync_do_update_mailbox(struct sync_client_state *sync_cs,
         sync_send_apply(kl, sync_cs->backend->out);
         r = sync_parse_response("MAILBOX", sync_cs->backend->in, NULL);
 
-        dlist_free(&kl);
-        mboxlist_entry_free(&mbentry);
-
         // we never want to cache intermediate records
         sync_uncache(sync_cs, mbentry->name);
+
+        dlist_free(&kl);
+        mboxlist_entry_free(&mbentry);
 
         return r;
     }
