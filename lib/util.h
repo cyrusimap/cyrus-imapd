368b4da87d8a0f2ab8796fde1477fb242f85e11c
Ken Murchison <murch@fastmail.com>
2025-02-20 14:03:57 -0500

    util.h: asan/ubsan fix

--- lib/util.h
+++ lib/util.h
@@ -160,7 +160,8 @@ extern const unsigned char convert_to_uppercase[256];
 }
 #endif
 
-#define TIMESPEC_TO_NANOSEC(ts) ((ts)->tv_sec * 1000000000 + (ts)->tv_nsec)
+#define TIMESPEC_TO_NANOSEC(ts) \
+    ((uint64_t) (ts)->tv_sec * 1000000000 + (ts)->tv_nsec)
 
 #define TIMESPEC_FROM_NANOSEC(ts, nanosec) {    \
         (ts)->tv_sec  = (nanosec) / 1000000000; \
