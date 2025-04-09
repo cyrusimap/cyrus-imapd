018ee0c42800c0db1dbea15cc36a0aa96becc343
ellie timoney <ellie@fastmail.com>
2025-04-09 11:36:01 +1000

    various: add braces where clang-format would unwrap badly
    This may be obsolete if we switch to an always-braces style enforced
    by clang-format

--- imap/annotate.c
+++ imap/annotate.c
@@ -457,9 +457,11 @@ EXPORTED size_t sizeentryatts(const struct entryattlist *l)
     size_t sz = 0;
     struct attvaluelist *av;
 
-    for ( ; l ; l = l->next)
-        for (av = l->attvalues ; av ; av = av->next)
+    for ( ; l ; l = l->next) {
+        for (av = l->attvalues ; av ; av = av->next) {
             sz += av->value.len;
+        }
+    }
     return sz;
 }
 
