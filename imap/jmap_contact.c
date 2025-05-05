cyrusimap/cyrus-imapd

commit 0275f902dd3f84dc23d2a802d323c2b66bd5c56f

jmap_contact.c: cache X-ABADR group names case-insensitively
Fixes a regression introduced in 19aec717541fe03c3c29d5032256256e49701afb

imap/jmap_contact.c
@@ -1623,8 +1623,11 @@ static json_t *jmap_contact_from_vcard(const char *userid,
     construct_hash_table(&abadr_by_group, nabadr + 1, 0);
     for (struct vparse_entry *it = card->properties; it; it = it->next) {
         if (!strcasecmp(it->name, VCARD_APPLE_ABADR_PROPERTY)) {
-            if (it->group && it->v.value)
-                hash_insert(it->group, xstrdup(it->v.value), &abadr_by_group);
+            if (it->group && it->v.value) {
+                buf_setcstr(&buf, it->group);
+                hash_insert(
+                    buf_ucase(&buf), xstrdup(it->v.value), &abadr_by_group);
+            }
         }
     }
 
@@ -1698,7 +1701,8 @@ static json_t *jmap_contact_from_vcard(const char *userid,
             /* Read countryCode from same-grouped ABADR property, if any */
             const char *countrycode = NULL;
             if (entry->group) {
-                countrycode = hash_lookup(entry->group, &abadr_by_group);
+                buf_setcstr(&buf, entry->group);
+                countrycode = hash_lookup(buf_ucase(&buf), &abadr_by_group);
             }
 
             json_object_set_new(item, "street",

commit 136d96ff1aa331ee921e6daaf33d9fe6c87a5775

jmap_contact.c: Don't stomp on street property with group name
buf is used to build up the elements of 'street' and then added to the
response entry. We can't overwrite buf until 'street' has been written out,
so we'll move the countrycode logic below that.

Fixes a regression introduced in 0275f902dd

imap/jmap_contact.c
@@ -1698,13 +1698,6 @@ static json_t *jmap_contact_from_vcard(const char *userid,
                 buf_appendcstr(&buf, street);
             }
 
-            /* Read countryCode from same-grouped ABADR property, if any */
-            const char *countrycode = NULL;
-            if (entry->group) {
-                buf_setcstr(&buf, entry->group);
-                countrycode = hash_lookup(buf_ucase(&buf), &abadr_by_group);
-            }
-
             json_object_set_new(item, "street",
                                 jmap_utf8string(buf_cstring(&buf)));
             json_object_set_new(item, "locality",
@@ -1715,11 +1708,18 @@ static json_t *jmap_contact_from_vcard(const char *userid,
                                 jmap_utf8string(strarray_safenth(a, 5)));
             json_object_set_new(item, "country",
                                 jmap_utf8string(strarray_safenth(a, 6)));
-            if (countrycode) {
-                buf_setcstr(&buf, countrycode);
-                buf_lcase(&buf);
-                json_object_set_new(item, "countryCode",
-                        jmap_utf8string(buf_cstring(&buf)));
+
+            /* Read countryCode from same-grouped ABADR property, if any */
+            if (entry->group) {
+                buf_setcstr(&buf, entry->group);
+                const char *countrycode = hash_lookup(buf_ucase(&buf), &abadr_by_group);
+
+                if (countrycode) {
+                    buf_setcstr(&buf, countrycode);
+                    buf_lcase(&buf);
+                    json_object_set_new(item, "countryCode",
+                            jmap_utf8string(buf_cstring(&buf)));
+                }
             }
 
             json_array_append_new(adr, item);

cassandane/tiny-tests/JMAPContacts/contact_get_apple_countrycode
@@ -48,5 +48,9 @@ EOF
     ]);
     $self->assert_str_equals('us', $res->[0][1]{list}[0]{addresses}[0]{countryCode});
     $self->assert_str_equals('xyz', $res->[0][1]{list}[0]{addresses}[0]{label});
+    $self->assert_str_equals('2 Example Avenue', $res->[0][1]{list}[0]{addresses}[0]{street});
+
     $self->assert_str_equals('de', $res->[0][1]{list}[0]{addresses}[1]{countryCode});
+    $self->assert_str_equals('Beispielstrasse 2', $res->[0][1]{list}[0]{addresses}[1]{street});
+
 }