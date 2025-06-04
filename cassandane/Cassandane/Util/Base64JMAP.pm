f7a6923d8f7f6b97bb15979049e5fe9b113d6adc
Matthew Horsfall <wolfsage@gmail.com>
2025-06-04 08:31:15 -0400

    Cass: Fixup encode_base64jmap and add decode_base64jmap and tests
    encode_base64jmap failed to encode '-' since it was being treated as a range
    character in the tr// rather than a literal, so we must escape it first with
    '\'.
    
    This caused heisenfails in Cyrus::Conversations.move_200 when the ids would
    contain a '-' on occasion.
    
    Add tests, and also add a related decoder incase tests need it.

--- cassandane/Cassandane/Util/Base64JMAP.pm
+++ cassandane/Cassandane/Util/Base64JMAP.pm
@@ -41,22 +41,32 @@ package Cassandane::Util::Base64JMAP;
 use strict;
 use warnings;
 use base qw(Exporter);
-use MIME::Base64 qw(encode_base64url);
+use MIME::Base64 qw(encode_base64url decode_base64url);
 
 use lib '.';
 use Cassandane::Util::Log;
 
-our @EXPORT = qw(&encode_base64jmap);
+our @EXPORT = qw(&encode_base64jmap &decode_base64jmap);
 
 sub encode_base64jmap
 {
     my ($bytes) = @_;
     my $encoded = encode_base64url($bytes, '');
 
-    $encoded =~ tr{ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_}
+    $encoded =~ tr{ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\-_}
                   {-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz};
 
     return $encoded;
+}
+
+sub decode_base64jmap
+{
+    my $encoded = shift;
+
+    $encoded =~ tr{-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz}
+                  {ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\-_};
+
+    return decode_base64url($encoded);
 }
 
 1;
