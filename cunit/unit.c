08b0d451bb116af57b9ea073a09881bc0464cdc6
ellie timoney <ellie@fastmail.com>
2025-07-14 10:17:14 +1000

    cunit: clean up test registry handling

--- cunit/unit.c
+++ cunit/unit.c
@@ -51,17 +51,15 @@
 #include <valgrind/valgrind.h>
 #endif
 #include <setjmp.h>
-#include "timeout.h"
-#include "cyrunit.h"
-#include "util.h"
-#include "xmalloc.h"
+#include "cunit/cyrunit.h"
+#include "cunit/registry.h"
+#include "cunit/timeout.h"
 
-#include "lib/retry.h"
 #include "lib/libconfig.h"
+#include "lib/retry.h"
+#include "lib/util.h"
+#include "lib/xmalloc.h"
 #include "lib/xunlink.h"
-
-/* generated headers are not necessarily in current directory */
-#include "cunit/registers.h"
 
 int verbose = 0;
 int num_testspecs = 0;
