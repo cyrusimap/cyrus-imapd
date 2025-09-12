372683cecbc4c9ebea80558467ad739d3344b4f9
Bron Gondwana <brong@fastmailteam.com>
2025-09-12 10:43:27 -0400

    imapd: treat 'replicaonly' the same as 'readonly'
    It's never going to be a good idea to run commands that can't be run
    in readonly when you're on a replica server.

--- imap/imapd.c
+++ imap/imapd.c
@@ -1498,7 +1498,7 @@ static void cmdloop(void)
     struct sync_reserve_list *reserve_list =
         sync_reserve_list_create(SYNC_MESSAGE_LIST_HASH_SIZE);
     struct applepushserviceargs applepushserviceargs;
-    int readonly = config_getswitch(IMAPOPT_READONLY);
+    int readonly = config_getswitch(IMAPOPT_READONLY) || config_getswitch(IMAPOPT_REPLICAONLY);
     int syntax_errors = 0;
     const int syntax_errors_limit = 10; /* XXX make this configurable? */
     unsigned command_count = 0;
@@ -2465,6 +2465,9 @@ static void cmdloop(void)
             }
             else if (!strcmp(cmd.s, "Syncapply")) {
                 if (!imapd_userisadmin) goto adminsonly;
+                // we don't check the readonly variable here because syncapply
+                // CAN run with `replicaonly` set.
+                if (config_getswitch(IMAPOPT_READONLY)) goto noreadonly;
 
                 struct dlist *kl = sync_parseline(imapd_in, sync_archive_enabled);
 
@@ -2495,6 +2498,9 @@ static void cmdloop(void)
             }
             else if (!strcmp(cmd.s, "Syncrestore")) {
                 if (!imapd_userisadmin) goto adminsonly;
+                // we don't check the readonly variable here because syncapply
+                // CAN run with `replicaonly` set.
+                if (config_getswitch(IMAPOPT_READONLY)) goto noreadonly;
 
                 struct dlist *kl = sync_parseline(imapd_in, sync_archive_enabled);
 
@@ -5013,7 +5019,9 @@ static void cmd_select(char *tag, char *cmd, char *name)
     init.userid = imapd_userid;
     init.authstate = imapd_authstate;
     init.out = imapd_out;
-    init.examine_mode = (cmd[0] == 'E') || config_getswitch(IMAPOPT_READONLY);
+    init.examine_mode = (cmd[0] == 'E')
+                     || config_getswitch(IMAPOPT_READONLY)
+                     || config_getswitch(IMAPOPT_REPLICAONLY);
     init.select = 1;
     init.stay_locked = 1;
     if (!strcasecmpsafe(imapd_magicplus, "+dav")) init.want_dav = 1;
