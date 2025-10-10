cyrusimap/cyrus-imapd

commit edf8d09d6cf5bc0bbbdf58182b774f7ac51a2382

imap: delete INBOX last when deleting a user

imap/imapd.c
@@ -7485,6 +7485,16 @@ static void cmd_delete(char *tag, char *name, int localonly, int force)
         goto done;
     }
 
+    /* was it a top-level user mailbox? delete all the child mailboxes first */
+    /* localonly deletes are only per-mailbox */
+    delete_user = mboxname_isusermailbox(mbname_intname(mbname), 1);
+    if (!r && !localonly && delete_user) {
+        const char *userid = mbname_userid(mbname);
+        if (userid) {
+            r = mboxlist_usermboxtree(userid, NULL, delmbox, NULL, MBOXTREE_INTERMEDIATES|MBOXTREE_SKIP_ROOT);
+        }
+    }
+
     mboxevent = mboxevent_new(EVENT_MAILBOX_DELETE);
 
     /* local mailbox */
@@ -7501,7 +7511,6 @@ static void cmd_delete(char *tag, char *name, int localonly, int force)
             r = IMAP_PERMISSION_DENIED;
         }
         else {
-            delete_user = mboxname_isusermailbox(mbname_intname(mbname), 1);
             int delflags = (1-force) ? MBOXLIST_DELETE_CHECKACL : 0;
 
             if (!delete_user && mboxlist_haschildren(mbname_intname(mbname))) {
@@ -7519,6 +7528,9 @@ static void cmd_delete(char *tag, char *name, int localonly, int force)
                                                    imapd_authstate, mboxevent,
                                                    delflags);
             }
+            if (!r && !localonly && delete_user) {
+                r = user_deletedata(mbentry, 1);
+            }
         }
     }
 
@@ -7526,16 +7538,6 @@ static void cmd_delete(char *tag, char *name, int localonly, int force)
     if (!r)
         mboxevent_notify(&mboxevent);
     mboxevent_free(&mboxevent);
-
-    /* was it a top-level user mailbox? */
-    /* localonly deletes are only per-mailbox */
-    if (!r && !localonly && delete_user) {
-        const char *userid = mbname_userid(mbname);
-        if (userid) {
-            r = mboxlist_usermboxtree(userid, NULL, delmbox, NULL, MBOXTREE_INTERMEDIATES);
-            if (!r) r = user_deletedata(mbentry, 1);
-        }
-    }
 
     if (!r && config_getswitch(IMAPOPT_DELETE_UNSUBSCRIBE)) {
         mboxlist_changesub(mbname_intname(mbname), imapd_userid, imapd_authstate,

commit 36e594b2725c067ff016d3c38ba9397ab1b0f990

INBOX Delete test (thanks alh)
This checks for a bug where a non-admin deleting an entire user would
cause each subfolder to be deleted before the INBOX; but was missing
the "must be admin" check

The bug was introduced in edf8d09d6cf5bc0bbbdf58182b774f7ac51a2382

cassandane/tiny-tests/JMAPSieve/sieve_set
@@ -2,7 +2,7 @@
 use Cassandane::Tiny;
 
 sub test_sieve_set
-    :min_version_3_3 :JMAPExtensions
+    :min_version_3_3 :JMAPExtensions :MagicPlus :NoAltNamespace
 {
     my ($self) = @_;
 
@@ -157,4 +157,14 @@ EOF
     $self->assert_not_null($res->[1][1]{destroyed});
     $self->assert_null($res->[1][1]{notDestroyed});
     $self->assert_num_equals(1, scalar @{$res->[2][1]{list}});
+    my $talk = $self->{store}->get_client();
+    $talk->getacl("INBOX");
+
+    $talk->delete("INBOX");
+    my $plusstore = $self->{instance}->get_service('imap')->create_store(username => 'cassandane+dav');
+    my $imaptalk = $plusstore->get_client();
+    $res = $imaptalk->select("INBOX.#sieve");
+    $self->assert_str_equals("read-write", $res);
+
+
 }