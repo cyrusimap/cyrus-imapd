b1ad32ab333f08d34b21b04414676555b311622f
ellie timoney <ellie@fastmail.com>
2025-10-13 15:25:24 +1100

    logfmt: add utf8 support

--- lib/logfmt.c
+++ lib/logfmt.c
@@ -45,17 +45,43 @@
 #include "lib/logfmt.h"
 #include "lib/sessionid.h"
 
+#include "unicode/uchar.h"
+#include "unicode/utext.h"
+#include "unicode/utf8.h"
+#include "unicode/utypes.h"
+
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
 
-/* visible for testing; probably don't call this directly */
-EXPORTED void logfmt_escape(struct buf *buf, const char *val)
+static bool val_needs_dquotes(const char *val)
 {
-    bool need_dquotes = false;
     const unsigned char *p;
+
+    for (p = (const unsigned char *) val; *p; p++) {
+        switch (*p) {
+        case 0x21:
+        case 0x23 ... 0x3C:
+        case 0x3E ... 0x5B:
+        case 0X5D ... 0x7E:
+            /* okchr doesn't need quotes */
+            break;
+        default:
+            /* but anything else does */
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/* visible for testing; probably don't call this directly */
+EXPORTED void logfmt_escape_bytestring(struct buf *buf, const char *val)
+{
+    const unsigned char *p;
+    bool need_dquotes;
 
     if (val == NULL) {
         buf_setcstr(buf, "~null~");
@@ -80,22 +106,7 @@ EXPORTED void logfmt_escape(struct buf *buf, const char *val)
      * quoted-ok       = SP / "="
      * eightbit        = %x80-ff
      */
-
-    /* partial first pass to see whether we'll need to quote */
-    for (p = (const unsigned char *) val; *p && !need_dquotes; p++) {
-        switch (*p) {
-        case 0x21:
-        case 0x23 ... 0x3C:
-        case 0x3E ... 0x5B:
-        case 0X5D ... 0x7E:
-            /* okchr doesn't need quotes */
-            break;
-        default:
-            /* but anything else does */
-            need_dquotes = true;
-            break;
-        }
-    }
+    need_dquotes = val_needs_dquotes(val);
 
     /* build escaped string forward */
     buf_reset(buf);
@@ -131,17 +142,12 @@ EXPORTED void logfmt_escape(struct buf *buf, const char *val)
             buf_appendmap(buf, "\\\\", 2);
             break;
 
-        /* escaped-hex */
+        /* escaped-hex, eightbit */
         case 0x00 ... 0x08:
         case 0x0B ... 0x0C:
         case 0x0E ... 0x1F:
         case 0x7F:
-            buf_printf(buf, "\\x{%2.2x}", *p);
-            break;
-
-        /* eightbit */
         case 0x80 ... 0xFF:
-            /* XXX unicode will require different behaviour here... */
             buf_printf(buf, "\\x{%2.2x}", *p);
             break;
 
@@ -154,6 +160,113 @@ EXPORTED void logfmt_escape(struct buf *buf, const char *val)
     }
 
     if (need_dquotes) buf_putc(buf, '\"');
+}
+
+static inline bool is_utf8_escaped_hex(uint32_t cp)
+{
+    switch (cp) {
+    case 0x000b: /* LINE TABULATION */
+    case 0x000c: /* FORM FEED */
+    case 0x0085: /* NEXT LINE */
+    case 0x2028: /* LINE SEPARATOR */
+    case 0x2029: /* PARAGRAPH SEPARATOR */
+        return true;
+    default:
+        return u_iscntrl(cp);
+    }
+}
+
+/* visible for testing; probably don't call this directly */
+EXPORTED void logfmt_escape_utf8(struct buf *buf, const char *utf8val)
+{
+    UText ut = UTEXT_INITIALIZER; /* XXX cache between calls? */
+    UErrorCode status = U_ZERO_ERROR;
+    bool need_dquotes;
+    uint32_t cp;
+
+    if (utf8val == NULL) {
+        buf_setcstr(buf, "~null~");
+        return;
+    }
+
+    if (!utf8val[0]) {
+        buf_setcstr(buf, "\"\"");
+        return;
+    }
+
+    /* utext_openUTF8:
+     * > Any invalid UTF-8 in the input will be handled in this way: a sequence
+     * > of bytes that has the form of a truncated, but otherwise valid, UTF-8
+     * > sequence will be replaced by a single unicode replacement character,
+     * > \uFFFD. Any other illegal bytes will each be replaced by a \uFFFD.
+     *
+     * The "replacement character" \uFFFD is �.  This character doesn't need to
+     * be escaped for logfmt, so we don't need (and can't have) explicit error
+     * handling for invalid utf8.
+     */
+    utext_openUTF8(&ut, utf8val, -1, &status);
+    if (U_FAILURE(status)) {
+        /* it's not clear what circumstances this might error in, but fall back
+         * to a bytestring escape anyway
+         */
+        syslog(LOG_DEBUG, "%s: utext_openUTF8() failed with status %s",
+                          __func__, u_errorName(status));
+        logfmt_escape_bytestring(buf, utf8val);
+        return;
+    }
+
+    need_dquotes = val_needs_dquotes(utf8val);
+
+    buf_reset(buf);
+    buf_ensure(buf, strlen(utf8val) + 2 * need_dquotes);
+    if (need_dquotes) buf_putc(buf, '"');
+
+    while ((uint32_t) U_SENTINEL != (cp = utext_next32(&ut))) {
+        uint8_t scratch[8] = {0};
+        int32_t i, len = 0;
+        UBool is_error = false;
+
+        /* escaped-special */
+        switch (cp) {
+        case '\\':
+            buf_appendmap(buf, "\\\\", 2);
+            continue;
+        case '"':
+            buf_appendmap(buf, "\\\"", 2);
+            continue;
+        case '\n':
+            buf_appendmap(buf, "\\n", 2);
+            continue;
+        case '\r':
+            buf_appendmap(buf, "\\r", 2);
+            continue;
+        case '\t':
+            buf_appendmap(buf, "\\t", 2);
+            continue;
+        }
+
+        /* rebuild the utf8 bytes into scratch */
+        U8_APPEND(scratch, len, (int32_t) sizeof(scratch), cp, is_error);
+        if (is_error) {
+            /* only errors if scratch is too small? should never happen */
+            syslog(LOG_DEBUG, "%s: U8_APPEND reported an error", __func__);
+            logfmt_escape_bytestring(buf, utf8val);
+            return;
+        }
+
+        if (is_utf8_escaped_hex(cp)) {
+            /* escaped-hex */
+            for (i = 0; i < len; i++)
+                buf_printf(buf, "\\x{%2.2x}", scratch[i]);
+        }
+        else {
+            /* eightbit */
+            buf_appendmap(buf, (char *) scratch, len);
+        }
+    }
+    utext_close(&ut);
+
+    if (need_dquotes) buf_putc(buf, '"');
 }
 
 EXPORTED void logfmt_init(struct logfmt *lf, const char *event)
@@ -183,7 +296,21 @@ EXPORTED void logfmt_push(struct logfmt *lf,
     if (buf_len(&lf->msg))
         buf_putc(&lf->msg, ' ');
 
-    logfmt_escape(&lf->scratch, value);
+    logfmt_escape_bytestring(&lf->scratch, value);
+    buf_printf(&lf->msg, "%s=%s", key, buf_cstring(&lf->scratch));
+    buf_reset(&lf->scratch);
+}
+
+EXPORTED void logfmt_push_utf8(struct logfmt *lf,
+                               const char *key,
+                               const char *value)
+{
+    assert(key && *key);
+
+    if (buf_len(&lf->msg))
+        buf_putc(&lf->msg, ' ');
+
+    logfmt_escape_utf8(&lf->scratch, value);
     buf_printf(&lf->msg, "%s=%s", key, buf_cstring(&lf->scratch));
     buf_reset(&lf->scratch);
 }
