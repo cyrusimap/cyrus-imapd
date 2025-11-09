0bf6a7786880149e0f073c8fa2b30c8755d3908c
Ricardo Signes <rjbs@semiotic.systems>
2025-11-09 11:49:42 -0500

    Cassandane: introduce a "user" object to tests
    This object will serve as a handle for a user held in an instance, and
    will let the test programmer easily get a handle for clients to that
    user's data, and eventually also create and interact with test data.

--- cassandane/Cassandane/Cyrus/TestCase.pm
+++ cassandane/Cassandane/Cyrus/TestCase.pm
@@ -894,6 +894,125 @@ sub _need_http_tiny_env
     return 1;
 }
 
+package Cassandane::Cyrus::TestCase::User {
+  use Moo;
+
+  has username => (is => 'ro', required => 1);
+  has password => (is => 'ro', required => 1);
+  has instance => (is => 'ro', required => 1);
+
+  has _common_service_args => (
+      is => 'ro',
+      lazy => 1,
+      default => sub {
+          my ($self) = @_;
+          my $service = $self->{instance}->get_service("https");
+          $service ||= $self->{instance}->get_service("http");
+          return if !$service; # XXX die
+
+          my $ca_file = Cwd::abs_path("data/certs/cacert.pem");
+
+          my %common_args = (
+              user => 'cassandane',
+              password => 'pass',
+              host => $service->host(),
+              port => $service->port(),
+              scheme => ($service->is_ssl() ? 'https' : 'http'),
+              SSL_options => {
+                  SSL_ca_file => $ca_file,
+                  SSL_verifycn_scheme => 'none',
+              },
+          );
+
+          return \%common_args;
+      },
+  );
+
+  has jmap => (
+      is => 'ro',
+      lazy => 1,
+      default => sub {
+          my ($self) = @_;
+          local $ENV{PERL_HTTP_TINY_SSL_INSECURE_BY_DEFAULT} =
+              Cassandane::Cyrus::TestCase::_need_http_tiny_env();
+
+          unless ($self->instance->{config}->get_bit('httpmodules', 'jmap')) {
+              Carp::croak("User JMAP client requested, but jmap httpmodule not enabled");
+          }
+
+          require Mail::JMAPTalk;
+          $ENV{DEBUGJMAP} = 1;
+          my $jmap = Mail::JMAPTalk->new(
+              $self->_common_service_args->%*,
+              url => '/jmap/',
+          );
+
+          # preload default UA while the HTTP::Tiny env var is still set
+          $jmap->ua();
+
+          return $jmap;
+      }
+  );
+
+  has carddav => (
+      is => 'ro',
+      lazy => 1,
+      default => sub {
+          my ($self) = @_;
+          local $ENV{PERL_HTTP_TINY_SSL_INSECURE_BY_DEFAULT} =
+              Cassandane::Cyrus::TestCase::_need_http_tiny_env();
+
+          unless ($self->instance->{config}->get_bit('httpmodules', 'carddav')) {
+              Carp::croak("User CardDAV client requested, but carddav httpmodule not enabled");
+          }
+
+          require Net::CardDAVTalk;
+          return Net::CardDAVTalk->new(
+              $self->_common_service_args->%*,
+              url => '/',
+              expandurl => 1,
+          );
+      },
+  );
+
+  has caldav => (
+      is => 'ro',
+      lazy => 1,
+      default => sub {
+          my ($self) = @_;
+          local $ENV{PERL_HTTP_TINY_SSL_INSECURE_BY_DEFAULT} =
+              Cassandane::Cyrus::TestCase::_need_http_tiny_env();
+
+          unless ($self->instance->{config}->get_bit('httpmodules', 'caldav')) {
+              Carp::croak("User CalDAV client requested, but caldav httpmodule not enabled");
+          }
+
+          require Net::CalDAVTalk;
+          my $caldav = Net::CalDAVTalk->new(
+              $self->_common_service_args->%*,
+              url => '/',
+              expandurl => 1,
+          );
+
+          # XXX get users all with domain, etc.
+          $caldav->UpdateAddressSet("Test User", $self->username . '@example.com');
+
+          return $caldav;
+      },
+  );
+
+  sub imap {
+      my ($self) = @_;
+
+      my $imap_svc = $self->instance->get_service('imap');
+      my $imap_store = $imap_svc->create_store(username => $self->username);
+
+      return $imap_store->get_client;
+  }
+
+  no Moo;
+}
+
 sub _setup_http_service_objects
 {
     my ($self) = @_;
@@ -905,48 +1024,14 @@ sub _setup_http_service_objects
 
     my $ca_file = abs_path("data/certs/cacert.pem");
 
-    my %common_args = (
-        user => 'cassandane',
-        password => 'pass',
-        host => $service->host(),
-        port => $service->port(),
-        scheme => ($service->is_ssl() ? 'https' : 'http'),
-        SSL_options => {
-            SSL_ca_file => $ca_file,
-            SSL_verifycn_scheme => 'none',
-        },
-    );
-
-    local $ENV{PERL_HTTP_TINY_SSL_INSECURE_BY_DEFAULT} = _need_http_tiny_env();
-
     if ($self->{instance}->{config}->get_bit('httpmodules', 'carddav')) {
-        require Net::CardDAVTalk;
-        $self->{carddav} = Net::CardDAVTalk->new(
-            %common_args,
-            url => '/',
-            expandurl => 1,
-        );
+        $self->{carddav} = $self->default_user->carddav;
     }
     if ($self->{instance}->{config}->get_bit('httpmodules', 'caldav')) {
-        require Net::CalDAVTalk;
-        $self->{caldav} = Net::CalDAVTalk->new(
-            %common_args,
-            url => '/',
-            expandurl => 1,
-        );
-        $self->{caldav}->UpdateAddressSet("Test User",
-                                          "cassandane\@example.com");
+        $self->{caldav} = $self->default_user->caldav;
     }
     if ($self->{instance}->{config}->get_bit('httpmodules', 'jmap')) {
-        require Mail::JMAPTalk;
-        $ENV{DEBUGJMAP} = 1;
-        $self->{jmap} = Mail::JMAPTalk->new(
-            %common_args,
-            url => '/jmap/',
-        );
-
-        # preload default UA while the HTTP::Tiny env var is still set
-        $self->{jmap}->ua();
+        $self->{jmap} = $self->default_user->jmap;
     }
 
     xlog $self, "http service objects setup complete!";
@@ -981,6 +1066,12 @@ sub set_up
     }
 
     xlog $self, "Calling test function";
+}
+
+sub default_user
+{
+    my ($self) = @_;
+    return $self->{default_user};
 }
 
 sub _start_instances
@@ -1079,6 +1170,16 @@ sub _start_instances
                 if ($self->{_want}->{adminstore});
         }
     }
+
+    my $user = Cassandane::Cyrus::TestCase::User->new({
+        username => 'cassandane',
+        password => 'cassandane',
+        instance => $self->{instance},
+    });
+
+    $self->{default_user} = $user;
+
+    return;
 }
 
 sub tear_down
