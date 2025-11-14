f7401fa1beed95b4c5c250ad2fb7ec8d31edf2d8
Ricardo Signes <rjbs@semiotic.systems>
2025-11-14 14:58:20 -0500

    Cassandane: add ->as_vcardX to ContactCard entity

--- cassandane/Cassandane/TestEntity/Instance/ContactCard.pm
+++ cassandane/Cassandane/TestEntity/Instance/ContactCard.pm
@@ -9,6 +9,28 @@ sub datatype_properties {
     qw( id addressBookIds kind members name prodId )
 }
 
+sub _as_vcard {
+    my ($self, $version) = @_;
+
+    my $href = $self->properties->{'cyrusimap.org:href'};
+    my $res = $self->user->carddav->Request(
+        'GET', $href, '',
+        Accept => "text/vcard; version=$version",
+    );
+
+    return $res->{content};
+}
+
+sub as_vcard3 {
+    my ($self) = @_;
+    $self->_as_vcard('3.0');
+}
+
+sub as_vcard4 {
+    my ($self) = @_;
+    $self->_as_vcard('4.0');
+}
+
 __PACKAGE__->initialize_accessors;
 
 no Moo;
