92aa4607dd181a204f4cebdc2d62cb72d8a62558
Ricardo Signes <rjbs@semiotic.systems>
2025-11-19 16:17:56 -0500

    ContactCard test entity: implement ->add_member and ->create_member
    For groups!

--- cassandane/Cassandane/TestEntity/Instance/ContactCard.pm
+++ cassandane/Cassandane/TestEntity/Instance/ContactCard.pm
@@ -38,5 +38,40 @@ sub as_vcard4 {
     $self->_as_vcard('4.0');
 }
 
+sub add_member {
+    my ($self, $card_or_uid) = @_;
+
+    if (ref $card_or_uid && !$card_or_uid->isa(__PACKAGE__)) {
+        Carp::confess("argument to ->add_member must be a ContactCard or UID string");
+    }
+
+    my $kind = $self->kind;
+    $kind eq 'group' || Carp::confess("called ->add_member on a non-group (kind is $kind)");
+
+    my $uid = ref $card_or_uid ? $card_or_uid->uid : $card_or_uid;
+    my %new_members = map {; $_ => JSON::true() } (
+        keys %{ $self->members // {} },
+        $uid
+    );
+
+    # We're sending _all_ the members instead of patching because the "update
+    # instance in situ" code in $factory->_update is not sophisticated enough
+    # to cope with patches.  We need to address that. -- rjbs, 2025-11-19
+    $self->factory->_update($self => { members => \%new_members });
+    return;
+}
+
+sub create_member {
+    my ($self, $props) = @_;
+
+    my $kind = $self->kind;
+    $kind eq 'group' || Carp::confess("called ->create_member on a non-group (kind is $kind)");
+
+    my $card = $self->factory->create($props);
+    $self->add_member($card);
+
+    return $card;
+}
+
 no Moo;
 1;
