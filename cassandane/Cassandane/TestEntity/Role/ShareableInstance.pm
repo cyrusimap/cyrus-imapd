a3503afc9ae7ad5f70d71c95c21b2d8b24f310ba
Ricardo Signes <rjbs@semiotic.systems>
2025-11-24 14:47:53 -0500

    Cassandane test entities: fix share_with method to patch
    It only patches at the user level, not user-permission-bit level.  This
    is by design.

--- cassandane/Cassandane/TestEntity/Role/ShareableInstance.pm
+++ cassandane/Cassandane/TestEntity/Role/ShareableInstance.pm
@@ -4,6 +4,11 @@ use Moo::Role;
 use Sub::Install ();
 
 # $mailbox->share_with($user => [ qw( mayFoo mayBar ) ], $user2 => ...)
+#
+# This sets the exact bits for the given users, leaving other users intact.
+# That means that if you want to turn on mayFoo and mayBar for a single user,
+# you must do it in one share_with call.  A second call would *replace*, not
+# *union* permissions.
 sub share_with {
     my ($self, @input) = @_;
 
@@ -13,10 +18,11 @@ sub share_with {
     while (my ($target, $bits) = splice @input, 0, 2) {
         my $ident = ref $target ? $target->username : $target;
         my @bits  = ref $bits   ? @$bits : $bits; # the BITS! 🍟
-        $to_update{$ident} = { map {; $_ => JSON::true() } @bits };
+
+        $to_update{"shareWith/$ident"} = { map {; $_ => JSON::true() } @bits };
     }
 
-    $self->factory->_update($self => { shareWith => \%to_update });
+    $self->factory->_update($self => \%to_update);
     return;
 }
 
