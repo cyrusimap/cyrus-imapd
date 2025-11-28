4d029784473f1f6bd26479763d75cbd3729c5187
Bron Gondwana <brong@fastmailteam.com>
2025-11-28 18:49:02 +1100

    mupdate: call mboxlist_yield() when sending anything to the other end
    This allows the recipient (either mupdate or the client) to
    lock the mailboxes.db without causing a deadlock.  This will
    allow us to not force yield for the mailboxes.db in all other
    foreach cases.

--- imap/mupdate-client.c
+++ imap/mupdate-client.c
@@ -210,6 +210,11 @@ EXPORTED int mupdate_activate(mupdate_handle *handle,
         return MUPDATE_BADPARAM;
     }
 
+    if (mboxlist_yield()) {
+        syslog(LOG_ERR, "%s: databases locked", __func__);
+        return MUPDATE_FAIL;
+    }
+
     if (config_mupdate_config == IMAP_ENUM_MUPDATE_CONFIG_REPLICATED) {
         /* we don't care about the server part, everything is local */
         if (p) location = p + 1;
@@ -272,6 +277,11 @@ HIDDEN int mupdate_reserve(mupdate_handle *handle,
         return MUPDATE_BADPARAM;
     }
 
+    if (mboxlist_yield()) {
+        syslog(LOG_ERR, "%s: databases locked", __func__);
+        return MUPDATE_FAIL;
+    }
+
     if (config_mupdate_config == IMAP_ENUM_MUPDATE_CONFIG_REPLICATED) {
         /* we don't care about the location part, everything is local */
         if (p) location = p + 1;
@@ -332,6 +342,11 @@ EXPORTED int mupdate_deactivate(mupdate_handle *handle,
         return MUPDATE_BADPARAM;
     }
 
+    if (mboxlist_yield()) {
+        syslog(LOG_ERR, "%s: databases locked", __func__);
+        return MUPDATE_FAIL;
+    }
+
     if (config_mupdate_config == IMAP_ENUM_MUPDATE_CONFIG_REPLICATED) {
         /* we don't care about the server part, everything is local */
         if (p) location = p + 1;
@@ -373,6 +388,11 @@ EXPORTED int mupdate_delete(mupdate_handle *handle,
     }
 
     if (!handle->saslcompleted) return MUPDATE_NOAUTH;
+
+    if (mboxlist_yield()) {
+        syslog(LOG_ERR, "%s: databases locked", __func__);
+        return MUPDATE_FAIL;
+    }
 
     prot_printf(handle->conn->out,
                 "X%u DELETE {" SIZE_T_FMT "+}\r\n%s\r\n", handle->tagn++,
@@ -443,6 +463,11 @@ EXPORTED int mupdate_find(mupdate_handle *handle, const char *mailbox,
         return MUPDATE_BADPARAM;
     }
 
+    if (mboxlist_yield()) {
+        syslog(LOG_ERR, "%s: databases locked", __func__);
+        return MUPDATE_FAIL;
+    }
+
     prot_printf(handle->conn->out,
                 "X%u FIND {" SIZE_T_FMT "+}\r\n%s\r\n", handle->tagn++,
                 strlen(mailbox), mailbox);
@@ -481,6 +506,11 @@ EXPORTED int mupdate_list(mupdate_handle *handle, mupdate_callback callback,
     if (!callback) {
         syslog(LOG_ERR, "%s: no callback", __func__);
         return MUPDATE_BADPARAM;
+    }
+
+    if (mboxlist_yield()) {
+        syslog(LOG_ERR, "%s: databases locked", __func__);
+        return MUPDATE_FAIL;
     }
 
     if (prefix) {
