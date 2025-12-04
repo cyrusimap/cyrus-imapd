86d3f0b07619b0841c1d84c114d0846ea3da26bf
Ricardo Signes <rjbs@semiotic.systems>
2025-12-04 15:13:43 -0500

    Cassandane::JMAPTester: emulation for JMAPTalk ->CallMethods and ->Upload
    With these, once Cassandane starts providing JMAP::Tester instances
    instead of Mail::JMAPTalk instances, most code can *just work*, because
    these methods will still be there and will provide the existing API on
    top of the new API.
    
    Later, these can be removed, once all client code is updated.  That will
    take some time.

--- cassandane/Cassandane/JMAPTester.pm
+++ cassandane/Cassandane/JMAPTester.pm
@@ -7,5 +7,47 @@ has fallback_account_id => (
     required => 1,
 );
 
+# This emulates JMAPTalk's CallMethods, which returns the plain ol'
+# array-of-arrays that is the methodResponses
+sub CallMethods {
+    my ($self, $sentences, $using, %headers) = @_;
+
+    %headers
+        && Carp::confess("Cassandane::JMAPTester does not support custom HTTP headers");
+
+    my $res = $self->request({
+        methodCalls => $sentences,
+        ($using ? (using => $using) : ()),
+    });
+
+    return $res->as_stripped_triples;
+}
+
+# This emulates JMAPTalk's Upload, called like this:
+#   $jmaptalk->Upload(\%optional_headers?, $bytes, $content_type, $accountId)
+#
+# scalar context, JMAPTalk returns the decoded result data
+# list context, JMAPTalk returns (the underlying UA response, the decoded result)
+#
+# We will start by forbidding custom headers or list context.
+sub Upload {
+    my $self    = shift;
+    my $headers = (ref $_[0] eq 'HASH') ? shift : {};
+    my $bytes   = shift;
+    my $type    = shift;
+    my $accountId = shift // $self->fallback_account_id;
+
+    wantarray
+        && Carp::confess("Cassandane::JMAPTester can't emulate JMAPTalk->Upload in list context");
+
+    my $upload = $self->upload({
+        type => $type,
+        blob => \$bytes,
+        accountId => $accountId,
+    });
+
+    return $upload->{payload};
+}
+
 no Moo;
 1;
