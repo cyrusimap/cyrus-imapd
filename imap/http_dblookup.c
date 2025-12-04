298aa091ad43f001e87faaf0009c74091cae8df9
Ken Murchison <murch@fastmail.com>
2025-12-04 15:17:54 -0500

    http_dblookup.c: remove uid2emails and uid2members endpoints

--- imap/http_dblookup.c
+++ imap/http_dblookup.c
@@ -306,61 +306,6 @@ done:
     return ret;
 }
 
-static int get_uid2emails(struct transaction_t *txn,
-                          const char *userid, const char *uid)
-{
-    struct auth_state *authstate = auth_newstate(userid);
-    int ret = HTTP_NO_CONTENT;
-
-    if (!authstate) goto done;
-
-    struct carddav_db *carddavdb = carddav_open_userid(userid);
-    if (!carddavdb) goto done;
-
-    ptrarray_t *abook_sets = jmap_get_accessible_addressbooks(userid, authstate,
-                                                              &httpd_namespace,
-                                                              carddavdb);
-
-    /* Lookup emails in the given card and any group members */
-    strarray_t card_uids = STRARRAY_INITIALIZER;
-    strarray_t emails = STRARRAY_INITIALIZER;
-    strarray_t *member_uids = NULL;
-            
-    strarray_append(&card_uids, uid);
-    jmap_get_card_emails(&card_uids, CARDDAV_KIND_ANY, abook_sets,
-                         &emails, &member_uids);
-    strarray_fini(&card_uids);
-
-    /* Lookup group member email addresses (ignore subgroup members) */
-    jmap_get_card_emails(member_uids, CARDDAV_KIND_ANY, abook_sets,
-                         &emails, NULL);
-    strarray_free(member_uids);
-    jmap_free_abook_sets(abook_sets);
-
-    if (!strarray_size(&emails)) goto done;
-
-    json_t *json = json_array();
-    for (int i = 0; i < strarray_size(&emails); i++) {
-        json_array_append_new(json, json_string(strarray_nth(&emails, i)));
-    }
-    strarray_fini(&emails);
-
-    char *result = json_dumps(json, JSON_PRESERVE_ORDER|JSON_COMPACT);
-    json_decref(json);
-
-    txn->resp_body.type = "application/json";
-    txn->resp_body.len = strlen(result);
-
-    write_body(HTTP_OK, txn, result, txn->resp_body.len);
-    free(result);
-    ret = 0;
-
-done:
-    if (authstate) auth_freestate(authstate);
-
-    return ret;
-}
-
 struct get_cards_rock {
     hash_table *cards_by_uid;
     const char *group_uid;
@@ -386,80 +331,6 @@ static int _get_cards_cb(void *rock, struct carddav_data *cdata)
     }
 
     return 0;
-}
-
-static int get_uid2members(struct transaction_t *txn,
-                           const char *userid, const char *uid)
-{
-    struct auth_state *authstate = auth_newstate(userid);
-    hash_table uid_to_userid = HASH_TABLE_INITIALIZER;
-    ptrarray_t *abook_sets = NULL;
-    int ret = HTTP_NO_CONTENT;
-
-    if (!authstate) goto done;
-
-    /* Open DAV DB for the specified user.
-     * It will be assigned to an abook_set in jmap_get_accessible_addressbooks()
-     * and closed in jmap_free_abook_sets().
-     */
-    struct carddav_db *carddavdb = carddav_open_userid(userid);
-    if (!carddavdb) goto done;
-
-    abook_sets = jmap_get_accessible_addressbooks(userid, authstate,
-                                                  &httpd_namespace, carddavdb);
-
-    construct_hash_table(&uid_to_userid, 1024 * ptrarray_size(abook_sets), 0);
-
-    struct get_cards_rock grock = { &uid_to_userid, uid, NULL, NULL };
-
-    /* Fetch all cards from each addressbook in each set.
-       Also, get members in the specified group card */
-    for (int i = 0; i < ptrarray_size(abook_sets); i++) {
-        struct abook_set *set = ptrarray_nth(abook_sets, i);
-
-        if (set->carddavdb) {
-            grock.abook_set = set;
-
-            for (int j = 0; j < ptrarray_size(&set->mbentrys); j++) {
-                mbentry_t *mbentry = ptrarray_nth(&set->mbentrys, j);
-
-                carddav_foreach(set->carddavdb, mbentry, &_get_cards_cb, &grock);
-            }
-        }
-    }
-
-    if (!grock.member_uids) goto done;
-
-    /* Lookup group members and build response */
-    json_t *jres = json_object();
-    for (int i = 0; i < strarray_size(grock.member_uids); i++) {
-        const char *member = strarray_nth(grock.member_uids, i);
-        struct abook_set *abook_set = hash_lookup(member, &uid_to_userid);
-
-        if (abook_set) {
-            json_t *uids = json_object_get_vanew(jres, abook_set->userid, "[]");
-            json_array_append_new(uids, json_string(member));
-        }
-    }
-
-    strarray_free(grock.member_uids);
-
-    char *res = json_dumps(jres, JSON_PRESERVE_ORDER|JSON_COMPACT);
-    json_decref(jres);
-
-    txn->resp_body.type = "application/json";
-    txn->resp_body.len = strlen(res);
-
-    write_body(HTTP_OK, txn, res, txn->resp_body.len);
-    free(res);
-    ret = 0;
-
-done:
-    if (authstate) auth_freestate(authstate);
-    if (abook_sets) jmap_free_abook_sets(abook_sets);
-    free_hash_table(&uid_to_userid, NULL);
-
-    return ret;
 }
 
 static int get_expandcard(struct transaction_t *txn,
@@ -616,12 +487,6 @@ static int meth_get_db(struct transaction_t *txn,
 
     if (!strcmp(txn->req_uri->path, "/dblookup/uid2groups"))
         return get_uid2groups(txn, userhdrs[0], keyhdrs[0]);
-
-    if (!strcmp(txn->req_uri->path, "/dblookup/uid2emails"))
-        return get_uid2emails(txn, userhdrs[0], keyhdrs[0]);
-
-    if (!strcmp(txn->req_uri->path, "/dblookup/uid2members"))
-        return get_uid2members(txn, userhdrs[0], keyhdrs[0]);
 
     if (!strcmp(txn->req_uri->path, "/dblookup/expandcard"))
         return get_expandcard(txn, userhdrs[0], keyhdrs[0]);
