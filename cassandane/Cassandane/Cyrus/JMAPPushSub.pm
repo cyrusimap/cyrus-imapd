e22c516653e9efa3a549cd978f276b3ece66b98d
Ken Murchison <murch@fastmail.com>
2025-12-30 12:34:48 -0500

    jmap_push_subscription.c: beginning of PushSubscription/{get,set}

---
+++ cassandane/Cassandane/Cyrus/JMAPPushSub.pm
@@ -0,0 +1,60 @@
+# SPDX-License-Identifier: BSD-3-Clause-CMU
+# See COPYING file at the root of the distribution for more details.
+
+package Cassandane::Cyrus::JMAPPushSub;
+use strict;
+use warnings;
+use DateTime;
+use JSON;
+use JSON::XS;
+use Mail::JMAPTalk 0.13;
+use Data::Dumper;
+use Storable 'dclone';
+use File::Basename;
+use IO::File;
+
+use lib '.';
+use base qw(Cassandane::Cyrus::TestCase);
+use Cassandane::Util::Log;
+
+use charnames ':full';
+
+sub new
+{
+    my ($class, @args) = @_;
+
+    my $config = Cassandane::Config->default()->clone();
+
+    $config->set(caldav_realm => 'Cassandane',
+                 conversations => 'yes',
+                 httpmodules => 'jmap',
+                 httpallowcompress => 'no',
+                 event_groups => 'mailbox message flags calendar applepushservice jmap',
+                 jmap_nonstandard_extensions => 'yes');
+
+    return $class->SUPER::new({
+        config => $config,
+        jmap => 1,
+        adminstore => 1,
+        services => [ 'imap', 'http' ]
+    }, @args);
+}
+
+sub set_up
+{
+    my ($self) = @_;
+    $self->SUPER::set_up();
+    $self->{jmap}->DefaultUsing([
+        'urn:ietf:params:jmap:core',
+    ]);
+}
+
+sub tear_down
+{
+    my ($self) = @_;
+    $self->SUPER::tear_down();
+}
+
+use Cassandane::Tiny::Loader;
+
+1;
