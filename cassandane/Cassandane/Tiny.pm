ed995c3e094c2d05710c60152512c0519a5b0884
Matthew Horsfall <wolfsage@gmail.com>
2026-01-13 09:01:49 -0500

    Cassandane::Tiny: Make Test::Deep symbols available to all
    Test::Deep provides many useful comparisons (bag(), superhashof(), etc...)
    that will be useful with `$self->assert_cmp_deeply()`.

--- cassandane/Cassandane/Tiny.pm
+++ cassandane/Cassandane/Tiny.pm
@@ -2,13 +2,20 @@ package Cassandane::Tiny;
 use strict;
 use warnings;
 
+use Test::Deep ();
+use Import::Into ();
+
 sub import {
+  my $caller = caller;
+
   no warnings 'once';
   $Cassandane::Tiny::Loader::RELOADED = 1;
 
   # Everyone gets strict and warnings
   strict->import();
   warnings->import();
+
+  Test::Deep->import::into($caller, ':v1');
 
   return;
 }
