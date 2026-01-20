64fb9d2e9691f2db5838921c4d879661538e7217
Ricardo Signes <rjbs@semiotic.systems>
2026-01-20 16:39:45 -0500

    Cassandane/Cyrus/Mbpath.pm: make tiny

--- cassandane/Cassandane/Cyrus/Mbpath.pm
+++ cassandane/Cassandane/Cyrus/Mbpath.pm
@@ -63,23 +63,6 @@ sub tear_down
     $self->SUPER::tear_down();
 }
 
-sub test_mbpath_8bit
-{
-    my ($self) = @_;
-
-    xlog $self, "Test mbpath 8 bit name parsing";
-
-    # create and prepare the user
-    my $imaptalk = $self->{store}->get_client();
-    $imaptalk->create('A & B');
-
-    xlog "check with 8bit name";
-    my $mbpath = $self->{instance}->run_mbpath('user.cassandane.A & B');
-    $self->assert_str_equals($mbpath->{mbname}{intname}, 'user.cassandane.A &- B');
-
-    xlog "check with 7bit name";
-    $mbpath = $self->{instance}->run_mbpath("-7", 'user.cassandane.A &- B');
-    $self->assert_str_equals($mbpath->{mbname}{intname}, 'user.cassandane.A &- B');
-}
+use Cassandane::Tiny::Loader 'tiny-tests/Mbpath';
 
 1;
