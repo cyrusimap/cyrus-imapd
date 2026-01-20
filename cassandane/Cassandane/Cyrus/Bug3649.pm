5214b3fa801714e5304d2a2ffa5ebd15904e948f
Ricardo Signes <rjbs@semiotic.systems>
2026-01-20 16:39:42 -0500

    Cassandane/Cyrus/Bug3649.pm: make tiny

--- cassandane/Cassandane/Cyrus/Bug3649.pm
+++ cassandane/Cassandane/Cyrus/Bug3649.pm
@@ -63,23 +63,6 @@ sub tear_down
     $self->SUPER::tear_down();
 }
 
-sub test_delete_subuser
-{
-    my ($self) = @_;
-    my $adminstore = $self->{adminstore};
-    my $admintalk = $adminstore->get_client();
-
-    xlog $self, "Test Cyrus extension which renames a user to a different partition";
-
-    # create and prepare the user
-    $self->{instance}->create_user('admin1');
-    $adminstore->set_folder('user.admin1');
-    for ('A'..'Z') {
-        $self->make_message("Message $_", store => $adminstore);
-    }
-    $admintalk->unselect();
-
-    $admintalk->delete('user.admin1');
-}
+use Cassandane::Tiny::Loader 'tiny-tests/Bug3649';
 
 1;
