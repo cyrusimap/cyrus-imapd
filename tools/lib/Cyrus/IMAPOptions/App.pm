8174e87782cf772915bd1000e011166a921c14c2
ellie timoney <ellie@fastmail.com>
2026-02-11 11:22:01 +1100

    tools/imapoptions.pl: stub new generator tool

---
+++ tools/lib/Cyrus/IMAPOptions/App.pm
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: BSD-3-Clause-CMU
+# See COPYING file at the root of the distribution for more details.
+package Cyrus::IMAPOptions::App;
+use warnings;
+use strict;
+use App::Cmd::Setup -app;
+1;
