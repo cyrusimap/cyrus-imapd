7bea921945509ecb316d476e8f660b217afab1a7
ellie timoney <ellie@fastmail.com>
2026-02-13 11:47:51 +1100

    tools/imapoptions.pl: add the UNRELEASED warning/error behaviour

--- tools/lib/Cyrus/IMAPOptions/App/Command/sample.pm
+++ tools/lib/Cyrus/IMAPOptions/App/Command/sample.pm
@@ -12,6 +12,8 @@ sub abstract { 'build a sample imapd.conf file from imapoptions' }
 
 sub usage_desc { 'sample %o imapoptions' }
 
+sub opt_spec { return Cyrus::IMAPOptions::App::common_opt_spec; }
+
 sub validate_args
 {
     my ($self, $opt, $args) = @_;
@@ -27,7 +29,12 @@ sub execute
 {
     my ($self, $opt, $args) = @_;
 
-    my $imapoptions = Cyrus::IMAPOptions->new(from_path => $args->[0]);
+    my $imapoptions = Cyrus::IMAPOptions->new(
+        from_path => $args->[0],
+        forbid_unreleased => $opt->{forbid_unreleased},
+    );
+
+    $imapoptions->check_unreleased($self->app->arg0);
 
     $self->header();
     $imapoptions->iterate(\&_print_option);
