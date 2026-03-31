f6cab8100f4e29aba9130e1dfc1fffdba2e84a64
Ricardo Signes <rjbs@semiotic.systems>
2026-03-31 07:43:10 -0400

    docsrc/conf.py: load custom CSS
    In 5cbdca45625a1c2d we removed the overrides for the default page
    layout, as the great majority of that page was never invoked.
    Unfortunately, it was the one place we set up custom CSS, so we lost the
    change from red to blue code spans.
    
    Using a custom layout for that is overkill.  Instead, we should use the
    standard Sphinx mechanism: `html_css_files`.  Done!

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -219,6 +219,10 @@ html_theme_path = ["exts/themes"]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_css_files = [
+    'cyrus.css',
+]
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
