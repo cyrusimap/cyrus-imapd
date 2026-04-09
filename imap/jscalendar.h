9c3750085664c6e1eb53523182e0de173a744626
Robert Stepanek <rsto@fastmailteam.com>
2026-04-09 08:45:11 +0200

    jscalendar.h: add Doxygen documentation

--- imap/jscalendar.h
+++ imap/jscalendar.h
@@ -1,6 +1,6 @@
 /* jscalendar.h -- Routines for converting JSCalendar and iCalendar
  *
- * Copyright (c) 2025 Fastmail Pty Ltd
+ * Copyright (c) 2026 Fastmail Pty Ltd
  *
  */
 
@@ -16,34 +16,55 @@ extern "C" {
 #include <jansson.h>
 #include <libical/ical.h>
 
+/** @brief Configuration for JSCalendar/iCalendar conversion */
 typedef struct {
-    // When converting an Alert object with action "email", this is
-    // the default email address URI to set in the ATTENDEE property
-    // of the VALARM, unless the "iCalComponent" property of the Alert
-    // object already contains ATTENDEE properties.
-    //
-    // If the value is NULL then no ATTENDEE property is set. Note this
-    // violates the VALARM definition in RFC 5545, section 3.6.6.
+    /**
+     *  When converting an Alert object with action "email", this is
+     *  the default email address URI to set in the ATTENDEE property
+     *  of the VALARM, unless the "iCalComponent" property of the Alert
+     *  object already contains ATTENDEE properties.
+     *
+     *  If the value is NULL then no ATTENDEE property is set. Note this
+     *  violates the VALARM definition in RFC 5545, section 3.6.6.*
+     */
     const char *emailalert_default_uri;
 
-    // When converting an Alert object with action "display", this is
-    // the default string value to set in the DESCRIPTION property
-    // of the VALARM, unless the "iCalComponent" property of the Alert
-    // object already contains DESCRIPTION property.
-    //
-    // If the value is NULL then no DESCRIPTION property is set. Note this
-    // violates the VALARM definition in RFC 5545, section 3.6.6.
+    /** 
+     *  When converting an Alert object with action "display", this is
+     *  the default string value to set in the DESCRIPTION property
+     *  of the VALARM, unless the "iCalComponent" property of the Alert
+     *  object already contains DESCRIPTION property.
+     *
+     *  If the value is NULL then no DESCRIPTION property is set. Note this
+     *  violates the VALARM definition in RFC 5545, section 3.6.6. */
     const char *displayalert_default_description;
 
-    // Toggles if to ignore iCalendar conversion properties. If true, the
-    // "iCalComponent" conversion property will be ignored when converting
-    // to iCalendar and not set when converting to JSCalendar.
+    /**
+     *  Toggles if to ignore iCalendar conversion properties. If true, the
+     *  "iCalComponent" conversion property will be ignored when converting
+     *  to iCalendar and not set when converting to JSCalendar.
+     */
     bool ignore_icalendar_convprops;
 } jscalendar_cfg_t;
 
-extern icalcomponent *jscalendar_to_ical(jscalendar_cfg_t *, json_t *jgroup,
+/** @brief Convert a JSCalendar object to an iCalendar object.
+ *
+ *  @param cfg     Conversion configuration, or NULL for defaults.
+ *  @param jobj    JSON object representing a JSCalendar Group or Event.
+ *  @param parser  JMAP parser used to report property errors.
+ *  @return        A newly allocated iCalendar object, or NULL on error. */
+extern icalcomponent *jscalendar_to_ical(jscalendar_cfg_t *cfg,
+                                         json_t *jobj,
                                          struct jmap_parser *parser);
-extern json_t *jscalendar_from_ical(jscalendar_cfg_t *, icalcomponent *vcal);
+
+/** @brief Convert an iCalendar object to a JSCalendar Group object.
+ *
+ *  @param cfg   Conversion configuration, or NULL for defaults.
+ *  @param vcal  An iCalendar VCALENDAR component.
+ *  @return      A newly allocated JSON object representing a JSCalendar Group,
+ *               or NULL on error. */
+extern json_t *jscalendar_from_ical(jscalendar_cfg_t *cfg,
+                                    icalcomponent *vcal);
 
 #ifdef __cplusplus
 }
