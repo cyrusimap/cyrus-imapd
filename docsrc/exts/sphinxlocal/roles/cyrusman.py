63c119266c9b31e425a0a381d6e8c20da47f42ec
ellie timoney <ellie@fastmail.com>
2026-05-01 11:13:35 +1000

    cyrusman.py: declutter, indicate parallel r/w support
    * cut it back to the bare minimum so we can clearly see what it
      actually does
    * return a dictionary as expected by the Sphinx API
    * included parallel r/w support fields in dictionary (shushes
      warning with sphinx-build v7)

--- docsrc/exts/sphinxlocal/roles/cyrusman.py
+++ docsrc/exts/sphinxlocal/roles/cyrusman.py
@@ -1,35 +1,13 @@
-"""
-    sphinxlocal.roles.cyrusman
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    Turn :cyrusman: links into manpage references to the cyrus imap doc tree
-
-    :version: 0.1
-    :author: Nicola Nye <nicolan@fastmail.com>
-
-    :copyright: Copyright 2007-2016 by the Sphinx team, see AUTHORS.
-    :license: BSD, see LICENSE for details.
-"""
-
-from sphinx.errors import SphinxError
-from docutils import nodes, utils
-from docutils.parsers.rst.roles import set_classes
-from string import Template
-import re
-
-try:
-    from sphinx.util import logging
-    logger = logging.getLogger(__name__)
-except:
-    logger = None
+# SPDX-License-Identifier: BSD-3-Clause-CMU
+# See COPYING file at the root of the distribution for more details.
+import docutils
 
 def setup(app):
-    global logger
-    if logger is None:
-        logger = app
-    logger.info('Initializing cyrusman plugin')
-    app.add_crossref_type('cyrusman', 'cyrusman', '%s', nodes.generated)
-    return
-
-class CyrusManExtension(SphinxError):
-        category = ':cyrusman: error'
+    app.add_crossref_type('cyrusman',
+                          'cyrusman',
+                          '%s',
+                          docutils.nodes.generated)
+    return {
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
