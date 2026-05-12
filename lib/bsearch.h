4a0deb66e347c465869b5eea6e169d0d22cf2175
Дилян Палаузов <git-dpa@aegee.org>
2026-05-12 20:33:25 +0300

    Remove unused bsearch_memtree_mbox()

--- lib/bsearch.h
+++ lib/bsearch.h
@@ -11,9 +11,6 @@ extern int bsearch_mem_mbox(const char *word,
                             unsigned long *linelenp);
 
 extern int bsearch_ncompare_mbox(const char *s1, size_t l1, const char *s2, size_t l2);
-extern int bsearch_memtree_mbox(const unsigned char *s1, size_t l1,
-                                const unsigned char *s2, size_t l2);
-
 extern int bsearch_ncompare_raw(const char *s1, size_t l1, const char *s2, size_t l2);
 
 extern int cmpstringp_raw(const void *p1, const void *p2);
