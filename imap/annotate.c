48486caecc70649153f12a374e4df85fd1c8ac41
Дилян Палаузов <git-dpa@aegee.org>
2026-05-13 07:17:04 +0000

    annotation_set_tofile() - unreachable code

--- imap/annotate.c
+++ imap/annotate.c
@@ -3530,8 +3530,6 @@ static int annotation_set_tofile(annotate_state_t *state
 {
     const char *filename = (const char *)entry->desc->rock;
     char path[MAX_MAILBOX_PATH+1];
-    int r;
-    FILE *f;
 
     if (!maywrite) return IMAP_PERMISSION_DENIED;
 
@@ -3540,21 +3538,18 @@ static int annotation_set_tofile(annotate_state_t *state
     /* XXX how do we do this atomically with other annotations? */
     if (entry->shared.s == NULL)
         return xunlink(path);
-    else {
-        r = cyrus_mkdir(path, 0755);
-        if (r)
-            return r;
-        f = fopen(path, "w");
-        if (!f) {
-            syslog(LOG_ERR, "cannot open %s for writing: %m", path);
-            return IMAP_IOERROR;
-        }
-        fwrite(entry->shared.s, 1, entry->shared.len, f);
-        fputc('\n', f);
-        return fclose(f);
-    }
 
-    return IMAP_IOERROR;
+    int r = cyrus_mkdir(path, 0755);
+    if (r)
+        return r;
+    FILE *f = fopen(path, "w");
+    if (!f) {
+        syslog(LOG_ERR, "cannot open %s for writing: %m", path);
+        return IMAP_IOERROR;
+    }
+    fwrite(entry->shared.s, 1, entry->shared.len, f);
+    fputc('\n', f);
+    return fclose(f);
 }
 
 static int annotation_set_todb(annotate_state_t *state,
