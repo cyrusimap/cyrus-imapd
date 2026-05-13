1cc10d3d461d8de0364b338bc519d2ada7225e2c
Дилян Палаузов <git-dpa@aegee.org>
2026-05-13 09:23:54 +0300

    annotatemore_open() and caldav_scheddefault() - remove variable r

--- imap/annotate.c
+++ imap/annotate.c
@@ -713,12 +713,10 @@ HIDDEN void annotate_putdb(annotate_db_t **dbp)
 
 EXPORTED void annotatemore_open(void)
 {
-    int r;
     annotate_db_t *d = NULL;
 
     /* force opening the global annotations db */
-    r = _annotate_getdb(NULL, NULL, 0, CYRUSDB_CREATE, &d);
-    if (r)
+    if (_annotate_getdb(NULL, NULL, 0, CYRUSDB_CREATE, &d))
         fatal("can't open global annotations database", EX_TEMPFAIL);
 
     annotatemore_dbopen = 1;
