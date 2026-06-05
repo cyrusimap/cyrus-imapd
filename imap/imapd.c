f759b5d2d7ad329b8b537ee571e6f1b4b1ea2434
ellie timoney <ellie@fastmail.com>
2026-06-05 10:25:09 +1000

    imapd: fix mboxkey_db leak

--- imap/imapd.c
+++ imap/imapd.c
@@ -14185,6 +14185,7 @@ static void cmd_genurlauth(char *tag)
         if (c == IMAP_LITERAL_TOO_LARGE) {
             prot_printf(imapd_out, "%s NO %s in Genurlauth\r\n",
                         tag, error_message(c));
+            mboxkey_close(mboxkey_db);
             return;
         }
         if (c != ' ') {
@@ -14192,6 +14193,7 @@ static void cmd_genurlauth(char *tag)
                         "%s BAD Missing required argument to Genurlauth\r\n",
                         tag);
             eatline(imapd_in, c);
+            mboxkey_close(mboxkey_db);
             return;
         }
         c = getword(imapd_in, &arg2);
@@ -14200,6 +14202,7 @@ static void cmd_genurlauth(char *tag)
                         "%s BAD Unknown auth mechanism to Genurlauth %s\r\n",
                         tag, arg2.s);
             eatline(imapd_in, c);
+            mboxkey_close(mboxkey_db);
             return;
         }
 
@@ -14232,6 +14235,7 @@ static void cmd_genurlauth(char *tag)
             eatline(imapd_in, c);
             free(url.freeme);
             free(intname);
+            mboxkey_close(mboxkey_db);
             return;
         }
 
@@ -14272,6 +14276,7 @@ static void cmd_genurlauth(char *tag)
                         r == IMAP_BADURL ? error_message(r) : cyrusdb_strerror(r));
             free(url.freeme);
             free(intname);
+            mboxkey_close(mboxkey_db);
             return;
         }
 
