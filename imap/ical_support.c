923575ce239f77bc499b0aba89088c2a0b7e277d
Robert Stepanek <rsto@fastmailteam.com>
2026-07-24 13:33:02 +0200

    ical_support.c: do not free parameters that were removed by ref
    Their memory still is managed within libical for the iCalendar
    component and remove_by_ref keeps their reference count unchanged.

--- imap/ical_support.c
+++ imap/ical_support.c
@@ -2101,7 +2101,6 @@ static void apply_property_updates(struct patch_data_t *patch,
             }
 
             icalproperty_remove_parameter_by_ref(newprop, actionp);
-            icalparameter_free(actionp);
         }
 
         if (action != ICAL_PATCHACTION_CREATE) {
